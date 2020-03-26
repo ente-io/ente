86362a7225669f898fdbccefe4746902028cdd92
Vishnu Mohandas <vishnumohandas@gmail.com>
2020-03-26 20:09:31 +0530

    Update API interactions

--- lib/db/db_helper.dart
+++ lib/db/db_helper.dart
@@ -5,14 +5,15 @@ import 'package:sqflite/sqflite.dart';
 import 'package:path_provider/path_provider.dart';
 
 class DatabaseHelper {
-  static final _databaseName = "Orma.db";
+  static final _databaseName = "orma.db";
   static final _databaseVersion = 1;
 
-  static final table = 'synced_photos';
-  
+  static final table = 'uploaded_photos';
+
   static final columnId = 'photo_id';
-  static final columnLocalUrl = 'local_url';
-  static final columnTimestamp = 'timestamp';
+  static final columnPath = 'path';
+  static final columnHash = 'hash';
+  static final columnUploadTimestamp = 'upload_timestamp';
 
   // make this a singleton class
   DatabaseHelper._privateConstructor();
@@ -26,27 +27,38 @@ class DatabaseHelper {
     _database = await _initDatabase();
     return _database;
   }
-  
+
   // this opens the database (and creates it if it doesn't exist)
   _initDatabase() async {
     Directory documentsDirectory = await getApplicationDocumentsDirectory();
     String path = join(documentsDirectory.path, _databaseName);
     return await openDatabase(path,
-        version: _databaseVersion,
-        onCreate: _onCreate);
+        version: _databaseVersion, onCreate: _onCreate);
   }
 
   // SQL code to create the database table
   Future _onCreate(Database db, int version) async {
     await db.execute('''
           CREATE TABLE $table (
-            $columnId INTEGER PRIMARY KEY,
-            $columnLocalUrl TEXT NOT NULL,
-            $columnTimestamp INTEGER NOT NULL
+            $columnId VARCHAR(255) PRIMARY KEY,
+            $columnPath TEXT NOT NULL,
+            $columnHash TEXT NOT NULL,
+            $columnUploadTimestamp TEXT NOT NULL
           )
           ''');
   }
-  
+
+  Future<int> insertPhoto(
+      String photoID, String path, String hash, int uploadTimestamp) async {
+    Database db = await instance.database;
+    var row = new Map<String, dynamic>();
+    row[columnId] = photoID;
+    row[columnPath] = path;
+    row[columnHash] = hash;
+    row[columnUploadTimestamp] = uploadTimestamp;
+    return await db.insert(table, row);
+  }
+
   // Helper methods
 
   // Inserts a row in the database where each key in the Map is a column name
@@ -57,21 +69,14 @@ class DatabaseHelper {
     return await db.insert(table, row);
   }
 
-  // All of the rows are returned as a list of maps, where each map is 
+  // All of the rows are returned as a list of maps, where each map is
   // a key-value list of columns.
   Future<List<Map<String, dynamic>>> queryAllRows() async {
     Database db = await instance.database;
     return await db.query(table);
   }
 
-  // All of the methods (insert, query, update, delete) can also be done using
-  // raw SQL commands. This method uses a raw query to give the row count.
-  Future<int> queryLastTimestamp() async {
-    Database db = await instance.database;
-    return Sqflite.firstIntValue(await db.rawQuery('SELECT MAX($columnTimestamp) FROM $table'));
-  }
-
-  // We are assuming here that the id column in the map is set. The other 
+  // We are assuming here that the id column in the map is set. The other
   // column values will be used to update the row.
   Future<int> update(Map<String, dynamic> row) async {
     Database db = await instance.database;
@@ -79,10 +84,17 @@ class DatabaseHelper {
     return await db.update(table, row, where: '$columnId = ?', whereArgs: [id]);
   }
 
-  // Deletes the row specified by the id. The number of affected rows is 
+  // Deletes the row specified by the id. The number of affected rows is
   // returned. This should be 1 as long as the row exists.
   Future<int> delete(int id) async {
     Database db = await instance.database;
     return await db.delete(table, where: '$columnId = ?', whereArgs: [id]);
+  }
+
+  Future<bool> containsPath(String path) async {
+    Database db = await instance.database;
+    return (await db.query(table, where: '$columnPath =?', whereArgs: [path]))
+            .length >
+        0;
   }
 }
--- lib/photo_sync_manager.dart
+++ lib/photo_sync_manager.dart
@@ -1,13 +1,28 @@
 import 'package:logger/logger.dart';
+import 'package:myapp/db/db_helper.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:photo_manager/photo_manager.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:dio/dio.dart';
 
+class RemotePhoto {
+  String photoID;
+  int syncTimestamp;
+  String url;
+
+  RemotePhoto.fromJson(Map<String, dynamic> json)
+      : photoID = json["photoID"],
+        syncTimestamp = json["syncTimestamp"],
+        url = json["url"];
+}
+
 class PhotoSyncManager {
   final logger = Logger();
   final dio = Dio();
-  final uploadUrl = "http://192.168.0.106:8080/upload";
-  static final lastUploadedItemTimestampKey = "last_uploaded_item_timestamp_5";
+  final endpoint = "http://192.168.0.106:8080";
+  final user = "umbu";
+  static final lastSyncTimestampKey = "last_sync_timestamp_0";
 
   PhotoSyncManager(List<AssetEntity> assets) {
     logger.i("PhotoSyncManager init");
@@ -16,35 +31,69 @@ class PhotoSyncManager {
 
   _syncPhotos(List<AssetEntity> assets) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    var lastSyncTimestamp = prefs.getInt(lastUploadedItemTimestampKey);
+    var lastSyncTimestamp = prefs.getInt(lastSyncTimestampKey);
     if (lastSyncTimestamp == null) {
       lastSyncTimestamp = 0;
     }
     logger.i("Last sync timestamp: " + lastSyncTimestamp.toString());
-    assets.sort((a, b) => a.modifiedDateTime.millisecondsSinceEpoch
-        .compareTo(b.modifiedDateTime.millisecondsSinceEpoch));
+
+    await _downloadDiff(lastSyncTimestamp, prefs);
+
+    await _uploadDiff(assets, prefs);
+
+    // TODO:  Fix race conditions triggered due to concurrent syncs.
+    //        Add device_id/last_sync_timestamp to the upload request?
+  }
+
+  Future _uploadDiff(List<AssetEntity> assets, SharedPreferences prefs) async {
+    assets.sort((first, second) => second
+        .modifiedDateTime.millisecondsSinceEpoch
+        .compareTo(first.modifiedDateTime.millisecondsSinceEpoch));
     for (AssetEntity asset in assets) {
-      if (asset.modifiedDateTime.millisecondsSinceEpoch > lastSyncTimestamp) {
-        var response = await _uploadFile(asset);
-        if (response.statusCode == 200) {
-          prefs.setInt(lastUploadedItemTimestampKey,
-              asset.modifiedDateTime.millisecondsSinceEpoch);
-          logger.i("Updated for: " + asset.id);
+      DatabaseHelper.instance
+          .containsPath((await asset.originFile).path)
+          .then((containsPath) async {
+        if (!containsPath) {
+          var response = await _uploadFile(asset);
+          prefs.setInt(lastSyncTimestampKey, response.syncTimestamp);
         }
-      }
+      });
     }
   }
 
-  Future<Response<Object>> _uploadFile(AssetEntity entity) async {
-    logger.i("Uploading: " + entity.id);
-    var formData = FormData.fromMap({
-      "file": await MultipartFile.fromFile((await entity.originFile).path,
-          filename: entity.title),
-      "user": "umbu",
-      "timestamp": entity.modifiedDateTime.millisecondsSinceEpoch
+  Future _downloadDiff(int lastSyncTimestamp, SharedPreferences prefs) async {
+    Response response = await dio.get(endpoint + "/diff", queryParameters: {
+      "user": user,
+      "lastSyncTimestamp": lastSyncTimestamp
     });
-    var response = await dio.post(uploadUrl, data: formData);
+    var externalPath = (await getExternalStorageDirectory()).path;
+    logger.i("External path: " + externalPath);
+    var path = externalPath + "/photos/";
+
+    List<RemotePhoto> photos = (response.data["diff"] as List)
+        .map((photo) => new RemotePhoto.fromJson(photo))
+        .toList();
+    for (RemotePhoto photo in photos) {
+      logger.i("Downloading " + endpoint + photo.url + " to " + path);
+      await dio.download(endpoint + photo.url, path + basename(photo.url));
+      prefs.setInt(lastSyncTimestampKey, photo.syncTimestamp);
+      logger.i("Downloaded " + photo.url + " to " + path);
+    }
+  }
+
+  Future<RemotePhoto> _uploadFile(AssetEntity entity) async {
+    logger.i("Uploading: " + entity.id);
+    var path = (await entity.originFile).path;
+    var formData = FormData.fromMap({
+      "file": await MultipartFile.fromFile(path, filename: entity.title),
+      "user": user,
+    });
+    var response = await dio.post(endpoint + "/upload", data: formData);
     logger.i(response.toString());
-    return response;
+    var remotePhoto = RemotePhoto.fromJson(response.data);
+    // TODO: Compute hash
+    DatabaseHelper.instance.insertPhoto(
+        remotePhoto.photoID, path, "hash", remotePhoto.syncTimestamp);
+    return remotePhoto;
   }
 }
