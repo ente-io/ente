c96afe7af45ece325f6bed6d87c0c845469908b8
Vishnu Mohandas <vishnumohandas@gmail.com>
2020-05-02 17:16:59 +0530

    Add Sentry for crash analytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:logger/logger.dart';
 import 'core/configuration.dart';
@@ -5,12 +7,41 @@ import 'photo_loader.dart';
 import 'photo_sync_manager.dart';
 import 'ui/home_widget.dart';
 import 'package:provider/provider.dart';
+import 'package:sentry/sentry.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(MyApp());
   Configuration.instance.init();
   PhotoSyncManager.instance.sync();
+
+  final SentryClient sentry = new SentryClient(
+      dsn: "http://96780dc0b00f4c69a16c02e90d379996@3.211.17.56/2");
+
+  FlutterError.onError = (FlutterErrorDetails details) async {
+    print('Flutter error caught by Sentry');
+    FlutterError.dumpErrorToConsole(details, forceReport: true);
+    _sendErrorToSentry(sentry, details.exception, details.stack);
+  };
+
+  runZoned(
+    () => runApp(MyApp()),
+    onError: (Object error, StackTrace stackTrace) =>
+        _sendErrorToSentry(sentry, error, stackTrace),
+  );
+}
+
+void _sendErrorToSentry(
+    SentryClient sentry, Object error, StackTrace stackTrace) {
+  try {
+    sentry.captureException(
+      exception: error,
+      stackTrace: stackTrace,
+    );
+    print('Error sent to sentry.io: $error');
+  } catch (e) {
+    print('Sending report to sentry.io failed: $e');
+    print('Original error: $error');
+  }
 }
 
 class MyApp extends StatelessWidget with WidgetsBindingObserver {
