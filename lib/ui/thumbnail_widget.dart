ca18d9bc638de2c13668c8557dd571d2ab1c19fe
Vishnu Mohandas <vishnumohandas@gmail.com>
2020-05-28 02:34:15 +0530

    Remove unused import

--- lib/ui/thumbnail_widget.dart
+++ lib/ui/thumbnail_widget.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:photos/core/cache/thumbnail_cache.dart';
-import 'package:photos/core/configuration.dart';
 import 'package:photos/models/photo.dart';
 import 'package:logging/logging.dart';
 import 'package:photos/core/constants.dart';
