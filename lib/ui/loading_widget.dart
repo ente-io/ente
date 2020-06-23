71fd7277b1f5d06dbfba1e28c5b2217d4db28de3
Vishnu Mohandas <vishnumohandas@gmail.com>
2020-06-23 23:09:45 +0530

    Update loading widget style

--- lib/ui/loading_widget.dart
+++ lib/ui/loading_widget.dart
@@ -1,13 +1,9 @@
-import 'dart:io';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 final loadWidget = Center(
   child: SizedBox.fromSize(
     size: Size.square(30),
-    child: Platform.isIOS
-        ? CupertinoActivityIndicator()
-        : CircularProgressIndicator(),
+    child: CupertinoActivityIndicator(),
   ),
 );
