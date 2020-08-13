44dddb818f5873be96eae7a489100e2d2ab11301
Vishnu Mohandas <vishnumohandas@gmail.com>
2020-08-13 07:04:16 +0530

    Rename for consistency

--- lib/core/cache/image_cache.dart
+++ lib/core/cache/image_cache.dart
@@ -1,28 +1,28 @@
-import 'dart:io' as dart;
+import 'dart:io' as io;
 
 import 'package:photos/core/cache/lru_map.dart';
 import 'package:photos/models/file.dart';
 
 class FileLruCache {
-  static LRUMap<String, dart.File> _map = LRUMap(25);
+  static LRUMap<String, io.File> _map = LRUMap(25);
 
-  static dart.File get(File file) {
+  static io.File get(File file) {
     return _map.get(file.tag());
   }
 
-  static void put(File file, dart.File imageData) {
+  static void put(File file, io.File imageData) {
     _map.put(file.tag(), imageData);
   }
 }
 
 class ThumbnailFileLruCache {
-  static LRUMap<String, dart.File> _map = LRUMap(500);
+  static LRUMap<String, io.File> _map = LRUMap(500);
 
-  static dart.File get(File file) {
+  static io.File get(File file) {
     return _map.get(file.tag());
   }
 
-  static void put(File file, dart.File imageData) {
+  static void put(File file, io.File imageData) {
     _map.put(file.tag(), imageData);
   }
 }
