f124edecb1cc8ebd7828a54f41731c0645a37495
Vishnu Mohandas <vishnumohandas@gmail.com>
2020-08-22 04:57:29 +0530

    Subscribe to ente://deeplinks

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:computer/computer.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:photos/core/constants.dart';
 import 'package:photos/core/configuration.dart';
@@ -13,6 +14,7 @@ import 'package:photos/ui/home_widget.dart';
 import 'package:sentry/sentry.dart';
 import 'package:super_logging/super_logging.dart';
 import 'package:logging/logging.dart';
+import 'package:uni_links/uni_links.dart';
 
 final logger = Logger("main");
 
@@ -34,6 +36,7 @@ void _main() async {
   await PhotoSyncManager.instance.init();
   await MemoriesService.instance.init();
   await FavoriteFilesRepository.instance.init();
+  await initDeepLinks();
   _sync();
 
   final SentryClient sentry = new SentryClient(dsn: SENTRY_DSN);
@@ -71,6 +74,31 @@ void _sendErrorToSentry(SentryClient sentry, Object error, StackTrace stack) {
     print('Sending report to sentry.io failed: $e');
     print('Original error: $error');
   }
+}
+
+Future<void> initDeepLinks() async {
+  // Platform messages may fail, so we use a try/catch PlatformException.
+  try {
+    String initialLink = await getInitialLink();
+    // Parse the link and warn the user, if it is not correct,
+    // but keep in mind it could be `null`.
+    if (initialLink != null) {
+      logger.info("Initial link received: " + initialLink);
+    } else {
+      logger.info("No initial link received.");
+    }
+  } on PlatformException {
+    // Handle exception by warning the user their action did not succeed
+    // return?
+    logger.severe("PlatformException thrown while getting initial link");
+  }
+
+  // Attach a listener to the stream
+  getLinksStream().listen((String link) {
+    logger.info("Link received: " + link);
+  }, onError: (err) {
+    logger.severe(err);
+  });
 }
 
 class MyApp extends StatelessWidget with WidgetsBindingObserver {
