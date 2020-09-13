295d8eaa3284a2c7b116844d10e701d73fad1e2d
Pushkar Anand <pushkar8723@gmail.com>
2020-09-13 14:11:01 +0530

    Changes to deploy on netlify

--- src/utils/storage/localStorage.ts
+++ src/utils/storage/localStorage.ts
@@ -5,13 +5,22 @@ export enum LS_KEYS {
 }
 
 export const setData = (key: LS_KEYS, value: object) => {
+    if (typeof localStorage === 'undefined') {
+        return null;
+    }
     localStorage.setItem(key, JSON.stringify(value));
 }
 
 export const getData = (key: LS_KEYS) => {
+    if (typeof localStorage === 'undefined') {
+        return null;
+    }
     return JSON.parse(localStorage.getItem(key));
 }
 
 export const clearData = () => {
+    if (typeof localStorage === 'undefined') {
+        return null;
+    }
     localStorage.clear();
 }
--- src/utils/storage/sessionStorage.ts
+++ src/utils/storage/sessionStorage.ts
@@ -3,13 +3,22 @@ export enum SESSION_KEYS {
 }
 
 export const setKey = (key: SESSION_KEYS, value: object) => {
+    if (typeof sessionStorage === 'undefined') {
+        return null;
+    }
     sessionStorage.setItem(key, JSON.stringify(value));
 }
 
 export const getKey = (key: SESSION_KEYS) => {
+    if (typeof sessionStorage === 'undefined') {
+        return null;
+    }
     return JSON.parse(sessionStorage.getItem(key));
 }
 
 export const clearKeys = () => {
+    if (typeof sessionStorage === 'undefined') {
+        return null;
+    }
     sessionStorage.clear();
 }
