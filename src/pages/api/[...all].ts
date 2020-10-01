5d4dc2e671d86eb2735c9c34ca7973d69ef16f7f
Vishnu Mohandas <vishnumohandas@gmail.com>
2020-10-01 17:21:36 +0530

    Revert endpoint to staging

--- src/pages/api/[...all].ts
+++ src/pages/api/[...all].ts
@@ -1,15 +1,15 @@
 import { createProxyMiddleware } from 'http-proxy-middleware';
 
 export const config = {
-  api: {
-    bodyParser: false,
-  },
+    api: {
+      bodyParser: false,
+    },
 };
 
-const API_ENDPOINT = process.env.NEXT_PUBLIC_ENTE_ENDPOINT || "http://192.168.0.100";
+const API_ENDPOINT = process.env.NEXT_PUBLIC_ENTE_ENDPOINT || "https://api.staging.ente.io";
 
 export default createProxyMiddleware({
-  target: API_ENDPOINT,
-  changeOrigin: true,
-  pathRewrite: { '^/api': '/' },
+    target: API_ENDPOINT,
+    changeOrigin: true,
+    pathRewrite: { '^/api': '/' },
 });
--- src/utils/common/apiUtil.ts
+++ src/utils/common/apiUtil.ts
@@ -1,6 +1,6 @@
 export const getEndpoint = () => {
     const dev = process.env.NODE_ENV === 'development';
-    const apiEndpoint = process.env.NEXT_PUBLIC_ENTE_ENDPOINT || "http://192.168.0.100";
+    const apiEndpoint = process.env.NEXT_PUBLIC_ENTE_ENDPOINT || "https://api.staging.ente.io";
     const endpoint = !dev ? apiEndpoint : '/api';
     return endpoint;
 }
