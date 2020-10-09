792c606c7efb8e95a35749b4208065d13f9bb8d1
Vishnu Mohandas <vishnumohandas@gmail.com>
2020-10-10 05:17:00 +0530

    Add a util function to display a button

--- lib/ui/common_elements.dart
+++ lib/ui/common_elements.dart
@@ -1,3 +1,14 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 final nothingToSeeHere = Center(child: Text("Nothing to see here! 👀"));
+
+RaisedButton button(String text, {VoidCallback onPressed}) {
+  return RaisedButton(
+    child: Text(text),
+    onPressed: onPressed,
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(6.0),
+    ),
+  );
+}
