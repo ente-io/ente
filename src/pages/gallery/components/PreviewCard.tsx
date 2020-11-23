437682a901dacf852adea62c3a0d07d6a228c997
Pushkar Anand <pushkar8723@gmail.com>
2020-11-23 08:45:28 +0530

    Build fix.

--- src/pages/gallery/components/PreviewCard.tsx
+++ src/pages/gallery/components/PreviewCard.tsx
@@ -48,7 +48,7 @@ export default function PreviewCard(props: IProps) {
         }
     }
 
-    return <Cont onClick={handleClick} disabled={!data.src && !imgSrc}>
-        <img src={data.src || imgSrc} />
+    return <Cont onClick={handleClick} disabled={!data?.src && !imgSrc}>
+        <img src={data?.src || imgSrc} />
     </Cont>;
 }
