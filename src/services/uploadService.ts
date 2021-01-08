2f4ebdf0a927014092c99b45dbce0990b408264a
Abhinav-grd <abhinavk.grd@gmail.com>
2021-01-08 18:46:27 +0530

    completed file upload logic

--- src/services/uploadService.ts
+++ src/services/uploadService.ts
@@ -1,7 +1,6 @@
 import { getEndpoint } from 'utils/common/apiUtil';
 import HTTPService from './HTTPService';
 import * as Comlink from 'comlink';
-import { getData, LS_KEYS } from 'utils/storage/localStorage';
 import localForage from 'localforage';
 import { fileAttribute, collectionLatestFile, collection, file } from './fileService';
 const CryptoWorker: any =
@@ -20,23 +19,55 @@ interface encryptionResult {
     file: fileAttribute,
     key: Uint8Array
 }
+interface KeyEncryptionResult {
+    encryptedData: string,
+    key: Uint8Array,
+    nonce: string,
+}
 
-
-interface UploadURL {
+interface uploadURL {
     url: string,
     objectKey: string
 }
 
-interface Uploadfiles {
-    file: Uint8Array,
-    metaData: string,
+interface formatedFile {
+    filedata: Uint8Array,
+    metadata: string,
     thumbnail: Uint8Array
+}
+
+interface encryptedFile {
+    filedata: fileAttribute;
+    thumbnail: fileAttribute;
+    metadata: KeyEncryptionResult;
+    encryptedKey: string;
+    keyDecryptionNonce: string;
+    key: Uint8Array;
+}
+
+interface objectKey {
+    objectId: string,
+    decryptionHeader: string
+}
+interface objectKeys {
+    file: objectKey
+    thumbnail: objectKey
+}
+
+interface uploadFile extends objectKeys {
+    collectionID: string,
+    encryptedKey: string;
+    keyDecryptionNonce: string;
+    metadata: {
+        encryptedData: string,
+        decryptionHeader: string
+    }
 }
 
 class Queue<T> {
     _store: T[] = [];
-    push(val: T) {
-        this._store.push(val);
+    push(vals: T[]): void {
+        vals.forEach((val) => this._store.push(val));
     }
     pop(): T {
         return this._store.shift();
@@ -48,101 +79,183 @@ class Queue<T> {
 
 class UploadService {
 
-    private uploadUrls = new Queue<UploadURL>();
-    private UploadQueue = new Queue<number>();
+    private uploadURLs = new Queue<uploadURL>();
+    private uploadURLFetchInProgress: Promise<any> = null
 
-    public async uploadFiles(recivedFiles: any[], collectionLatestFile: collectionLatestFile) {
+    public async uploadFiles(recievedFiles: File[], collectionLatestFile: collectionLatestFile, token) {
         try {
-            const files: Uploadfiles[] = await this.formatData(recivedFiles);
-            const Encryptedfiles: files[] = await this.encryptFiles(files, collectionLatestFile.collection.key);
+            recievedFiles.map(async (recievedFile: File) => {
+                const file = await this.formatData(recievedFile);
+                console.log(file);
+
+                const encryptedfile: encryptedFile = await this.encryptFiles(file, collectionLatestFile.collection.key);
+                console.log(encryptedfile);
+
+                const objectKeys = await this.uploadtoBucket(encryptedfile, token);
+                console.log(objectKeys);
+
+                const uploadedfile = await this.uploadFile(collectionLatestFile, encryptedfile, objectKeys, token);
+            });
         }
         catch (e) {
             console.log(e);
         }
-
     }
 
-    public async formatData(filesToUpload: any[]) {
-        const formatedDataPromises: Promise<Uploadfiles>[] = filesToUpload.map(async (uploadedFile): Promise<Uploadfiles> => {
-            console.log(uploadedFile);
-            const fileData: Uint8Array = await new Promise((resolve, reject) => {
-                const reader = new FileReader()
+    public async formatData(recievedFile: File) {
+        const filedata: Uint8Array = await this.getUint8ArrayView(recievedFile);
+        return {
+            filedata,
+            metadata: JSON.stringify({
+                name: recievedFile.name,
+                size: recievedFile.size,
+                type: recievedFile.type
+            }),
+            thumbnail: await this.generateThumbnail(recievedFile)
+        }
+    }
+    private async encryptFiles(file: formatedFile, encryptionKey: string): Promise<encryptedFile> {
+        const worker = await new CryptoWorker();
 
-                reader.onabort = () => reject('file reading was aborted')
-                reader.onerror = () => reject('file reading has failed')
-                reader.onload = () => {
-                    // Do whatever you want with the file contents
-                    const result = typeof reader.result === "string" ? new TextEncoder().encode(reader.result) : new Uint8Array(reader.result);
-                    resolve(result);
+        const encryptFileResult = await worker.encryptFile(
+            file.filedata,
+            null
+        );
+
+        const { key: fileKey, file: filedata }: encryptionResult = encryptFileResult;
+
+        const { file: encryptedThumbnail }: encryptionResult = await worker.encryptThumbnail(file.thumbnail, fileKey);
+
+        const encryptedMetadata: KeyEncryptionResult = await worker.encryptMetadata(file.metadata, fileKey)
+
+        const { encryptedData: encryptedKey, nonce: keyDecryptionNonce }: KeyEncryptionResult = await worker.encrypt(fileKey, encryptionKey);
+
+
+        const result: encryptedFile = {
+            key: fileKey,
+            filedata: filedata,
+            thumbnail: encryptedThumbnail,
+            metadata: encryptedMetadata,
+            encryptedKey,
+            keyDecryptionNonce,
+        };
+        return result;
+    }
+
+    private async uploadtoBucket(file: encryptedFile, token): Promise<objectKeys> {
+        const fileUploadURL = await this.getUploadURL(token);
+        const fileObjectKey = await this.putFile(fileUploadURL, file.filedata.encryptedData)
+
+        const thumbnailUploadURL = await this.getUploadURL(token);
+        const thumbnailObjectKey = await this.putFile(thumbnailUploadURL, file.thumbnail.encryptedData)
+
+        return {
+            file: { objectId: fileObjectKey, decryptionHeader: file.filedata.decryptionHeader },
+            thumbnail: { objectId: thumbnailObjectKey, decryptionHeader: file.thumbnail.decryptionHeader }
+        };
+    }
+
+    private async uploadFile(collectionLatestFile: collectionLatestFile, encryptedFile: encryptedFile, objectKeys: objectKeys, token) {
+        const uploadFile: uploadFile = {
+            collectionID: collectionLatestFile.collection.id,
+            encryptedKey: encryptedFile.encryptedKey,
+            keyDecryptionNonce: encryptedFile.encryptedKey,
+            metadata: {
+                encryptedData: encryptedFile.metadata.encryptedData,
+                decryptionHeader: encryptedFile.metadata.nonce
+
+            },
+            ...objectKeys
+        }
+
+
+        const response = await HTTPService.post(`${ENDPOINT}/files`, uploadFile, { token });
+
+        return response.data;
+    }
+
+    private async generateThumbnail(file: File): Promise<Uint8Array> {
+        let canvas = document.createElement("canvas");
+        let canvas_CTX = canvas.getContext("2d");
+        let type = file.type.split('/')[0];
+        console.log(type);
+        if (type === "image") {
+            let image = new Image();
+            image.setAttribute("src", URL.createObjectURL(file));
+            await new Promise((resolve, reject) => {
+                image.onload = () => {
+                    console.log(image);
+                    canvas.width = image.width;
+                    canvas.height = image.height;
+                    resolve(null);
                 }
-                reader.readAsArrayBuffer(uploadedFile)
             });
-            return {
-                file: fileData,
-                metaData: null,
-                thumbnail: null
-            }
-        })
-        return await Promise.all(formatedDataPromises);
-    }
-    private async encryptFiles(files: Uploadfiles[], encryptionKey: string): Promise<file[]> {
-        let encryptedfilesPromises: Promise<file>[] = files.map(async (file): Promise<file> => {
-            const worker = await new CryptoWorker();
-            const encryptResult = await worker.encryptFile(
-                files,
-                null
-            );
-
-            const { key: fileKey, file: fileAttributes }: encryptionResult = encryptResult;
-
-            // console.log(file, key);
-
-            // const reDecrypted: any = await worker.decryptFile(
-            //   fileAttributes.encryptedData,
-            //   await worker.fromB64(fileAttributes.decryptionHeader),
-            //   key
-            // );
-            // console.log(URL.createObjectURL(new Blob([reDecrypted])));
-
-            let thumbnailData = await this.generateThumbnail(file);
-
-            const encryptedThumbnailData = await worker.encryptThumbnail(thumbnailData, fileKey);
-
-            const encryptedMetaData=await worker.
-
-            const keyEncryptionResult = await worker.encrypt(fileKey, encryptionKey);
-
-            const fileUploadURL = await this.getUploadURL();
-            // string fileObjectKey = await putFile(fileUploadURL, encryptedFile);
-
-            // final thumbnailUploadURL = await _getUploadURL();
-            // String thumbnailObjectKey =
-            //     await _putFile(thumbnailUploadURL, encryptedThumbnailFile);
-            const fileToBeUploaded = {
-                collectionID: Number(collection.id),
-                file: fileAttributes,
-                encryptedKey: keyEncryptionResult.encryptedData,
-                keyDecryptionNonce: keyEncryptionResult.nonce,
-                key,
-                id: 0,
-            }
-            return fileToBeUploaded;
+            canvas_CTX.drawImage(image, 0, 0, image.width, image.height);
         }
-    }
+        else {
+            let video = document.createElement('video');
+            video.setAttribute("src", URL.createObjectURL(file));
 
-    private async generateThumbnail(fileData) {
-        return fileData;
-    }
-
-    private async getUploadURL() {
-        if (this.uploadUrls.isEmpty()) {
-            await this.fetchUploadURLs();
+            await new Promise((resolve, reject) => {
+                video.addEventListener('loadedmetadata', function () {
+                    console.log(video);
+                    canvas.width = video.videoWidth;
+                    canvas.height = video.videoHeight;
+                    canvas_CTX.drawImage(video, 0, 0, video.videoWidth, video.videoHeight);
+                    resolve(null);
+                });
+            });
         }
-        return this.uploadUrls.pop();
+        console.log(canvas.toDataURL());
+        const thumbnail: Uint8Array = await new Promise((resolve, reject) => {
+            canvas.toBlob(async (blob) => {
+                console.log(URL.createObjectURL(blob));
+                resolve(await this.getUint8ArrayView(blob));
+            })
+        });
+        return thumbnail;
+    }
+    private async getUint8ArrayView(file): Promise<Uint8Array> {
+        return await new Promise((resolve, reject) => {
+            const reader = new FileReader()
+
+            reader.onabort = () => reject('file reading was aborted')
+            reader.onerror = () => reject('file reading has failed')
+            reader.onload = () => {
+                // Do whatever you want with the file contents
+                const result = typeof reader.result === "string" ? new TextEncoder().encode(reader.result) : new Uint8Array(reader.result);
+                resolve(result);
+            }
+            reader.readAsArrayBuffer(file)
+        });
     }
 
-    private async fetchUploadURLs() {
+    private async getUploadURL(token) {
+        if (this.uploadURLs.isEmpty()) {
+            await this.fetchUploadURLs(token);
+        }
+        return this.uploadURLs.pop();
+    }
 
+    private async fetchUploadURLs(token): Promise<void> {
+        if (!this.uploadURLFetchInProgress) {
+            this.uploadURLFetchInProgress = HTTPService.get(`${ENDPOINT}/files/upload-urls`,
+                {
+                    token: token,
+                    count: "42"  //m4gic number
+                })
+            const response = await this.uploadURLFetchInProgress;
+
+            this.uploadURLFetchInProgress = null;
+            this.uploadURLs.push(response.data["urls"]);
+        }
+        return this.uploadURLFetchInProgress;
+    }
+
+    private async putFile(fileUploadURL: uploadURL, file: Uint8Array): Promise<string> {
+        const fileSize = file.length.toString();
+        await HTTPService.post(fileUploadURL.url, file, null, { contentLengthHeader: fileSize })
+        return fileUploadURL.objectKey;
     }
 }
 
