faa1fb329a9c66ebde1897707c952451afb2a91c
Abhinav-grd <abhinavk.grd@gmail.com>
2021-01-13 18:17:00 +0530

    Cleaned up uploadButton component

--- src/pages/gallery/components/UploadButton.tsx
+++ src/pages/gallery/components/UploadButton.tsx
@@ -1,15 +1,14 @@
-import CollectionSelector from 'pages/gallery/components/CollectionSelector';
-import React, { useRef } from 'react';
+import React from 'react';
 import { Button } from 'react-bootstrap';
 
 const UploadButton = ({ showModal }) => {
-  return (
-    <>
-      <Button variant='primary' onClick={showModal}>
-        Upload New Photos
+    return (
+        <>
+            <Button variant='primary' onClick={showModal}>
+                Upload New Photos
       </Button>
-    </>
-  );
+        </>
+    );
 };
 
 export default UploadButton;
