de1256c710620c03085b8d28ca147a76cf34fe1d
Vishnu Mohandas <vishnumohandas@gmail.com>
2021-01-13 21:36:23 +0530

    Upgrade to Flutter's v2 embedding

--- android/app/src/main/kotlin/io/ente/photos/MainActivity.kt
+++ android/app/src/main/kotlin/io/ente/photos/MainActivity.kt
@@ -1,12 +1,6 @@
 package io.ente.photos
 
-import androidx.annotation.NonNull
-import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.plugins.GeneratedPluginRegistrant
+import io.flutter.embedding.android.FlutterActivity;
 
 class MainActivity : FlutterActivity() {
-    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
-        GeneratedPluginRegistrant.registerWith(flutterEngine)
-    }
 }
