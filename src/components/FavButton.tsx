6dee89bb621d7ceabb373ac4f4099f87a2ae6686
Abhinav-grd <abhinavk.grd@gmail.com>
2021-01-19 10:01:11 +0530

    removed className prop from favButton

--- src/components/FavButton.tsx
+++ src/components/FavButton.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import styled from "styled-components";
 
 const HeartUI = styled.button.attrs(props => ({
-    className: props.className,
     isClick: props['isClick']
 }))`
   width: 100px;
