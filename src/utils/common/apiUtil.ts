2d45f0dfb51a6a07c5b6d6f6289bff1b659dc23b
Abhinav-grd <abhinavk.grd@gmail.com>
2021-01-19 08:40:14 +0530

    added localEndpoint

--- src/utils/common/apiUtil.ts
+++ src/utils/common/apiUtil.ts
@@ -1,3 +1,3 @@
 export const getEndpoint = () => {
-    return process.env.NEXT_PUBLIC_ENTE_ENDPOINT || "https://api.ente.io";
+    return process.env.ENV == "local" ? "http://localhost:8080" : process.env.NEXT_PUBLIC_ENTE_ENDPOINT || "https://api.ente.io";
 }
