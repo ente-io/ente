11ad219163d43f784f007e2af8412127751cc7e4
Abhinav-grd <abhinavk.grd@gmail.com>
2021-01-19 14:05:51 +0530

    corrected environment variable name to expose it in browser

--- src/utils/common/apiUtil.ts
+++ src/utils/common/apiUtil.ts
@@ -1,3 +1,5 @@
 export const getEndpoint = () => {
-    return process.env.ENV == "local" ? "http://localhost:8080" : process.env.NEXT_PUBLIC_ENTE_ENDPOINT || "https://api.ente.io";
+    const endPoint = process.env.NEXT_PUBLIC_ENV == "local" ? "http://localhost:8080" : process.env.NEXT_PUBLIC_ENTE_ENDPOINT || "https://api.ente.io";
+    console.log(endPoint);
+    return endPoint;
 }
