025c97b336e7ddbbfd73dae745dfbc76c8c1695b
Abhinav-grd <abhinavk.grd@gmail.com>
2021-01-24 21:43:49 +0530

    used env variable to set endpoint

--- src/utils/common/apiUtil.ts
+++ src/utils/common/apiUtil.ts
@@ -1,5 +1,5 @@
 export const getEndpoint = () => {
-    const endPoint = process.env.NEXT_PUBLIC_ENV == "local" ? "http://localhost:8080" : process.env.NEXT_PUBLIC_ENTE_ENDPOINT || "https://api.ente.io";
+    const endPoint = process.env.NEXT_PUBLIC_ENTE_ENDPOINT ?? "https://api.ente.io";
     console.log(endPoint);
     return endPoint;
 }
