5d09e6b6019c2b9d1cc65dbfe420747ba137fdbf
Vishnu Mohandas <vishnu@vishnu.tech>
2021-02-02 23:27:53 +0530

    Reduce the thumbnail until it's lower than the thumbnail limit

--- lib/utils/file_uploader.dart
+++ lib/utils/file_uploader.dart
@@ -194,12 +194,13 @@ class FileUploader {
         _logger.severe("Could not generate thumbnail for " + file.toString());
         throw InvalidFileError();
       }
-      final thumbnailSize = thumbnailData.length;
-      if (thumbnailSize > THUMBNAIL_DATA_LIMIT) {
+      var thumbnailSize = thumbnailData.length;
+      while (thumbnailSize > THUMBNAIL_DATA_LIMIT) {
         thumbnailData = await compressThumbnail(thumbnailData);
         _logger.info("Thumbnail size " + thumbnailSize.toString());
         _logger.info(
             "Compressed thumbnail size " + thumbnailData.length.toString());
+        thumbnailSize = thumbnailData.length;
       }
 
       final encryptedThumbnailData =
