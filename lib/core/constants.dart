74892f8bbdc4efae63c922efab35a26390bd7762
Vishnu Mohandas <vishnu@vishnu.tech>
2021-02-06 23:56:55 +0530

    Update Sentry DSN

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -4,4 +4,4 @@ const int THUMBNAIL_LARGE_SIZE = 512;
 const int COMPRESSED_THUMBNAIL_RESOLUTION = 1080;
 const int THUMBNAIL_DATA_LIMIT = 100 * 1024;
 const String SENTRY_DSN =
-    "https://1e4a7745cb3848f1a304002f990dd801@errors.ente.io/2";
+    "https://93b8ea6f54f442dc8408ebccdff6fe7a@errors.ente.io/2";
