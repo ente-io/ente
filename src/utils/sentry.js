3f78395106c8487321758bbaf25dd29bb7fb002f
Abhinav-grd <abhinavk.grd@gmail.com>
2021-03-08 17:37:19 +0530

    remove enabled prop from sentry init

--- src/utils/sentry.js
+++ src/utils/sentry.js
@@ -29,7 +29,6 @@ export const init = () => {
         }
 
         Sentry.init({
-            // enabled: process.env.NODE_ENV === 'production',
             environment: process.env.NODE_ENV,
             integrations,
             dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
