86b733982dbff5ea54e96e71d02bd14a7bbac54d
Abhinav-grd <abhinavk.grd@gmail.com>
2021-03-11 21:56:16 +0530

    added subscription change disable message component

---
+++ src/components/ChangeDisabledMessage.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Button, Modal } from 'react-bootstrap';
+import constants from 'utils/strings/constants';
+
+function ChangeDisabledMessage({ logout, ...props }) {
+    return (
+        <Modal
+            {...props}
+            size="lg"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+        >
+            <Modal.Body style={{ padding: '24px' }}>
+                <Modal.Title id="contained-modal-title-vcenter">
+                    {constants.SUBSCRIPTION_CHANGE_DISABLED}
+                </Modal.Title>
+            </Modal.Body>
+            <Modal.Footer style={{ borderTop: 'none' }}>
+                <Button variant="secondary" onClick={props.onHide}>
+                    {constants.CLOSE}
+                </Button>
+            </Modal.Footer>
+        </Modal>
+    );
+}
+export default ChangeDisabledMessage;
