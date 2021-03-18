5bf0c7d354646f06ef13a2795d730295a52adee2
Abhinav-grd <abhinavk.grd@gmail.com>
2021-03-19 05:06:40 +0530

    abstracted out collection create component

---
+++ src/pages/gallery/components/CreateCollection.tsx
@@ -0,0 +1,63 @@
+import React, { useEffect, useRef, useState } from 'react';
+import { Modal, Form, Button } from 'react-bootstrap';
+import constants from 'utils/strings/constants';
+
+interface Props {
+    createCollectionView;
+    setCreateCollectionView;
+    genAutoFilledName;
+    uploadFiles;
+}
+export default function CreateCollection(props: Props) {
+    const [albumName, setAlbumName] = useState('');
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    const handleChange = (event) => {
+        setAlbumName(event.target.value);
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        await props.uploadFiles(albumName, null);
+    };
+    useEffect(() => {
+        setAlbumName(props.genAutoFilledName());
+        setTimeout(() => {
+            inputRef.current && inputRef.current.focus();
+        }, 1);
+    }, []);
+    return (
+        <Modal
+            show={props.createCollectionView}
+            onHide={() => props.setCreateCollectionView(false)}
+            centered
+            backdrop="static"
+            style={{ background: 'rgba(0, 0, 0, 0.8)' }}
+            dialogClassName="ente-modal"
+        >
+            <Modal.Header closeButton>
+                <Modal.Title>{constants.CREATE_COLLECTION}</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <Form onSubmit={handleSubmit}>
+                    <Form.Group controlId="formBasicEmail">
+                        <Form.Control
+                            type="text"
+                            placeholder={constants.ALBUM_NAME}
+                            value={albumName}
+                            onChange={handleChange}
+                            ref={inputRef}
+                        />
+                    </Form.Group>
+                    <Button
+                        variant="primary"
+                        type="submit"
+                        style={{ width: '100%' }}
+                    >
+                        {constants.CREATE}
+                    </Button>
+                </Form>
+            </Modal.Body>
+        </Modal>
+    );
+}
