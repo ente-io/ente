7034c6ff8f8e467de6b8f284082162cfe8bba732
Abhinav-grd <abhinavk.grd@gmail.com>
2021-03-30 17:07:32 +0530

    added logic to send notification of export start and finish and show progress on ssytem tray icon

--- src/services/exportService.ts
+++ src/services/exportService.ts
@@ -2,6 +2,10 @@ import { runningInBrowser } from 'utils/common/utilFunctions';
 import downloadManager from './downloadManager';
 import { file } from './fileService';
 
+enum ExportNotification {
+    START = 'export started',
+    FINISH = 'export finished',
+}
 class ExportService {
     ElectronAPIs: any = runningInBrowser() && window['ElectronAPIs'];
     async exportFiles(files: file[]) {
@@ -9,13 +13,16 @@ class ExportService {
         const exportedFiles: Set<string> = await this.ElectronAPIs.getExportedFiles(
             dir
         );
-        for (let file of files) {
+        this.ElectronAPIs.sendNotification(ExportNotification.START);
+        for (let [index, file] of files.entries()) {
             const uid = `${file.id}_${file.metadata.title}`;
             if (!exportedFiles.has(uid)) {
                 await this.downloadAndSave(file, `${dir}/${uid}`);
                 this.ElectronAPIs.updateExportRecord(dir, uid);
+                this.ElectronAPIs.updateExportProgress(index, files.length);
             }
         }
+        this.ElectronAPIs.sendNotification(ExportNotification.FINISH);
     }
 
     async downloadAndSave(file: file, path) {
