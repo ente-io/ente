9c34231dff9909ada65b683150a40e4ca8c0431b
Abhinav-grd <abhinavk.grd@gmail.com>
2021-03-30 18:30:09 +0530

    added try catch to export files function

--- src/services/exportService.ts
+++ src/services/exportService.ts
@@ -9,25 +9,34 @@ enum ExportNotification {
 class ExportService {
     ElectronAPIs: any = runningInBrowser() && window['ElectronAPIs'];
     async exportFiles(files: file[]) {
-        const dir = await this.ElectronAPIs.selectDirectory();
-        const exportedFiles: Set<string> = await this.ElectronAPIs.getExportedFiles(
-            dir
-        );
-        this.ElectronAPIs.sendNotification(ExportNotification.START);
-        for (let [index, file] of files.entries()) {
-            const uid = `${file.id}_${file.metadata.title}`;
-            if (!exportedFiles.has(uid)) {
-                await this.downloadAndSave(file, `${dir}/${uid}`);
-                this.ElectronAPIs.updateExportRecord(dir, uid);
-                this.ElectronAPIs.updateExportProgress(index, files.length);
+        try {
+            const dir = await this.ElectronAPIs.selectDirectory();
+            if (!dir) {
+                // directory selector closed
+                return;
             }
+            const exportedFiles: Set<string> = await this.ElectronAPIs.getExportedFiles(
+                dir
+            );
+            this.ElectronAPIs.sendNotification(ExportNotification.START);
+            for (let [index, file] of files.entries()) {
+                const uid = `${file.id}_${file.metadata.title}`;
+                if (!exportedFiles.has(uid)) {
+                    await this.downloadAndSave(file, `${dir}/${uid}`);
+                    this.ElectronAPIs.updateExportRecord(dir, uid);
+                }
+                this.ElectronAPIs.showOnTray([
+                    { label: `${index + 1} / ${files.length} files exported` },
+                ]);
+            }
+            this.ElectronAPIs.sendNotification(ExportNotification.FINISH);
+            this.ElectronAPIs.showOnTray([]);
+        } catch (e) {
+            console.error(e);
         }
-        this.ElectronAPIs.sendNotification(ExportNotification.FINISH);
     }
 
     async downloadAndSave(file: file, path) {
-        console.log(this.ElectronAPIs);
-
         const fileStream = await downloadManager.downloadFile(file);
 
         this.ElectronAPIs.saveToDisk(path, fileStream);
