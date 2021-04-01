7c0aea258b6445cc5cb156abbd8c22a6be1de547
Abhinav-grd <abhinavk.grd@gmail.com>
2021-04-01 18:45:00 +0530

    updated keyAttribute interface

--- src/types.ts
+++ src/types.ts
@@ -4,6 +4,9 @@ export interface keyAttributes {
     keyDecryptionNonce: string;
     opsLimit: number;
     memLimit: number;
+    publicKey: string;
+    encryptedSecretKey: string;
+    secretKeyDecryptionNonce: string;
 }
 
 export const ENCRYPTION_CHUNK_SIZE = 4 * 1024 * 1024;
