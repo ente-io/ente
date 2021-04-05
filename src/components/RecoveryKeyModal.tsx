6cdfc9fee1ec407007660c1f85f0e3b2e2f780b9
Abhinav-grd <abhinavk.grd@gmail.com>
2021-04-05 17:30:17 +0530

    use message dailog with recoverykeyModal

--- src/components/RecoveryKeyModal.tsx
+++ src/components/RecoveryKeyModal.tsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import { Button, Modal, Spinner } from 'react-bootstrap';
+import { Spinner } from 'react-bootstrap';
 import { downloadAsFile } from 'utils/common';
 import { getRecoveryKey } from 'utils/crypto';
 import constants from 'utils/strings/constants';
+import { MessageDialog } from './MessageDailog';
 
-export interface Props {
+interface Props {
     show: boolean;
     onHide: () => void;
 }
@@ -15,7 +16,7 @@ function RecoveryKeyModal(props: Props) {
             return;
         }
         const main = async () => {
-            const recoveryKey = await getRecoveryKey();
+            const recoveryKey = null; //await getRecoveryKey();
 
             setRecoveryKey(recoveryKey);
         };
@@ -23,59 +24,52 @@ function RecoveryKeyModal(props: Props) {
     }, [props.show]);
 
     return (
-        <Modal {...props} size="lg" centered>
-            <Modal.Body>
-                <Modal.Title>
-                    <strong>{constants.DOWNLOAD_RECOVERY_KEY}</strong>
-                </Modal.Title>
-                <hr />
-                {constants.PASSPHRASE_DISCLAIMER()}
-                <div
-                    style={{
-                        display: 'flex',
-                        alignItems: 'center',
-                        justifyContent: 'center',
-                        background: '#1a1919',
-                        height: '150px',
-                        padding: '40px',
-                        color: 'white',
-                        margin: '20px 0',
-                    }}
-                >
-                    {recoveryKey ? (
-                        <div
-                            style={{
-                                wordWrap: 'break-word',
-                                overflowWrap: 'break-word',
-                                minWidth: '40%',
-                            }}
-                        >
-                            {recoveryKey}
-                        </div>
-                    ) : (
-                        <Spinner animation="border" />
-                    )}
-                </div>
-                {constants.KEY_NOT_STORED_DISCLAIMER()}
-            </Modal.Body>
-            <Modal.Footer style={{ borderTop: 'none' }}>
-                <Button variant="danger" onClick={props.onHide}>
-                    {constants.SAVE_LATER}
-                </Button>
-                <Button
-                    variant="success"
-                    onClick={() => {
+        <MessageDialog
+            {...props}
+            attributes={{
+                title: constants.DOWNLOAD_RECOVERY_KEY,
+                close: { text: constants.SAVE_LATER, action: props.onHide },
+                proceed: {
+                    text: constants.SAVE,
+                    action: () => {
                         downloadAsFile(
                             constants.RECOVERY_KEY_FILENAME,
                             recoveryKey
                         );
                         props.onHide();
-                    }}
-                >
-                    {constants.SAVE}
-                </Button>
-            </Modal.Footer>
-        </Modal>
+                    },
+                },
+            }}
+        >
+            {constants.PASSPHRASE_DISCLAIMER()}
+            <div
+                style={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    background: '#1a1919',
+                    height: '150px',
+                    padding: '40px',
+                    color: 'white',
+                    margin: '20px 0',
+                }}
+            >
+                {recoveryKey ? (
+                    <div
+                        style={{
+                            wordWrap: 'break-word',
+                            overflowWrap: 'break-word',
+                            minWidth: '40%',
+                        }}
+                    >
+                        {recoveryKey}
+                    </div>
+                ) : (
+                    <Spinner animation="border" />
+                )}
+            </div>
+            {constants.KEY_NOT_STORED_DISCLAIMER()}
+        </MessageDialog>
     );
 }
 export default RecoveryKeyModal;
