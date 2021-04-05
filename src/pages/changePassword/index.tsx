4faa7cddd66d1eece4bb2762c61403eccf17056d
Abhinav-grd <abhinavk.grd@gmail.com>
2021-04-05 11:47:31 +0530

    updated changePasswod and generate to use Password Form

--- src/pages/changePassword/index.tsx
+++ src/pages/changePassword/index.tsx
@@ -1,13 +1,6 @@
 import React, { useState, useEffect, useContext } from 'react';
-import Container from 'components/Container';
-import styled from 'styled-components';
-import Card from 'react-bootstrap/Card';
-import Form from 'react-bootstrap/Form';
 import constants from 'utils/strings/constants';
-import { Formik, FormikHelpers } from 'formik';
-import * as Yup from 'yup';
-import Button from 'react-bootstrap/Button';
-import { getData, LS_KEYS, setData } from 'utils/storage/localStorage';
+import { getData, LS_KEYS } from 'utils/storage/localStorage';
 import { useRouter } from 'next/router';
 import { getKey, SESSION_KEYS, setKey } from 'utils/storage/sessionStorage';
 import { B64EncryptionResult } from 'services/uploadService';
@@ -15,20 +8,9 @@ import CryptoWorker, {
     setSessionKeys,
     generateAndSaveIntermediateKeyAttributes,
 } from 'utils/crypto';
-import { Spinner } from 'react-bootstrap';
 import { getActualKey } from 'utils/common/key';
-import { setKeys, UpdatedKey } from 'services/userService';
-
-const Image = styled.img`
-    width: 200px;
-    margin-bottom: 20px;
-    max-width: 100%;
-`;
-
-interface formValues {
-    passphrase: string;
-    confirm: string;
-}
+import { logoutUser, setKeys, UpdatedKey } from 'services/userService';
+import PasswordForm from 'components/PasswordForm';
 
 export interface KEK {
     key: string;
@@ -37,7 +19,6 @@ export interface KEK {
 }
 
 export default function Generate() {
-    const [loading, setLoading] = useState(false);
     const [token, setToken] = useState<string>();
     const router = useRouter();
     const key = getKey(SESSION_KEYS.ENCRYPTION_KEY);
@@ -51,154 +32,48 @@ export default function Generate() {
         }
     }, []);
 
-    const onSubmit = async (
-        values: formValues,
-        { setFieldError }: FormikHelpers<formValues>
-    ) => {
-        setLoading(true);
+    const onSubmit = async (passphrase, setFieldError) => {
+        const cryptoWorker = await new CryptoWorker();
+        const key: string = await getActualKey();
+        const keyAttributes = getData(LS_KEYS.KEY_ATTRIBUTES);
+        const kekSalt: string = await cryptoWorker.generateSaltToDeriveKey();
+        let kek: KEK;
         try {
-            const { passphrase, confirm } = values;
-            if (passphrase === confirm) {
-                const cryptoWorker = await new CryptoWorker();
-                const key: string = await getActualKey();
-                const keyAttributes = getData(LS_KEYS.KEY_ATTRIBUTES);
-                const kekSalt: string = await cryptoWorker.generateSaltToDeriveKey();
-                let kek: KEK;
-                try {
-                    kek = await cryptoWorker.deriveSensitiveKey(
-                        passphrase,
-                        kekSalt
-                    );
-                } catch (e) {
-                    setFieldError(
-                        'confirm',
-                        constants.PASSWORD_GENERATION_FAILED
-                    );
-                    return;
-                }
-                const encryptedKeyAttributes: B64EncryptionResult = await cryptoWorker.encryptToB64(
-                    key,
-                    kek.key
-                );
-                const updatedKey: UpdatedKey = {
-                    kekSalt,
-                    encryptedKey: encryptedKeyAttributes.encryptedData,
-                    keyDecryptionNonce: encryptedKeyAttributes.nonce,
-                    opsLimit: kek.opsLimit,
-                    memLimit: kek.memLimit,
-                };
-
-                await setKeys(token, updatedKey);
-
-                const updatedKeyAttributes = Object.assign(
-                    keyAttributes,
-                    updatedKey
-                );
-                await generateAndSaveIntermediateKeyAttributes(
-                    passphrase,
-                    updatedKeyAttributes,
-                    key
-                );
-
-                setSessionKeys(key);
-                router.push('/gallery');
-            } else {
-                setFieldError('confirm', constants.PASSPHRASE_MATCH_ERROR);
-            }
+            kek = await cryptoWorker.deriveSensitiveKey(passphrase, kekSalt);
         } catch (e) {
-            setFieldError(
-                'passphrase',
-                `${constants.UNKNOWN_ERROR} ${e.message}`
-            );
-        } finally {
-            setLoading(false);
+            setFieldError('confirm', constants.PASSWORD_GENERATION_FAILED);
+            return;
         }
+        const encryptedKeyAttributes: B64EncryptionResult = await cryptoWorker.encryptToB64(
+            key,
+            kek.key
+        );
+        const updatedKey: UpdatedKey = {
+            kekSalt,
+            encryptedKey: encryptedKeyAttributes.encryptedData,
+            keyDecryptionNonce: encryptedKeyAttributes.nonce,
+            opsLimit: kek.opsLimit,
+            memLimit: kek.memLimit,
+        };
+
+        await setKeys(token, updatedKey);
+
+        const updatedKeyAttributes = Object.assign(keyAttributes, updatedKey);
+        await generateAndSaveIntermediateKeyAttributes(
+            passphrase,
+            updatedKeyAttributes,
+            key
+        );
+
+        setSessionKeys(key);
+        router.push('/gallery');
     };
 
     return (
-        <Container>
-            {/* <Image alt="vault" src="/vault.png" style={{ paddingBottom: '40px' }} /> */}
-            <Card style={{ maxWidth: '540px', padding: '20px' }}>
-                <Card.Body>
-                    <div
-                        className="text-center"
-                        style={{ marginBottom: '40px' }}
-                    >
-                        <p>{constants.ENTER_ENC_PASSPHRASE}</p>
-                        {constants.PASSPHRASE_DISCLAIMER()}
-                    </div>
-                    <Formik<formValues>
-                        initialValues={{ passphrase: '', confirm: '' }}
-                        validationSchema={Yup.object().shape({
-                            passphrase: Yup.string().required(
-                                constants.REQUIRED
-                            ),
-                            confirm: Yup.string().required(constants.REQUIRED),
-                        })}
-                        onSubmit={onSubmit}
-                    >
-                        {({
-                            values,
-                            touched,
-                            errors,
-                            handleChange,
-                            handleBlur,
-                            handleSubmit,
-                        }) => (
-                            <Form noValidate onSubmit={handleSubmit}>
-                                <Form.Group>
-                                    <Form.Control
-                                        type="password"
-                                        placeholder={constants.PASSPHRASE_HINT}
-                                        value={values.passphrase}
-                                        onChange={handleChange('passphrase')}
-                                        onBlur={handleBlur('passphrase')}
-                                        isInvalid={Boolean(
-                                            touched.passphrase &&
-                                                errors.passphrase
-                                        )}
-                                        autoFocus={true}
-                                        disabled={loading}
-                                    />
-                                    <Form.Control.Feedback type="invalid">
-                                        {errors.passphrase}
-                                    </Form.Control.Feedback>
-                                </Form.Group>
-                                <Form.Group>
-                                    <Form.Control
-                                        type="password"
-                                        placeholder={
-                                            constants.PASSPHRASE_CONFIRM
-                                        }
-                                        value={values.confirm}
-                                        onChange={handleChange('confirm')}
-                                        onBlur={handleBlur('confirm')}
-                                        isInvalid={Boolean(
-                                            touched.confirm && errors.confirm
-                                        )}
-                                        disabled={loading}
-                                    />
-                                    <Form.Control.Feedback type="invalid">
-                                        {errors.confirm}
-                                    </Form.Control.Feedback>
-                                </Form.Group>
-                                <Button
-                                    type="submit"
-                                    block
-                                    disabled={loading}
-                                    style={{ marginTop: '28px' }}
-                                >
-                                    {loading ? (
-                                        <Spinner animation="border" />
-                                    ) : (
-                                        constants.CHANGE_PASSWORD
-                                    )}
-                                </Button>
-                            </Form>
-                        )}
-                    </Formik>
-                </Card.Body>
-            </Card>
-        </Container>
+        <PasswordForm
+            callback={onSubmit}
+            buttonText={constants.CHANGE_PASSWORD}
+            cancel={() => router.push('/gallery')}
+        />
     );
 }
--- src/pages/generate/index.tsx
+++ src/pages/generate/index.tsx
@@ -1,14 +1,8 @@
-import React, { useState, useEffect, useContext } from 'react';
-import Container from 'components/Container';
-import styled from 'styled-components';
-import Card from 'react-bootstrap/Card';
-import Form from 'react-bootstrap/Form';
+import React, { useState, useEffect } from 'react';
 import constants from 'utils/strings/constants';
-import { Formik, FormikHelpers } from 'formik';
 import * as Yup from 'yup';
-import Button from 'react-bootstrap/Button';
 import { logoutUser, putAttributes } from 'services/userService';
-import { getData, LS_KEYS, setData } from 'utils/storage/localStorage';
+import { getData, LS_KEYS } from 'utils/storage/localStorage';
 import { useRouter } from 'next/router';
 import { getKey, SESSION_KEYS, setKey } from 'utils/storage/sessionStorage';
 import { B64EncryptionResult } from 'services/uploadService';
@@ -16,13 +10,7 @@ import CryptoWorker, {
     setSessionKeys,
     generateAndSaveIntermediateKeyAttributes,
 } from 'utils/crypto';
-import { Spinner } from 'react-bootstrap';
-
-const Image = styled.img`
-    width: 200px;
-    margin-bottom: 20px;
-    max-width: 100%;
-`;
+import PasswordForm from 'components/PasswordForm';
 
 interface formValues {
     passphrase: string;
@@ -53,166 +41,55 @@ export default function Generate() {
         }
     }, []);
 
-    const onSubmit = async (
-        values: formValues,
-        { setFieldError }: FormikHelpers<formValues>
-    ) => {
-        setLoading(true);
+    const onSubmit = async (passphrase, setFieldError) => {
+        const cryptoWorker = await new CryptoWorker();
+        const key: string = await cryptoWorker.generateMasterKey();
+        const kekSalt: string = await cryptoWorker.generateSaltToDeriveKey();
+        let kek: KEK;
         try {
-            const { passphrase, confirm } = values;
-            if (passphrase === confirm) {
-                const cryptoWorker = await new CryptoWorker();
-                const key: string = await cryptoWorker.generateMasterKey();
-                const kekSalt: string = await cryptoWorker.generateSaltToDeriveKey();
-                let kek: KEK;
-                try {
-                    kek = await cryptoWorker.deriveSensitiveKey(
-                        passphrase,
-                        kekSalt
-                    );
-                } catch (e) {
-                    setFieldError(
-                        'confirm',
-                        constants.PASSWORD_GENERATION_FAILED
-                    );
-                    return;
-                }
-                const encryptedKeyAttributes: B64EncryptionResult = await cryptoWorker.encryptToB64(
-                    key,
-                    kek.key
-                );
-                const keyPair = await cryptoWorker.generateKeyPair();
-                const encryptedKeyPairAttributes: B64EncryptionResult = await cryptoWorker.encryptToB64(
-                    keyPair.privateKey,
-                    key
-                );
-
-                const keyAttributes = {
-                    kekSalt,
-                    encryptedKey: encryptedKeyAttributes.encryptedData,
-                    keyDecryptionNonce: encryptedKeyAttributes.nonce,
-                    publicKey: keyPair.publicKey,
-                    encryptedSecretKey:
-                        encryptedKeyPairAttributes.encryptedData,
-                    secretKeyDecryptionNonce: encryptedKeyPairAttributes.nonce,
-                    opsLimit: kek.opsLimit,
-                    memLimit: kek.memLimit,
-                };
-                await putAttributes(
-                    token,
-                    getData(LS_KEYS.USER).name,
-                    keyAttributes
-                );
-                await generateAndSaveIntermediateKeyAttributes(
-                    passphrase,
-                    keyAttributes,
-                    key
-                );
-
-                setSessionKeys(key);
-                router.push('/gallery');
-            } else {
-                setFieldError('confirm', constants.PASSPHRASE_MATCH_ERROR);
-            }
+            kek = await cryptoWorker.deriveSensitiveKey(passphrase, kekSalt);
         } catch (e) {
-            setFieldError(
-                'passphrase',
-                `${constants.UNKNOWN_ERROR} ${e.message}`
-            );
-        } finally {
-            setLoading(false);
+            setFieldError('confirm', constants.PASSWORD_GENERATION_FAILED);
+            return;
         }
+        const encryptedKeyAttributes: B64EncryptionResult = await cryptoWorker.encryptToB64(
+            key,
+            kek.key
+        );
+        const keyPair = await cryptoWorker.generateKeyPair();
+        const encryptedKeyPairAttributes: B64EncryptionResult = await cryptoWorker.encryptToB64(
+            keyPair.privateKey,
+            key
+        );
+
+        const keyAttributes = {
+            kekSalt,
+            encryptedKey: encryptedKeyAttributes.encryptedData,
+            keyDecryptionNonce: encryptedKeyAttributes.nonce,
+            publicKey: keyPair.publicKey,
+            encryptedSecretKey: encryptedKeyPairAttributes.encryptedData,
+            secretKeyDecryptionNonce: encryptedKeyPairAttributes.nonce,
+            opsLimit: kek.opsLimit,
+            memLimit: kek.memLimit,
+        };
+        await putAttributes(token, getData(LS_KEYS.USER).name, keyAttributes);
+        await generateAndSaveIntermediateKeyAttributes(
+            passphrase,
+            keyAttributes,
+            key
+        );
+
+        setSessionKeys(key);
+        router.push('/gallery');
     };
 
     return (
-        <Container>
-            {/* <Image alt="vault" src="/vault.png" style={{ paddingBottom: '40px' }} /> */}
-            <Card style={{ maxWidth: '540px', padding: '20px' }}>
-                <Card.Body>
-                    <div
-                        className="text-center"
-                        style={{ marginBottom: '40px' }}
-                    >
-                        <p>{constants.ENTER_ENC_PASSPHRASE}</p>
-                        {constants.PASSPHRASE_DISCLAIMER()}
-                    </div>
-                    <Formik<formValues>
-                        initialValues={{ passphrase: '', confirm: '' }}
-                        validationSchema={Yup.object().shape({
-                            passphrase: Yup.string().required(
-                                constants.REQUIRED
-                            ),
-                            confirm: Yup.string().required(constants.REQUIRED),
-                        })}
-                        onSubmit={onSubmit}
-                    >
-                        {({
-                            values,
-                            touched,
-                            errors,
-                            handleChange,
-                            handleBlur,
-                            handleSubmit,
-                        }) => (
-                            <Form noValidate onSubmit={handleSubmit}>
-                                <Form.Group>
-                                    <Form.Control
-                                        type="password"
-                                        placeholder={constants.PASSPHRASE_HINT}
-                                        value={values.passphrase}
-                                        onChange={handleChange('passphrase')}
-                                        onBlur={handleBlur('passphrase')}
-                                        isInvalid={Boolean(
-                                            touched.passphrase &&
-                                                errors.passphrase
-                                        )}
-                                        autoFocus={true}
-                                        disabled={loading}
-                                    />
-                                    <Form.Control.Feedback type="invalid">
-                                        {errors.passphrase}
-                                    </Form.Control.Feedback>
-                                </Form.Group>
-                                <Form.Group>
-                                    <Form.Control
-                                        type="password"
-                                        placeholder={
-                                            constants.PASSPHRASE_CONFIRM
-                                        }
-                                        value={values.confirm}
-                                        onChange={handleChange('confirm')}
-                                        onBlur={handleBlur('confirm')}
-                                        isInvalid={Boolean(
-                                            touched.confirm && errors.confirm
-                                        )}
-                                        disabled={loading}
-                                    />
-                                    <Form.Control.Feedback type="invalid">
-                                        {errors.confirm}
-                                    </Form.Control.Feedback>
-                                </Form.Group>
-                                <Button
-                                    type="submit"
-                                    block
-                                    disabled={loading}
-                                    style={{ marginTop: '28px' }}
-                                >
-                                    {loading ? (
-                                        <Spinner animation="border" />
-                                    ) : (
-                                        constants.SET_PASSPHRASE
-                                    )}
-                                </Button>
-                            </Form>
-                        )}
-                    </Formik>
-                    <div className="text-center" style={{ marginTop: '20px' }}>
-                        <a href="#" onClick={logoutUser}>
-                            {constants.LOGOUT}
-                        </a>
-                    </div>
-                </Card.Body>
-            </Card>
-        </Container>
+        <>
+            <PasswordForm
+                callback={onSubmit}
+                buttonText={constants.SET_PASSPHRASE}
+                cancel={logoutUser}
+            />
+        </>
     );
 }
