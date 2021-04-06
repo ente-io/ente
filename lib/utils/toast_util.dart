c65f7ee77073b152c9e66466be99fb4a5419edf9
Vishnu Mohandas <vishnu@vishnu.tech>
2021-04-06 05:36:54 +0530

    Change background color for toasts

--- lib/utils/toast_util.dart
+++ lib/utils/toast_util.dart
@@ -7,7 +7,7 @@ Future<void> showToast(String message, {toastLength: Toast.LENGTH_LONG}) {
       toastLength: toastLength,
       gravity: ToastGravity.BOTTOM,
       timeInSecForIosWeb: 1,
-      backgroundColor: Colors.grey[800],
+      backgroundColor: Colors.blueGrey[900],
       textColor: Colors.white,
       fontSize: 16.0);
 }
