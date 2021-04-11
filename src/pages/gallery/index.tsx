bf7083442ef979d14c5f22e499ac4b51295f99d7
Abhinav-grd <abhinavk.grd@gmail.com>
2021-04-11 13:42:44 +0530

    passed files to uploadService

--- src/pages/gallery/index.tsx
+++ src/pages/gallery/index.tsx
@@ -447,6 +447,7 @@ export default function Gallery(props: Props) {
                 refetchData={syncWithRemote}
                 setBannerMessage={setBannerMessage}
                 acceptedFiles={props.acceptedFiles}
+                existingFiles={data}
             />
             <Sidebar
                 files={data}
