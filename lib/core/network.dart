13b0acbe32ecbb323ffe61aab259a5d8a2ff7552
Vishnu Mohandas <vishnu@vishnu.tech>
2021-04-19 18:16:16 +0530

    Remove API call logger

--- lib/core/network.dart
+++ lib/core/network.dart
@@ -1,11 +1,7 @@
-import 'dart:convert';
-import 'dart:developer';
 import 'dart:io';
 
 import 'package:dio/dio.dart';
 import 'package:flutter_user_agent/flutter_user_agent.dart';
-import 'package:pretty_dio_logger/pretty_dio_logger.dart';
-import 'package:super_logging/super_logging.dart';
 
 class Network {
   Dio _dio;
@@ -14,22 +10,6 @@ class Network {
     await FlutterUserAgent.init();
     _dio = Dio(BaseOptions(
         headers: {HttpHeaders.userAgentHeader: FlutterUserAgent.userAgent}));
-    _dio.interceptors.add(PrettyDioLogger(
-        requestHeader: false,
-        responseHeader: false,
-        requestBody: true,
-        responseBody: true,
-        logPrint: (object) async {
-          log(object);
-          if (Platform.isAndroid) {
-            await SuperLogging.logFile.writeAsString(
-              object.toString() + "\n",
-              encoding: Utf8Codec(allowMalformed: true),
-              mode: FileMode.append,
-              flush: true,
-            );
-          }
-        }));
   }
 
   Network._privateConstructor();
