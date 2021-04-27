2aab881c57eaf21e9d0e0fe6ef00f01a6660acdb
Abhinav-grd <abhinavk.grd@gmail.com>
2021-04-27 17:15:35 +0530

    added collection util to add to collection

---
+++ src/utils/collection/index.ts
@@ -0,0 +1,35 @@
+import {
+    addToCollection,
+    Collection,
+    CollectionType,
+    createCollection,
+} from 'services/collectionService';
+import { getSelectedFiles } from 'utils/file';
+import { File } from 'services/fileService';
+
+export async function addFilesToCollection(
+    setCollectionSelectorView: (value: boolean) => void,
+    selected: any,
+    files: File[],
+    clearSelection: () => void,
+    syncWithRemote: () => Promise<void>,
+    selectCollection: (id: number) => void,
+    collectionName: string,
+    existingCollection: Collection
+) {
+    setCollectionSelectorView(false);
+    let collection;
+    if (!existingCollection) {
+        collection = await createCollection(
+            collectionName,
+            CollectionType.album
+        );
+    } else {
+        collection = existingCollection;
+    }
+    const selectedFiles = getSelectedFiles(selected, files);
+    await addToCollection(collection, selectedFiles);
+    clearSelection();
+    await syncWithRemote();
+    selectCollection(collection.id);
+}
