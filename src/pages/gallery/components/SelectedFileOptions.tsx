bf4871aa008516ef245a48725b3f75ceaddd74da
Abhinav-grd <abhinavk.grd@gmail.com>
2021-04-27 17:28:27 +0530

    added selectedFileOptions component

---
+++ src/pages/gallery/components/SelectedFileOptions.tsx
@@ -0,0 +1,53 @@
+import AddToCollectionBtn from 'components/AddToCollectionBtn';
+import DeleteBtn from 'components/DeleteBtn';
+import React from 'react';
+import { SetDialogMessage } from 'utils/billingUtil';
+import constants from 'utils/strings/constants';
+import { SetCollectionSelectorAttributes } from './CollectionSelector';
+
+interface Props {
+    addToCollectionHelper: (collectionName, collection) => Promise<void>;
+    showCreateCollectionModal: () => void;
+    setDialogMessage: SetDialogMessage;
+    setCollectionSelectorAttributes: SetCollectionSelectorAttributes;
+    deleteFileHelper: () => Promise<void>;
+}
+const SelectedFileOptions = ({
+    addToCollectionHelper,
+    showCreateCollectionModal,
+    setDialogMessage,
+    setCollectionSelectorAttributes,
+    deleteFileHelper,
+}: Props) => {
+    return (
+        <>
+            <AddToCollectionBtn
+                onClick={() =>
+                    setCollectionSelectorAttributes({
+                        callback: (collection) =>
+                            addToCollectionHelper(null, collection),
+                        showNextModal: showCreateCollectionModal,
+                        title: 'add to collection',
+                    })
+                }
+            />
+            <DeleteBtn
+                onClick={() =>
+                    setDialogMessage({
+                        title: constants.CONFIRM_DELETE_FILE,
+                        content: constants.DELETE_FILE_MESSAGE,
+                        staticBackdrop: true,
+                        proceed: {
+                            action: deleteFileHelper,
+                            text: constants.DELETE,
+                            variant: 'danger',
+                        },
+                        close: { text: constants.CANCEL },
+                    })
+                }
+            />
+        </>
+    );
+};
+
+export default SelectedFileOptions;
