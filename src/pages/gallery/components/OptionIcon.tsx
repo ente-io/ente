60a7b94d0bade182b138115951c741aec157e1af
Abhinav-grd <abhinavk.grd@gmail.com>
2021-04-29 03:02:00 +0530

    abstracted out optionIcon

---
+++ src/pages/gallery/components/OptionIcon.tsx
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+
+export const OptionIconWrapper = styled.div`
+    display: inline-block;
+    opacity: 0;
+    font-weight: bold;
+    width: 24px;
+`;
+interface Props {
+    onClick: () => void;
+}
+const OptionIcon = ({ onClick }: Props) => {
+    return (
+        <OptionIconWrapper
+            onClick={(e) => {
+                onClick();
+                e.stopPropagation();
+            }}
+        >
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="24px"
+                width="24px"
+                viewBox="0 0 24 24"
+                fill="#000000"
+            >
+                <path d="M0 0h24v24H0V0z" fill="none" />
+                <path d="M12 8c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm0 2c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0 6c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z" />
+            </svg>
+        </OptionIconWrapper>
+    );
+};
+export default OptionIcon;
