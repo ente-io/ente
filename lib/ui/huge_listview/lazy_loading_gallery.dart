7dee10ee8bf3231e2cfe110b8aee5cb48eecd3bf
Vishnu Mohandas <vishnu@vishnu.tech>
2021-05-03 21:36:58 +0530

    Prevent glitches while opening a new route

--- lib/ui/huge_listview/lazy_loading_gallery.dart
+++ lib/ui/huge_listview/lazy_loading_gallery.dart
@@ -281,10 +281,22 @@ class _LazyLoadingGridViewState extends State<LazyLoadingGridView> {
       widget.tag,
     );
     Navigator.of(context).push(
-      MaterialPageRoute(
-        builder: (BuildContext context) {
+      PageRouteBuilder(
+        pageBuilder: (BuildContext context, Animation<double> animation,
+            Animation<double> secondaryAnimation) {
           return page;
         },
+        transitionsBuilder: (BuildContext context, Animation<double> animation,
+            Animation<double> secondaryAnimation, Widget child) {
+          return Align(
+            child: FadeTransition(
+              opacity: animation,
+              child: child,
+            ),
+          );
+        },
+        transitionDuration: Duration(milliseconds: 200),
+        opaque: false,
       ),
     );
   }
