703377dc114280f40d612f0a313ff84c39ff7933
Vishnu Mohandas <vishnu@vishnu.tech>
2021-05-07 17:11:51 +0530

    Ignore debug mode endpoint for now

--- lib/core/configuration.dart
+++ lib/core/configuration.dart
@@ -240,9 +240,9 @@ class Configuration {
   }
 
   String getHttpEndpoint() {
-    if (kDebugMode) {
-      return "http://192.168.1.111:8080";
-    }
+    // if (kDebugMode) {
+    //   return "http://192.168.1.111:8080";
+    // }
     return "https://api.ente.io";
   }
 
