14299458742bc73d91a1e67db4532063a8979276
Vishnu Mohandas <vishnu@vishnu.tech>
2021-05-10 21:13:20 +0530

    Improve LoadingPhotosPage

--- lib/ui/loading_photos_page.dart
+++ lib/ui/loading_photos_page.dart
@@ -15,6 +15,21 @@ class LoadingPhotosPage extends StatefulWidget {
 
 class _LoadingPhotosPageState extends State<LoadingPhotosPage> {
   StreamSubscription<SyncStatusUpdate> _firstImportEvent;
+  int _currentPage = 0;
+  PageController _pageController = PageController(
+    initialPage: 0,
+  );
+  final List<String> _messages = [
+    "we have an amazing web app @ ente.io",
+    "we have preserved over 500,000 files so far",
+    "all of our apps are open source",
+    "our encryption protocols have been reviewed by engineers at Google, Apple, Amazon, and Facebook",
+    "you can share files and folders with your loved ones, end-to-end encrypted",
+    "our mobile apps run in the background to encrypt and backup new photos you take",
+    "we use Xchacha20Poly1305 to safely encrypt your data",
+    "one of our data centers is in a fall out shelter 25m underground",
+    "our web app lets you drag and drop photos from other providers",
+  ];
 
   @override
   void initState() {
@@ -25,6 +40,20 @@ class _LoadingPhotosPageState extends State<LoadingPhotosPage> {
           event.status == SyncStatus.completed_first_gallery_import) {
         showBackupFolderSelectionDialog(context);
       }
+    });
+
+    Timer.periodic(Duration(seconds: 5), (Timer timer) {
+      if (_currentPage < _messages.length - 1) {
+        _currentPage++;
+      } else {
+        _currentPage = 0;
+      }
+
+      _pageController.animateToPage(
+        _currentPage,
+        duration: Duration(milliseconds: 300),
+        curve: Curves.easeIn,
+      );
     });
   }
 
@@ -45,15 +74,55 @@ class _LoadingPhotosPageState extends State<LoadingPhotosPage> {
             LoadingBouncingGrid.square(
               inverted: true,
               backgroundColor: Theme.of(context).buttonColor,
+              size: 64,
             ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Text(
-                "hang on tight, your photos will appear in a jiffy! 🐣",
-                textAlign: TextAlign.center,
+            Padding(padding: const EdgeInsets.all(20.0)),
+            Text(
+              "loading your gallery...",
+              style: TextStyle(
+                fontSize: 16,
+              ),
+            ),
+            Padding(padding: EdgeInsets.all(6)),
+            Text(
+              "this might take upto 30 seconds 🐣",
+              style: TextStyle(color: Colors.white.withOpacity(0.3)),
+            ),
+            Padding(padding: const EdgeInsets.all(60.0)),
+            Text(
+              "did you know?",
+              style: TextStyle(
+                fontSize: 16,
+                color: Theme.of(context).buttonColor,
+              ),
+            ),
+            Padding(padding: const EdgeInsets.all(12)),
+            Container(
+              height: 80,
+              child: PageView.builder(
+                scrollDirection: Axis.vertical,
+                controller: _pageController,
+                itemBuilder: (context, index) {
+                  return _getMessage(_messages[index]);
+                },
+                itemCount: _messages.length,
               ),
             ),
           ],
+        ),
+      ),
+    );
+  }
+
+  Widget _getMessage(String text) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 24, right: 24),
+      child: Text(
+        text,
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          color: Colors.white.withOpacity(0.6),
+          height: 1.5,
         ),
       ),
     );
