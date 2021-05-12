ddbd291e003141b2e593468d775d285b9a362814
Vishnu Mohandas <vishnu@vishnu.tech>
2021-05-12 20:53:22 +0530

    Update grant permissions page

--- lib/ui/grant_permissions_page.dart
+++ lib/ui/grant_permissions_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:photo_manager/photo_manager.dart';
 import 'package:photos/services/sync_service.dart';
+import 'package:photos/ui/common_elements.dart';
 import 'package:photos/ui/loading_photos_page.dart';
 
 class GrantPermissionsPage extends StatelessWidget {
@@ -11,31 +12,40 @@ class GrantPermissionsPage extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Padding(
-            padding: const EdgeInsets.all(32),
-            child: Text(
-              "ente needs your permission to display your gallery",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 16,
-                height: 1.4,
-              ),
+          Text.rich(
+            TextSpan(
+              children: <TextSpan>[
+                TextSpan(
+                  text: "ente",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Montserrat',
+                    fontSize: 16,
+                  ),
+                ),
+                TextSpan(
+                  text: " needs your permission",
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
+                ),
+              ],
             ),
+            textAlign: TextAlign.center,
           ),
+          Padding(padding: EdgeInsets.all(2)),
+          Text(
+            "to access your gallery",
+            style: TextStyle(fontSize: 16),
+          ),
+          Padding(padding: const EdgeInsets.all(32)),
           Container(
             width: double.infinity,
             height: 64,
             padding: const EdgeInsets.fromLTRB(80, 0, 80, 0),
-            child: RaisedButton(
-              child: Text(
-                "grant permission",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 18,
-                  letterSpacing: 1.0,
-                ),
-                textAlign: TextAlign.center,
-              ),
+            child: button(
+              "grant permission",
+              fontSize: 16,
               onPressed: () async {
                 final granted = await PhotoManager.requestPermission();
                 if (granted) {
@@ -49,9 +59,27 @@ class GrantPermissionsPage extends StatelessWidget {
                   );
                 }
               },
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0),
-              ),
+            ),
+          ),
+          Padding(padding: const EdgeInsets.all(50)),
+          Text(
+            "we value your privacy",
+            style: TextStyle(
+              color: Colors.white30,
+            ),
+          ),
+          Padding(padding: const EdgeInsets.all(12)),
+          Text(
+            "all your photos and videos will be",
+            style: TextStyle(
+              color: Colors.white30,
+            ),
+          ),
+          Padding(padding: const EdgeInsets.all(2)),
+          Text(
+            "end-to-end encrypted",
+            style: TextStyle(
+              color: Colors.white30,
             ),
           ),
         ],
