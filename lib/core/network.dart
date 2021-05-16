22ba7e86c5f87a6604cde218b5274ae205d13fa9
Vishnu Mohandas <vishnu@vishnu.tech>
2021-05-16 14:26:20 +0530

    Send client version with HTTP requests

--- lib/core/network.dart
+++ lib/core/network.dart
@@ -2,18 +2,27 @@ import 'dart:io';
 
 import 'package:dio/dio.dart';
 import 'package:flutter_user_agent/flutter_user_agent.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
 class Network {
   Dio _dio;
 
   Future<void> init() async {
     await FlutterUserAgent.init();
-    _dio = Dio(BaseOptions(
-        headers: {HttpHeaders.userAgentHeader: FlutterUserAgent.userAgent}));
+    final version = await _getAppVersion();
+    _dio = Dio(BaseOptions(headers: {
+      HttpHeaders.userAgentHeader: FlutterUserAgent.userAgent,
+      'X-Client-Version': version,
+    }));
   }
 
   Network._privateConstructor();
   static Network instance = Network._privateConstructor();
 
   Dio getDio() => _dio;
+
+  Future<String> _getAppVersion() async {
+    final pkgInfo = await PackageInfo.fromPlatform();
+    return pkgInfo.version;
+  }
 }
