9760525bea352649f3adac1f42c78e02b8c10653
Vishnu Mohandas <vishnu@vishnu.tech>
2021-05-21 18:28:19 +0530

    Build AAR for background fetch from source

---
+++ build-apk.sh
@@ -0,0 +1,7 @@
+cd thirdparty/transistor-background-fetch/android
+./gradlew :tsbackgroundfetch:publishRelease
+cd ../../../
+mkdir android/app/libs
+cp -rf thirdparty/transistor-background-fetch/android/tsbackgroundfetch/build/repo/* android/app/libs
+flutter build apk
+rm -rf android/app/libs
