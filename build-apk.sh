d940875ef12bd2a4f35ad8949fd7ec9d4eafc836
Vishnu Mohandas <vishnu@vishnu.tech>
2021-05-21 20:35:58 +0530

    Replicate build steps

--- build-apk.sh
+++ build-apk.sh
@@ -3,5 +3,8 @@ gradle :tsbackgroundfetch:publishRelease
 cd ../../../
 mkdir android/app/libs
 cp -rf thirdparty/transistor-background-fetch/android/tsbackgroundfetch/build/repo/* android/app/libs
+flutter config --no-analytics
+flutter packages pub get
+flutter packages pub run flutter_launcher_icons:main
 flutter build apk
 rm -rf android/app/libs
