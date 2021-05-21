6bfad2a0d8ac34368efc0398a89d29ceff2c4a7e
Abhinav-grd <abhinavk.grd@gmail.com>
2021-05-21 15:17:28 +0530

    updated to handle poi location type responses

--- src/services/searchService.ts
+++ src/services/searchService.ts
@@ -1,6 +1,12 @@
 import HTTPService from './HTTPService';
 import * as chrono from 'chrono-node';
 
+const KM_IN_DEGREE = 0.01;
+export type Bbox = [number, number, number, number];
+export interface LocationSearchResponse {
+    placeName: string;
+    bbox: Bbox;
+}
 export const getMapboxToken = () => {
     return process.env.NEXT_PUBLIC_MAPBOX_TOKEN;
 };
@@ -11,12 +17,25 @@ export function parseHumanDate(humanDate: string) {
 
 export async function searchLocation(
     location: string
-): Promise<[number, number, number, number]> {
+): Promise<LocationSearchResponse[]> {
     const resp = await HTTPService.get(
         `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURI(
             location
         )}.json`,
-        { access_token: getMapboxToken(), limit: 1 }
+        { access_token: getMapboxToken(), limit: 4 }
     );
-    return resp.data.features.length > 0 && resp.data.features[0].bbox;
+
+    return resp.data.features.length == 0
+        ? new Array<LocationSearchResponse>()
+        : resp.data.features.map((feature) => ({
+              placeName: feature.place_name,
+              bbox:
+                  feature.bbox ??
+                  ([
+                      feature.center[0] - KM_IN_DEGREE,
+                      feature.center[1] - KM_IN_DEGREE,
+                      feature.center[0] + KM_IN_DEGREE,
+                      feature.center[1] + KM_IN_DEGREE,
+                  ] as Bbox),
+          }));
 }
