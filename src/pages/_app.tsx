1aeaa23ebd9307720cbf7304a4a08629a7d95ba1
Abhinav-grd <abhinavk.grd@gmail.com>
2021-05-23 20:29:11 +0530

    passed setLoading to component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -332,7 +332,7 @@ export default function App({ Component, pageProps, err }) {
                     </EnteSpinner>
                 </Container>
             ) : (
-                <Component err={err} />
+                <Component err={err} setLoading={setLoading} />
             )}
         </>
     );
