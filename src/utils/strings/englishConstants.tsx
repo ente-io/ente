04a6f2c3cf9d5fe7ad1e4e475e63c17e0afdcbce
Abhinav-grd <abhinavk.grd@gmail.com>
2021-05-23 21:07:44 +0530

    updated string

--- src/utils/strings/englishConstants.tsx
+++ src/utils/strings/englishConstants.tsx
@@ -61,7 +61,7 @@ const englishConstants = {
     ENTER_ALBUM_NAME: 'album name',
     CLOSE: 'close',
     NO: 'no',
-    NOTHING_HERE: `nothing to see here, yet 👀`,
+    NOTHING_HERE: `nothing to see here, yet`,
     UPLOAD: {
         0: 'preparing to upload',
         1: 'reading google metadata files',
