cdd814cdbdb9eb83983d89dd176132e208be63f7
Vishnu Mohandas <vishnu@vishnu.tech>
2021-05-25 17:30:48 +0530

    Switch to older version of WebView

--- lib/ui/web_page.dart
+++ lib/ui/web_page.dart
@@ -1,6 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
-import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:photos/ui/loading_widget.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class WebPage extends StatefulWidget {
   final String title;
@@ -16,15 +18,22 @@ class _WebPageState extends State<WebPage> {
   bool _hasLoadedPage = false;
 
   @override
+  void initState() {
+    super.initState();
+    if (Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
         actions: [_hasLoadedPage ? Container() : loadWidget],
       ),
-      body: InAppWebView(
-        initialUrlRequest: URLRequest(url: Uri.parse(widget.url)),
-        onLoadStop: (c, url) {
+      body: WebView(
+        initialUrl: widget.url,
+        javascriptMode: JavascriptMode.unrestricted,
+        onPageFinished: (url) {
           setState(() {
             _hasLoadedPage = true;
           });
