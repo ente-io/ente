fe838aab17928cca869b440d68564c965ef51881
Abhinav-grd <abhinavk.grd@gmail.com>
2021-06-07 16:15:46 +0530

    updated to @sentry/nextjs package

--- next.config.js
+++ next.config.js
@@ -1,67 +1,17 @@
-// Use the SentryWebpack plugin to upload the source maps during build step
-const SentryWebpackPlugin = require('@sentry/webpack-plugin');
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
     enabled: process.env.ANALYZE === 'true',
 });
 const withWorkbox = require('next-with-workbox');
 
-const {
-    NEXT_PUBLIC_SENTRY_DSN: SENTRY_DSN,
-    SENTRY_ORG,
-    SENTRY_PROJECT,
-    SENTRY_AUTH_TOKEN,
-    NODE_ENV,
-    GITHUB_COMMIT_SHA: COMMIT_SHA,
-} = process.env;
+const { withSentryConfig } = require('@sentry/nextjs');
 
-process.env.SENTRY_DSN = SENTRY_DSN;
-const basePath = '';
 
-module.exports = withWorkbox(withBundleAnalyzer({
-    productionBrowserSourceMaps: true,
+module.exports = withSentryConfig(withWorkbox(withBundleAnalyzer({
     future: {
         webpack5: true,
-    },
-    env: {
-        // Make the COMMIT_SHA available to the client so that Sentry events can be
-        // marked for the release they belong to. It may be undefined if running
-        // outside of Vercel
-        NEXT_PUBLIC_COMMIT_SHA: COMMIT_SHA,
     },
     workbox: {
         swSrc: 'src/serviceWorker.js',
         exclude: ['/manifest.json'],
     },
-    webpack: (config, { isServer, webpack }) => {
-        if (!isServer) {
-            config.resolve.alias['@sentry/node'] = '@sentry/browser';
-        }
-        // Define an environment variable so source code can check whether or not
-        // it's running on the server so we can correctly initialize Sentry
-        config.plugins.push(
-            new webpack.DefinePlugin({
-                'process.env.NEXT_IS_SERVER': JSON.stringify(
-                    isServer.toString(),
-                ),
-            }),
-        );
-        if (
-            false &&
-            SENTRY_DSN &&
-            SENTRY_ORG &&
-            SENTRY_PROJECT &&
-            SENTRY_AUTH_TOKEN &&
-            NODE_ENV === 'production'
-        ) {
-            config.plugins.push(
-                new SentryWebpackPlugin({
-                    include: '.next',
-                    ignore: ['node_modules'],
-                    stripPrefix: ['webpack://_N_E/'],
-                    urlPrefix: `~${basePath}/_next`,
-                }),
-            );
-        }
-        return config;
-    },
-}));
+})));
---
+++ sentry.client.config.js
@@ -0,0 +1,15 @@
+import * as Sentry from '@sentry/nextjs';
+
+const SENTRY_DSN = process.env.NEXT_PUBLIC_SENTRY_DSN;
+
+Sentry.init({
+    dsn: SENTRY_DSN,
+    // We recommend adjusting this value in production, or using tracesSampler
+    // for finer control
+    tracesSampleRate: 1.0,
+    release: 'bada-frame-v' + process.env.npm_package_version,
+    // ...
+    // Note: if you want to override the automatic release value, do not set a
+    // `release` value here - use the environment variable `SENTRY_RELEASE`, so
+    // that it will also get attached to your source maps
+});
