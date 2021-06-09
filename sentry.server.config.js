ce64a6eb4fbe84c5cd7c9c57f2a59651ef435c53
Abhinav-grd <abhinavk.grd@gmail.com>
2021-06-09 18:03:23 +0530

    added envvironment variable to sentry init

--- sentry.server.config.js
+++ sentry.server.config.js
@@ -8,6 +8,7 @@ Sentry.init({
     // for finer control
     tracesSampleRate: 1.0,
     release: 'bada-frame-v' + process.env.npm_package_version,
+    environment: process.env.NODE_ENV,
     // ...
     // Note: if you want to override the automatic release value, do not set a
     // `release` value here - use the environment variable `SENTRY_RELEASE`, so
