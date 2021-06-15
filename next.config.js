641aa62da07a0525704d0c10b86e1eed51a3e3fa
Pushkar Anand <pushkar8723@gmail.com>
2021-06-15 10:32:46 +0530

    Exclude manifest.json precaching

--- next.config.js
+++ next.config.js
@@ -29,7 +29,7 @@ module.exports = withWorkbox(withBundleAnalyzer({
     },
     workbox: {
         swSrc: 'src/serviceWorker.js',
-        exclude: ['/manifest.json'],
+        exclude: [/manifest.json$/i],
     },
     webpack: (config, { isServer, webpack }) => {
         if (!isServer) {
