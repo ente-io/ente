51978eb1644c19f8efe32774a2981da2c18d0771
Abhinav-grd <abhinavk.grd@gmail.com>
2021-06-15 10:16:10 +0530

    set fallback value for SENTRY_DSN and SENTRY_ENV

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -1,11 +1,12 @@
 import * as Sentry from '@sentry/nextjs';
 
-const SENTRY_DSN = process.env.NEXT_PUBLIC_SENTRY_DSN;
+const SENTRY_DSN = process.env.SENTRY_DSN ?? 'https://860186db60c54c7fbacfe255124958e8@errors.ente.io/4';
+const SENTRY_ENV = process.env.SENTRY_ENV ?? 'development';
 
 Sentry.init({
     dsn: SENTRY_DSN,
     tracesSampleRate: 1.0,
-    environment: process.env.NODE_ENV,
+    environment: SENTRY_ENV,
     release: process.env.SENTRY_RELEASE,
     attachStacktrace: true,
     // ...
--- sentry.server.config.js
+++ sentry.server.config.js
@@ -1,10 +1,11 @@
 import * as Sentry from '@sentry/nextjs';
 
-const SENTRY_DSN = process.env.SENTRY_DSN;
+const SENTRY_DSN = process.env.SENTRY_DSN ?? 'https://860186db60c54c7fbacfe255124958e8@errors.ente.io/4';
+const SENTRY_ENV = process.env.SENTRY_ENV ?? 'development';
 
 Sentry.init({
     dsn: SENTRY_DSN,
     tracesSampleRate: 1.0,
-    environment: process.env.NODE_ENV,
+    environment: SENTRY_ENV,
     release: process.env.SENTRY_RELEASE,
 });
