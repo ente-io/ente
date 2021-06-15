31c0771062f4a4a36998faadbfe6bef004fe78d0
Pushkar Anand <pushkar8723@gmail.com>
2021-06-15 10:33:35 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -29,7 +29,7 @@ module.exports = withWorkbox(withBundleAnalyzer({
     },
     workbox: {
         swSrc: 'src/serviceWorker.js',
-        exclude: [/manifest.json$/i],
+        exclude: [/manifest\.json$/i],
     },
     webpack: (config, { isServer, webpack }) => {
         if (!isServer) {
