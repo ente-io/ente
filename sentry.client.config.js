c3fdcea63178193f01309e26c77ed2a9e8b5d406
Abhinav-grd <abhinavk.grd@gmail.com>
2021-06-16 12:18:10 +0530

    checking sentryenv value

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -2,7 +2,7 @@ import * as Sentry from '@sentry/nextjs';
 
 const SENTRY_DSN = process.env.SENTRY_DSN ?? 'https://860186db60c54c7fbacfe255124958e8@errors.ente.io/4';
 const SENTRY_ENV = process.env.SENTRY_ENV ?? 'development';
-
+console.log(SENTRY_ENV);
 Sentry.init({
     dsn: SENTRY_DSN,
     enabled: SENTRY_ENV !== 'development',
