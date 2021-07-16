3c8af97f695176b0dcf1c98893a5b4238dc89941
Abhinav-grd <abhinavk.grd@gmail.com>
2021-07-16 11:45:22 +0530

    removed debugging logic

--- src/appUpdater.ts
+++ src/appUpdater.ts
@@ -1,35 +1,14 @@
-import {  BrowserWindow, dialog } from "electron"
+import {   dialog } from "electron"
 import { autoUpdater } from "electron-updater"
 import log from "electron-log"
-import { createDefaultWindow, setUpVersionInfoWindow } from "./util";
 
  class AppUpdater {
-    win:BrowserWindow;
     constructor() {
         log.transports.file.level = "debug"
         autoUpdater.logger = log;
     }
 
-    initUpdater(){
-        this.initWin();
-        setUpVersionInfoWindow(this.win);
-    }
-
-    initWin(){
-        this.win=createDefaultWindow();
-        this.win.on('closed', () => {
-            this.win=null;
-        });
-    }
-
-    destroyUpdateWindow(){
-      this.win=null;
-    }
-
     async checkForUpdate(){
-        if(!this.win){
-            this.initUpdater();
-        }
         await autoUpdater.checkForUpdatesAndNotify()
         autoUpdater.on('update-downloaded', () => {
             dialog.showMessageBox({
