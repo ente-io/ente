f07deb65c852e28842ccaccd469f7b12f778ee79
Abhinav-grd <abhinavk.grd@gmail.com>
2021-07-17 14:30:10 +0530

    send userid with sentry

--- src/utils/sentry/index.ts
+++ src/utils/sentry/index.ts
@@ -1,8 +1,11 @@
 import * as Sentry from '@sentry/nextjs';
+import { getData, LS_KEYS } from 'utils/storage/localStorage';
 
 export const logError = (e: any, msg?: string) => {
+    const userID = getData(LS_KEYS.USER)?.id;
     Sentry.captureException(e, {
         level: Sentry.Severity.Info,
+        user: { id: userID },
         contexts: {
             context: {
                 message: msg,
