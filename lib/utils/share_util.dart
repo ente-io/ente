64f44ff50c59bcdc4bcd5ad2b24b6a7301fe6f57
Neeraj Gupta <neeraj.gupta983@gmail.com>
2021-07-23 11:14:18 +0530

    Shared files: parse time from exif data

--- lib/utils/share_util.dart
+++ lib/utils/share_util.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
-import 'dart:convert';
-import 'package:crypto/crypto.dart';
+import 'package:logging/logging.dart';
+import 'package:intl/intl.dart';
 import 'package:path/path.dart';
 import 'dart:io' as dartio;
 import 'package:exif/exif.dart';
@@ -11,7 +11,8 @@ import 'package:flutter/widgets.dart';
 import 'package:photos/models/file.dart';
 import 'package:photos/utils/dialog_util.dart';
 import 'package:photos/utils/file_util.dart';
-
+DateFormat _exifDateFormat = DateFormat('yyyy:MM:dd HH:mm:ss');
+final _logger = Logger("ShareUtil");
 // share is used to share media/files from ente to other apps
 Future<void> share(BuildContext context, List<File> files) async {
   final dialog = createProgressDialog(context, "preparing...");
@@ -33,29 +34,39 @@ Future<List<File>> convertIncomingSharedMediaToFile(
     List<SharedMediaFile> sharedMedia, int collectionID) async {
   List<File> localFiles = [];
   for (var media in sharedMedia) {
-    //'0.0001'.replaceFirst(RegExp(r'0'), ''); // '.0001'
-    var map = await readExifFromFile(dartio.File(media.path));
-    var id = md5.convert(utf8.encode(media.path)).toString();
-    var fromAsset = File();
-    fromAsset.localID = "ente-upload-cache:" + media.path;
-    fromAsset.collectionID = collectionID;
-    fromAsset.fileType = FileType.image;
-    fromAsset.title = basename(media.path);
-    if (fromAsset.creationTime == null || fromAsset.creationTime == 0) {
-      fromAsset.creationTime = 0;
+    var enteFile = File();
+    var exifMap = await readExifFromFile(dartio.File(media.path));
+    if (exifMap != null && exifMap["Image DateTime"] != null) {
+      try {
+        final exifTime =
+            _exifDateFormat.parse(exifMap["Image DateTime"].toString());
+        enteFile.creationTime = exifTime.microsecondsSinceEpoch;
+      } catch (e) {
+        _logger.warning("Failed to parse time from exif",
+            exifMap["Image DateTime"].toString());
+        // ignore
+      }
+    }
+    enteFile.localID = "ente-upload-cache:" + media.path;
+    enteFile.collectionID = collectionID;
+    enteFile.fileType = FileType.image;
+    enteFile.title = basename(media.path);
+    if (enteFile.creationTime == null || enteFile.creationTime == 0) {
+      enteFile.creationTime = 0;
       try {
         final parsedDateTime = DateTime.parse(
             basenameWithoutExtension(media.path)
                 .replaceAll("IMG_", "")
                 .replaceAll("DCIM_", "")
                 .replaceAll("_", " "));
-        fromAsset.creationTime = parsedDateTime.microsecondsSinceEpoch;
+        enteFile.creationTime = parsedDateTime.microsecondsSinceEpoch;
       } catch (e) {
-        fromAsset.creationTime = 0;
+        enteFile.creationTime =
+            dartio.File(media.path).lastModifiedSync().microsecondsSinceEpoch;
       }
     }
-    fromAsset.modificationTime = fromAsset.creationTime;
-    localFiles.add(fromAsset);
+    enteFile.modificationTime = enteFile.creationTime;
+    localFiles.add(enteFile);
   }
   return localFiles;
 }
