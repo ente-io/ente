0b32796633b15cbf90ea0af3f8d792def8157211
Neeraj Gupta <neeraj.gupta983@gmail.com>
2021-07-23 18:15:11 +0530

    improve time parse logic from name for shared files

--- lib/utils/share_util.dart
+++ lib/utils/share_util.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:io';
 import 'package:logging/logging.dart';
 import 'package:intl/intl.dart';
 import 'package:path/path.dart';
@@ -11,6 +12,7 @@ import 'package:flutter/widgets.dart';
 import 'package:photos/models/file.dart';
 import 'package:photos/utils/dialog_util.dart';
 import 'package:photos/utils/file_util.dart';
+
 DateFormat _exifDateFormat = DateFormat('yyyy:MM:dd HH:mm:ss');
 final _logger = Logger("ShareUtil");
 // share is used to share media/files from ente to other apps
@@ -35,38 +37,50 @@ Future<List<File>> convertIncomingSharedMediaToFile(
   List<File> localFiles = [];
   for (var media in sharedMedia) {
     var enteFile = File();
+    enteFile.localID = "ente-upload-cache:" + media.path;
+    enteFile.collectionID = collectionID;
+    enteFile.fileType = FileType.image;
+    enteFile.title = basename(media.path);
+
     var exifMap = await readExifFromFile(dartio.File(media.path));
-    if (exifMap != null && exifMap["Image DateTime"] != null) {
+    if (exifMap != null &&
+        exifMap["Image DateTime"] != null &&
+        '0000:00:00 00:00:00' != exifMap["Image DateTime"].toString()) {
       try {
         final exifTime =
             _exifDateFormat.parse(exifMap["Image DateTime"].toString());
         enteFile.creationTime = exifTime.microsecondsSinceEpoch;
       } catch (e) {
-        _logger.warning("Failed to parse time from exif",
-            exifMap["Image DateTime"].toString());
-        // ignore
+        //ignore
       }
     }
-    enteFile.localID = "ente-upload-cache:" + media.path;
-    enteFile.collectionID = collectionID;
-    enteFile.fileType = FileType.image;
-    enteFile.title = basename(media.path);
     if (enteFile.creationTime == null || enteFile.creationTime == 0) {
-      enteFile.creationTime = 0;
-      try {
-        final parsedDateTime = DateTime.parse(
-            basenameWithoutExtension(media.path)
-                .replaceAll("IMG_", "")
-                .replaceAll("DCIM_", "")
-                .replaceAll("_", " "));
+      final parsedDateTime =
+          parseDateFromFileName(basenameWithoutExtension(media.path));
+      if (parsedDateTime != null) {
         enteFile.creationTime = parsedDateTime.microsecondsSinceEpoch;
-      } catch (e) {
-        enteFile.creationTime =
-            dartio.File(media.path).lastModifiedSync().microsecondsSinceEpoch;
+      } else {
+        enteFile.creationTime = DateTime.now().microsecondsSinceEpoch;
       }
     }
     enteFile.modificationTime = enteFile.creationTime;
     localFiles.add(enteFile);
   }
   return localFiles;
+}
+
+DateTime parseDateFromFileName(String fileName) {
+  if (fileName.startsWith('IMG-') || fileName.startsWith('VID-')) {
+    // Whatsapp media files
+    return DateTime.tryParse(fileName.split('-')[1]);
+  } else if (fileName.startsWith("Screenshot_")) {
+    // Screenshots on droid
+    return DateTime.tryParse(
+        (fileName).replaceAll('Screenshot_', '').replaceAll('-', 'T'));
+  } else {
+    return DateTime.tryParse((fileName)
+        .replaceAll("IMG_", "")
+        .replaceAll("DCIM_", "")
+        .replaceAll("_", " "));
+  }
 }
