de52f9f7e46de0e16d084a69b613279f39176bf8
vishnukvmd <vishnu@ente.io>
2021-08-09 12:36:46 +0530

    Parse live photo only on debug mode

--- lib/models/file.dart
+++ lib/models/file.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/foundation.dart';
 import 'package:path/path.dart';
 import 'package:photo_manager/photo_manager.dart';
@@ -59,7 +58,8 @@ class File {
         type = FileType.image;
         // PHAssetMediaSubtype.photoLive.rawValue is 8
         // This hack should go away once photos_manager support livePhotos
-        if (asset.subTypes != null &&
+        if (kDebugMode &&
+            asset.subTypes != null &&
             asset.subTypes > -1 &&
             (asset.subTypes & 8) != 0) {
           type = FileType.livePhoto;
