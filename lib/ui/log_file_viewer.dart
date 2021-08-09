474c9cc41db21e12c2a912015a19eeb9599222ac
vishnukvmd <vishnu@ente.io>
2021-08-09 20:05:17 +0530

    Add an option to view logs

---
+++ lib/ui/log_file_viewer.dart
@@ -0,0 +1,56 @@
+import 'dart:io';
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:photos/ui/loading_widget.dart';
+
+class LogFileViewer extends StatefulWidget {
+  final File file;
+  const LogFileViewer(this.file, {Key key}) : super(key: key);
+
+  @override
+  _LogFileViewerState createState() => _LogFileViewerState();
+}
+
+class _LogFileViewerState extends State<LogFileViewer> {
+  String _logs;
+  @override
+  void initState() {
+    widget.file.readAsString().then((logs) {
+      setState(() {
+        _logs = logs;
+      });
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("today's logs"),
+      ),
+      body: _getBody(),
+    );
+  }
+
+  Widget _getBody() {
+    if (_logs == null) {
+      return loadWidget;
+    }
+    return Container(
+      padding: EdgeInsets.only(left: 12, right: 12),
+      child: SingleChildScrollView(
+        child: Text(
+          _logs,
+          style: TextStyle(
+            fontFeatures: const [
+              FontFeature.tabularFigures(),
+            ],
+            color: Colors.white.withOpacity(0.7),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/utils/email_util.dart
+++ lib/utils/email_util.dart
@@ -6,8 +6,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_email_sender/flutter_email_sender.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:photos/ui/log_file_viewer.dart';
 import 'package:photos/utils/dialog_util.dart';
+import 'package:photos/utils/navigation_util.dart';
 import 'package:share/share.dart';
+import 'package:super_logging/super_logging.dart';
 
 bool isValidEmail(String email) {
   return EmailValidator.validate(email);
@@ -21,6 +24,70 @@ Future<void> sendLogs(
   String subject,
   String body,
 }) async {
+  final List<Widget> actions = [
+    TextButton(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Icon(
+            Icons.feed_outlined,
+            color: Colors.white.withOpacity(0.85),
+          ),
+          Padding(padding: EdgeInsets.all(4)),
+          Text(
+            "view logs",
+            style: TextStyle(
+              color: Colors.white.withOpacity(0.85),
+            ),
+          ),
+        ],
+      ),
+      onPressed: () async {
+        // routeToPage(context, LogFileViewer(SuperLogging.logFile));
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return LogFileViewer(SuperLogging.logFile);
+          },
+          barrierColor: Colors.black87,
+          barrierDismissible: false,
+        );
+      },
+    ),
+    TextButton(
+      child: Text(
+        title,
+        style: TextStyle(
+          color: Theme.of(context).buttonColor,
+        ),
+      ),
+      onPressed: () async {
+        Navigator.of(context, rootNavigator: true).pop('dialog');
+        await _sendLogs(context, toEmail, subject, body);
+        if (postShare != null) {
+          postShare();
+        }
+      },
+    ),
+  ];
+  final List<Widget> content = [];
+  content.addAll(
+    [
+      Text(
+        "this will send across logs and metrics that will help us debug your issue better",
+        style: TextStyle(
+          height: 1.5,
+          fontFamily: 'Ubuntu',
+          fontSize: 16,
+        ),
+      ),
+      Padding(padding: EdgeInsets.all(12)),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: actions,
+      ),
+    ],
+  );
   final confirmation = AlertDialog(
     title: Text(
       title,
@@ -29,37 +96,10 @@ Future<void> sendLogs(
       ),
     ),
     content: SingleChildScrollView(
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: const [
-          Text(
-            "this will send across metrics and logs that will help us debug your issue better",
-            style: TextStyle(
-              height: 1.5,
-              fontFamily: 'Ubuntu',
-              fontSize: 16,
-            ),
-          ),
-        ],
+      child: ListBody(
+        children: content,
       ),
     ),
-    actions: [
-      TextButton(
-        child: Text(
-          title,
-          style: TextStyle(
-            color: Theme.of(context).buttonColor,
-          ),
-        ),
-        onPressed: () async {
-          Navigator.of(context).pop();
-          await _sendLogs(context, toEmail, subject, body);
-          if (postShare != null) {
-            postShare();
-          }
-        },
-      ),
-    ],
   );
   showDialog(
     context: context,
