82e70a343ddb3f86de0c208df11b60decce7463d
Abhinav-grd <abhinavk.grd@gmail.com>
2021-08-11 11:41:09 +0530

    formatted metadataService

--- src/services/upload/metadataService.ts
+++ src/services/upload/metadataService.ts
@@ -3,41 +3,41 @@ import { logError } from 'utils/sentry';
 import { getExifData } from './exifService';
 import { MetadataObject } from './uploadService';
 
-
 export interface Location {
     latitude: number;
     longitude: number;
 }
 
-export interface ParsedMetaDataJSON{
-    title:string;
-    creationTime:number;
-    modificationTime:number;
-    location:Location;
+export interface ParsedMetaDataJSON {
+    title: string;
+    creationTime: number;
+    modificationTime: number;
+    location: Location;
 }
 
-export const NULL_LOCATION: Location =
-{ latitude: null,
-    longitude: null };
+export const NULL_LOCATION: Location = { latitude: null, longitude: null };
 
-const NULL_PARSED_METADATA_JSON:ParsedMetaDataJSON=
-{ title: null,
+const NULL_PARSED_METADATA_JSON: ParsedMetaDataJSON = {
+    title: null,
     creationTime: null,
     modificationTime: null,
     location: NULL_LOCATION,
 };
 
-export async function extractMetatdata(reader:FileReader, receivedFile:globalThis.File, fileType:FILE_TYPE) {
+export async function extractMetatdata(
+    reader: FileReader,
+    receivedFile: globalThis.File,
+    fileType: FILE_TYPE,
+) {
     const { location, creationTime } = await getExifData(
         reader,
         receivedFile,
         fileType,
     );
 
-    const extractedMetadata:MetadataObject = {
+    const extractedMetadata: MetadataObject = {
         title: receivedFile.name,
-        creationTime:
-                creationTime || receivedFile.lastModified * 1000,
+        creationTime: creationTime || receivedFile.lastModified * 1000,
         modificationTime: receivedFile.lastModified * 1000,
         latitude: location?.latitude,
         longitude: location?.latitude,
@@ -46,31 +46,29 @@ export async function extractMetatdata(reader:FileReader, receivedFile:globalThi
     return extractedMetadata;
 }
 
-
 export async function parseMetadataJSON(receivedFile: globalThis.File) {
     try {
-        const metadataJSON: object = await new Promise(
-            (resolve, reject) => {
-                const reader = new FileReader();
-                reader.onabort = () => reject(Error('file reading was aborted'));
-                reader.onerror = () => reject(Error('file reading has failed'));
-                reader.onload = () => {
-                    const result =
-                        typeof reader.result !== 'string' ?
-                            new TextDecoder().decode(reader.result) :
-                            reader.result;
-                    resolve(JSON.parse(result));
-                };
-                reader.readAsText(receivedFile);
-            },
-        );
+        const metadataJSON: object = await new Promise((resolve, reject) => {
+            const reader = new FileReader();
+            reader.onabort = () => reject(Error('file reading was aborted'));
+            reader.onerror = () => reject(Error('file reading has failed'));
+            reader.onload = () => {
+                const result =
+                    typeof reader.result !== 'string'
+                        ? new TextDecoder().decode(reader.result)
+                        : reader.result;
+                resolve(JSON.parse(result));
+            };
+            reader.readAsText(receivedFile);
+        });
 
-        const parsedMetaDataJSON:ParsedMetaDataJSON = NULL_PARSED_METADATA_JSON;
+        const parsedMetaDataJSON: ParsedMetaDataJSON =
+            NULL_PARSED_METADATA_JSON;
         if (!metadataJSON || !metadataJSON['title']) {
             return;
         }
 
-        parsedMetaDataJSON.title=metadataJSON['title'];
+        parsedMetaDataJSON.title = metadataJSON['title'];
         if (
             metadataJSON['photoTakenTime'] &&
             metadataJSON['photoTakenTime']['timestamp']
@@ -85,7 +83,7 @@ export async function parseMetadataJSON(receivedFile: globalThis.File) {
             parsedMetaDataJSON.modificationTime =
                 metadataJSON['modificationTime']['timestamp'] * 1000000;
         }
-        let locationData:Location = NULL_LOCATION;
+        let locationData: Location = NULL_LOCATION;
         if (
             metadataJSON['geoData'] &&
             (metadataJSON['geoData']['latitude'] !== 0.0 ||
@@ -100,7 +98,7 @@ export async function parseMetadataJSON(receivedFile: globalThis.File) {
             locationData = metadataJSON['geoDataExif'];
         }
         if (locationData !== null) {
-            parsedMetaDataJSON.location=locationData;
+            parsedMetaDataJSON.location = locationData;
         }
         return parsedMetaDataJSON;
     } catch (e) {
