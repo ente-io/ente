e08d4d1002f8b46c8b077793394ab39f81607888
Abhinav-grd <abhinavk.grd@gmail.com>
2021-08-13 09:12:01 +0530

    specify time unit variable name  retrySleepTimeInMilliSeconds

--- src/utils/network/index.ts
+++ src/utils/network/index.ts
@@ -1,6 +1,6 @@
 import { sleep } from 'utils/common';
 
-const retrySleepTime = [2000, 5000, 10000];
+const retrySleepTimeInMilliSeconds = [2000, 5000, 10000];
 
 export async function retryAsyncFunction(
     func: () => Promise<any>,
@@ -11,7 +11,7 @@ export async function retryAsyncFunction(
         return resp;
     } catch (e) {
         if (retryCount > 0) {
-            await sleep(retrySleepTime[3 - retryCount]);
+            await sleep(retrySleepTimeInMilliSeconds[3 - retryCount]);
             await retryAsyncFunction(func, retryCount - 1);
         } else {
             throw e;
