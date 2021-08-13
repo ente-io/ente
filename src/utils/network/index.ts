457e068f7da3d7fc86c2c62d183488455baaaef9
Abhinav-grd <abhinavk.grd@gmail.com>
2021-08-13 09:35:23 +0530

    add sub function to retryAsyncFunction and pass retyCount internally

--- src/utils/network/index.ts
+++ src/utils/network/index.ts
@@ -2,19 +2,22 @@ import { sleep } from 'utils/common';
 
 const retrySleepTimeInMilliSeconds = [2000, 5000, 10000];
 
-export async function retryAsyncFunction(
-    func: () => Promise<any>,
-    retryCount: number = 3
-) {
-    try {
-        const resp = await func();
-        return resp;
-    } catch (e) {
-        if (retryCount > 0) {
-            await sleep(retrySleepTimeInMilliSeconds[3 - retryCount]);
-            await retryAsyncFunction(func, retryCount - 1);
-        } else {
-            throw e;
+export async function retryAsyncFunction(func: () => Promise<any>) {
+    const retrier = async (
+        func: () => Promise<any>,
+        retryCount: number = 3
+    ) => {
+        try {
+            const resp = await func();
+            return resp;
+        } catch (e) {
+            if (retryCount > 0) {
+                await sleep(retrySleepTimeInMilliSeconds[3 - retryCount]);
+                await retrier(func, retryCount - 1);
+            } else {
+                throw e;
+            }
         }
-    }
+    };
+    return await retrier(func);
 }
