484842a652f377dddfffeb36f31d95c2025be199
Abhinav-grd <abhinavk.grd@gmail.com>
2021-08-26 16:31:33 +0530

    avoid multiple load call and run call at one time

--- src/services/ffmpegService.ts
+++ src/services/ffmpegService.ts
@@ -1,8 +1,12 @@
 import { createFFmpeg, FFmpeg } from '@ffmpeg/ffmpeg';
+import QueueProcessor from './upload/queueProcessor';
 import { getUint8ArrayView } from './upload/readFileService';
 
 class FFmpegService {
     private ffmpeg: FFmpeg = null;
+    private isLoading = null;
+
+    private generateThumbnailProcessor = new QueueProcessor<Uint8Array>(1);
     async init() {
         try {
             this.ffmpeg = createFFmpeg({
@@ -10,7 +14,10 @@ class FFmpegService {
                 corePath: '/js/ffmpeg-core.js',
             });
             console.log('Loading ffmpeg-core.js');
-            await this.ffmpeg.load();
+            this.isLoading = this.ffmpeg.load();
+            await this.isLoading;
+            this.isLoading = null;
+            console.log('ffmpeg loaded');
         } catch (e) {
             throw Error('ffmpeg load failed');
         }
@@ -20,25 +27,37 @@ class FFmpegService {
         if (!this.ffmpeg) {
             await this.init();
         }
-        this.ffmpeg.FS(
-            'writeFile',
-            file.name,
-            await getUint8ArrayView(new FileReader(), file)
+        if (this.isLoading) {
+            await this.isLoading;
+        }
+        const response = this.generateThumbnailProcessor.queueUpRequest(
+            generateThumbnailHelper.bind(null, this.ffmpeg, file)
         );
 
-        await this.ffmpeg.run(
-            '-i',
-            file.name,
-            '-ss',
-            '00:00:01.000',
-            '-vframes',
-            '1',
-            'thumb.png'
-        );
-        console.log('Complete transcoding');
-        const thumb = this.ffmpeg.FS('readFile', 'thumb.png');
-        return thumb;
+        const thumbnail = await response.promise;
+        return thumbnail;
     }
+}
+
+async function generateThumbnailHelper(ffmpeg: FFmpeg, file: File) {
+    ffmpeg.FS(
+        'writeFile',
+        file.name,
+        await getUint8ArrayView(new FileReader(), file)
+    );
+
+    await ffmpeg.run(
+        '-i',
+        file.name,
+        '-ss',
+        '00:00:01.000',
+        '-vframes',
+        '1',
+        'thumb.png'
+    );
+    console.log('Complete transcoding');
+    const thumb = ffmpeg.FS('readFile', 'thumb.png');
+    return thumb;
 }
 
 export default new FFmpegService();
