8921466258abd3bbba4737e55e8af8b0583b5eb0
Neeraj Gupta <neeraj.gupta983@gmail.com>
2021-08-28 11:29:56 +0530

    Android: enable stripe for new users

--- lib/ui/payment/subscription.dart
+++ lib/ui/payment/subscription.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/cupertino.dart';
+import 'package:photos/core/configuration.dart';
+import 'package:photos/services/feature_flag_service.dart';
 import 'package:photos/services/update_service.dart';
 import 'package:photos/ui/payment/stripe_subscription_page.dart';
 import 'package:photos/ui/payment/subscription_page.dart';
@@ -6,7 +8,19 @@ import 'package:photos/ui/payment/subscription_page.dart';
 StatefulWidget getSubscriptionPage({bool isOnBoarding = false}) {
   if (UpdateService.instance.isIndependentFlavor()) {
     return StripeSubscriptionPage(isOnboarding: isOnBoarding);
+  }
+  if (FeatureFlagService.instance.enableStripe() &&
+      _isUserCreatedPostStripeSupport()) {
+    return StripeSubscriptionPage(isOnboarding: isOnBoarding);
   } else {
     return SubscriptionPage(isOnboarding: isOnBoarding);
   }
+}
+
+// return true if the user was created after we added support for stripe payment
+// on frame. We do this check to avoid showing Stripe payment option for earlier
+// users who might have paid via playStore. This method should be removed once
+// we have better handling for active play/app store subscription & stripe plans.
+bool _isUserCreatedPostStripeSupport() {
+  return Configuration.instance.getUserID() > 1580559962386460;
 }
