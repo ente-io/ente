02d7cf3802bd6affaa6ea26b63d2dc10d355bdb2
Abhinav-grd <abhinavk.grd@gmail.com>
2021-09-07 14:55:41 +0530

    remove console and try catch

--- src/utils/file/convertHEIC.ts
+++ src/utils/file/convertHEIC.ts
@@ -1,15 +1,9 @@
 import * as HeicConvert from 'heic-convert';
 
 export async function convertHEIC2JPEG(fileBlob: Blob): Promise<Blob> {
-    try {
-        const filedata = new Uint8Array(await fileBlob.arrayBuffer());
-        console.log(filedata);
-        const result = await HeicConvert({ buffer: filedata, format: 'JPEG' });
-        const convertedFileData = new Uint8Array(result);
-        const convertedFileBlob = new Blob([convertedFileData]);
-        console.log(URL.createObjectURL(convertedFileBlob));
-        return convertedFileBlob;
-    } catch (e) {
-        console.log(e);
-    }
+    const filedata = new Uint8Array(await fileBlob.arrayBuffer());
+    const result = await HeicConvert({ buffer: filedata, format: 'JPEG' });
+    const convertedFileData = new Uint8Array(result);
+    const convertedFileBlob = new Blob([convertedFileData]);
+    return convertedFileBlob;
 }
