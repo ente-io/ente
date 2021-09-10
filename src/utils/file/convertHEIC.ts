7633cb1c440a17d70e47fe237def84befe2710f6
abhinav-grd <abhinavk.grd@gmail.com>
2021-09-10 08:49:03 +0530

    convert thumbnail to png instead of jpeg
    test suggestion in [comment](https://github.com/catdad-experiments/heic-convert/issues/9#issuecomment-860890240) to improve oom performance

--- src/utils/file/convertHEIC.ts
+++ src/utils/file/convertHEIC.ts
@@ -2,7 +2,7 @@ import * as HeicConvert from 'heic-convert';
 
 export async function convertHEIC2JPEG(fileBlob: Blob): Promise<Blob> {
     const filedata = new Uint8Array(await fileBlob.arrayBuffer());
-    const result = await HeicConvert({ buffer: filedata, format: 'JPEG' });
+    const result = await HeicConvert({ buffer: filedata, format: 'PNG' });
     const convertedFileData = new Uint8Array(result);
     const convertedFileBlob = new Blob([convertedFileData]);
     return convertedFileBlob;
