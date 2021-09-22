dcc3f6665d58d8fe6adf51eccc273bf366a7b1a7
abhinav-grd <abhinavk.grd@gmail.com>
2021-09-23 00:16:02 +0530

    searchCollection service function

--- src/services/searchService.ts
+++ src/services/searchService.ts
@@ -3,6 +3,7 @@ import { getEndpoint } from 'utils/common/apiUtil';
 import { getToken } from 'utils/common/key';
 import { DateValue, Suggestion, SuggestionType } from 'components/SearchBar';
 import HTTPService from './HTTPService';
+import { Collection } from './collectionService';
 
 const ENDPOINT = getEndpoint();
 const DIGITS = new Set(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']);
@@ -99,4 +100,13 @@ export function getYearSuggestion(searchPhrase: string): Suggestion[] {
         }
     }
     return [];
+}
+
+export function searchCollection(
+    searchPhrase: string,
+    collections: Collection[]
+): Collection[] {
+    return collections.filter((collection) =>
+        collection.name.toLowerCase().includes(searchPhrase.toLowerCase())
+    );
 }
