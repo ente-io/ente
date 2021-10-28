2902cf90c53fd201884222d67f40fbcb2e71fb45
Neeraj Gupta <254676+ua741@users.noreply.github.com>
2021-10-28 10:45:48 +0530

    Fix trash reload on emptying trash

--- lib/services/trash_sync_service.dart
+++ lib/services/trash_sync_service.dart
@@ -1,9 +1,11 @@
 import 'package:dio/dio.dart';
 import 'package:logging/logging.dart';
 import 'package:photos/core/configuration.dart';
+import 'package:photos/core/event_bus.dart';
 import 'package:photos/core/network.dart';
 import 'package:photos/db/ignored_files_db.dart';
 import 'package:photos/db/trash_db.dart';
+import 'package:photos/events/files_updated_event.dart';
 import 'package:photos/models/file.dart';
 import 'package:photos/models/ignored_file.dart';
 import 'package:photos/models/trash_file.dart';
@@ -143,7 +145,12 @@ class TrashSyncService {
         ),
         data: params,
       );
+      var recentlyTrashedFile = await _trashDB.getRecentlyTrashedFile();
       await _trashDB.clearTable();
+      if (recentlyTrashedFile != null) {
+        Bus.instance.fire(FilesUpdatedEvent(List.of([recentlyTrashedFile]),
+            type: EventType.deleted));
+      }
     } catch (e, s) {
       _logger.severe("failed to empty trash", e, s);
       rethrow;
