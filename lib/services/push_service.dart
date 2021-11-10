c89bfac4cc9f62189038d488ab3ed6fa7b3de809
vishnukvmd <vishnu@ente.io>
2021-11-11 00:49:33 +0530

    Define the API for setting the push token on server

--- lib/services/push_service.dart
+++ lib/services/push_service.dart
@@ -1,18 +1,23 @@
+import 'package:dio/dio.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:logging/logging.dart';
 import 'package:photos/core/configuration.dart';
 import 'package:photos/core/event_bus.dart';
+import 'package:photos/core/network.dart';
 import 'package:photos/events/signed_in_event.dart';
 import 'package:photos/services/sync_service.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class PushService {
+  static const kFCMPushTokenKey = "fcm_push_token";
+
   static final PushService instance = PushService._privateConstructor();
   static final _logger = Logger("PushService");
 
+  final _dio = Network.instance.getDio();
+
   SharedPreferences _prefs;
-  static const kFCMPushTokenKey = "fcm_push_token";
 
   PushService._privateConstructor();
 
@@ -47,11 +52,28 @@ class PushService {
       final apnsToken = await FirebaseMessaging.instance.getAPNSToken();
       await _setPushTokenOnServer(fcmToken, apnsToken);
       await _prefs.setString(kFCMPushTokenKey, fcmToken);
+      _logger.info("Push token updated on server");
     }
   }
 
-  Future<void> _setPushTokenOnServer(String fcmToken, String apnsToken) {
-    // TODO
+  Future<void> _setPushTokenOnServer(String fcmToken, String apnsToken) async {
+    try {
+      await _dio.post(
+        Configuration.instance.getHttpEndpoint() + "/push/token",
+        data: {
+          "fcmToken": fcmToken,
+          "apnsToken": apnsToken,
+        },
+        options: Options(
+          headers: {
+            "X-Auth-Token": Configuration.instance.getToken(),
+          },
+        ),
+      );
+    } catch (e, s) {
+      _logger.severe(e, s);
+      rethrow;
+    }
   }
 
   void _handlePushMessage(RemoteMessage message) {
