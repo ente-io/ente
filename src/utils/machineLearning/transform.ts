162d760b8034f0b8d05002e66f6f9a9d0f3f2bcf
Shailesh Pandit <shailesh.pandit@gmail.com>
2021-12-29 10:32:01 +0530

    Use transformation-matrix package for transforms

--- src/utils/machineLearning/transform.ts
+++ src/utils/machineLearning/transform.ts
@@ -1,55 +1,24 @@
 import { Box, Point } from '../../../thirdparty/face-api/classes';
-import { Matrix } from 'ml-matrix';
 import { newBoxFromPoints } from '.';
 
-export function translation(x: number, y: number) {
-    return new Matrix([
-        [1, 0, x],
-        [0, 1, y],
-        [0, 0, 1],
-    ]);
-}
-
-export function scale(sx: number, sy: number) {
-    return new Matrix([
-        [sx, 0, 0],
-        [0, sy, 0],
-        [0, 0, 1],
-    ]);
-}
-
-export function rotation(angle: number) {
-    const cosa = Math.cos(angle);
-    const sina = Math.sin(angle);
-    return new Matrix([
-        [cosa, -sina, 0],
-        [sina, cosa, 0],
-        [0, 0, 1],
-    ]);
-}
+import {
+    scale,
+    translate,
+    compose,
+    Matrix,
+    applyToPoint,
+} from 'transformation-matrix';
 
 export function computeTransformToBox(inBox: Box, toBox: Box): Matrix {
-    return translation(toBox.x, toBox.y).mmul(
+    return compose(
+        translate(toBox.x, toBox.y),
         scale(toBox.width / inBox.width, toBox.height / inBox.height)
     );
 }
 
-export function pointToArray(point: Point) {
-    return [point.x, point.y];
-}
-
-export function transformPointVec(point: number[], transform: Matrix) {
-    point[2] = 1;
-    const mat = new Matrix([point]).transpose();
-    const mulmat = new Matrix(transform).mmul(mat).to1DArray();
-    // console.log({point, mat, mulmat});
-
-    return mulmat;
-}
-
 export function transformPoint(point: Point, transform: Matrix) {
-    const pointVec = transformPointVec(pointToArray(point), transform);
-    return new Point(pointVec[0], pointVec[1]);
+    const txdPoint = applyToPoint(transform, point);
+    return new Point(txdPoint.x, txdPoint.y);
 }
 
 export function transformPoints(points: Point[], transform: Matrix) {
