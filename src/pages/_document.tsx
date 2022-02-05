5852fa65f3252e96fa9c296b6e8b25f5a5be52d1
Abhinav <abhinavk.grd@gmail.com>
2022-02-05 18:25:20 +0530

    add back the web description meta

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -33,6 +33,10 @@ export default class MyDocument extends Document {
         return (
             <Html lang="en">
                 <Head>
+                    <meta
+                        name="description"
+                        content="ente is a privacy focussed photo storage service that offers end-to-end encryption."
+                    />
                     <link
                         rel="icon"
                         href="/images/favicon.png"
