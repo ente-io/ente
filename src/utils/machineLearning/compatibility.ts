599472a252ca966a33ebed09334c72c85e099471
Shailesh Pandit <shailesh.pandit@gmail.com>
2022-02-08 10:18:21 +0530

    Update comatibility log to include Ente Desktop

--- src/utils/machineLearning/compatibility.ts
+++ src/utils/machineLearning/compatibility.ts
@@ -6,11 +6,10 @@ import {
 
 import isElectron from 'is-electron';
 
-// TODO: check electron env to be campatible with this
 export function canEnableMlSearch(): boolean {
     // check if is chrome or ente desktop
     if (!runningInChrome(false) && !isElectron()) {
-        console.log('Not running in Chrome Desktop');
+        console.log('Not running in Chrome Desktop or Ente Desktop App');
         return false;
     }
 
