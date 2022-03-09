ec15d63a7fd88e67f7689f3468bb0800267acd76
Abhinav <abhinavk.grd@gmail.com>
2022-03-09 11:10:20 +0530

    handle HIEC multiple externally , process two files at a time , using a double the size of that as worker pool also use png as it didn't crash for two parralel files and add a timeout to heic conversion

--- src/services/heicConverter/heicConverterClient.ts
+++ src/services/heicConverter/heicConverterClient.ts
@@ -1,10 +1,15 @@
 import * as HeicConvert from 'heic-convert';
 
+const WAIT_TIME_IN_MICROSECONDS = 60 * 1000;
+
 export async function convertHEIC(
     fileBlob: Blob,
     format: string
 ): Promise<Blob> {
     const filedata = new Uint8Array(await fileBlob.arrayBuffer());
+    setTimeout(() => {
+        throw Error('wait time exceeded');
+    }, WAIT_TIME_IN_MICROSECONDS);
     const result = await HeicConvert({ buffer: filedata, format });
     const convertedFileData = new Uint8Array(result);
     const convertedFileBlob = new Blob([convertedFileData]);
---
+++ src/services/heicConverter/heicConverterService.ts
@@ -0,0 +1,56 @@
+import QueueProcessor from 'services/queueProcessor';
+import { getDedicatedConvertWorker } from 'utils/comlink';
+import { CustomError } from 'utils/error';
+import { logError } from 'utils/sentry';
+
+const WORKER_POOL_SIZE = 4;
+const MAX_CONVERSION_IN_PARALLEL = 2;
+
+class HEICConverter {
+    private convertProcessor = new QueueProcessor<Blob>(
+        MAX_CONVERSION_IN_PARALLEL
+    );
+    private workerPool: any[];
+    private ready: Promise<void>;
+
+    constructor() {
+        this.ready = this.init();
+    }
+    async init() {
+        this.workerPool = [];
+        for (let i = 0; i < WORKER_POOL_SIZE; i++) {
+            const worker = getDedicatedConvertWorker().comlink;
+            if (!worker) {
+                return;
+            }
+            this.workerPool.push({
+                id: i,
+                worker: await new worker(),
+            });
+        }
+    }
+    async convert(fileBlob: Blob, format = 'PNG'): Promise<Blob> {
+        await this.ready;
+        const response = this.convertProcessor.queueUpRequest(async () => {
+            const { id, worker } = this.workerPool.shift();
+            try {
+                return await worker.convertHEIC(fileBlob, format);
+            } finally {
+                this.workerPool.push({ id, worker });
+            }
+        });
+        try {
+            return await response.promise;
+        } catch (e) {
+            if (e.message === CustomError.REQUEST_CANCELLED) {
+                // ignore
+                return null;
+            } else {
+                logError(e, 'heic conversion failed');
+                throw e;
+            }
+        }
+    }
+}
+
+export default new HEICConverter();
