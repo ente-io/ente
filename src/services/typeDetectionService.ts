6c9385a65d34662ae4a046a19a2ca62f08ed88f2
Abhinav <abhinavk.grd@gmail.com>
2022-03-10 09:21:13 +0530

    Revert "Merge pull request #435 from ente-io/skip-jpeg-type-detection"
    This reverts commit 4f6e3158b4248e1297e30d4e16011c4543e29c11, reversing
    changes made to 0ced60ac056690017b3321e319efa3600c3e87e0.

--- src/services/typeDetectionService.ts
+++ src/services/typeDetectionService.ts
@@ -1,33 +1,21 @@
 import { FILE_TYPE } from 'constants/file';
+import { FORMAT_MISSED_BY_FILE_TYPE_LIB } from 'constants/upload';
 import { FileTypeInfo } from 'types/upload';
 import { CustomError } from 'utils/error';
 import { getFileExtension } from 'utils/file';
 import { logError } from 'utils/sentry';
 import { getUint8ArrayView } from './readerService';
-import FileType from 'file-type';
+import FileType from 'file-type/browser';
 
 const TYPE_VIDEO = 'video';
 const TYPE_IMAGE = 'image';
 const CHUNK_SIZE_FOR_TYPE_DETECTION = 4100;
 
-// list of format whose detection should be skipped
-export const FORMATS_TO_SKIP_TYPE_DETECTION = [
-    { fileType: FILE_TYPE.IMAGE, exactType: 'jpeg' },
-    { fileType: FILE_TYPE.IMAGE, exactType: 'jpg' },
-];
-
 export async function getFileType(
     reader: FileReader,
     receivedFile: File
 ): Promise<FileTypeInfo> {
-    const fileExtension = getFileExtension(receivedFile.name);
     try {
-        const formatToBeSkipped = FORMATS_TO_SKIP_TYPE_DETECTION.find(
-            (a) => a.exactType === fileExtension
-        );
-        if (formatToBeSkipped) {
-            return formatToBeSkipped;
-        }
         let fileType: FILE_TYPE;
         const typeResult = await extractFileType(reader, receivedFile);
         const mimTypeParts = typeResult.mime?.split('/');
@@ -46,10 +34,17 @@ export async function getFileType(
         }
         return { fileType, exactType: typeResult.ext };
     } catch (e) {
+        const fileFormat = getFileExtension(receivedFile.name);
+        const formatMissedByTypeDetection = FORMAT_MISSED_BY_FILE_TYPE_LIB.find(
+            (a) => a.exactType === fileFormat
+        );
+        if (formatMissedByTypeDetection) {
+            return formatMissedByTypeDetection;
+        }
         logError(e, CustomError.TYPE_DETECTION_FAILED, {
-            fileExtension,
+            fileFormat,
         });
-        return { fileType: FILE_TYPE.OTHERS, exactType: fileExtension };
+        return { fileType: FILE_TYPE.OTHERS, exactType: fileFormat };
     }
 }
 
