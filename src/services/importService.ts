96edd9384daa685d6f76a8501325a70a5db63e6c
Abhinav <abhinavk.grd@gmail.com>
2022-04-12 19:23:47 +0530

    remove unneeded allElectronAPIsExist check

--- src/services/importService.ts
+++ src/services/importService.ts
@@ -4,46 +4,37 @@ import { runningInBrowser } from 'utils/common';
 
 class ImportService {
     ElectronAPIs: any;
-    private allElectronAPIsExist: boolean = false;
 
     constructor() {
         this.ElectronAPIs = runningInBrowser() && window['ElectronAPIs'];
-        this.allElectronAPIsExist = !!this.ElectronAPIs?.exists;
     }
 
     async showUploadFilesDialog(): Promise<ElectronFile[]> {
-        if (this.allElectronAPIsExist) {
-            return this.ElectronAPIs.showUploadFilesDialog();
-        }
+        return this.ElectronAPIs.showUploadFilesDialog();
     }
 
     async showUploadDirsDialog(): Promise<ElectronFile[]> {
-        if (this.allElectronAPIsExist) {
-            return this.ElectronAPIs.showUploadDirsDialog();
-        }
+        return this.ElectronAPIs.showUploadDirsDialog();
     }
 
     async getPendingUploads() {
-        if (this.allElectronAPIsExist) {
-            const { files, collectionName } =
-                (await this.ElectronAPIs.getPendingUploads()) as {
-                    files: ElectronFile[];
-                    collectionName: string;
-                };
-            return {
-                files,
-                collectionName,
+        const { files, collectionName } =
+            (await this.ElectronAPIs.getPendingUploads()) as {
+                files: ElectronFile[];
+                collectionName: string;
             };
-        }
+        return {
+            files,
+            collectionName,
+        };
     }
 
     async setToUploadFiles(
         files: FileWithCollection[],
         collections: Collection[]
     ) {
-        if (this.allElectronAPIsExist) {
-            let collectionName: string;
-            /* collection being one suggest one of two things
+        let collectionName: string;
+        /* collection being one suggest one of two things
                 1. Either the user has upload to a single existing collection
                 2. Created a new single collection to upload to 
                     may have had multiple folder, but chose to upload
@@ -51,24 +42,17 @@ class ImportService {
                 hence saving the collection name when upload collection count is 1
                 helps the info of user choosing this options
                 and on next upload we can directly start uploading to this collection 
-            */
-            if (collections.length === 1) {
-                collectionName = collections[0].name;
-            }
-            const filePaths = files.map(
-                (file) => (file.file as ElectronFile).path
-            );
-            this.ElectronAPIs.setToUploadFiles(filePaths, collectionName);
+        */
+        if (collections.length === 1) {
+            collectionName = collections[0].name;
         }
+        const filePaths = files.map((file) => (file.file as ElectronFile).path);
+        this.ElectronAPIs.setToUploadFiles(filePaths, collectionName);
     }
 
     updatePendingUploads(files: FileWithCollection[]) {
-        if (this.allElectronAPIsExist) {
-            const filePaths = files.map(
-                (file) => (file.file as ElectronFile).path
-            );
-            this.ElectronAPIs.updatePendingUploadsFilePaths(filePaths);
-        }
+        const filePaths = files.map((file) => (file.file as ElectronFile).path);
+        this.ElectronAPIs.updatePendingUploadsFilePaths(filePaths);
     }
 }
 export default new ImportService();
