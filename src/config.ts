6bb04912d2a0d7482242687507994fecceb6fc7a
Rushikesh Tote <rushi.tote@gmail.com>
2022-04-12 17:41:41 +0530

    rename renderer to ui

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 const PROD_HOST_URL: string = 'next://app';
-const RENDERER_OUTPUT_DIR: string = './src/renderer/out';
+const RENDERER_OUTPUT_DIR: string = './src/ui/out';
 const FILE_STREAM_CHUNK_SIZE: number = 4 * 1024 * 1024;
 
 export { PROD_HOST_URL, RENDERER_OUTPUT_DIR, FILE_STREAM_CHUNK_SIZE };
