8fd5ad7a57e216afe2bc3d8ad30a60ee4fd4e8e8
Rushikesh Tote <rushi.tote@gmail.com>
2022-04-14 13:26:55 +0530

    rename host to ente

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-const PROD_HOST_URL: string = 'next://app';
+const PROD_HOST_URL: string = 'ente://app';
 const RENDERER_OUTPUT_DIR: string = './src/ui/out';
 const FILE_STREAM_CHUNK_SIZE: number = 4 * 1024 * 1024;
 
