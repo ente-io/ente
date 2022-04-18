262c3693e451619f90856e9bc91c6ffda4d86096
Abhinav <abhinavk.grd@gmail.com>
2022-04-18 18:37:57 +0530

    cancel upload corectly at different stages of upload

--- src/services/upload/uploadManager.ts
+++ src/services/upload/uploadManager.ts
@@ -60,8 +60,10 @@ class UploadManager {
         UIService.init(progressUpdater);
         this.setFiles = setFiles;
     }
+    private uploadCancelled: boolean;
 
     private resetState() {
+        this.uploadCancelled = false;
         this.filesToBeUploaded = [];
         this.remainingFiles = [];
         this.failedFiles = [];
@@ -157,6 +159,9 @@ class UploadManager {
             const reader = new FileReader();
             for (const { file, collectionID } of metadataFiles) {
                 try {
+                    if (this.uploadCancelled) {
+                        break;
+                    }
                     logUploadInfo(
                         `parsing metadata json file ${getFileNameSize(file)}`
                     );
@@ -201,6 +206,9 @@ class UploadManager {
             const reader = new FileReader();
             for (const { file, localID, collectionID } of mediaFiles) {
                 try {
+                    if (this.uploadCancelled) {
+                        break;
+                    }
                     const { fileTypeInfo, metadata } = await (async () => {
                         if (file.size >= MAX_FILE_SIZE_SUPPORTED) {
                             logUploadInfo(
@@ -261,6 +269,9 @@ class UploadManager {
     }
 
     private async uploadMediaFiles(mediaFiles: FileWithCollection[]) {
+        if (this.uploadCancelled) {
+            return;
+        }
         logUploadInfo(`uploadMediaFiles called`);
         this.filesToBeUploaded.push(...mediaFiles);
 
@@ -297,6 +308,9 @@ class UploadManager {
 
     private async uploadNextFileInQueue(worker: any, reader: FileReader) {
         while (this.filesToBeUploaded.length > 0) {
+            if (this.uploadCancelled) {
+                return;
+            }
             const fileWithCollection = this.filesToBeUploaded.pop();
             const { collectionID } = fileWithCollection;
             const existingFilesInCollection =
@@ -364,7 +378,7 @@ class UploadManager {
     }
 
     cancelRemainingUploads() {
-        this.resetState();
+        this.uploadCancelled = true;
     }
 }
 
