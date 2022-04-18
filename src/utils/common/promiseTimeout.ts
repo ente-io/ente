67f831114ae1cf58f65bb778d4a7bbd24a734a77
Abhinav <abhinavk.grd@gmail.com>
2022-04-18 12:22:53 +0530

    minor bug fix

--- src/utils/common/promiseTimeout.ts
+++ src/utils/common/promiseTimeout.ts
@@ -4,11 +4,19 @@ export const promiseWithTimeout = async (
     request: Promise<any>,
     timeout: number
 ) => {
+    const timeoutRef = { current: null };
     const rejectOnTimeout = new Promise((_, reject) => {
-        setTimeout(
+        timeoutRef.current = setTimeout(
             () => reject(Error(CustomError.WAIT_TIME_EXCEEDED)),
             timeout
         );
     });
-    return await Promise.race([request, rejectOnTimeout]);
+    return await Promise.race([
+        (async () => {
+            const resp = await request;
+            clearTimeout(timeoutRef.current);
+            return resp;
+        })(),
+        rejectOnTimeout,
+    ]);
 };
