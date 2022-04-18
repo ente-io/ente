89ef900bcb1a0fb0af04c0f656aff96a8451f28f
ashilkn <ashilkn99@gmail.com>
2022-04-18 20:00:50 +0530

    minor button bg color fix

--- lib/ui/gallery_footer_widget.dart
+++ lib/ui/gallery_footer_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:photo_manager/photo_manager.dart';
+import 'package:photos/ente_theme_data.dart';
 import 'package:photos/services/local_sync_service.dart';
 import 'package:photos/ui/backup_folder_selection_page.dart';
 import 'package:photos/utils/navigation_util.dart';
@@ -18,13 +19,20 @@ class GalleryFooterWidget extends StatelessWidget {
         ),
         Container(
           padding: EdgeInsets.fromLTRB(20, 24, 20, 72),
-          child: OutlinedButton(
-            style: OutlinedButton.styleFrom(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10),
-              ),
-              padding: EdgeInsets.fromLTRB(50, 20, 50, 20),
-            ),
+          child: ElevatedButton(
+            style: Theme.of(context).colorScheme.primaryActionButtonStyle,
+            onPressed: () async {
+              if (LocalSyncService.instance.hasGrantedLimitedPermissions()) {
+                await PhotoManager.presentLimited();
+              } else {
+                routeToPage(
+                  context,
+                  BackupFolderSelectionPage(
+                    buttonText: "preserve",
+                  ),
+                );
+              }
+            },
             child: Row(
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
@@ -40,18 +48,6 @@ class GalleryFooterWidget extends StatelessWidget {
                 ),
               ],
             ),
-            onPressed: () async {
-              if (LocalSyncService.instance.hasGrantedLimitedPermissions()) {
-                await PhotoManager.presentLimited();
-              } else {
-                routeToPage(
-                  context,
-                  BackupFolderSelectionPage(
-                    buttonText: "preserve",
-                  ),
-                );
-              }
-            },
           ),
         ),
       ],
