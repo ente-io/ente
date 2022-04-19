04ee2f501364669b812a9c623696bc4aca98367b
Abhinav Kumar <46242073+abhinavkgrd@users.noreply.github.com>
2022-04-19 11:30:50 +0530

    Revert "added web app as submodule"

--- src/utils/sentry.ts
+++ src/utils/sentry.ts
@@ -5,7 +5,7 @@ import * as isDev from 'electron-is-dev';
 const SENTRY_DSN = 'https://e9268b784d1042a7a116f53c58ad2165@sentry.ente.io/5';
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const version = require('../../../package.json').version;
+const version = require('../../package.json').version;
 
 function initSentry(): void {
     Sentry.init({
