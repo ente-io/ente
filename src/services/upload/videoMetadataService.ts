6215669cd93f678d6ab8ad93f9845ede5cb07955
Abhinav <abhinavk.grd@gmail.com>
2022-04-21 00:49:51 +0530

    add file blob creation logs for getVideoMetadata

--- src/services/upload/videoMetadataService.ts
+++ src/services/upload/videoMetadataService.ts
@@ -2,13 +2,16 @@ import { NULL_EXTRACTED_METADATA } from 'constants/upload';
 import ffmpegService from 'services/ffmpeg/ffmpegService';
 import { ElectronFile } from 'types/upload';
 import { logError } from 'utils/sentry';
+import { logUploadInfo } from 'utils/upload';
 
 export async function getVideoMetadata(file: File | ElectronFile) {
     let videoMetadata = NULL_EXTRACTED_METADATA;
     if (!(file instanceof File)) {
+        logUploadInfo('getting file blob for video metadata extraction');
         file = new File([await file.blob()], file.name, {
             lastModified: file.lastModified,
         });
+        logUploadInfo('file blob for video metadata extraction successfully');
     }
     try {
         videoMetadata = await ffmpegService.extractMetadata(file);
