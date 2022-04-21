e54dc79101db4908bed972bae6f87861680bb1c5
Abhinav <abhinavk.grd@gmail.com>
2022-04-21 19:37:45 +0530

    adds mui and setup it with styled-component

--- next.config.js
+++ next.config.js
@@ -6,6 +6,11 @@ const withWorkbox = require('@ente-io/next-with-workbox');
 const { withSentryConfig } = require('@sentry/nextjs');
 const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
 
+const withTM = require('next-transpile-modules')([
+    '@mui/material',
+    '@mui/system',
+]);
+
 const {
     getGitSha,
     convertToNextHeaderFormat,
@@ -25,37 +30,40 @@ const IS_SENTRY_ENABLED = getIsSentryEnabled();
 module.exports = (phase) =>
     withSentryConfig(
         withWorkbox(
-            withBundleAnalyzer({
-                env: {
-                    SENTRY_RELEASE: GIT_SHA,
-                    NEXT_PUBLIC_LATEST_COMMIT_HASH: GIT_SHA,
-                },
-                workbox: WORKBOX_CONFIG,
+            withBundleAnalyzer(
+                withTM({
+                    env: {
+                        SENTRY_RELEASE: GIT_SHA,
+                        NEXT_PUBLIC_LATEST_COMMIT_HASH: GIT_SHA,
+                    },
+                    workbox: WORKBOX_CONFIG,
 
-                headers() {
-                    return [
-                        {
-                            // Apply these headers to all routes in your application....
-                            source: ALL_ROUTES,
-                            headers: convertToNextHeaderFormat({
-                                ...COOP_COEP_HEADERS,
-                                ...WEB_SECURITY_HEADERS,
-                                ...buildCSPHeader(CSP_DIRECTIVES),
-                            }),
-                        },
-                    ];
-                },
-                // https://dev.to/marcinwosinek/how-to-add-resolve-fallback-to-webpack-5-in-nextjs-10-i6j
-                webpack: (config, { isServer }) => {
-                    if (!isServer) {
-                        config.resolve.fallback.fs = false;
-                    }
-                    return config;
-                },
-            })
-        ),
-        {
-            release: GIT_SHA,
-            dryRun: phase === PHASE_DEVELOPMENT_SERVER || !IS_SENTRY_ENABLED,
-        }
+                    headers() {
+                        return [
+                            {
+                                // Apply these headers to all routes in your application....
+                                source: ALL_ROUTES,
+                                headers: convertToNextHeaderFormat({
+                                    ...COOP_COEP_HEADERS,
+                                    ...WEB_SECURITY_HEADERS,
+                                    ...buildCSPHeader(CSP_DIRECTIVES),
+                                }),
+                            },
+                        ];
+                    },
+                    // https://dev.to/marcinwosinek/how-to-add-resolve-fallback-to-webpack-5-in-nextjs-10-i6j
+                    webpack: (config, { isServer }) => {
+                        if (!isServer) {
+                            config.resolve.fallback.fs = false;
+                        }
+                        return config;
+                    },
+                })
+            ),
+            {
+                release: GIT_SHA,
+                dryRun:
+                    phase === PHASE_DEVELOPMENT_SERVER || !IS_SENTRY_ENABLED,
+            }
+        )
     );
