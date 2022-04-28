60d44138213b5799492925151eafef956733ada8
Abhinav <abhinavk.grd@gmail.com>
2022-04-28 15:08:21 +0530

    add theme toggler

---
+++ src/components/Sidebar/ThemeToggler.tsx
@@ -0,0 +1,30 @@
+import { ToggleButton, ToggleButtonGroup } from '@mui/material';
+import React from 'react';
+import { THEMES } from '.';
+import DarkModeIcon from '@mui/icons-material/DarkMode';
+import LightModeIcon from '@mui/icons-material/LightMode';
+interface Iprops {
+    theme: THEMES;
+    setTheme: (theme: THEMES) => void;
+}
+export default function ThemeToggler({ theme, setTheme }: Iprops) {
+    const handleChange = (event, theme: THEMES) => {
+        setTheme(theme);
+    };
+
+    return (
+        <ToggleButtonGroup
+            color="primary"
+            size="small"
+            value={theme}
+            exclusive
+            onChange={handleChange}>
+            <ToggleButton value={THEMES.LIGHT}>
+                <LightModeIcon />
+            </ToggleButton>
+            <ToggleButton value={THEMES.DARK}>
+                <DarkModeIcon />
+            </ToggleButton>
+        </ToggleButtonGroup>
+    );
+}
