265ccd51c8a28b8113b6501c3bf83e1cab9d9ad9
Abhinav <abhinavk.grd@gmail.com>
2022-05-05 10:23:54 +0530

    update message dialog base style

--- src/components/MessageDialog/MessageDialogBase.tsx
+++ src/components/MessageDialog/MessageDialogBase.tsx
@@ -3,19 +3,27 @@ import styled from 'styled-components';
 
 const MessageDialogBase = styled(Dialog)(({ theme }) => ({
     '& .MuiPaper-root': {
-        padding: theme.spacing(4, 3),
+        padding: '32px 32px 32px 28px',
+        margin: '16px',
     },
     '& .MuiDialogTitle-root': {
         padding: 0,
-        ...theme.typography.h5,
+        fontSize: '30px',
         fontWeight: 600,
+        lineHeight: '36.31px',
     },
     '& .MuiDialogContent-root': {
         padding: 0,
+        fontSize: '18px',
+        lineHeight: '21.78px',
         paddingTop: theme.spacing(2),
         [theme.breakpoints.down('sm')]: {
             paddingTop: '12px',
         },
+    },
+
+    '& .MuiDialogContentText-root': {
+        fontSize: '18px',
     },
     '& .MuiDialogActions-root': {
         padding: 0,
@@ -25,11 +33,10 @@ const MessageDialogBase = styled(Dialog)(({ theme }) => ({
         },
     },
     '& .MuiDialogActions-root .MuiButton-root': {
-        marginLeft: theme.spacing(3),
-        [theme.breakpoints.down('sm')]: {
-            fontSize: '14px',
-            marginLeft: theme.spacing(2),
-        },
+        fontSize: '18px',
+        lineHeight: '21.78px',
+        padding: theme.spacing(2),
+        marginLeft: theme.spacing(2),
     },
 }));
 
