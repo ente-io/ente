0452e323e48adda9e72791bac53b334eb79942f6
Abhinav <abhinavk.grd@gmail.com>
2022-05-11 18:11:29 +0530

    restyle collection sort

--- src/components/pages/gallery/CollectionSort.tsx
+++ src/components/pages/gallery/CollectionSort.tsx
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
 import { COLLECTION_SORT_BY } from 'constants/collection';
 import Menu from '@mui/material/Menu';
-import { IconButton, MenuItem } from '@mui/material';
-import SortIcon from 'components/icons/SortIcon';
-import TickIcon from 'components/icons/TickIcon';
+import { IconButton, MenuItem, styled } from '@mui/material';
 import constants from 'utils/strings/constants';
-import { ListItemIcon, ListItemText, MenuList, Paper } from '@mui/material';
+import { ListItemIcon, ListItemText, MenuList } from '@mui/material';
+import SortIcon from '@mui/icons-material/Sort';
+import { default as TickIcon } from '@mui/icons-material/Done';
 
 interface Props {
     setCollectionSortBy: (sortBy: COLLECTION_SORT_BY) => void;
@@ -24,14 +24,17 @@ const SortByOptionCreator =
             close();
         };
         return (
-            <MenuItem onClick={handleClick}>
-                <ListItemIcon
-                    sx={{
-                        minWidth: '30px',
-                    }}>
-                    {activeSortBy === props.sortBy && <TickIcon />}
+            <MenuItem onClick={handleClick} style={{ paddingLeft: '5px' }}>
+                <ListItemIcon style={{ minWidth: '25px' }}>
+                    {activeSortBy === props.sortBy && (
+                        <TickIcon
+                            css={`
+                                height: 16px;
+                                width: 16px;
+                            `}
+                        />
+                    )}
                 </ListItemIcon>
-
                 <ListItemText>{props.children}</ListItemText>
             </MenuItem>
         );
@@ -41,27 +44,28 @@ const CollectionSortOptions = (props: OptionProps) => {
     const SortByOption = SortByOptionCreator(props);
 
     return (
-        <Paper sx={{ maxWidth: '100%' }}>
-            <MenuList>
-                <SortByOption sortBy={COLLECTION_SORT_BY.NAME}>
-                    {constants.SORT_BY_NAME}
-                </SortByOption>
-                <SortByOption
-                    sortBy={COLLECTION_SORT_BY.CREATION_TIME_DESCENDING}>
-                    {constants.SORT_BY_CREATION_TIME_DESCENDING}
-                </SortByOption>
-                <SortByOption
-                    sortBy={COLLECTION_SORT_BY.CREATION_TIME_ASCENDING}>
-                    {constants.SORT_BY_CREATION_TIME_ASCENDING}
-                </SortByOption>
-                <SortByOption
-                    sortBy={COLLECTION_SORT_BY.UPDATION_TIME_DESCENDING}>
-                    {constants.SORT_BY_UPDATION_TIME_DESCENDING}
-                </SortByOption>
-            </MenuList>
-        </Paper>
+        <MenuList>
+            <SortByOption sortBy={COLLECTION_SORT_BY.NAME}>
+                {constants.SORT_BY_NAME}
+            </SortByOption>
+            <SortByOption sortBy={COLLECTION_SORT_BY.CREATION_TIME_DESCENDING}>
+                {constants.SORT_BY_CREATION_TIME_DESCENDING}
+            </SortByOption>
+            <SortByOption sortBy={COLLECTION_SORT_BY.CREATION_TIME_ASCENDING}>
+                {constants.SORT_BY_CREATION_TIME_ASCENDING}
+            </SortByOption>
+            <SortByOption sortBy={COLLECTION_SORT_BY.UPDATION_TIME_DESCENDING}>
+                {constants.SORT_BY_UPDATION_TIME_DESCENDING}
+            </SortByOption>
+        </MenuList>
     );
 };
+
+const StyledMenu = styled(Menu)`
+    & .MuiList-root {
+        padding: 0;
+    }
+`;
 
 export default function CollectionSort(props: Props) {
     const [sortByEl, setSortByEl] = useState(null);
@@ -75,7 +79,7 @@ export default function CollectionSort(props: Props) {
                 aria-expanded={sortByEl ? 'true' : undefined}>
                 <SortIcon />
             </IconButton>
-            <Menu
+            <StyledMenu
                 id="collection-sort"
                 anchorEl={sortByEl}
                 open={Boolean(sortByEl)}
@@ -84,7 +88,7 @@ export default function CollectionSort(props: Props) {
                     'aria-labelledby': 'collection-sort',
                 }}>
                 <CollectionSortOptions {...props} close={handleClose} />
-            </Menu>
+            </StyledMenu>
         </>
     );
 }
