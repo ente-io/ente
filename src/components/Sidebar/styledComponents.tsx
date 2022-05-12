b6e0b822f3e757fb3339f344831a52dab68cce0c
Abhinav <abhinavk.grd@gmail.com>
2022-05-12 14:23:22 +0530

    udpate styled components

--- src/components/Sidebar/styledComponents.tsx
+++ src/components/Sidebar/styledComponents.tsx
@@ -1,14 +1,30 @@
-import { Drawer, Divider } from '@mui/material';
+import { Drawer, Divider, styled } from '@mui/material';
 import { default as MuiStyled } from '@mui/styled-engine';
+import CircleIcon from '@mui/icons-material/Circle';
 
-export const DrawerSidebar = MuiStyled(Drawer)(() => ({
+export const DrawerSidebar = MuiStyled(Drawer)(({ theme }) => ({
     '& .MuiPaper-root': {
         width: '320px',
-        padding: '20px',
+        padding: theme.spacing(2),
     },
 }));
 
-export const DividerWithMargin = MuiStyled(Divider)(() => ({
-    marginTop: '20px',
-    marginBottom: '20px',
+DrawerSidebar.defaultProps = { anchor: 'left' };
+
+export const PaddedDivider = MuiStyled(Divider)<{
+    invisible?: boolean;
+    dense?: boolean;
+}>(({ theme, invisible, dense }) => ({
+    margin: theme.spacing(dense ? 1 : 2, 0),
+    opacity: invisible ? 0 : 1,
 }));
+
+export const DotSeparator = styled(CircleIcon)`
+    height: 4px;
+    width: 4px;
+    left: 86px;
+    top: 18px;
+    border-radius: 0px;
+    margin: 0 ${({ theme }) => theme.spacing(2)};
+    color: ${({ theme }) => theme.palette.text.secondary};
+`;
