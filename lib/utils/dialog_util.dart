e142e19e60e010ffcf28a2f9cb1a41107e25f50a
Neeraj Gupta <254676+ua741@users.noreply.github.com>
2022-05-17 22:56:04 +0530

    remove unused code

--- lib/utils/dialog_util.dart
+++ lib/utils/dialog_util.dart
@@ -3,7 +3,6 @@ import 'dart:ui';
 
 import 'package:confetti/confetti.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:photos/ui/loading_widget.dart';
 import 'package:photos/ui/progress_dialog.dart';
 
@@ -36,7 +35,7 @@ Future<dynamic> showErrorDialog(
     actions: [
       TextButton(
         child: Text(
-          "OK",
+          "Ok",
           style: TextStyle(
             color: Theme.of(context).colorScheme.onSurface,
           ),
@@ -92,7 +91,8 @@ Future<T> showConfettiDialog<T>({
               confettiController: _confettiController,
               blastDirection: pi / 2,
               emissionFrequency: 0,
-              numberOfParticles: 100, // a lot of particles at once
+              numberOfParticles: 100,
+              // a lot of particles at once
               gravity: 1,
               blastDirectionality: BlastDirectionality.explosive,
             ),
@@ -105,130 +105,5 @@ Future<T> showConfettiDialog<T>({
     useSafeArea: useSafeArea,
     useRootNavigator: useRootNavigator,
     routeSettings: routeSettings,
-  );
-}
-
-Widget test() {
-  return Container(
-    width: 355,
-    height: 236,
-    child: Row(
-      mainAxisSize: MainAxisSize.min,
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Container(
-          width: 355,
-          height: 236,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(8),
-            color: Color(0x4c000000),
-          ),
-          padding: const EdgeInsets.only(
-            top: 20,
-            bottom: 16,
-          ),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.end,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              SizedBox(
-                width: 234,
-                child: Text(
-                  "Are you sure you want to logout?",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 24,
-                    fontFamily: "SF Pro Display",
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
-              ),
-              SizedBox(height: 16),
-              Container(
-                width: 323,
-                height: 48,
-                child: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Container(
-                      width: 323,
-                      height: 48,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(8),
-                        color: Color(0x4c000000),
-                      ),
-                      padding: const EdgeInsets.only(
-                        left: 135,
-                        right: 136,
-                      ),
-                      child: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Text(
-                            "Cancel",
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 16,
-                              fontFamily: "SF Pro Text",
-                              fontWeight: FontWeight.w500,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              SizedBox(height: 16),
-              Container(
-                width: 323,
-                height: 48,
-                child: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Container(
-                      width: 323,
-                      height: 48,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(8),
-                        color: Colors.white,
-                      ),
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 120,
-                        vertical: 12,
-                      ),
-                      child: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Text(
-                            "Yes Logout",
-                            style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 16,
-                              fontFamily: "SF Pro Text",
-                              fontWeight: FontWeight.w500,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
-      ],
-    ),
   );
 }
