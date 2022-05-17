1739be8c1c9f0ee5312052922497f7c52d51772b
Abhinav <abhinavk.grd@gmail.com>
2022-05-17 15:08:00 +0530

    style things better

--- src/components/TwoFactor/styledComponents.ts
+++ src/components/TwoFactor/styledComponents.ts
@@ -1,15 +1,19 @@
 import Container from 'components/Container';
 import styled from 'styled-components';
 
-export const QRCode = styled.img`
+export const QRCode = styled.img(
+    ({ theme }) => `
     height: 200px;
     width: 200px;
-    margin: ${({ theme }) => theme.spacing(2)};
-`;
+    margin: ${theme.spacing(2)};
+`
+);
 
-export const LoadingQRCode = styled(Container)(({ theme }) => ({
-    flex: '0 0 200px',
-    border: `1px solid ${theme.palette.grey[700]}`,
-    width: 200,
-    margin: theme.spacing(2),
-}));
+export const LoadingQRCode = styled(Container)(
+    ({ theme }) => `
+    width:200px;
+    aspect-ratio:1;
+    border: 1px solid ${theme.palette.grey.A200};
+    margin: ${theme.spacing(2)};
+    `
+);
