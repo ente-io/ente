8f19404870cd6e1e6a0c6d7ca870b5266a07eb8e
Abhinav <abhinavk.grd@gmail.com>
2022-05-19 13:02:37 +0530

    create local copy of convertBytesToHumanReadable to prevent loading window through indirect import

--- src/services/readerService.ts
+++ src/services/readerService.ts
@@ -1,5 +1,4 @@
 import { ElectronFile } from 'types/upload';
-import { convertBytesToHumanReadable } from 'utils/billing';
 
 export async function getUint8ArrayView(
     reader: FileReader,
@@ -82,4 +81,17 @@ async function* fileChunkReaderMaker(
         offset += chunkSize;
     }
     return null;
+}
+
+export function convertBytesToHumanReadable(
+    bytes: number,
+    precision = 2
+): string {
+    if (bytes === 0) {
+        return '0 MB';
+    }
+    const i = Math.floor(Math.log(bytes) / Math.log(1024));
+    const sizes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+
+    return (bytes / Math.pow(1024, i)).toFixed(precision) + ' ' + sizes[i];
 }
