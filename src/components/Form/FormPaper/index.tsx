7a64c45fd104fda03d70e68939d155ed02e4f051
Abhinav <abhinavk.grd@gmail.com>
2022-05-20 19:08:59 +0530

    update form container and paper

--- src/components/Form/FormPaper/index.tsx
+++ src/components/Form/FormPaper/index.tsx
@@ -3,10 +3,6 @@ import { Paper, styled } from '@mui/material';
 const FormPaper = styled(Paper)(({ theme }) => ({
     padding: theme.spacing(4, 2),
     maxWidth: '360px',
-    marginRight: theme.spacing(10),
-    [theme.breakpoints.down('md')]: {
-        marginRight: theme.spacing(5),
-    },
     textAlign: 'left',
 }));
 export default FormPaper;
