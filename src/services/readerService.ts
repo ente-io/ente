6fdefff035d1faa3f7f6cad20d0740c8931cf603
Abhinav <abhinavk.grd@gmail.com>
2022-05-23 16:41:54 +0530

    add error log to getUint8ArrayView

--- src/services/readerService.ts
+++ src/services/readerService.ts
@@ -1,7 +1,13 @@
 import { ElectronFile } from 'types/upload';
+import { logError } from 'utils/sentry';
 
 export async function getUint8ArrayView(file: Blob): Promise<Uint8Array> {
-    return new Uint8Array(await file.arrayBuffer());
+    try {
+        return new Uint8Array(await file.arrayBuffer());
+    } catch (e) {
+        logError(e, 'reading file blob failed', { fileSize: file.size });
+        throw e;
+    }
 }
 
 export function getFileStream(file: File, chunkSize: number) {
