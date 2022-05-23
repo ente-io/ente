b53aac496196d81a4565b2a31844094fff9e5455
Abhinav <abhinavk.grd@gmail.com>
2022-05-23 16:49:45 +0530

    use getUint8ArrayView function inconvertHEIC

--- src/services/heicConverter/heicConverterClient.ts
+++ src/services/heicConverter/heicConverterClient.ts
@@ -1,10 +1,11 @@
 import * as HeicConvert from 'heic-convert';
+import { getUint8ArrayView } from 'services/readerService';
 
 export async function convertHEIC(
     fileBlob: Blob,
     format: string
 ): Promise<Blob> {
-    const filedata = new Uint8Array(await fileBlob.arrayBuffer());
+    const filedata = await getUint8ArrayView(fileBlob);
     const result = await HeicConvert({ buffer: filedata, format });
     const convertedFileData = new Uint8Array(result);
     const convertedFileBlob = new Blob([convertedFileData]);
