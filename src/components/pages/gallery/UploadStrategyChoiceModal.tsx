383ae1062f4896d6dfa99fed3a6af45be1ed5c66
Abhinav <abhinavk.grd@gmail.com>
2022-05-26 21:58:10 +0530

    update strategy choice modal

--- src/components/pages/gallery/UploadStrategyChoiceModal.tsx
+++ src/components/pages/gallery/UploadStrategyChoiceModal.tsx
@@ -1,6 +1,9 @@
-import DialogBox from 'components/DialogBox';
+import { Button, DialogContent } from '@mui/material';
+import { CenteredFlex, SpaceBetweenFlex } from 'components/Container';
+import DialogBoxBase, { dialogCloseHandler } from 'components/DialogBox/base';
+import MessageText from 'components/DialogBox/messageText';
+import DialogTitleWithCloseButton from 'components/DialogBox/titleWithCloseButton';
 import React from 'react';
-import { Button } from 'react-bootstrap';
 import constants from 'utils/strings/constants';
 
 interface Props {
@@ -14,62 +17,47 @@ function UploadStrategyChoiceModal({
     uploadToSingleCollection,
     ...props
 }: Props) {
+    const handleClose = dialogCloseHandler({
+        staticBackdrop: true,
+        onClose: props.onClose,
+    });
+
     return (
-        <DialogBox
-            {...props}
-            attributes={{ title: constants.MULTI_FOLDER_UPLOAD }}>
-            <p
-                style={{
-                    fontSize: '18px',
-                    textAlign: 'center',
-                    marginBottom: '24px',
-                    marginTop: '4px',
-                }}>
-                {constants.UPLOAD_STRATEGY_CHOICE}
-            </p>
-            <div
-                style={{
-                    display: 'flex',
-                    flexWrap: 'wrap',
-                    paddingBottom: '12px',
-                    justifyContent: 'center',
-                }}>
-                <Button
-                    variant="outline-success"
-                    onClick={() => {
-                        props.onClose();
-                        uploadToSingleCollection();
-                    }}
-                    style={{
-                        padding: '12px 24px',
-                        whiteSpace: 'nowrap',
-                        fontWeight: 900,
-                    }}>
-                    {constants.UPLOAD_STRATEGY_SINGLE_COLLECTION}
-                </Button>
-                <div
-                    style={{
-                        textAlign: 'center',
-                        minWidth: '100px',
-                        margin: '2%',
-                    }}>
+        <DialogBoxBase open={props.open} onClose={handleClose}>
+            <DialogTitleWithCloseButton onClose={handleClose}>
+                {constants.MULTI_FOLDER_UPLOAD}
+            </DialogTitleWithCloseButton>
+            <DialogContent>
+                <CenteredFlex mb={1}>
+                    <MessageText>
+                        {constants.UPLOAD_STRATEGY_CHOICE}
+                    </MessageText>
+                </CenteredFlex>
+                <SpaceBetweenFlex>
+                    <Button
+                        size="large"
+                        color="accent"
+                        onClick={() => {
+                            props.onClose();
+                            uploadToSingleCollection();
+                        }}>
+                        {constants.UPLOAD_STRATEGY_SINGLE_COLLECTION}
+                    </Button>
+
                     <strong>{constants.OR}</strong>
-                </div>
-                <Button
-                    variant="outline-success"
-                    onClick={() => {
-                        props.onClose();
-                        uploadToMultipleCollection();
-                    }}
-                    style={{
-                        padding: '12px 24px',
-                        whiteSpace: 'nowrap',
-                        fontWeight: 900,
-                    }}>
-                    {constants.UPLOAD_STRATEGY_COLLECTION_PER_FOLDER}
-                </Button>
-            </div>
-        </DialogBox>
+
+                    <Button
+                        size="large"
+                        color="accent"
+                        onClick={() => {
+                            props.onClose();
+                            uploadToMultipleCollection();
+                        }}>
+                        {constants.UPLOAD_STRATEGY_COLLECTION_PER_FOLDER}
+                    </Button>
+                </SpaceBetweenFlex>
+            </DialogContent>
+        </DialogBoxBase>
     );
 }
 export default UploadStrategyChoiceModal;
