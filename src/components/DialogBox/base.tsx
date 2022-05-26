4a5b353b2b2747a69801ec6f1ab1c0d1e28aa416
Abhinav <abhinavk.grd@gmail.com>
2022-05-26 13:53:04 +0530

    update dialog stylog

--- src/components/DialogBox/base.tsx
+++ src/components/DialogBox/base.tsx
@@ -1,19 +1,22 @@
 import { Dialog, styled } from '@mui/material';
 
 const DialogBoxBase = styled(Dialog)(({ theme }) => ({
+    '& .MuiDialog-paper': {
+        padding: theme.spacing(2, 0),
+    },
     '& .MuiDialogTitle-root': {
-        padding: theme.spacing(4, 3, 2),
+        padding: theme.spacing(2, 3),
     },
     '& .MuiDialogContent-root': {
-        padding: theme.spacing(0, 3, 2),
+        padding: theme.spacing(2, 3),
     },
     '& .MuiDialogActions-root': {
-        padding: theme.spacing(4, 3),
+        padding: theme.spacing(2, 3),
     },
     '& .MuiDialogActions-root button': {
         fontSize: '18px',
         lineHeight: '21.78px',
-        padding: theme.spacing(2),
+        paddingTop: theme.spacing(2),
     },
     '& .MuiDialogActions-root button:not(:first-child)': {
         marginLeft: theme.spacing(2),
