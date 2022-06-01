8ef00678a20fb30c8c6caa73250dea0c4bdb0357
Rushikesh Tote <rushi.tote@gmail.com>
2022-06-01 13:35:21 +0530

    refactor

--- src/services/machineLearning/imageSceneService.ts
+++ src/services/machineLearning/imageSceneService.ts
@@ -6,11 +6,11 @@ import {
     SceneDetectionService,
     Versioned,
 } from 'types/machineLearning';
-import sceneMap from 'utils/machineLearning/sceneMap';
 
 class ImageScene implements SceneDetectionService {
     method: Versioned<SceneDetectionMethod>;
     model: tfjsConverter.GraphModel;
+    sceneMap: { [key: string]: string };
 
     public constructor() {
         this.method = {
@@ -24,11 +24,20 @@ class ImageScene implements SceneDetectionService {
             return;
         }
 
+        this.sceneMap = await (
+            await fetch('/models/imagescene/sceneMap.json')
+        ).json();
+
         const model = await tfjsConverter.loadGraphModel(
             '/models/imagescene/model.json'
         );
         console.log('loaded ImageScene model', model, tf.getBackend());
         this.model = model;
+
+        // warmup the model
+        const warmupResult = this.model.predict(tf.zeros([1, 224, 224, 3]));
+        await (warmupResult as tf.Tensor).data();
+        (warmupResult as tf.Tensor).dispose();
     }
 
     async detectScenes(image: ImageBitmap, minScore: number) {
@@ -39,18 +48,23 @@ class ImageScene implements SceneDetectionService {
         }
 
         const output = tf.tidy(() => {
-            let tensor = tf.browser.fromPixels(image);
+            const tensor = tf.browser.fromPixels(image);
 
-            tensor = tf.image.resizeBilinear(tensor, [224, 224]);
-            tensor = tf.expandDims(tensor);
-            tensor = tf.cast(tensor, 'float32');
+            // This model takes fixed-shaped (224x224) inputs
+            // https://tfhub.dev/sayannath/lite-model/image-scene/1
+            let resizedTensor = tf.image.resizeBilinear(tensor, [224, 224]);
 
-            const output = this.model.predict(tensor);
+            resizedTensor = tf.expandDims(resizedTensor);
+            resizedTensor = tf.cast(resizedTensor, 'float32');
+
+            const output = this.model.predict(resizedTensor);
 
             return output;
         });
 
         const data = await (output as tf.Tensor).data();
+        (output as tf.Tensor).dispose();
+
         const scenes = this.getScenes(
             data as Float32Array,
             image.width,
@@ -71,7 +85,7 @@ class ImageScene implements SceneDetectionService {
         for (let i = 0; i < outputData.length; i++) {
             if (outputData[i] >= minScore) {
                 scenes.push({
-                    class: sceneMap.get(i),
+                    class: this.sceneMap[i.toString()],
                     score: outputData[i],
                     bbox: [0, 0, width, height],
                 });
