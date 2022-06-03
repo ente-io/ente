fdad1dcd5db118a54ac4102ec3c6a3731b86d9d7
ashilkn <ashilkn99@gmail.com>
2022-06-03 15:41:26 +0530

    minor changes

--- lib/utils/toast_util.dart
+++ lib/utils/toast_util.dart
@@ -13,14 +13,13 @@ Future<void> showToast(String message,
         toastLength: toastLength,
         gravity: ToastGravity.TOP,
         timeInSecForIosWeb: 1,
-        // backgroundColor: Colors.blueGrey[900]
-        backgroundColor: Color.fromRGBO(59, 59, 59, 0.23),
+        backgroundColor: Color.fromRGBO(127, 127, 127, 0.5),
         textColor: Colors.white,
         fontSize: 16.0);
   } else {
     EasyLoading.instance
-      ..backgroundColor = Color.fromRGBO(59, 59, 59, 0.23)
-      ..indicatorColor = Color.fromRGBO(59, 59, 59, 0.23)
+      ..backgroundColor = Color.fromRGBO(127, 127, 127, 0.5)
+      ..indicatorColor = Color.fromRGBO(127, 127, 127, 0.5)
       ..textColor = Colors.white
       ..userInteractions = true
       ..loadingStyle = EasyLoadingStyle.custom;
