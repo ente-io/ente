843e0f41e28c51f4123fdec74580b896e1c56658
ashilkn <ashilkn99@gmail.com>
2022-06-07 09:20:41 +0530

    Dynamic FAB's transition fix

--- lib/ui/email_entry_page.dart
+++ lib/ui/email_entry_page.dart
@@ -67,7 +67,7 @@ class _EmailEntryPageState extends State<EmailEntryPage> {
 
   @override
   Widget build(BuildContext context) {
-    final isKeypadOpen = MediaQuery.of(context).viewInsets.bottom != 0;
+    final isKeypadOpen = MediaQuery.of(context).viewInsets.bottom > 125;
 
     FloatingActionButtonLocation fabLocation() {
       if (isKeypadOpen) {
