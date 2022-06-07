1d9bc49909a88d887af650eec5306232b39a21c9
Rushikesh Tote <rushi.tote@gmail.com>
2022-06-07 12:25:49 +0530

    move watch modal to components

--- src/components/Sidebar/UtilitySection.tsx
+++ src/components/Sidebar/UtilitySection.tsx
@@ -7,7 +7,7 @@ import TwoFactorModal from 'components/TwoFactor/Modal';
 import { PAGES } from 'constants/pages';
 import { useRouter } from 'next/router';
 import { AppContext } from 'pages/_app';
-import WatchModal from './WatchModal';
+import WatchModal from '../WatchModal';
 
 export default function UtilitySection({ closeSidebar }) {
     const router = useRouter();
