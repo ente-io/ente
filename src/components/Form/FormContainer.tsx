ffde74d8bb813d180d78de101352018f645ac693
Abhinav <abhinavk.grd@gmail.com>
2022-06-10 00:19:41 +0530

    center the forms

--- src/components/Form/FormContainer.tsx
+++ src/components/Form/FormContainer.tsx
@@ -2,7 +2,7 @@ import { styled } from '@mui/material/styles';
 import VerticallyCentered from 'components/Container';
 
 const FormContainer = styled(VerticallyCentered)(({ theme }) => ({
-    alignItems: 'flex-end',
+    alignItems: 'center',
     paddingRight: theme.spacing(10),
     [theme.breakpoints.down('md')]: {
         paddingRight: theme.spacing(5),
