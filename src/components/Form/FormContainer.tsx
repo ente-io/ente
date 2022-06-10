a06bdf8c0f919661cd56790dd217c1253942cd14
Abhinav <abhinavk.grd@gmail.com>
2022-06-10 10:57:04 +0530

    remove all right padding from form container

--- src/components/Form/FormContainer.tsx
+++ src/components/Form/FormContainer.tsx
@@ -1,12 +1,5 @@
-import { styled } from '@mui/material/styles';
 import VerticallyCentered from 'components/Container';
 
-const FormContainer = styled(VerticallyCentered)(({ theme }) => ({
-    alignItems: 'center',
-
-    [theme.breakpoints.down('md')]: {
-        paddingRight: theme.spacing(5),
-    },
-}));
+const FormContainer = VerticallyCentered;
 
 export default FormContainer;
