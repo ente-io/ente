174c6961112019e73deb91de3c9c405160df0d58
vishnukvmd <vishnu@ente.io>
2022-06-12 22:07:05 +0530

    Fix "On ente"

--- lib/ui/collections_gallery_widget.dart
+++ lib/ui/collections_gallery_widget.dart
@@ -192,7 +192,7 @@ class _CollectionsGalleryWidgetState extends State<CollectionsGalleryWidget>
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               crossAxisAlignment: CrossAxisAlignment.end,
               children: [
-                SectionTitle("On ente"),
+                EnteSectionTitle(),
                 _sortMenu(),
               ],
             ),
@@ -734,6 +734,50 @@ class SectionTitle extends StatelessWidget {
               title,
               style:
                   Theme.of(context).textTheme.headline6.copyWith(fontSize: 22),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class EnteSectionTitle extends StatelessWidget {
+  final double opacity;
+
+  const EnteSectionTitle({
+    this.opacity = 0.8,
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.fromLTRB(16, 12, 0, 0),
+      child: Column(
+        children: [
+          Align(
+            alignment: Alignment.centerLeft,
+            child: RichText(
+              text: TextSpan(
+                children: [
+                  TextSpan(
+                    text: "On ",
+                    style: Theme.of(context)
+                        .textTheme
+                        .headline6
+                        .copyWith(fontSize: 22),
+                  ),
+                  TextSpan(
+                    text: "ente",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Montserrat',
+                      fontSize: 22,
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ],
