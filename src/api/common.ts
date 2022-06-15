64acef79d3ea3455a6983552d01b1f99654f3ae6
Abhinav <abhinavk.grd@gmail.com>
2022-06-15 11:44:56 +0530

    move doesFolderExists to common api

--- src/api/common.ts
+++ src/api/common.ts
@@ -1,4 +1,5 @@
 import { ipcRenderer } from 'electron';
+import fs from 'promise-fs';
 
 export const sendNotification = (content: string) => {
     ipcRenderer.send('send-notification', content);
@@ -9,3 +10,12 @@ export const showOnTray = (content: string) => {
 export const reloadWindow = () => {
     ipcRenderer.send('reload-window');
 };
+
+export async function doesFolderExists(dirPath: string) {
+    return await fs
+        .stat(dirPath)
+        .then((stats) => {
+            return stats.isDirectory();
+        })
+        .catch(() => false);
+}
