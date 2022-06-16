ebecb02238cd1f4773a77ec4e243c30012b6115a
Abhinav <abhinavk.grd@gmail.com>
2022-06-16 10:31:35 +0530

    put null metadataAndFileTypeInfoMap entry when  extractFileTypeAndMetadata fails

--- src/services/upload/uploadManager.ts
+++ src/services/upload/uploadManager.ts
@@ -21,6 +21,7 @@ import { CustomError } from 'utils/error';
 import { Collection } from 'types/collection';
 import { EnteFile } from 'types/file';
 import {
+    ElectronFile,
     FileWithCollection,
     MetadataAndFileTypeInfo,
     MetadataAndFileTypeInfoMap,
@@ -227,50 +228,20 @@ class UploadManager {
             logUploadInfo(`extractMetadataFromFiles executed`);
             UIService.reset(mediaFiles.length);
             for (const { file, localID, collectionID } of mediaFiles) {
+                let fileTypeInfo = null;
+                let metadata = null;
                 try {
-                    const { fileTypeInfo, metadata } = await (async () => {
-                        if (file.size >= MAX_FILE_SIZE_SUPPORTED) {
-                            logUploadInfo(
-                                `${getFileNameSize(
-                                    file
-                                )} rejected  because of large size`
-                            );
-
-                            return { fileTypeInfo: null, metadata: null };
-                        }
-                        const fileTypeInfo = await UploadService.getFileType(
-                            file
-                        );
-                        if (fileTypeInfo.fileType === FILE_TYPE.OTHERS) {
-                            logUploadInfo(
-                                `${getFileNameSize(
-                                    file
-                                )} rejected  because of unknown file format`
-                            );
-                            return { fileTypeInfo, metadata: null };
-                        }
-                        logUploadInfo(
-                            ` extracting ${getFileNameSize(file)} metadata`
-                        );
-                        const metadata =
-                            (await UploadService.extractFileMetadata(
-                                file,
-                                collectionID,
-                                fileTypeInfo
-                            )) || null;
-                        return { fileTypeInfo, metadata };
-                    })();
-
+                    const result = await this.extractFileTypeAndMetadata(
+                        file,
+                        collectionID
+                    );
+                    fileTypeInfo = result?.fileTypeInfo;
+                    metadata = result?.metadata;
                     logUploadInfo(
                         `metadata extraction successful${getFileNameSize(
                             file
                         )} `
                     );
-                    this.metadataAndFileTypeInfoMap.set(localID, {
-                        fileTypeInfo: fileTypeInfo && { ...fileTypeInfo },
-                        metadata: metadata && { ...metadata },
-                    });
-                    UIService.increaseFileUploaded();
                 } catch (e) {
                     logError(e, 'metadata extraction failed for a file');
                     logUploadInfo(
@@ -279,11 +250,46 @@ class UploadManager {
                         )} error: ${e.message}`
                     );
                 }
+                this.metadataAndFileTypeInfoMap.set(localID, {
+                    fileTypeInfo: fileTypeInfo && { ...fileTypeInfo },
+                    metadata: metadata && { ...metadata },
+                });
+                UIService.increaseFileUploaded();
             }
         } catch (e) {
             logError(e, 'error extracting metadata');
             throw e;
         }
+    }
+
+    private async extractFileTypeAndMetadata(
+        file: File | ElectronFile,
+        collectionID: number
+    ) {
+        if (file.size >= MAX_FILE_SIZE_SUPPORTED) {
+            logUploadInfo(
+                `${getFileNameSize(file)} rejected  because of large size`
+            );
+
+            return { fileTypeInfo: null, metadata: null };
+        }
+        const fileTypeInfo = await UploadService.getFileType(file);
+        if (fileTypeInfo.fileType === FILE_TYPE.OTHERS) {
+            logUploadInfo(
+                `${getFileNameSize(
+                    file
+                )} rejected  because of unknown file format`
+            );
+            return { fileTypeInfo, metadata: null };
+        }
+        logUploadInfo(` extracting ${getFileNameSize(file)} metadata`);
+        const metadata =
+            (await UploadService.extractFileMetadata(
+                file,
+                collectionID,
+                fileTypeInfo
+            )) || null;
+        return { fileTypeInfo, metadata };
     }
 
     private async uploadMediaFiles(mediaFiles: FileWithCollection[]) {
