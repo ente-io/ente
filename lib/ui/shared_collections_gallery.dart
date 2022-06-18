2512ae9727ca5f78d5469849137c98bdda60ceb8
Manav <manav@ente.io>
2022-06-18 14:53:59 +0530

    Remove please
    The toast is short and disappears too quickly, so we need to reduce
    the amount of text.
    
    The longer term fix would be to improve this interaction.

--- lib/ui/shared_collections_gallery.dart
+++ lib/ui/shared_collections_gallery.dart
@@ -251,7 +251,7 @@ class _SharedCollectionGalleryState extends State<SharedCollectionGallery>
               onTap: () async {
                 await showToast(
                   context,
-                  "Please select an album and tap the share button on the top right to share.",
+                  "Select an album and tap the share button on the top right to share.",
                   toastLength: Toast.LENGTH_LONG,
                 );
                 Bus.instance.fire(
