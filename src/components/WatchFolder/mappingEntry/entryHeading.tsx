0138873881dba21ca2906e20ba6d2235b154a312
Abhinav <abhinavk.grd@gmail.com>
2022-06-19 16:32:14 +0530

    update EntryHeading styles

--- src/components/WatchFolder/mappingEntry/entryHeading.tsx
+++ src/components/WatchFolder/mappingEntry/entryHeading.tsx
@@ -3,22 +3,22 @@ import { Typography } from '@mui/material';
 import watchService from 'services/watchFolderService';
 import { AppContext } from 'pages/_app';
 import { SyncProgressIcon } from './syncProgressIcon';
+import { FlexWrapper } from 'components/Container';
 
 export function EntryHeading({ mapping }) {
     const appContext = useContext(AppContext);
     return (
-        <Typography
-            sx={{
-                fontSize: '16px',
-                lineHeight: '20px',
-                marginBottom: '4px',
-            }}>
-            <>
+        <FlexWrapper sx={{ marginBottom: '4px' }}>
+            <Typography
+                sx={{
+                    fontSize: '16px',
+                    lineHeight: '20px',
+                }}>
                 {mapping.collectionName}
-                {appContext.isFolderSyncRunning &&
-                    watchService.currentEvent?.collectionName ===
-                        mapping.collectionName && <SyncProgressIcon />}
-            </>
-        </Typography>
+            </Typography>
+            {appContext.isFolderSyncRunning &&
+                watchService.currentEvent?.collectionName ===
+                    mapping.collectionName && <SyncProgressIcon />}
+        </FlexWrapper>
     );
 }
