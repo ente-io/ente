6832ae46ff9f3ab9a00a842380a5c44d2b237bf1
Abhinav <abhinavk.grd@gmail.com>
2022-06-19 16:44:03 +0530

    add more typing

--- src/components/WatchFolder/mappingList.tsx
+++ src/components/WatchFolder/mappingList.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import { WatchMapping } from 'types/watchFolder';
 import { MappingEntry } from './mappingEntry';
 import { MappingsContainer } from './styledComponents';
-export function MappingList({ mappings, handleRemoveWatchMapping }) {
+interface Iprops {
+    mappings: WatchMapping[];
+    handleRemoveWatchMapping: (value: WatchMapping) => void;
+}
+
+export function MappingList({ mappings, handleRemoveWatchMapping }: Iprops) {
     return (
         <MappingsContainer>
             {mappings.map((mapping) => {
-                console.log(mapping);
                 return (
                     <MappingEntry
                         key={mapping.collectionName}
