3fc5b1dfaeb9dd74a8b99244291f14bb859a0bf5
Abhinav <abhinavk.grd@gmail.com>
2022-06-21 14:34:24 +0530

    update menu option

--- src/components/OverflowMenu/option.tsx
+++ src/components/OverflowMenu/option.tsx
@@ -1,40 +1,37 @@
-import {
-    MenuItem,
-    ListItemIcon,
-    ListItemText,
-    ButtonProps,
-} from '@mui/material';
+import { MenuItem, ListItemIcon, ButtonProps, Typography } from '@mui/material';
 import React from 'react';
 
 interface Iprops {
-    handleClick: () => void;
-    color: ButtonProps['color'];
-    startIcon: React.ReactNode;
-    label: string;
+    onClick: () => void;
+    color?: ButtonProps['color'];
+    startIcon?: React.ReactNode;
+    label: any;
 }
 export function OverflowMenuOption({
-    handleClick,
-    color,
+    onClick,
+    color = 'primary',
     startIcon,
     label,
 }: Iprops) {
     return (
         <MenuItem
-            onClick={handleClick}
+            onClick={onClick}
             sx={{
                 color: (theme) => theme.palette[color].main,
                 padding: '12px',
             }}>
-            <ListItemIcon
-                sx={{
-                    color: 'inherit',
-                    padding: '0',
-                    paddingRight: '12px',
-                    fontSize: '20px',
-                }}>
-                {startIcon}
-            </ListItemIcon>
-            <ListItemText>{label}</ListItemText>
+            {startIcon && (
+                <ListItemIcon
+                    sx={{
+                        color: 'inherit',
+                        padding: '0',
+                        paddingRight: '12px',
+                        fontSize: '20px',
+                    }}>
+                    {startIcon}
+                </ListItemIcon>
+            )}
+            <Typography variant="button">{label}</Typography>
         </MenuItem>
     );
 }
