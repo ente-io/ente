13fbc92fad6ca77d20a867e0af21831e98ab97de
Abhinav <abhinavk.grd@gmail.com>
2022-06-22 10:40:56 +0530

    add stroke colors

--- src/themes/darkThemeOptions.tsx
+++ src/themes/darkThemeOptions.tsx
@@ -2,16 +2,19 @@ import {
     createTheme,
     PaletteColor,
     PaletteColorOptions,
+    TypeText,
 } from '@mui/material/styles';
 
 declare module '@mui/material/styles' {
     interface Palette {
         accent: PaletteColor;
         danger: PaletteColor;
+        stroke: TypeText;
     }
     interface PaletteOptions {
         accent?: PaletteColorOptions;
         danger?: PaletteColorOptions;
+        stroke?: Partial<TypeText>;
     }
 
     interface TypographyVariants {
@@ -130,6 +133,11 @@ const darkThemeOptions = createTheme({
 
         danger: {
             main: '#c93f3f',
+        },
+        stroke: {
+            primary: '#ffffff',
+            secondary: 'rgba(256,256,256,0.24)',
+            disabled: 'rgba(256,256,256,0.12)',
         },
         background: { default: '#000000', paper: '#1b1b1b' },
         grey: {
