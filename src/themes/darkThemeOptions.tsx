7f9c1ec547d9b54ebee73da7b86741f54e64570f
Abhinav <abhinavk.grd@gmail.com>
2022-06-25 21:49:24 +0530

    update overlau box shadow

--- src/themes/darkThemeOptions.tsx
+++ src/themes/darkThemeOptions.tsx
@@ -69,7 +69,7 @@ const darkThemeOptions = createTheme({
             styleOverrides: {
                 root: {
                     '& .MuiDialog-paper': {
-                        padding: '16px 0',
+                        boxShadow: '0px 0px 10px 0px rgba(0, 0, 0, 0.25)',
                     },
                     '& .MuiDialogTitle-root': {
                         padding: '16px',
