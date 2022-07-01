815e12eeb4da38541807f7642afdecc9d80e1115
Neeraj Gupta <254676+ua741@users.noreply.github.com>
2022-07-01 20:08:33 +0530

    reactor: move widgets location

---
+++ lib/ui/common/common_elements.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+Widget nothingToSeeHere({Color textColor}) {
+  return Center(
+    child: Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Text(
+        "Nothing to see here! 👀",
+        style: TextStyle(
+          color: textColor.withOpacity(0.35),
+        ),
+      ),
+    ),
+  );
+}
+
+final emptyContainer = const SizedBox.shrink();
