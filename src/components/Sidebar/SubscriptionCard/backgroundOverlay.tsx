bb840a5444fd3633f333ea7c7f3daba8c625a73a
Abhinav <abhinavk.grd@gmail.com>
2022-07-01 21:15:56 +0530

    fix background image overflow

--- src/components/Sidebar/SubscriptionCard/backgroundOverlay.tsx
+++ src/components/Sidebar/SubscriptionCard/backgroundOverlay.tsx
@@ -4,6 +4,8 @@ export function BackgroundOverlay() {
     return (
         <Overlay zIndex={-1}>
             <img
+                width="100%"
+                height="100%"
                 src="/images/subscription-card-background/1x.png"
                 srcSet="/images/subscription-card-background/2x.png 2x,
                         /images/subscription-card-background/3x.png 3x"
