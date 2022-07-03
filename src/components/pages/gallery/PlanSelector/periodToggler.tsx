1b8a257aba46a6a8744f067c7a9eb6a3a1dc3b13
Abhinav <abhinavk.grd@gmail.com>
2022-07-03 18:08:38 +0530

    fix period toggler width

--- src/components/pages/gallery/PlanSelector/periodToggler.tsx
+++ src/components/pages/gallery/PlanSelector/periodToggler.tsx
@@ -2,23 +2,25 @@ import { styled, ToggleButton, ToggleButtonGroup } from '@mui/material';
 import { PLAN_PERIOD } from 'constants/gallery';
 import React from 'react';
 import constants from 'utils/strings/constants';
-export function PeriodToggler({ planPeriod, togglePeriod }) {
-    const CustomToggleButton = styled(ToggleButton)(({ theme }) => ({
-        textTransform: 'none',
-        padding: '12px 16px',
-        borderRadius: '4px',
-        backgroundColor: theme.palette.fill.dark,
-        color: theme.palette.text.disabled,
-        '&.Mui-selected': {
-            backgroundColor: theme.palette.accent.main,
-            color: theme.palette.accent.contrastText,
-        },
-        '&.Mui-selected:hover': {
-            backgroundColor: theme.palette.accent.main,
-            color: theme.palette.accent.contrastText,
-        },
-    }));
 
+const CustomToggleButton = styled(ToggleButton)(({ theme }) => ({
+    textTransform: 'none',
+    padding: '12px 16px',
+    borderRadius: '4px',
+    backgroundColor: theme.palette.fill.dark,
+    color: theme.palette.text.disabled,
+    '&.Mui-selected': {
+        backgroundColor: theme.palette.accent.main,
+        color: theme.palette.accent.contrastText,
+    },
+    '&.Mui-selected:hover': {
+        backgroundColor: theme.palette.accent.main,
+        color: theme.palette.accent.contrastText,
+    },
+    width: '97.433px',
+}));
+
+export function PeriodToggler({ planPeriod, togglePeriod }) {
     const handleChange = (_, newPlanPeriod: PLAN_PERIOD) => {
         if (newPlanPeriod !== null && newPlanPeriod !== planPeriod) {
             togglePeriod();
