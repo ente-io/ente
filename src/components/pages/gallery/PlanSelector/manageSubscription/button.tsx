381911cb4956d5f8fa44950299a7bcdbdd59f95f
Abhinav <abhinavk.grd@gmail.com>
2022-07-03 15:05:39 +0530

    update manage section button

--- src/components/pages/gallery/PlanSelector/manageSubscription/button.tsx
+++ src/components/pages/gallery/PlanSelector/manageSubscription/button.tsx
@@ -1,33 +1,18 @@
 import ChevronRight from '@mui/icons-material/ChevronRight';
-import { Box, ButtonProps, styled } from '@mui/material';
-import { FlexWrapper } from 'components/Container';
+import { Button, ButtonProps } from '@mui/material';
+import { FluidContainer } from 'components/Container';
 import React from 'react';
 
-const ManageSubscriptionButtonBase = styled(Box)<{
-    color?: ButtonProps['color'];
-}>(({ theme, color = 'primary' }) => ({
-    textDecoration: 'none',
-    padding: '20px 24px',
-    width: '100%',
-    border: `1px solid ${theme.palette.divider}`,
-    borderBottomWidth: '0px',
-    cursor: 'pointer',
-    '&:hover .children': {
-        color: theme.palette[color].main,
-    },
-    '&:last-of-type': {
-        borderRadius: '0 0 8px 8px',
-        borderBottomWidth: '1px',
-    },
-}));
-
-const ManageSubscriptionButton = ({ children, ...props }) => (
-    <ManageSubscriptionButtonBase {...props}>
-        <FlexWrapper>
-            <div className="children">{children}</div>
-            <ChevronRight />
-        </FlexWrapper>
-    </ManageSubscriptionButtonBase>
+const ManageSubscriptionButton = ({ children, ...props }: ButtonProps) => (
+    <Button
+        {...props}
+        endIcon={<ChevronRight />}
+        sx={{
+            ...props.sx,
+            justifyContent: 'flex-start',
+        }}>
+        <FluidContainer>{children}</FluidContainer>
+    </Button>
 );
 
 export default ManageSubscriptionButton;
