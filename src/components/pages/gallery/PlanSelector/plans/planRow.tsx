6703d2a5b92668f315097912723ce8ad30b172e8
Abhinav <abhinavk.grd@gmail.com>
2022-07-05 17:24:16 +0530

    add space after slash

--- src/components/pages/gallery/PlanSelector/plans/planRow.tsx
+++ src/components/pages/gallery/PlanSelector/plans/planRow.tsx
@@ -92,10 +92,11 @@ export function PlanRow({
                             {plan.price}{' '}
                         </Typography>{' '}
                         <Typography color="text.secondary" variant="body2">
-                            /
-                            {plan.period === PLAN_PERIOD.MONTH
-                                ? constants.MONTH_SHORT
-                                : constants.YEAR_SHORT}
+                            {`/ ${
+                                plan.period === PLAN_PERIOD.MONTH
+                                    ? constants.MONTH_SHORT
+                                    : constants.YEAR_SHORT
+                            }`}
                         </Typography>
                     </Box>
                 </PlanButton>
