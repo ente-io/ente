caf3883c3e7035684c9237f7e41156a9be99aff2
Abhinav <abhinavk.grd@gmail.com>
2022-07-05 13:16:52 +0530

    fix casing

--- src/utils/strings/englishConstants.tsx
+++ src/utils/strings/englishConstants.tsx
@@ -47,7 +47,7 @@ const englishConstants = {
             <div> everywhere</div>
         </>
     ),
-    HERO_SLIDE_3: 'Android, iOS, web, Desktop',
+    HERO_SLIDE_3: 'Android, iOS, Web, Desktop',
     COMPANY_NAME: 'ente',
     LOGIN: 'Login',
     SIGN_UP: 'Signup',
