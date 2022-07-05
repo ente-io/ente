0a96aae04f8b0b344a53775531fe6eefecc16e2e
Abhinav <abhinavk.grd@gmail.com>
2022-07-05 19:56:31 +0530

    remove logo from gallery navbar

--- src/components/pages/gallery/Navbar.tsx
+++ src/components/pages/gallery/Navbar.tsx
@@ -3,8 +3,6 @@ import NavbarBase from 'components/Navbar/base';
 import SidebarToggler from 'components/Navbar/SidebarToggler';
 import { getNonTrashedUniqueUserFiles } from 'utils/file';
 import SearchBar from 'components/Search/SearchBar';
-import { FluidContainer } from 'components/Container';
-import { EnteLogo } from 'components/EnteLogo';
 import { Collection } from 'types/collection';
 import { EnteFile } from 'types/file';
 import { UpdateSearch } from 'types/search';
@@ -23,7 +21,6 @@ interface Iprops {
 }
 export function GalleryNavbar({
     openSidebar,
-    isFirstFetch,
     openUploader,
     isInSearchMode,
     collections,
@@ -35,27 +32,15 @@ export function GalleryNavbar({
     return (
         <NavbarBase sx={{ background: 'transparent', position: 'absolute' }}>
             {!isInSearchMode && <SidebarToggler openSidebar={openSidebar} />}
-
-            {isFirstFetch ? (
-                <FluidContainer style={{ justifyContent: 'center' }}>
-                    <EnteLogo />
-                </FluidContainer>
-            ) : (
-                <SearchBar
-                    isInSearchMode={isInSearchMode}
-                    setIsInSearchMode={setIsInSearchMode}
-                    collections={collections}
-                    files={getNonTrashedUniqueUserFiles(files)}
-                    setActiveCollection={setActiveCollection}
-                    updateSearch={updateSearch}
-                />
-            )}
-            {!isInSearchMode && (
-                <UploadButton
-                    isFirstFetch={isFirstFetch}
-                    openUploader={openUploader}
-                />
-            )}
+            <SearchBar
+                isInSearchMode={isInSearchMode}
+                setIsInSearchMode={setIsInSearchMode}
+                collections={collections}
+                files={getNonTrashedUniqueUserFiles(files)}
+                setActiveCollection={setActiveCollection}
+                updateSearch={updateSearch}
+            />
+            {!isInSearchMode && <UploadButton openUploader={openUploader} />}
         </NavbarBase>
     );
 }
