e97647a270103b4e352e9b274716eb1f1836e792
Abhinav <abhinavk.grd@gmail.com>
2022-07-05 18:52:01 +0530

    update upload type option

--- src/components/Upload/UploadTypeSelector/option.tsx
+++ src/components/Upload/UploadTypeSelector/option.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import { Button } from '@mui/material';
+import ChevronRight from '@mui/icons-material/ChevronRight';
+import { FluidContainer } from 'components/Container';
 
 export function UploadTypeOption({ uploadFunc, Icon, uploadName }) {
     return (
         <Button
             size="large"
-            sx={{ justifyContent: 'flex-start' }}
             onClick={uploadFunc}
             color="secondary"
-            startIcon={<Icon />}>
-            {uploadName}
+            startIcon={<Icon />}
+            endIcon={<ChevronRight />}>
+            <FluidContainer>{uploadName}</FluidContainer>
         </Button>
     );
 }
