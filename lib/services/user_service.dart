e0cd9e02336184a13358e460e12ffdcbd5a2d856
Neeraj Gupta <254676+ua741@users.noreply.github.com>
2022-07-07 13:39:38 +0530

    Add api for deleting user

--- lib/services/user_service.dart
+++ lib/services/user_service.dart
@@ -209,6 +209,33 @@ class UserService {
     }
   }
 
+  Future<void> delete(BuildContext context) async {
+    final dialog = createProgressDialog(context, "Deleting account...");
+    await dialog.show();
+    try {
+      final response = await _dio.delete(
+        _config.getHttpEndpoint() + "/users/delete",
+        options: Options(
+          headers: {
+            "X-Auth-Token": _config.getToken(),
+          },
+        ),
+      );
+      if (response != null && response.statusCode == 200) {
+        // clear data
+        await Configuration.instance.logout();
+        await dialog.hide();
+        Navigator.of(context).popUntil((route) => route.isFirst);
+      } else {
+        throw Exception("delete action failed");
+      }
+    } catch (e) {
+      _logger.severe(e);
+      await dialog.hide();
+      showGenericErrorDialog(context);
+    }
+  }
+
   Future<void> verifyEmail(BuildContext context, String ott) async {
     final dialog = createProgressDialog(context, "Please wait...");
     await dialog.show();
