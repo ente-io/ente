22790f10b27e94fdd5f440a5c2df6ec520c4ff66
Neeraj Gupta <254676+ua741@users.noreply.github.com>
2022-07-08 14:21:01 +0530

    enable self-deletion for low-risk accounts

--- lib/ui/account/delete_account_page.dart
+++ lib/ui/account/delete_account_page.dart
@@ -1,7 +1,14 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_email_sender/flutter_email_sender.dart';
+import 'package:flutter_sodium/flutter_sodium.dart';
+import 'package:photos/core/configuration.dart';
+import 'package:photos/models/delete_account.dart';
+import 'package:photos/services/user_service.dart';
 import 'package:photos/ui/common/dialogs.dart';
 import 'package:photos/ui/common/gradient_button.dart';
-import 'package:photos/utils/toast_util.dart';
+import 'package:photos/utils/crypto_util.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class DeleteAccountPage extends StatelessWidget {
@@ -98,13 +105,13 @@ class DeleteAccountPage extends StatelessWidget {
                       backgroundColor: Colors.white,
                     ),
                     label: const Text(
-                      "No, delete account",
+                      "No, delete account :(",
                       style: TextStyle(
                         color: Colors.redAccent, // same for both themes
                       ),
                       textAlign: TextAlign.center,
                     ),
-                    onPressed: () async => {await _confirmDelete(context)},
+                    onPressed: () async => {await _initiateDelete(context)},
                     icon: const Icon(
                       Icons.no_accounts,
                       color: Colors.redAccent,
@@ -119,22 +126,117 @@ class DeleteAccountPage extends StatelessWidget {
     );
   }
 
-  Future<void> _confirmDelete(BuildContext context) async {
+  Future<void> _initiateDelete(BuildContext context) async {
+    final deleteChallengeResponse =
+        await UserService.instance.getDeleteChallenge(context);
+    if (deleteChallengeResponse == null) {
+      return;
+    }
+    if (deleteChallengeResponse.allowDelete) {
+      await _confirmAndDelete(context, deleteChallengeResponse);
+    } else {
+      await _requestEmailForDeletion(context);
+    }
+  }
+
+  Future<void> _confirmAndDelete(
+    BuildContext context,
+    DeleteChallengeResponse response,
+  ) async {
     final choice = await showChoiceDialog(
       context,
       'Are you sure you want to delete your account?',
       'Your uploaded data will be scheduled for deletion, and your account '
-          'will be permanently deleted. \n This action is not reversible.',
+          'will be permanently deleted. \n\nThis action is not reversible.',
       firstAction: 'Cancel',
       secondAction: 'Delete',
-      firstActionColor: Theme.of(context).buttonColor,
-      secondActionColor: Theme.of(context).colorScheme.onSurface,
+      firstActionColor: Theme.of(context).colorScheme.onSurface,
+      secondActionColor: Colors.red,
     );
     if (choice != DialogUserChoice.secondChoice) {
       return;
     }
-    Navigator.of(context, rootNavigator: true).pop('dialog');
-    showToast(context, "here we call delete");
-    // await UserService.instance.delete(context);
+    final decryptChallenge = CryptoUtil.openSealSync(
+      Sodium.base642bin(response.encryptedChallenge),
+      Sodium.base642bin(Configuration.instance.getKeyAttributes().publicKey),
+      Configuration.instance.getSecretKey(),
+    );
+    final challengeResponseStr = utf8.decode(decryptChallenge);
+    await UserService.instance.deleteAccount(context, challengeResponseStr);
+  }
+
+  Future<void> _requestEmailForDeletion(BuildContext context) async {
+    AlertDialog alert = AlertDialog(
+      title: const Text(
+        "Delete account",
+        style: TextStyle(
+          color: Colors.red,
+        ),
+      ),
+      content: RichText(
+        text: TextSpan(
+          children: [
+            const TextSpan(
+              text: "Please send an email to ",
+            ),
+            TextSpan(
+              text: "account-deletion@ente.io",
+              style: TextStyle(
+                color: Colors.orange[300],
+              ),
+            ),
+            const TextSpan(
+              text:
+                  " from your registered email address.\n\nYour request will be processed within 72 hours.",
+            ),
+          ],
+          style: TextStyle(
+            color: Theme.of(context).colorScheme.onSurface,
+            height: 1.5,
+            fontSize: 16,
+          ),
+        ),
+      ),
+      actions: [
+        TextButton(
+          child: const Text(
+            "Send email",
+            style: TextStyle(
+              color: Colors.red,
+            ),
+          ),
+          onPressed: () async {
+            Navigator.of(context, rootNavigator: true).pop('dialog');
+            try {
+              final Email email = Email(
+                recipients: ['account-deletion@ente.io'],
+                isHTML: false,
+              );
+              await FlutterEmailSender.send(email);
+            } catch (e) {
+              launch("mailto:account-deletion@ente.io");
+            }
+          },
+        ),
+        TextButton(
+          child: Text(
+            "Ok",
+            style: TextStyle(
+              color: Theme.of(context).colorScheme.onSurface,
+            ),
+          ),
+          onPressed: () {
+            Navigator.of(context, rootNavigator: true).pop('dialog');
+          },
+        ),
+      ],
+    );
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return alert;
+      },
+    );
   }
 }
--- lib/ui/settings/danger_section_widget.dart
+++ lib/ui/settings/danger_section_widget.dart
@@ -1,11 +1,11 @@
 import 'package:expandable/expandable.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_email_sender/flutter_email_sender.dart';
 import 'package:photos/services/user_service.dart';
+import 'package:photos/ui/account/delete_account_page.dart';
 import 'package:photos/ui/settings/common_settings.dart';
 import 'package:photos/ui/settings/settings_section_title.dart';
 import 'package:photos/ui/settings/settings_text_item.dart';
-import 'package:url_launcher/url_launcher.dart';
+import 'package:photos/utils/navigation_util.dart';
 
 class DangerSectionWidget extends StatefulWidget {
   const DangerSectionWidget({Key key}) : super(key: key);
@@ -40,7 +40,8 @@ class _DangerSectionWidgetState extends State<DangerSectionWidget> {
         GestureDetector(
           behavior: HitTestBehavior.translucent,
           onTap: () {
-            _onDeleteAccountTapped();
+            routeToPage(context, const DeleteAccountPage());
+            // _onDeleteAccountTapped();
           },
           child: const SettingsTextItem(
             text: "Delete account",
@@ -48,81 +49,6 @@ class _DangerSectionWidgetState extends State<DangerSectionWidget> {
           ),
         ),
       ],
-    );
-  }
-
-  Future<void> _onDeleteAccountTapped() async {
-    AlertDialog alert = AlertDialog(
-      title: const Text(
-        "Delete account",
-        style: TextStyle(
-          color: Colors.red,
-        ),
-      ),
-      content: RichText(
-        text: TextSpan(
-          children: [
-            const TextSpan(
-              text: "Please send an email to ",
-            ),
-            TextSpan(
-              text: "account-deletion@ente.io",
-              style: TextStyle(
-                color: Colors.orange[300],
-              ),
-            ),
-            const TextSpan(
-              text:
-                  " from your registered email address.\n\nYour request will be processed within 72 hours.",
-            ),
-          ],
-          style: TextStyle(
-            color: Theme.of(context).colorScheme.onSurface,
-            height: 1.5,
-            fontSize: 16,
-          ),
-        ),
-      ),
-      actions: [
-        TextButton(
-          child: const Text(
-            "Send email",
-            style: TextStyle(
-              color: Colors.red,
-            ),
-          ),
-          onPressed: () async {
-            Navigator.of(context, rootNavigator: true).pop('dialog');
-            try {
-              final Email email = Email(
-                recipients: ['account-deletion@ente.io'],
-                isHTML: false,
-              );
-              await FlutterEmailSender.send(email);
-            } catch (e) {
-              launch("mailto:account-deletion@ente.io");
-            }
-          },
-        ),
-        TextButton(
-          child: Text(
-            "Ok",
-            style: TextStyle(
-              color: Theme.of(context).colorScheme.onSurface,
-            ),
-          ),
-          onPressed: () {
-            Navigator.of(context, rootNavigator: true).pop('dialog');
-          },
-        ),
-      ],
-    );
-
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return alert;
-      },
     );
   }
 
