90eaa42038bf99aa2980ce5071f13ea361f5103b
Abhinav <abhinavk.grd@gmail.com>
2022-07-11 14:45:14 +0530

    remove redundant type

--- src/types/index.ts
+++ src/types/index.ts
@@ -32,12 +32,6 @@ export const FILE_PATH_KEYS: {
     [FILE_PATH_TYPE.FILES]: 'filePaths',
 };
 
-export interface KeyAttributes {
-    encryptedData: string;
-    key: string;
-    nonce: string;
-}
-
 export interface SafeStorageStoreType {
     encryptionKey: string;
 }
