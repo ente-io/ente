ed22698aa4ffb58d3e1187093fe2d99e951d5d58
Abhinav <abhinavk.grd@gmail.com>
2022-07-13 19:54:44 +0530

    fix configs

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,5 @@
-const PROD_HOST_URL: string = 'next://app';
-const RENDERER_OUTPUT_DIR: string = './src/renderer/out';
+const PROD_HOST_URL: string = 'ente://app';
+const RENDERER_OUTPUT_DIR: string = './ui/out';
 
 const FILE_STREAM_CHUNK_SIZE: number = 4 * 1024 * 1024;
 
