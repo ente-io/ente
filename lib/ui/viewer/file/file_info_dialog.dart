207bb34a6fb4dea9b1b3c0b9fd3e5135b4a972c1
ashilkn <ashilkn99@gmail.com>
2022-07-13 07:58:43 +0530

    removed dead code + prefer const

--- lib/ui/viewer/file/file_info_dialog.dart
+++ lib/ui/viewer/file/file_info_dialog.dart
@@ -39,7 +39,6 @@ class _FileInfoWidgetState extends State<FileInfoWidget> {
   };
 
   bool _isImage = false;
-  Color infoColor;
 
   @override
   void initState() {
@@ -62,8 +61,6 @@ class _FileInfoWidgetState extends State<FileInfoWidget> {
     final dateTime = DateTime.fromMicrosecondsSinceEpoch(file.creationTime);
     final dateTimeForUpdationTime =
         DateTime.fromMicrosecondsSinceEpoch(file.updationTime);
-    infoColor =
-        Theme.of(context).colorScheme.onSurface.withOpacity(0.85); //remove
 
     if (_isImage && _exif != null) {
       _generateExifForDetails(_exif);
@@ -213,10 +210,11 @@ class _FileInfoWidgetState extends State<FileInfoWidget> {
                     "  " +
                     dateTimeForUpdationTime.timeZoneName,
                 style: Theme.of(context).textTheme.bodyText2.copyWith(
-                    color: Theme.of(context)
-                        .colorScheme
-                        .defaultTextColor
-                        .withOpacity(0.5)),
+                      color: Theme.of(context)
+                          .colorScheme
+                          .defaultTextColor
+                          .withOpacity(0.5),
+                    ),
               ),
             )
           : const SizedBox.shrink(),
@@ -232,151 +230,6 @@ class _FileInfoWidgetState extends State<FileInfoWidget> {
             )
     ];
 
-    var items = <Widget>[
-      //remove
-      Row(
-        children: [
-          Icon(Icons.calendar_today_outlined, color: infoColor),
-          const SizedBox(height: 8),
-          Text(
-            getFormattedTime(
-              DateTime.fromMicrosecondsSinceEpoch(file.creationTime),
-            ),
-            style: TextStyle(color: infoColor),
-          ),
-        ],
-      ),
-      const SizedBox(height: 12),
-      Row(
-        children: [
-          Icon(Icons.folder_outlined, color: infoColor),
-          const Padding(padding: EdgeInsets.all(4)),
-          Text(
-            file.deviceFolder ??
-                CollectionsService.instance
-                    .getCollectionByID(file.collectionID)
-                    .name,
-            style: TextStyle(color: infoColor),
-          ),
-        ],
-      ),
-      const SizedBox(height: 12),
-    ];
-    items.addAll(
-      [
-        Row(
-          children: [
-            Icon(Icons.sd_storage_outlined, color: infoColor),
-            const Padding(padding: EdgeInsets.all(4)),
-            _getFileSize(),
-          ],
-        ),
-        const SizedBox(height: 12),
-      ],
-    );
-    if (file.localID != null && !_isImage) {
-      //remove
-      items.addAll(
-        [
-          Row(
-            children: [
-              Icon(Icons.timer_outlined, color: infoColor),
-              const Padding(padding: EdgeInsets.all(4)),
-              FutureBuilder(
-                future: file.getAsset(),
-                builder: (context, snapshot) {
-                  if (snapshot.hasData) {
-                    return Text(
-                      snapshot.data.videoDuration.toString().split(".")[0],
-                      style: TextStyle(color: infoColor),
-                    );
-                  } else {
-                    return Center(
-                      child: SizedBox.fromSize(
-                        size: const Size.square(24),
-                        child: const CupertinoActivityIndicator(
-                          radius: 8,
-                        ),
-                      ),
-                    );
-                  }
-                },
-              ),
-            ],
-          ),
-          const SizedBox(height: 12),
-        ],
-      );
-    }
-    if (_isImage && _exif != null) {
-      //remove
-      // items.add(_getExifWidgets(_exif));
-      _generateExifForDetails(_exif);
-    }
-    if (file.uploadedFileID != null && file.updationTime != null) {
-      //remove
-      items.addAll(
-        [
-          Row(
-            children: [
-              Icon(Icons.cloud_upload_outlined, color: infoColor),
-              const Padding(padding: EdgeInsets.all(4)),
-              Text(
-                getFormattedTime(
-                  DateTime.fromMicrosecondsSinceEpoch(file.updationTime),
-                ),
-                style: TextStyle(color: infoColor),
-              ),
-            ],
-          ),
-        ],
-      );
-    }
-    items.add(
-      const SizedBox(height: 12),
-    );
-    items.add(
-      Row(
-        mainAxisAlignment:
-            _isImage ? MainAxisAlignment.spaceBetween : MainAxisAlignment.end,
-        children: _getActions(),
-      ),
-    );
-
-    Widget titleContent;
-    if (file.uploadedFileID == null ||
-        file.ownerID != Configuration.instance.getUserID()) {
-      titleContent = Text(file.getDisplayName());
-    } else {
-      titleContent = InkWell(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Flexible(
-              child: Text(
-                file.getDisplayName(),
-                style: Theme.of(context).textTheme.headline5,
-              ),
-            ),
-            const SizedBox(width: 16),
-            Icon(Icons.edit, color: infoColor),
-          ],
-        ),
-        onTap: () async {
-          await editFilename(context, file);
-          setState(() {});
-        },
-      );
-    }
-
-    // return AlertDialog(
-    //   title: titleContent,
-    //   content: SingleChildScrollView(
-    //     child: ListBody(
-    //       children: items,
-    //     ),
-    //   ),
-    // );
     return Column(
       mainAxisSize: MainAxisSize.min,
       children: [
@@ -407,115 +260,6 @@ class _FileInfoWidgetState extends State<FileInfoWidget> {
         ...listTiles
       ],
     );
-  }
-
-  List<Widget> _getActions() {
-    final List<Widget> actions = [];
-    if (_isImage) {
-      if (_exif == null) {
-        actions.add(
-          TextButton(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Center(
-                  child: SizedBox.fromSize(
-                    size: const Size.square(24),
-                    child: const CupertinoActivityIndicator(
-                      radius: 8,
-                    ),
-                  ),
-                ),
-                const Padding(padding: EdgeInsets.all(4)),
-                Text(
-                  "EXIF",
-                  style: TextStyle(color: infoColor),
-                ),
-              ],
-            ),
-            onPressed: () {
-              showDialog(
-                context: context,
-                builder: (BuildContext context) {
-                  return ExifInfoDialog(widget.file);
-                },
-                barrierColor: Colors.black87,
-              );
-            },
-          ),
-        );
-      } else if (_exif.isNotEmpty) {
-        actions.add(
-          TextButton(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Icon(Icons.feed_outlined, color: infoColor),
-                const Padding(padding: EdgeInsets.all(4)),
-                Text(
-                  "View raw EXIF",
-                  style: TextStyle(color: infoColor),
-                ),
-              ],
-            ),
-            onPressed: () {
-              showDialog(
-                context: context,
-                builder: (BuildContext context) {
-                  return ExifInfoDialog(widget.file);
-                },
-                barrierColor: Colors.black87,
-              );
-            },
-          ),
-        );
-      } else {
-        actions.add(
-          TextButton(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Icon(
-                  Icons.feed_outlined,
-                  color: Theme.of(context)
-                      .colorScheme
-                      .defaultTextColor
-                      .withOpacity(0.5),
-                ),
-                const Padding(padding: EdgeInsets.all(4)),
-                Text(
-                  "No exif",
-                  style: TextStyle(
-                    color: Theme.of(context)
-                        .colorScheme
-                        .defaultTextColor
-                        .withOpacity(0.5),
-                  ),
-                ),
-              ],
-            ),
-            onPressed: () {
-              showShortToast(context, "This image has no exif data");
-            },
-          ),
-        );
-      }
-    }
-    actions.add(
-      //remove
-      TextButton(
-        child: Text(
-          "Close",
-          style: TextStyle(
-            color: infoColor,
-          ),
-        ),
-        onPressed: () {
-          Navigator.of(context, rootNavigator: true).pop("dialog");
-        },
-      ),
-    );
-    return actions;
   }
 
   _generateExifForDetails(Map<String, IfdTag> exif) {
@@ -559,126 +303,6 @@ class _FileInfoWidgetState extends State<FileInfoWidget> {
     if (exif["EXIF ISOSpeedRatings"] != null) {
       _exifData['ISO'] = exif["EXIF ISOSpeedRatings"].toString();
     }
-  }
-
-  Widget _getExifWidgets(Map<String, IfdTag> exif) {
-    final focalLength = exif["EXIF FocalLength"] != null
-        ? (exif["EXIF FocalLength"].values.toList()[0] as Ratio).numerator /
-            (exif["EXIF FocalLength"].values.toList()[0] as Ratio)
-                .denominator //to remove
-        : null;
-    final fNumber = exif["EXIF FNumber"] != null
-        ? (exif["EXIF FNumber"].values.toList()[0] as Ratio).numerator /
-            (exif["EXIF FNumber"].values.toList()[0] as Ratio)
-                .denominator //to remove
-        : null;
-
-    final List<Widget> children = [];
-    if (exif["EXIF ExifImageWidth"] != null &&
-        exif["EXIF ExifImageLength"] != null) {
-      children.addAll([
-        Row(
-          children: [
-            Icon(Icons.photo_size_select_actual_outlined, color: infoColor),
-            const Padding(padding: EdgeInsets.all(4)),
-            Text(
-              exif["EXIF ExifImageWidth"].toString() +
-                  " x " +
-                  exif["EXIF ExifImageLength"].toString(),
-              style: TextStyle(color: infoColor),
-            ),
-          ],
-        ),
-        const Padding(padding: EdgeInsets.all(6)),
-      ]);
-    } else if (exif["Image ImageWidth"] != null &&
-        exif["Image ImageLength"] != null) {
-      children.addAll([
-        Row(
-          children: [
-            Icon(Icons.photo_size_select_actual_outlined, color: infoColor),
-            const Padding(padding: EdgeInsets.all(4)),
-            Text(
-              exif["Image ImageWidth"].toString() +
-                  " x " +
-                  exif["Image ImageLength"].toString(),
-              style: TextStyle(color: infoColor),
-            ),
-          ],
-        ),
-        const Padding(padding: EdgeInsets.all(6)),
-      ]);
-    }
-    if (exif["Image Make"] != null && exif["Image Model"] != null) {
-      children.addAll(
-        [
-          Row(
-            children: [
-              Icon(Icons.camera_outlined, color: infoColor),
-              const Padding(padding: EdgeInsets.all(4)),
-              Flexible(
-                child: Text(
-                  exif["Image Make"].toString() +
-                      " " +
-                      exif["Image Model"].toString(),
-                  style: TextStyle(color: infoColor),
-                  overflow: TextOverflow.clip,
-                ),
-              ),
-            ],
-          ),
-          const Padding(padding: EdgeInsets.all(6)),
-        ],
-      );
-    }
-    if (fNumber != null) {
-      children.addAll([
-        Row(
-          children: [
-            Icon(CupertinoIcons.f_cursive, color: infoColor),
-            const Padding(padding: EdgeInsets.all(4)),
-            Text(
-              fNumber.toString(),
-              style: TextStyle(color: infoColor),
-            ),
-          ],
-        ),
-        const Padding(padding: EdgeInsets.all(6)),
-      ]);
-    }
-    if (focalLength != null) {
-      children.addAll([
-        Row(
-          children: [
-            Icon(Icons.center_focus_strong_outlined, color: infoColor),
-            const Padding(padding: EdgeInsets.all(4)),
-            Text(
-              focalLength.toString() + " mm",
-              style: TextStyle(color: infoColor),
-            ),
-          ],
-        ),
-        const Padding(padding: EdgeInsets.all(6)),
-      ]);
-    }
-    if (exif["EXIF ExposureTime"] != null) {
-      children.addAll([
-        Row(
-          children: [
-            Icon(Icons.shutter_speed, color: infoColor),
-            const Padding(padding: EdgeInsets.all(4)),
-            Text(
-              exif["EXIF ExposureTime"].toString(),
-              style: TextStyle(color: infoColor),
-            ),
-          ],
-        ),
-        const Padding(padding: EdgeInsets.all(6)),
-      ]);
-    }
-    return Column(
-      children: children,
-    );
   }
 
   Widget _getFileSize() {
@@ -798,55 +422,6 @@ class RawExifButton extends StatelessWidget {
       exifStatus = Status.noExif;
     }
     return GestureDetector(
-      child: Container(
-        height: 40,
-        width: 140,
-        decoration: BoxDecoration(
-          color: Theme.of(context)
-              .colorScheme
-              .inverseBackgroundColor
-              .withOpacity(0.12),
-          borderRadius: const BorderRadius.all(
-            Radius.circular(20),
-          ),
-        ),
-        child: Center(
-          child: exifStatus == Status.loading
-              ? Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    CupertinoActivityIndicator(
-                      radius: 8,
-                    ),
-                    const SizedBox(
-                      width: 8,
-                    ),
-                    Text('EXIF')
-                  ],
-                )
-              : exifStatus == Status.exifIsAvailable
-                  ? Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(Icons.feed_outlined),
-                        SizedBox(
-                          width: 8,
-                        ),
-                        Text('Raw EXIF'),
-                      ],
-                    )
-                  : Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(Icons.feed_outlined),
-                        SizedBox(
-                          width: 8,
-                        ),
-                        Text('No EXIF'),
-                      ],
-                    ),
-        ),
-      ),
       onTap:
           exifStatus == Status.loading || exifStatus == Status.exifIsAvailable
               ? () {
@@ -863,6 +438,55 @@ class RawExifButton extends StatelessWidget {
                       showShortToast(context, "This image has no exif data");
                     }
                   : null,
+      child: Container(
+        height: 40,
+        width: 140,
+        decoration: BoxDecoration(
+          color: Theme.of(context)
+              .colorScheme
+              .inverseBackgroundColor
+              .withOpacity(0.12),
+          borderRadius: const BorderRadius.all(
+            Radius.circular(20),
+          ),
+        ),
+        child: Center(
+          child: exifStatus == Status.loading
+              ? Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    CupertinoActivityIndicator(
+                      radius: 8,
+                    ),
+                    SizedBox(
+                      width: 8,
+                    ),
+                    Text('EXIF')
+                  ],
+                )
+              : exifStatus == Status.exifIsAvailable
+                  ? Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        Icon(Icons.feed_outlined),
+                        SizedBox(
+                          width: 8,
+                        ),
+                        Text('Raw EXIF'),
+                      ],
+                    )
+                  : Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        Icon(Icons.feed_outlined),
+                        SizedBox(
+                          width: 8,
+                        ),
+                        Text('No EXIF'),
+                      ],
+                    ),
+        ),
+      ),
     );
   }
 }
