ac0011f83b5a60c9596b117f4894c35bbd6bc34e
Neeraj Gupta <254676+ua741@users.noreply.github.com>
2022-07-19 12:09:45 +0530

    Add base EnteFile and use cacheKey for thumbnailCache

--- lib/core/cache/thumbnail_cache.dart
+++ lib/core/cache/thumbnail_cache.dart
@@ -2,38 +2,38 @@ import 'dart:typed_data';
 
 import 'package:photos/core/cache/lru_map.dart';
 import 'package:photos/core/constants.dart';
-import 'package:photos/models/file.dart';
+import 'package:photos/models/ente_file.dart';
 
 class ThumbnailLruCache {
   static final LRUMap<String, Uint8List> _map = LRUMap(1000);
 
-  static Uint8List get(File photo, [int size]) {
+  static Uint8List get(EnteFile photo, [int size]) {
     return _map.get(
-      photo.generatedID.toString() +
+      photo.cacheKey() +
           "_" +
           (size != null ? size.toString() : kThumbnailLargeSize.toString()),
     );
   }
 
   static void put(
-    File photo,
+    EnteFile photo,
     Uint8List imageData, [
     int size,
   ]) {
     _map.put(
-      photo.generatedID.toString() +
+      photo.cacheKey() +
           "_" +
           (size != null ? size.toString() : kThumbnailLargeSize.toString()),
       imageData,
     );
   }
 
-  static void clearCache(File file) {
+  static void clearCache(EnteFile file) {
     _map.remove(
-      file.generatedID.toString() + "_" + kThumbnailLargeSize.toString(),
+      file.cacheKey() + "_" + kThumbnailLargeSize.toString(),
     );
     _map.remove(
-      file.generatedID.toString() + "_" + kThumbnailSmallSize.toString(),
+      file.cacheKey() + "_" + kThumbnailSmallSize.toString(),
     );
   }
 }
