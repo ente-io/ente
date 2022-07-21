cb56fc0e6a3570b248bfa2dc204a87da917149cb
ashilkn <ashilkn99@gmail.com>
2022-07-21 10:59:57 +0530

    Widget structure and logic for search suggestion + p1 search for collections

--- lib/ui/viewer/search/SearchResultsSuggestions.dart
+++ lib/ui/viewer/search/SearchResultsSuggestions.dart
@@ -1,6 +1,9 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/widgets.dart';
+import 'package:photos/models/collection.dart';
+import 'package:photos/services/collections_service.dart';
 
 class SearchResultsSuggestions extends StatefulWidget {
   final Map<String, Set> collectionIDs;
@@ -15,15 +18,61 @@ class SearchResultsSuggestions extends StatefulWidget {
 class _SearchResultsSuggestionsState extends State<SearchResultsSuggestions> {
   @override
   Widget build(BuildContext context) {
-    return CollectionSuggestions();
+    List<Widget> p1suggestions = [];
+    p1suggestions = CollectionSuggestions(widget.collectionIDs)
+        .getSuggestions(); //add other p1 suggestions to this
+    return Container(
+      constraints:
+          BoxConstraints(maxHeight: MediaQuery.of(context).size.height * 0.7),
+      child: ListView.builder(
+        itemCount: p1suggestions.length,
+        itemBuilder: (context, index) {
+          return p1suggestions[index];
+        },
+      ),
+    );
   }
 }
 
-class CollectionSuggestions extends StatelessWidget {
-  const CollectionSuggestions({Key key}) : super(key: key);
+class CollectionSuggestions {
+  final Map<String, Set> collectionIDs;
+  const CollectionSuggestions(this.collectionIDs);
 
-  @override
-  Widget build(BuildContext context) {
-    return Text('test');
+  List<Widget> getSuggestions() {
+    List<int> p1IDs = collectionIDs['p1'].toList();
+    List<int> p2IDs = collectionIDs['p2'].toList();
+    List<int> p3IDs = collectionIDs['p3'].toList();
+    List<Widget> collectionsP1 = [];
+    List<Widget> collectionsP2 = [];
+    List<Widget> collectionsP3 = [];
+    Collection collection =
+        CollectionsService.instance.getCollectionByID(p1IDs[0]);
+    p1IDs.forEach(
+      (element) {
+        Collection collection =
+            CollectionsService.instance.getCollectionByID(element);
+        collectionsP1.add(
+          Row(
+            children: [
+              Column(
+                children: [
+                  const Text('Album'),
+                  Text(collection.name),
+                  Text('10 memories'),
+                ],
+              ),
+              Row(
+                children: [
+                  Icon(Icons.access_alarms),
+                  Icon(Icons.access_alarms),
+                  Icon(Icons.access_alarms),
+                ],
+              )
+            ],
+          ),
+        );
+      },
+    );
+    return collectionsP1;
   }
 }
