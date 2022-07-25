f6addb0eb7d8a34a3b3533f44bf25ea315119124
ashilkn <ashilkn99@gmail.com>
2022-07-25 19:38:11 +0530

    changed searchResultsSuggestion to a stateless widget

--- lib/ui/viewer/search/search_results_suggestions.dart
+++ lib/ui/viewer/search/search_results_suggestions.dart
@@ -3,21 +3,14 @@ import 'package:flutter/widgets.dart';
 import 'package:photos/models/collection.dart';
 import 'package:photos/ui/viewer/search/collection_suggestions.dart';
 
-class SearchResultsSuggestions extends StatefulWidget {
+class SearchResultsSuggestions extends StatelessWidget {
   final List<Collection> collections;
   const SearchResultsSuggestions({Key key, this.collections}) : super(key: key);
 
   @override
-  State<SearchResultsSuggestions> createState() =>
-      _SearchResultsSuggestionsState();
-}
-
-class _SearchResultsSuggestionsState extends State<SearchResultsSuggestions> {
-  @override
   Widget build(BuildContext context) {
     List<Widget> suggestions = [];
-    suggestions =
-        CollectionSuggestions(widget.collections, context).getSuggestions();
+    suggestions = CollectionSuggestions(collections, context).getSuggestions();
     return Container(
       constraints:
           BoxConstraints(maxHeight: MediaQuery.of(context).size.height * 0.6),
