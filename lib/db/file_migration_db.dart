b1ac8f4f3abb6b9f7a4096131efde7b8bc07a2bc
Neeraj Gupta <254676+ua741@users.noreply.github.com>
2022-07-29 11:34:00 +0530

    Add reason in re-upload tracker table

--- lib/db/file_migration_db.dart
+++ lib/db/file_migration_db.dart
@@ -1,28 +1,48 @@
 import 'dart:io';
 
+import 'package:flutter/foundation.dart';
 import 'package:logging/logging.dart';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
+import 'package:sqflite_migration/sqflite_migration.dart';
 
 class FilesMigrationDB {
   static const _databaseName = "ente.files_migration.db";
-  static const _databaseVersion = 1;
   static final Logger _logger = Logger((FilesMigrationDB).toString());
+
   static const tableName = 're_upload_tracker';
-
   static const columnLocalID = 'local_id';
+  static const _columnReason = 'reason';
 
-  Future _onCreate(Database db, int version) async {
-    await db.execute(
-      '''
-        CREATE TABLE $tableName (
-        $columnLocalID TEXT NOT NULL,
-          UNIQUE($columnLocalID)
-        );
+  // SQL code to create the database table
+  static List<String> _createTable() {
+    return [
+      ''' 
+      CREATE TABLE $tableName (
+      $columnLocalID TEXT NOT NULL,
+      UNIQUE($columnLocalID)
+      ); 
       ''',
-    );
+    ];
   }
+
+  static List<String> addReasonColumn() {
+    return [
+      '''
+        ALTER TABLE $tableName ADD COLUMN $_columnReason TEXT;
+      ''',
+    ];
+  }
+
+  static final initializationScript = [..._createTable()];
+  static final migrationScripts = [
+    ...addReasonColumn(),
+  ];
+  final dbConfig = MigrationConfig(
+    initializationScript: initializationScript,
+    migrationScripts: migrationScripts,
+  );
 
   FilesMigrationDB._privateConstructor();
 
@@ -42,11 +62,8 @@ class FilesMigrationDB {
   Future<Database> _initDatabase() async {
     Directory documentsDirectory = await getApplicationDocumentsDirectory();
     String path = join(documentsDirectory.path, _databaseName);
-    return await openDatabase(
-      path,
-      version: _databaseVersion,
-      onCreate: _onCreate,
-    );
+    debugPrint("DB path " + path);
+    return await openDatabaseWithMigration(path, dbConfig);
   }
 
   Future<void> clearTable() async {
