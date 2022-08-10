7fd2843d418dd42992ea54e2077d2991d8dd7809
Abhinav <abhinavk.grd@gmail.com>
2022-08-10 13:21:26 +0530

    add lru cache eviction strategy

--- main/utils/cache.ts
+++ main/utils/cache.ts
@@ -1,8 +1,27 @@
 import { ipcRenderer } from 'electron/renderer';
 import path from 'path';
-import { readFile, writeFile, existsSync, mkdir } from 'promise-fs';
+import {
+    readFile,
+    writeFile,
+    existsSync,
+    mkdir,
+    readdir,
+    stat,
+    utimes,
+    close,
+    open,
+    unlink,
+    rmdir,
+} from 'promise-fs';
+import getFolderSize from 'get-folder-size';
+
+interface LeastRecentlyUsedResult {
+    atime: Date;
+    path: string;
+}
 
 const CACHE_DIR = 'ente';
+const MAX_CACHE_SIZE = 1000 * 1000 * 1000; // 1GB
 
 const getCacheDir = async () => {
     const systemCacheDir = await ipcRenderer.invoke('get-path', 'cache');
@@ -23,6 +42,7 @@ class DiskCache {
 
     async put(cacheKey: string, response: Response): Promise<void> {
         const cachePath = path.join(this.cacheBucketDir, cacheKey);
+        evictLeastRecentlyUsed(this.cacheBucketDir, MAX_CACHE_SIZE);
         await writeFile(
             cachePath,
             new Uint8Array(await response.arrayBuffer())
@@ -32,9 +52,62 @@ class DiskCache {
     async match(cacheKey: string): Promise<Response> {
         const cachePath = path.join(this.cacheBucketDir, cacheKey);
         if (existsSync(cachePath)) {
+            touch(cachePath);
             return new Response(await readFile(cachePath));
         } else {
             return undefined;
         }
     }
+}
+
+async function touch(path: string) {
+    const time = new Date();
+    try {
+        await utimes(path, time, time);
+    } catch (err) {
+        await close(await open(path, 'w'));
+    }
+}
+
+async function evictLeastRecentlyUsed(cacheDir: string, maxSize: number) {
+    const folderSizeInfo = await getFolderSize(cacheDir);
+    if (folderSizeInfo.errors) {
+        throw folderSizeInfo.errors;
+    }
+    if (folderSizeInfo.size >= maxSize) {
+        // find least recently used file
+        const leastRecentlyUsed = await findLeastRecentlyUsed(cacheDir);
+        // and delete it
+        const { dir } = path.parse(leastRecentlyUsed.path);
+        await unlink(leastRecentlyUsed.path);
+        await rmdir(dir);
+        evictLeastRecentlyUsed(cacheDir, maxSize);
+    }
+}
+
+async function findLeastRecentlyUsed(
+    dir: string,
+    result?: LeastRecentlyUsedResult
+): Promise<LeastRecentlyUsedResult> {
+    const files = await readdir(dir);
+    result = result || { atime: new Date(), path: '' };
+
+    files.forEach(async (file) => {
+        const newBase = path.join(dir, file);
+        const stats = await stat(newBase);
+        if (stats.isDirectory()) {
+            result = await findLeastRecentlyUsed(newBase, result);
+        } else {
+            const { atime } = await stat(newBase);
+
+            if (atime < result.atime) {
+                result = {
+                    atime,
+                    path: newBase,
+                };
+            }
+        }
+    });
+
+    return result;
 }
