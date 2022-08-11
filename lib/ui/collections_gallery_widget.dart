f7c66d9124ef861bbe3acd1313d7c948751f350f
vishnukvmd <vishnu@ente.io>
2022-08-11 17:38:00 +0530

    Extract hidden collection button to a separate widget

--- lib/ui/collections_gallery_widget.dart
+++ lib/ui/collections_gallery_widget.dart
@@ -326,95 +326,7 @@ class _CollectionsGalleryWidgetState extends State<CollectionsGalleryWidget>
                     },
                   ),
                   const SizedBox(height: 12),
-                  OutlinedButton(
-                    style: OutlinedButton.styleFrom(
-                      backgroundColor: Theme.of(context).backgroundColor,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8),
-                      ),
-                      padding: const EdgeInsets.all(0),
-                      side: BorderSide(
-                        width: 0.5,
-                        color:
-                            Theme.of(context).iconTheme.color.withOpacity(0.24),
-                      ),
-                    ),
-                    child: SizedBox(
-                      height: 48,
-                      width: double.infinity,
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 16),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Row(
-                              children: [
-                                Icon(
-                                  Icons.visibility_off,
-                                  color: Theme.of(context).iconTheme.color,
-                                ),
-                                const Padding(padding: EdgeInsets.all(6)),
-                                FutureBuilder<int>(
-                                  future:
-                                      FilesDB.instance.fileCountWithVisibility(
-                                    kVisibilityArchive,
-                                    Configuration.instance.getUserID(),
-                                  ),
-                                  builder: (context, snapshot) {
-                                    if (snapshot.hasData && snapshot.data > 0) {
-                                      return RichText(
-                                        text: TextSpan(
-                                          style: trashAndHiddenTextStyle,
-                                          children: [
-                                            TextSpan(
-                                              text: "Hidden",
-                                              style: Theme.of(context)
-                                                  .textTheme
-                                                  .subtitle1,
-                                            ),
-                                            const TextSpan(text: "  \u2022  "),
-                                            TextSpan(
-                                              text: snapshot.data.toString(),
-                                            ),
-                                            //need to query in db and bring this value
-                                          ],
-                                        ),
-                                      );
-                                    } else {
-                                      return RichText(
-                                        text: TextSpan(
-                                          style: trashAndHiddenTextStyle,
-                                          children: [
-                                            TextSpan(
-                                              text: "Hidden",
-                                              style: Theme.of(context)
-                                                  .textTheme
-                                                  .subtitle1,
-                                            ),
-                                            //need to query in db and bring this value
-                                          ],
-                                        ),
-                                      );
-                                    }
-                                  },
-                                ),
-                              ],
-                            ),
-                            Icon(
-                              Icons.chevron_right,
-                              color: Theme.of(context).iconTheme.color,
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                    onPressed: () async {
-                      routeToPage(
-                        context,
-                        ArchivePage(),
-                      );
-                    },
-                  ),
+                  HiddenCollectionsButtonWidget(trashAndHiddenTextStyle),
                 ],
               ),
             ),
@@ -546,6 +458,102 @@ class _CollectionsGalleryWidgetState extends State<CollectionsGalleryWidget>
 
   @override
   bool get wantKeepAlive => true;
+}
+
+class HiddenCollectionsButtonWidget extends StatelessWidget {
+  final TextStyle textStyle;
+
+  const HiddenCollectionsButtonWidget(
+    this.textStyle, {
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return OutlinedButton(
+      style: OutlinedButton.styleFrom(
+        backgroundColor: Theme.of(context).backgroundColor,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8),
+        ),
+        padding: const EdgeInsets.all(0),
+        side: BorderSide(
+          width: 0.5,
+          color: Theme.of(context).iconTheme.color.withOpacity(0.24),
+        ),
+      ),
+      child: SizedBox(
+        height: 48,
+        width: double.infinity,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: [
+                  Icon(
+                    Icons.visibility_off,
+                    color: Theme.of(context).iconTheme.color,
+                  ),
+                  const Padding(padding: EdgeInsets.all(6)),
+                  FutureBuilder<int>(
+                    future: FilesDB.instance.fileCountWithVisibility(
+                      kVisibilityArchive,
+                      Configuration.instance.getUserID(),
+                    ),
+                    builder: (context, snapshot) {
+                      if (snapshot.hasData && snapshot.data > 0) {
+                        return RichText(
+                          text: TextSpan(
+                            style: textStyle,
+                            children: [
+                              TextSpan(
+                                text: "Hidden",
+                                style: Theme.of(context).textTheme.subtitle1,
+                              ),
+                              const TextSpan(text: "  \u2022  "),
+                              TextSpan(
+                                text: snapshot.data.toString(),
+                              ),
+                              //need to query in db and bring this value
+                            ],
+                          ),
+                        );
+                      } else {
+                        return RichText(
+                          text: TextSpan(
+                            style: textStyle,
+                            children: [
+                              TextSpan(
+                                text: "Hidden",
+                                style: Theme.of(context).textTheme.subtitle1,
+                              ),
+                              //need to query in db and bring this value
+                            ],
+                          ),
+                        );
+                      }
+                    },
+                  ),
+                ],
+              ),
+              Icon(
+                Icons.chevron_right,
+                color: Theme.of(context).iconTheme.color,
+              ),
+            ],
+          ),
+        ),
+      ),
+      onPressed: () async {
+        routeToPage(
+          context,
+          ArchivePage(),
+        );
+      },
+    );
+  }
 }
 
 class DeviceFolderIcon extends StatelessWidget {
