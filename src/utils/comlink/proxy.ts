e25618feab3ad9ab9a0adf3899066da7a76c72eb
Abhinav <abhinavk.grd@gmail.com>
2022-08-15 17:26:18 +0530

    fix serialization and deserialization for undefined params

--- src/utils/comlink/proxy.ts
+++ src/utils/comlink/proxy.ts
@@ -1,7 +1,11 @@
 export function serializeResponse(response: Response) {
-    return response?.arrayBuffer();
+    if (response) {
+        return response.arrayBuffer();
+    }
 }
 
 export function deserializeToResponse(arrayBuffer: ArrayBuffer) {
-    return new Response(arrayBuffer);
+    if (arrayBuffer) {
+        return new Response(arrayBuffer);
+    }
 }
