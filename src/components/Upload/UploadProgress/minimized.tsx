6cc326384fe60e3690760b457cfc16b4664ad98a
Abhinav <abhinavk.grd@gmail.com>
2022-08-16 13:00:32 +0530

    don't need to use expanded prop to show Snackbar as its already managed by UploadProgress component

--- src/components/Upload/UploadProgress/minimized.tsx
+++ src/components/Upload/UploadProgress/minimized.tsx
@@ -1,12 +1,10 @@
 import { Snackbar, Paper } from '@mui/material';
-import UploadProgressContext from 'contexts/uploadProgress';
-import React, { useContext } from 'react';
+import React from 'react';
 import { UploadProgressHeader } from './header';
 export function MinimizedUploadProgress() {
-    const { open } = useContext(UploadProgressContext);
     return (
         <Snackbar
-            open={open}
+            open
             anchorOrigin={{
                 horizontal: 'right',
                 vertical: 'bottom',
