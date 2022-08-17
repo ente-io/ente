52c6cb0fe7b99b6e5f48cb07ced9267c9fbd40d3
ashilkn <ashilkn99@gmail.com>
2022-08-17 16:02:57 +0530

    made MonthSearchResult and MonthWithNumber types + made an allMonths list

---
+++ lib/data/months.dart
@@ -0,0 +1,16 @@
+import 'package:photos/models/search/month_search_result.dart';
+
+List<MonthWihMonthNumber> allMonths = [
+  MonthWihMonthNumber('January', 1),
+  MonthWihMonthNumber('February', 2),
+  MonthWihMonthNumber('March', 3),
+  MonthWihMonthNumber('April', 4),
+  MonthWihMonthNumber('May', 5),
+  MonthWihMonthNumber('June', 6),
+  MonthWihMonthNumber('July', 7),
+  MonthWihMonthNumber('August', 8),
+  MonthWihMonthNumber('September', 9),
+  MonthWihMonthNumber('October', 10),
+  MonthWihMonthNumber('November', 11),
+  MonthWihMonthNumber('December', 12),
+];
