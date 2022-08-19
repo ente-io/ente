fb060c01dc2cd7735063b924663cbc14635c7a3a
ashilkn <ashilkn99@gmail.com>
2022-08-19 10:20:07 +0530

    adde animation for the search suffix icon

--- lib/ui/viewer/search/search_suffix_icon_widget.dart
+++ lib/ui/viewer/search/search_suffix_icon_widget.dart
@@ -3,32 +3,54 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:photos/ente_theme_data.dart';
 
-class SearchSuffixIcon extends StatelessWidget {
+class SearchSuffixIcon extends StatefulWidget {
   final Timer debounce;
   const SearchSuffixIcon(this.debounce, {Key key}) : super(key: key);
 
   @override
+  State<SearchSuffixIcon> createState() => _SearchSuffixIconState();
+}
+
+class _SearchSuffixIconState extends State<SearchSuffixIcon>
+    with TickerProviderStateMixin {
+  @override
   Widget build(BuildContext context) {
-    if (debounce == null || !debounce.isActive) {
-      return IconButton(
-        onPressed: () {
-          Navigator.pop(context);
-        },
-        icon: Icon(
-          Icons.close,
-          color: Theme.of(context).colorScheme.iconColor.withOpacity(0.5),
+    final controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 200),
+    );
+    final animation = Tween(
+      begin: 0.0,
+      end: 1.0,
+    ).animate(controller);
+    if (widget.debounce == null || !widget.debounce.isActive) {
+      controller.forward();
+      return FadeTransition(
+        opacity: animation,
+        child: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(
+            Icons.close,
+            color: Theme.of(context).colorScheme.iconColor.withOpacity(0.5),
+          ),
         ),
       );
     } else {
-      return Padding(
-        padding: const EdgeInsets.all(12),
-        child: SizedBox(
-          height: 6,
-          width: 6,
-          child: Center(
-            child: CircularProgressIndicator(
-              strokeWidth: 2,
-              color: Theme.of(context).colorScheme.iconColor.withOpacity(0.5),
+      controller.forward();
+      return FadeTransition(
+        opacity: animation,
+        child: Padding(
+          padding: const EdgeInsets.all(12),
+          child: SizedBox(
+            height: 6,
+            width: 6,
+            child: Center(
+              child: CircularProgressIndicator(
+                strokeWidth: 2,
+                color: Theme.of(context).colorScheme.iconColor.withOpacity(0.5),
+              ),
             ),
           ),
         ),
