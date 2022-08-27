a9f53e733d7554bb3b6f777e4f356e364ec35ded
Abhinav <abhinavk.grd@gmail.com>
2022-08-27 10:18:43 +0530

    better types

--- src/api/common.ts
+++ src/api/common.ts
@@ -1,7 +1,7 @@
 import { ipcRenderer } from 'electron/renderer';
 import { logError } from '../utils/logging';
 
-export const selectRootDirectory = async () => {
+export const selectRootDirectory = async (): Promise<string> => {
     try {
         return await ipcRenderer.invoke('select-dir');
     } catch (e) {
