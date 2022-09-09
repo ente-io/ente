64e7ba41ac4ddb41c373b563ea3cf44627616b8c
Abhinav <abhinavk.grd@gmail.com>
2022-09-09 13:04:59 +0530

    remove folderPath test logging

--- src/api/watch.ts
+++ src/api/watch.ts
@@ -13,7 +13,7 @@ export async function addWatchMapping(
     folderPath = path.normalize(folderPath);
     const watchMappings = getWatchMappings();
     if (isMappingPresent(watchMappings, folderPath)) {
-        throw new Error(`Watch mapping for ${folderPath} already exists`);
+        throw new Error(`Watch mapping already exists`);
     }
 
     await ipcRenderer.invoke('add-watcher', {
@@ -38,7 +38,7 @@ export async function removeWatchMapping(folderPath: string) {
     );
 
     if (!watchMapping) {
-        throw new Error(`Watch mapping for ${folderPath} does not exist`);
+        throw new Error(`Watch mapping does not exist`);
     }
 
     await ipcRenderer.invoke('remove-watcher', {
@@ -62,7 +62,7 @@ export function updateWatchMappingSyncedFiles(
     );
 
     if (!watchMapping) {
-        throw Error(`Watch mapping not found for ${folderPath}`);
+        throw Error(`Watch mapping not found`);
     }
 
     watchMapping.syncedFiles = files;
@@ -79,7 +79,7 @@ export function updateWatchMappingIgnoredFiles(
     );
 
     if (!watchMapping) {
-        throw Error(`Watch mapping not found for ${folderPath}`);
+        throw Error(`Watch mapping not found`);
     }
 
     watchMapping.ignoredFiles = files;
