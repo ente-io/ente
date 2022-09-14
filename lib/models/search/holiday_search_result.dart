af59fa397141c418aca78193fcf9b7cc830d40ae
Neeraj Gupta <254676+ua741@users.noreply.github.com>
2022-09-14 10:31:05 +0530

    Make dateTimeUtil null safe

--- lib/models/search/holiday_search_result.dart
+++ lib/models/search/holiday_search_result.dart
@@ -11,5 +11,5 @@ class HolidayData {
   final String name;
   final int month;
   final int day;
-  const HolidayData(this.name, this.month, this.day);
+  const HolidayData(this.name, {required this.month, required this.day});
 }
