bd7c73c87a664304ae97ae4fb1b0205820eb969d
Neeraj Gupta <254676+ua741@users.noreply.github.com>
2022-09-16 11:37:47 +0530

    remove unused import

--- lib/ui/status_bar_widget.dart
+++ lib/ui/status_bar_widget.dart
@@ -6,7 +6,6 @@ import 'package:flutter/material.dart';
 import 'package:photos/core/event_bus.dart';
 import 'package:photos/ente_theme_data.dart';
 import 'package:photos/events/sync_status_update_event.dart';
-import 'package:photos/services/feature_flag_service.dart';
 import 'package:photos/services/sync_service.dart';
 import 'package:photos/ui/header_error_widget.dart';
 import 'package:photos/ui/viewer/search/search_widget.dart';
@@ -85,11 +84,10 @@ class _StatusBarWidgetState extends State<StatusBarWidget> {
               right: 0,
               top: 0,
               child: Container(
-                      color:
-                          Theme.of(context).colorScheme.defaultBackgroundColor,
-                      height: kContainerHeight,
-                      child: const SearchIconWidget(),
-                    ),
+                color: Theme.of(context).colorScheme.defaultBackgroundColor,
+                height: kContainerHeight,
+                child: const SearchIconWidget(),
+              ),
             ),
           ],
         ),
