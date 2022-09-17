472fde37623059bf457476d4460cb4f64d2954f4
Abhinav <abhinavk.grd@gmail.com>
2022-09-17 17:28:41 +0530

    comments

--- src/utils/comlink/transferHandler.ts
+++ src/utils/comlink/transferHandler.ts
@@ -1,5 +1,6 @@
 import * as Comlink from 'comlink';
 
+// didn't work kept for reference, so that can try to make it work later in future hopefully
 export function setupResponseObjectTransferHandler() {
     const transferHandler: Comlink.TransferHandler<Response, ArrayBuffer> = {
         canHandle: (obj): obj is Response => obj instanceof Response,
