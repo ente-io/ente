87e38c67a37beb0ca015b9a9fa07b4bcdacea2bc
Abhinav <abhinavk.grd@gmail.com>
2022-09-18 01:39:59 +0530

    add ComlinkWorker util class

---
+++ src/utils/comlink/comlinkWorker.ts
@@ -0,0 +1,27 @@
+import { runningInBrowser } from 'utils/common';
+import { Remote, wrap } from 'comlink';
+import { addLocalLog } from 'utils/logging';
+
+export class ComlinkWorker<T> {
+    public remote: Remote<T>;
+    private worker: Worker;
+    private name: string;
+
+    constructor(name: string, url: string) {
+        this.name = name;
+        if (!runningInBrowser()) {
+            return;
+        }
+        this.worker = new Worker(new URL(url, import.meta.url), { name: name });
+        this.worker.onerror = (errorEvent) => {
+            console.error('Got error event from worker', errorEvent);
+        };
+        addLocalLog(() => `Initiated ${this.name}`);
+        this.remote = wrap<T>(this.worker);
+    }
+
+    public terminate() {
+        this.worker.terminate();
+        addLocalLog(() => `Terminated ${this.name}`);
+    }
+}
