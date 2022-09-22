94746b192462b3513f0e7723562e3d54a3be34a5
Neeraj Gupta <254676+ua741@users.noreply.github.com>
2022-09-22 14:32:52 +0530

    [Design-components] Add textStyles

---
+++ lib/theme/text_style.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+const FontWeight regularWeight = FontWeight.w500;
+const FontWeight boldWeight = FontWeight.w600;
+const String fontFamily = 'Inter';
+const TextStyle h1 =
+    TextStyle(fontSize: 48, height: 48, fontWeight: regularWeight);
+const TextStyle h2 =
+    TextStyle(fontSize: 32, height: 39, fontWeight: regularWeight);
+const TextStyle h3 =
+    TextStyle(fontSize: 24, height: 29, fontWeight: regularWeight);
+const TextStyle large =
+    TextStyle(fontSize: 18, height: 22, fontWeight: regularWeight);
+const TextStyle body =
+    TextStyle(fontSize: 16, height: 19, fontWeight: regularWeight);
+const TextStyle small =
+    TextStyle(fontSize: 14, height: 17, fontWeight: regularWeight);
+const TextStyle mini =
+    TextStyle(fontSize: 12, height: 15, fontWeight: regularWeight);
+const TextStyle tiny =
+    TextStyle(fontSize: 10, height: 12, fontWeight: regularWeight);
+
+const TextStyle h1Bold =
+    TextStyle(fontSize: 48, height: 48, fontWeight: boldWeight);
+const TextStyle h2Bold =
+    TextStyle(fontSize: 32, height: 39, fontWeight: boldWeight);
+const TextStyle h3Bold =
+    TextStyle(fontSize: 24, height: 29, fontWeight: boldWeight);
+const TextStyle largeBold =
+    TextStyle(fontSize: 18, height: 22, fontWeight: boldWeight);
+const TextStyle bodyBold =
+    TextStyle(fontSize: 16, height: 19, fontWeight: boldWeight);
+const TextStyle smallBold =
+    TextStyle(fontSize: 14, height: 17, fontWeight: boldWeight);
+const TextStyle miniBold =
+    TextStyle(fontSize: 12, height: 15, fontWeight: boldWeight);
+const TextStyle tinyBold =
+    TextStyle(fontSize: 10, height: 12, fontWeight: boldWeight);
