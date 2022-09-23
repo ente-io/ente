1fa34c9ae67670547763e5b386761656b37f072a
ashilkn <ashilkn99@gmail.com>
2022-09-23 11:09:54 +0530

    migrated trash_file.dart to null safety

--- lib/models/trash_file.dart
+++ lib/models/trash_file.dart
@@ -1,16 +1,14 @@
-// @dart=2.9
-
 import 'package:photos/models/file.dart';
 
 class TrashFile extends File {
   // time when file was put in the trash for first time
-  int createdAt;
+  int? createdAt;
 
   // for non-deleted trash items, updateAt is usually equal to the latest time
   // when the file was moved to trash
-  int updateAt;
+  int? updateAt;
 
   // time after which will will be deleted from trash & user's storage usage
   // will go down
-  int deleteBy;
+  int? deleteBy;
 }
