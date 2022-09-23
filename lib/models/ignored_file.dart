0bc03669e2b76579223a6339c12b195787887d03
ashilkn <ashilkn99@gmail.com>
2022-09-23 11:17:23 +0530

    migrated ignored_file.dart

--- lib/models/ignored_file.dart
+++ lib/models/ignored_file.dart
@@ -1,26 +1,24 @@
-// @dart=2.9
-
 import 'package:photos/models/trash_file.dart';
 
 const kIgnoreReasonTrash = "trash";
 const kIgnoreReasonInvalidFile = "invalidFile";
 
 class IgnoredFile {
-  final String localID;
-  final String title;
-  final String deviceFolder;
+  final String? localID;
+  final String? title;
+  final String? deviceFolder;
   String reason;
 
   IgnoredFile(this.localID, this.title, this.deviceFolder, this.reason);
 
-  factory IgnoredFile.fromTrashItem(TrashFile trashFile) {
+  static fromTrashItem(TrashFile? trashFile) {
     if (trashFile == null) return null;
     if (trashFile.localID == null ||
-        trashFile.localID.isEmpty ||
+        trashFile.localID!.isEmpty ||
         trashFile.title == null ||
-        trashFile.title.isEmpty ||
+        trashFile.title!.isEmpty ||
         trashFile.deviceFolder == null ||
-        trashFile.deviceFolder.isEmpty) {
+        trashFile.deviceFolder!.isEmpty) {
       return null;
     }
 
