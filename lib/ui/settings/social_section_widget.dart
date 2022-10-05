f035719aabd484d9cea44c3a07387309be9f62c1
ashilkn <ashilkn99@gmail.com>
2022-10-05 11:28:01 +0530

    used MenuItemWidget for Social's selectionOptions

--- lib/ui/settings/social_section_widget.dart
+++ lib/ui/settings/social_section_widget.dart
@@ -9,7 +9,6 @@ import 'package:photos/services/update_service.dart';
 import 'package:photos/ui/components/captioned_text_widget.dart';
 import 'package:photos/ui/components/menu_item_widget.dart';
 import 'package:photos/ui/settings/common_settings.dart';
-import 'package:photos/ui/settings/settings_text_item.dart';
 import 'package:url_launcher/url_launcher_string.dart';
 
 class SocialSectionWidget extends StatefulWidget {
@@ -53,58 +52,43 @@ class _SocialSectionWidgetState extends State<SocialSectionWidget> {
   Widget _getSectionOptions(BuildContext context) {
     final List<Widget> options = [
       sectionOptionDivider,
-      GestureDetector(
-        behavior: HitTestBehavior.translucent,
-        onTap: () {
-          launchUrlString("https://twitter.com/enteio");
-        },
-        child:
-            const SettingsTextItem(text: "Twitter", icon: Icons.navigate_next),
-      ),
-      sectionOptionDivider,
-      GestureDetector(
-        behavior: HitTestBehavior.translucent,
-        onTap: () {
-          launchUrlString("https://ente.io/discord");
-        },
-        child:
-            const SettingsTextItem(text: "Discord", icon: Icons.navigate_next),
-      ),
-      sectionOptionDivider,
-      GestureDetector(
-        behavior: HitTestBehavior.translucent,
-        onTap: () {
-          launchUrlString("https://reddit.com/r/enteio");
-        },
-        child:
-            const SettingsTextItem(text: "Reddit", icon: Icons.navigate_next),
-      ),
+      const SocialsMenuItemWidget("Twitter", "https://twitter.com/enteio"),
+      const SocialsMenuItemWidget("Discord", "https://ente.io/discord"),
+      const SocialsMenuItemWidget("Reddit", "https://reddit.com/r/enteio"),
     ];
     if (!UpdateService.instance.isIndependent()) {
       options.addAll(
         [
-          sectionOptionDivider,
-          GestureDetector(
-            behavior: HitTestBehavior.translucent,
-            onTap: () {
-              if (Platform.isAndroid) {
-                launchUrlString(
-                  "https://play.google.com/store/apps/details?id=io.ente.photos",
-                );
-              } else {
-                launchUrlString(
-                  "https://apps.apple.com/in/app/ente-photos/id1542026904",
-                );
-              }
-            },
-            child: const SettingsTextItem(
-              text: "Rate us! ✨",
-              icon: Icons.navigate_next,
-            ),
-          )
+          SocialsMenuItemWidget(
+            "Rate us! ✨",
+            Platform.isAndroid
+                ? "https://play.google.com/store/apps/details?id=io.ente.photos"
+                : "https://apps.apple.com/in/app/ente-photos/id1542026904",
+          ),
         ],
       );
     }
     return Column(children: options);
+  }
+}
+
+class SocialsMenuItemWidget extends StatelessWidget {
+  final String text;
+  final String urlSring;
+  const SocialsMenuItemWidget(this.text, this.urlSring, {Key key})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MenuItemWidget(
+      captionedTextWidget: CaptionedTextWidget(
+        text: text,
+      ),
+      trailingIcon: Icons.chevron_right_outlined,
+      trailingIconIsMuted: true,
+      onTap: () {
+        launchUrlString(urlSring);
+      },
+    );
   }
 }
