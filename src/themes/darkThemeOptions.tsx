f0676237225864d4db52a623243843df9628889c
Abhinav <abhinavk.grd@gmail.com>
2022-10-10 17:54:15 +0530

    use css baseline color scheme option to change scrollbar color

--- src/themes/darkThemeOptions.tsx
+++ src/themes/darkThemeOptions.tsx
@@ -82,9 +82,6 @@ const darkThemeOptions = createTheme({
                 body: {
                     fontFamily: ['Inter', 'sans-serif'].join(','),
                     letterSpacing: '-0.011em',
-                    '::-webkit-scrollbar-thumb': {
-                        backgroundColor: 'rgba(256, 256, 256, 0.12)',
-                    },
                 },
                 strong: { fontWeight: 900 },
             },
