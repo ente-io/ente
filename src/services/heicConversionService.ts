c901fcc14e23ee221f13be1090e78506fbeee46b
Abhinav <abhinavk.grd@gmail.com>
2022-10-15 00:42:23 +0530

    add HEICConversionService to handle both electron and wasm

---
+++ src/services/heicConversionService.ts
@@ -0,0 +1,24 @@
+import isElectron from 'is-electron';
+import { logError } from 'utils/sentry';
+import WasmHEICConverterService from './wasmHeicConverter/wasmHEICConverterService';
+import ElectronHEICConvertor from 'services/electron/heicConvertor';
+
+class HeicConversionService {
+    async convert(heicFileData: Blob): Promise<Blob> {
+        try {
+            if (isElectron() && ElectronHEICConvertor.apiExists()) {
+                try {
+                    return ElectronHEICConvertor.convert(heicFileData);
+                } catch (e) {
+                    return WasmHEICConverterService.convert(heicFileData);
+                }
+            } else {
+                return WasmHEICConverterService.convert(heicFileData);
+            }
+        } catch (e) {
+            logError(e, 'failed to convert heic file');
+            throw e;
+        }
+    }
+}
+export default new HeicConversionService();
