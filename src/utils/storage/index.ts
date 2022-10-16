16bfbe15c3c208770720897bab2264b2b1baf733
Abhinav <abhinavk.grd@gmail.com>
2022-10-16 10:33:54 +0530

    delete old logs if local storage is full

--- src/utils/storage/index.ts
+++ src/utils/storage/index.ts
@@ -1,4 +1,5 @@
-import { getData, LS_KEYS, setData } from './localStorage';
+import { logError } from 'utils/sentry';
+import { getData, LS_KEYS, removeData, setData } from './localStorage';
 
 export interface Log {
     timestamp: number;
@@ -30,9 +31,15 @@ export function setLivePhotoInfoShownCount(count) {
 }
 
 export function saveLogLine(log: Log) {
-    setData(LS_KEYS.LOGS, {
-        logs: [...getLogs(), log].slice(-1 * MAX_LOG_LINES),
-    });
+    try {
+        setData(LS_KEYS.LOGS, {
+            logs: [...getLogs(), log].slice(-1 * MAX_LOG_LINES),
+        });
+    } catch (e) {
+        logError(e, 'failed to save log line');
+        removeData(LS_KEYS.LOGS);
+        setData(LS_KEYS.LOGS, { logs: [log] });
+    }
 }
 
 export function getLogs(): Log[] {
