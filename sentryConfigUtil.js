481a1a803b687538374691829cae013cd275db5d
Abhinav <abhinavk.grd@gmail.com>
2022-10-18 09:34:26 +0530

    fix env variable name

--- sentryConfigUtil.js
+++ sentryConfigUtil.js
@@ -3,7 +3,7 @@ const ENV_DEVELOPMENT = 'development';
 module.exports.getIsSentryEnabled = () => {
     if (process.env.NEXT_PUBLIC_SENTRY_ENV === ENV_DEVELOPMENT) {
         return false;
-    } else if (process.env.DISABLE_SENTRY === 'true') {
+    } else if (process.env.NEXT_PUBLIC_DISABLE_SENTRY === 'true') {
         return false;
     } else {
         return true;
