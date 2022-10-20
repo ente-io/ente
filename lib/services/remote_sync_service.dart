635bc0bed0a1fe2cf9feef4b141aeec0c8e413c5
Neeraj Gupta <254676+ua741@users.noreply.github.com>
2022-10-20 14:56:56 +0530

    Remove device folder name from logs

--- lib/services/remote_sync_service.dart
+++ lib/services/remote_sync_service.dart
@@ -259,7 +259,6 @@ class RemoteSyncService {
         await _db.getDevicePathIDToLocalIDMap();
     bool moreFilesMarkedForBackup = false;
     for (final deviceCollection in deviceCollections) {
-      _logger.fine("processing ${deviceCollection.name}");
       final Set<String> localIDsToSync =
           pathIdToLocalIDs[deviceCollection.id] ?? {};
       if (deviceCollection.uploadStrategy == UploadStrategy.ifMissing) {
@@ -400,7 +399,7 @@ class RemoteSyncService {
         if (collectionByID == null || collectionByID.isDeleted) {
           _logger.info(
             "Collection $deviceCollectionID either deleted or missing "
-            "for path ${deviceCollection.name}",
+            "for path ${deviceCollection.id}",
           );
           deviceCollectionID = -1;
         }
