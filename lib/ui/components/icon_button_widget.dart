82750539991067c5113022f4bb883bfee5dd7d23
ashilkn <ashilkn99@gmail.com>
2022-10-27 17:06:10 +0530

    added option to disable gestureDetector for iconButton

--- lib/ui/components/icon_button_widget.dart
+++ lib/ui/components/icon_button_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:photos/theme/colors.dart';
 import 'package:photos/theme/ente_theme.dart';
 
 class IconButtonWidget extends StatefulWidget {
@@ -6,12 +7,14 @@ class IconButtonWidget extends StatefulWidget {
   final bool isSecondary;
   final bool isRounded;
   final IconData icon;
+  final bool disableGestureDetector;
   final VoidCallback? onTap;
   const IconButtonWidget({
     required this.icon,
     this.isPrimary = false,
     this.isSecondary = false,
     this.isRounded = false,
+    this.disableGestureDetector = false,
     this.onTap,
     super.key,
   });
@@ -38,25 +41,30 @@ class _IconButtonWidgetState extends State<IconButtonWidget> {
     final colorTheme = getEnteColorScheme(context);
     iconStateColor ??
         (iconStateColor = (widget.isRounded ? colorTheme.fillFaint : null));
-    return GestureDetector(
-      onTapDown: _onTapDown,
-      onTapUp: _onTapUp,
-      onTapCancel: _onTapCancel,
-      onTap: widget.onTap,
-      child: AnimatedContainer(
-        duration: const Duration(milliseconds: 20),
-        padding: const EdgeInsets.all(8),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(20),
-          color: iconStateColor,
-        ),
-        child: Icon(
-          Icons.close_outlined,
-          color: widget.isSecondary
-              ? colorTheme.strokeMuted
-              : colorTheme.strokeBase,
-          size: 24,
-        ),
+    return widget.disableGestureDetector
+        ? _iconButton(colorTheme)
+        : GestureDetector(
+            onTapDown: _onTapDown,
+            onTapUp: _onTapUp,
+            onTapCancel: _onTapCancel,
+            onTap: widget.onTap,
+            child: _iconButton(colorTheme),
+          );
+  }
+
+  Widget _iconButton(EnteColorScheme colorTheme) {
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 20),
+      padding: const EdgeInsets.all(8),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        color: iconStateColor,
+      ),
+      child: Icon(
+        Icons.close_outlined,
+        color:
+            widget.isSecondary ? colorTheme.strokeMuted : colorTheme.strokeBase,
+        size: 24,
       ),
     );
   }
