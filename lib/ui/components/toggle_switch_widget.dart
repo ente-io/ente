7f064b7a981fd4efbba4821a4a995311e706119c
ashilkn <ashilkn99@gmail.com>
2022-10-29 18:42:35 +0530

    show EnteLoadingWidget when changes are being made internally on toggling switch

--- lib/ui/components/toggle_switch_widget.dart
+++ lib/ui/components/toggle_switch_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:photos/ente_theme_data.dart';
+import 'package:photos/ui/common/loading_widget.dart';
 
 typedef OnChangedCallBack = Future<void> Function();
 typedef ValueCallBack = bool Function();
@@ -19,35 +20,48 @@ class ToggleSwitchWidget extends StatefulWidget {
 
 class _ToggleSwitchWidgetState extends State<ToggleSwitchWidget> {
   late bool toggleValue;
+  late bool inProgress;
   @override
   void initState() {
     toggleValue = widget.value.call();
+    inProgress = false;
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     final enteColorScheme = Theme.of(context).colorScheme.enteTheme.colorScheme;
-    return SizedBox(
-      height: 32,
-      child: FittedBox(
-        fit: BoxFit.contain,
-        child: Switch.adaptive(
-          activeColor: enteColorScheme.primary400,
-          inactiveTrackColor: enteColorScheme.fillMuted,
-          materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
-          value: toggleValue,
-          onChanged: (value) async {
-            setState(() {
-              toggleValue = value;
-            });
-            await widget.onChanged.call();
-            setState(() {
-              toggleValue = widget.value.call();
-            });
-          },
+    return Row(
+      children: [
+        inProgress
+            ? EnteLoadingWidget(color: enteColorScheme.strokeMuted)
+            : const SizedBox.shrink(),
+        const SizedBox(width: 8),
+        SizedBox(
+          height: 32,
+          child: FittedBox(
+            fit: BoxFit.contain,
+            child: Switch.adaptive(
+              activeColor: enteColorScheme.primary400,
+              inactiveTrackColor: enteColorScheme.fillMuted,
+              materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+              value: toggleValue,
+              onChanged: (negationOfToggleValue) async {
+                setState(() {
+                  toggleValue = negationOfToggleValue;
+                  inProgress = true;
+                });
+                await widget.onChanged.call();
+                setState(() {
+                  final newValue = widget.value.call();
+                  toggleValue = newValue;
+                  inProgress = false;
+                });
+              },
+            ),
+          ),
         ),
-      ),
+      ],
     );
   }
 }
