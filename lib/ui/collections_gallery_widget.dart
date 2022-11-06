c9163919a658a0c36c8f2ad46f89d757b694eb88
Neeraj Gupta <254676+ua741@users.noreply.github.com>
2022-11-06 14:48:54 +0530

    Change severity for lint rule

--- lib/ui/collections_gallery_widget.dart
+++ lib/ui/collections_gallery_widget.dart
@@ -22,7 +22,8 @@ import 'package:photos/ui/collections/section_title.dart';
 import 'package:photos/ui/collections/trash_button_widget.dart';
 import 'package:photos/ui/common/loading_widget.dart';
 import 'package:photos/ui/viewer/gallery/empty_state.dart';
-import 'package:photos/utils/local_settings.dart';
+
+import '../utils/local_settings.dart';
 
 class CollectionsGalleryWidget extends StatefulWidget {
   const CollectionsGalleryWidget({Key key}) : super(key: key);
