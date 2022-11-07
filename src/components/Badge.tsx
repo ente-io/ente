bc16ff59d37177d14070d849a958aee3149a3fe5
Abhinav <abhinavk.grd@gmail.com>
2022-11-07 15:43:15 +0530

    fix Badge style

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -4,7 +4,7 @@ import { CSSProperties } from '@mui/styled-engine';
 export const Badge = styled(Paper)(({ theme }) => ({
     padding: '2px 4px',
     backgroundColor: theme.palette.backdrop.main,
-    filter: `blur(${theme.palette.blur.muted})`,
+    backdropFilter: `blur(${theme.palette.blur.muted})`,
     color: theme.palette.primary.contrastText,
     textTransform: 'uppercase',
     ...(theme.typography.mini as CSSProperties),
