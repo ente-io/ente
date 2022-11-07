e62f02ec0bf33c828248dffab1048557833bfe8a
Abhinav <abhinavk.grd@gmail.com>
2022-11-07 15:42:30 +0530

    update badge component with blur and backdrop

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -3,8 +3,9 @@ import { CSSProperties } from '@mui/styled-engine';
 
 export const Badge = styled(Paper)(({ theme }) => ({
     padding: '2px 4px',
-    backgroundColor: theme.palette.glass.main,
-    color: theme.palette.glass.contrastText,
+    backgroundColor: theme.palette.backdrop.main,
+    filter: `blur(${theme.palette.blur.muted})`,
+    color: theme.palette.primary.contrastText,
     textTransform: 'uppercase',
     ...(theme.typography.mini as CSSProperties),
 }));
