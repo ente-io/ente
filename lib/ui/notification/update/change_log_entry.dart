69cb182d1973839c790f70be5d0d5e37a3c46ba2
Neeraj Gupta <254676+ua741@users.noreply.github.com>
2022-11-09 13:36:10 +0530

    UI: Change log UI changes

--- lib/ui/notification/update/change_log_entry.dart
+++ lib/ui/notification/update/change_log_entry.dart
@@ -1,16 +1,20 @@
 import 'package:flutter/widgets.dart';
 import 'package:photos/theme/ente_theme.dart';
 
-class ChangeLogEntry extends StatelessWidget {
+class ChangeLogEntry {
   final bool isFeature;
   final String title;
   final String description;
 
-  const ChangeLogEntry({
+  ChangeLogEntry(this.title, this.description, {this.isFeature = true});
+}
+
+class ChangeLogEntryWidget extends StatelessWidget {
+  final ChangeLogEntry entry;
+
+  const ChangeLogEntryWidget({
     super.key,
-    required this.isFeature,
-    required this.title,
-    required this.description,
+    required this.entry,
   });
 
   @override
@@ -18,18 +22,23 @@ class ChangeLogEntry extends StatelessWidget {
     final enteTheme = getEnteTextTheme(context);
     final colorScheme = getEnteColorScheme(context);
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Text(
-          title,
+          entry.title,
+          textAlign: TextAlign.left,
           style: enteTheme.largeBold.copyWith(
-            color: isFeature ? colorScheme.primary700 : colorScheme.textMuted,
+            color: entry.isFeature
+                ? colorScheme.primary700
+                : colorScheme.textMuted,
           ),
         ),
         const SizedBox(
           height: 18,
         ),
         Text(
-          description,
+          entry.description,
+          textAlign: TextAlign.left,
           style: enteTheme.body.copyWith(
             color: colorScheme.textMuted,
           ),
