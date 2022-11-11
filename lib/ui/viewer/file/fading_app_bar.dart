2de656234383cc7fd78bb51d658f0a340644b8a6
Neeraj Gupta <254676+ua741@users.noreply.github.com>
2022-11-11 11:53:01 +0530

    Do not show hide option for local only files

--- lib/ui/viewer/file/fading_app_bar.dart
+++ lib/ui/viewer/file/fading_app_bar.dart
@@ -110,8 +110,9 @@ class FadingAppBarState extends State<FadingAppBar> {
     final shouldShowActions = widget.shouldShowActions && !isTrashedFile;
     final bool isOwnedByUser =
         widget.file.ownerID == null || widget.file.ownerID == widget.userID;
+    final bool isFileUploaded = widget.file.isRemoteFile;
     bool isFileHidden = false;
-    if (isOwnedByUser && widget.file.uploadedFileID != null) {
+    if (isOwnedByUser && isFileUploaded) {
       isFileHidden = CollectionsService.instance
               .getCollectionByID(widget.file.collectionID)
               ?.isHidden() ??
@@ -189,7 +190,7 @@ class FadingAppBarState extends State<FadingAppBar> {
               ),
             );
           }
-          if (isOwnedByUser) {
+          if (isOwnedByUser && widget.file.isUploaded) {
             if (!isFileHidden) {
               items.add(
                 PopupMenuItem(
