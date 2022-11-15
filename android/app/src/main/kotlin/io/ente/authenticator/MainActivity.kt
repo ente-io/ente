691eaabf5044f8c423425f57abe21bd5aec68b28
vishnukvmd <vishnu@ente.io>
2022-11-15 14:04:17 +0530

    Switch to FlutterFragmentActivity

--- android/app/src/main/kotlin/io/ente/authenticator/MainActivity.kt
+++ android/app/src/main/kotlin/io/ente/authenticator/MainActivity.kt
@@ -1,6 +1,6 @@
 package io.ente.auth
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterFragmentActivity() {
 }
