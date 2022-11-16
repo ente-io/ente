de560de204d157987082e33fab9ad29b55dd2065
Abhinav <abhinavk.grd@gmail.com>
2022-11-16 16:12:20 +0530

    revert logging change

--- src/utils/logging.ts
+++ src/utils/logging.ts
@@ -4,6 +4,7 @@ import { LOG_FILENAME, MAX_LOG_SIZE } from '../config';
 export function setupLogging() {
     log.transports.file.fileName = LOG_FILENAME;
     log.transports.file.maxSize = MAX_LOG_SIZE;
+    log.transports.console.level = false;
 }
 
 export function makeID(length: number) {
