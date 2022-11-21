b6c3b6fc9c43ccc2a207cdee96ebd0979fb1a4cc
Abhinav <abhinavk.grd@gmail.com>
2022-11-21 17:25:43 +0530

    remove getVariantColor util and ButtonVariant enum

--- src/components/pages/gallery/LinkButton.tsx
+++ src/components/pages/gallery/LinkButton.tsx
@@ -1,32 +1,11 @@
 import { ButtonProps, Link, LinkProps } from '@mui/material';
 import React, { FC } from 'react';
 
-export enum ButtonVariant {
-    success = 'success',
-    danger = 'danger',
-    secondary = 'secondary',
-    warning = 'warning',
-}
 export type LinkButtonProps = React.PropsWithChildren<{
     onClick: () => void;
     variant?: string;
     style?: React.CSSProperties;
 }>;
-
-export function getVariantColor(variant: string) {
-    switch (variant) {
-        case ButtonVariant.success:
-            return '#51cd7c';
-        case ButtonVariant.danger:
-            return '#c93f3f';
-        case ButtonVariant.secondary:
-            return '#858585';
-        case ButtonVariant.warning:
-            return '#D7BB63';
-        default:
-            return '#d1d1d1';
-    }
-}
 
 const LinkButton: FC<LinkProps<'button', { color?: ButtonProps['color'] }>> = ({
     children,
