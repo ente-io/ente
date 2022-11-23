6bba526bb03451791fd8a65ae6493ea82f4b93e4
Abhinav <abhinavk.grd@gmail.com>
2022-11-23 18:33:20 +0530

    add titlebar component

---
+++ src/components/Titlebar.tsx
@@ -0,0 +1,33 @@
+import { Close } from '@mui/icons-material';
+import { Box, IconButton, Typography } from '@mui/material';
+import React from 'react';
+
+interface Iprops {
+    title: string;
+    caption?: string;
+    onClose: () => void;
+}
+
+export default function Titlebar({
+    title,
+    caption,
+    onClose,
+}: Iprops): JSX.Element {
+    return (
+        <>
+            <Box display={'flex'} height={48} alignItems={'center'}>
+                <IconButton onClick={onClose} color="secondary">
+                    <Close />
+                </IconButton>
+            </Box>
+            <Box py={0.5} px={2}>
+                <Typography variant="h3" fontWeight={'bold'}>
+                    {title}
+                </Typography>
+                <Typography variant="body2" minHeight={17}>
+                    {caption}
+                </Typography>
+            </Box>
+        </>
+    );
+}
