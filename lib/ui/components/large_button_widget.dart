ebb855f484778a1cc72df98af4b3420e15d14f80
ashilkn <ashilkn99@gmail.com>
2022-12-09 19:30:58 +0530

    code refactoring

--- lib/ui/components/large_button_widget.dart
+++ lib/ui/components/large_button_widget.dart
@@ -3,6 +3,7 @@ import 'package:photos/theme/colors.dart';
 import 'package:photos/theme/ente_theme.dart';
 import 'package:photos/theme/text_style.dart';
 import 'package:photos/ui/components/models/button_type.dart';
+import 'package:photos/ui/components/models/large_button_style.dart';
 
 class LargeButtonWidget extends StatelessWidget {
   final IconData? icon;
@@ -34,89 +35,130 @@ class LargeButtonWidget extends StatelessWidget {
     final inverseTextTheme = isInActionSheet
         ? lightTextTheme
         : getEnteTextTheme(context, inverse: true);
-    final defaultButtonColor = buttonType.defaultButtonColor(colorScheme);
-    final pressedButtonColor = buttonType.pressedButtonColor(colorScheme);
-    final disabledButtonColor = buttonType.disabledButtonColor(colorScheme);
-    final defaultBorderColor = buttonType.defaultBorderColor(colorScheme);
-    final pressedBorderColor = buttonType.pressedBorderColor(colorScheme);
-    final disabledBorderColor = buttonType.disabledBorderColor(colorScheme);
-    final defaultIconColor = buttonType.defaultIconColor(
+    final buttonStyle = LargeButtonStyle(
+      //Dummy default values since we need to keep these properties non-nullable
+      defaultButtonColor: Colors.transparent,
+      defaultBorderColor: Colors.transparent,
+      defaultIconColor: Colors.transparent,
+      defaultLabelStyle: textTheme.body,
+    );
+    buttonStyle.defaultButtonColor = buttonType.defaultButtonColor(colorScheme);
+    buttonStyle.pressedButtonColor = buttonType.pressedButtonColor(colorScheme);
+    buttonStyle.disabledButtonColor =
+        buttonType.disabledButtonColor(colorScheme);
+    buttonStyle.defaultBorderColor = buttonType.defaultBorderColor(colorScheme);
+    buttonStyle.pressedBorderColor = buttonType.pressedBorderColor(colorScheme);
+    buttonStyle.disabledBorderColor =
+        buttonType.disabledBorderColor(colorScheme);
+    buttonStyle.defaultIconColor = buttonType.defaultIconColor(
       colorScheme: colorScheme,
       inverseColorScheme: inverseColorScheme,
     );
-    final pressedIconColor = buttonType.pressedIconColor(colorScheme);
-    final disabledIconColor = buttonType.disabledIconColor(colorScheme);
-    final defaultLabelStyle = buttonType.defaultLabelStyle(
+    buttonStyle.pressedIconColor = buttonType.pressedIconColor(colorScheme);
+    buttonStyle.disabledIconColor = buttonType.disabledIconColor(colorScheme);
+    buttonStyle.defaultLabelStyle = buttonType.defaultLabelStyle(
       textTheme: textTheme,
       inverseTextTheme: inverseTextTheme,
     );
-    final pressedLabelStyle =
+    buttonStyle.pressedLabelStyle =
         buttonType.pressedLabelStyle(textTheme, colorScheme);
-    final disabledLabelStyle =
+    buttonStyle.disabledLabelStyle =
         buttonType.disabledLabelStyle(textTheme, colorScheme);
 
-    return GestureDetector(
+    return LargeButtonChildWidget(
+      buttonStyle: buttonStyle,
+      buttonType: buttonType,
       onTap: onTap,
+      labelText: labelText,
+      icon: icon,
+    );
+  }
+}
+
+class LargeButtonChildWidget extends StatefulWidget {
+  final LargeButtonStyle buttonStyle;
+  final VoidCallback? onTap;
+  final ButtonType buttonType;
+  final String? labelText;
+  final IconData? icon;
+  const LargeButtonChildWidget({
+    required this.buttonStyle,
+    required this.buttonType,
+    this.onTap,
+    this.labelText,
+    this.icon,
+    super.key,
+  });
+
+  @override
+  State<LargeButtonChildWidget> createState() => _LargeButtonChildWidgetState();
+}
+
+class _LargeButtonChildWidgetState extends State<LargeButtonChildWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: widget.onTap,
       child: Container(
         width: double.infinity,
         decoration: BoxDecoration(
           borderRadius: const BorderRadius.all(Radius.circular(4)),
-          color: defaultButtonColor,
-          border: Border.all(color: defaultBorderColor),
+          color: widget.buttonStyle.defaultButtonColor,
+          border: Border.all(color: widget.buttonStyle.defaultBorderColor),
         ),
         child: Padding(
           padding: const EdgeInsets.symmetric(vertical: 14, horizontal: 16),
           //todo: show loading or row depending on state of button
-          child: buttonType.hasTrailingIcon
+          child: widget.buttonType.hasTrailingIcon
               ? Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    labelText == null
+                    widget.labelText == null
                         ? const SizedBox.shrink()
                         : Flexible(
                             child: Padding(
-                              padding: icon == null
+                              padding: widget.icon == null
                                   ? const EdgeInsets.symmetric(horizontal: 8)
                                   : const EdgeInsets.only(right: 16),
                               child: Text(
-                                labelText!,
+                                widget.labelText!,
                                 overflow: TextOverflow.ellipsis,
                                 maxLines: 2,
-                                style: defaultLabelStyle,
+                                style: widget.buttonStyle.defaultLabelStyle,
                               ),
                             ),
                           ),
-                    icon == null
+                    widget.icon == null
                         ? const SizedBox.shrink()
                         : Icon(
-                            icon,
+                            widget.icon,
                             size: 20,
-                            color: defaultIconColor,
+                            color: widget.buttonStyle.defaultIconColor,
                           ),
                   ],
                 )
               : Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    icon == null
+                    widget.icon == null
                         ? const SizedBox.shrink()
                         : Icon(
-                            icon,
+                            widget.icon,
                             size: 20,
-                            color: defaultIconColor,
+                            color: widget.buttonStyle.defaultIconColor,
                           ),
-                    icon == null || labelText == null
+                    widget.icon == null || widget.labelText == null
                         ? const SizedBox.shrink()
                         : const SizedBox(width: 8),
-                    labelText == null
+                    widget.labelText == null
                         ? const SizedBox.shrink()
                         : Flexible(
                             child: Padding(
                               padding:
                                   const EdgeInsets.symmetric(horizontal: 8),
                               child: Text(
-                                labelText!,
-                                style: defaultLabelStyle,
+                                widget.labelText!,
+                                style: widget.buttonStyle.defaultLabelStyle,
                                 maxLines: 2,
                                 overflow: TextOverflow.ellipsis,
                               ),
