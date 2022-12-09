a481d77d71c2fbe9bc7b3327c16b95083362d62e
ashilkn <ashilkn99@gmail.com>
2022-12-09 12:25:32 +0530

    Refactored code by using enhanced enums

--- lib/ui/components/large_button_widget.dart
+++ lib/ui/components/large_button_widget.dart
@@ -34,22 +34,26 @@ class LargeButtonWidget extends StatelessWidget {
     final inverseTextTheme = isInActionSheet
         ? lightTextTheme
         : getEnteTextTheme(context, inverse: true);
-    final defaultButtonColor = _defaultButtonColor(colorScheme);
-    final pressedButtonColor = _pressedButtonColor(colorScheme);
-    final disabledButtonColor = _disabledButtonColor(colorScheme);
-    final defaultBorderColor = _defaultBorderColor(colorScheme);
-    final pressedBorderColor = _pressedBorderColor(colorScheme);
-    final disabledBorderColor = _disabledBorderColor(colorScheme);
-    final defaultIconColor = _defaultIconColor(
-        colorScheme: colorScheme, inverseColorScheme: inverseColorScheme);
-    final pressedIconColor = _pressedIconColor(colorScheme);
-    final disabledIconColor = _disabledIconColor(colorScheme);
-    final defaultLabelStyle = _defaultLabelStyle(
+    final defaultButtonColor = buttonType.defaultButtonColor(colorScheme);
+    final pressedButtonColor = buttonType.pressedButtonColor(colorScheme);
+    final disabledButtonColor = buttonType.disabledButtonColor(colorScheme);
+    final defaultBorderColor = buttonType.defaultBorderColor(colorScheme);
+    final pressedBorderColor = buttonType.pressedBorderColor(colorScheme);
+    final disabledBorderColor = buttonType.disabledBorderColor(colorScheme);
+    final defaultIconColor = buttonType.defaultIconColor(
+      colorScheme: colorScheme,
+      inverseColorScheme: inverseColorScheme,
+    );
+    final pressedIconColor = buttonType.pressedIconColor(colorScheme);
+    final disabledIconColor = buttonType.disabledIconColor(colorScheme);
+    final defaultLabelStyle = buttonType.defaultLabelStyle(
       textTheme: textTheme,
       inverseTextTheme: inverseTextTheme,
     );
-    final pressedLabelStyle = _pressedLabelStyle(textTheme, colorScheme);
-    final disabledLabelStyle = _disabledLabelStyle(textTheme, colorScheme);
+    final pressedLabelStyle =
+        buttonType.pressedLabelStyle(textTheme, colorScheme);
+    final disabledLabelStyle =
+        buttonType.disabledLabelStyle(textTheme, colorScheme);
 
     return GestureDetector(
       onTap: onTap,
@@ -63,7 +67,7 @@ class LargeButtonWidget extends StatelessWidget {
         child: Padding(
           padding: const EdgeInsets.symmetric(vertical: 14, horizontal: 16),
           //todo: show loading or row depending on state of button
-          child: _hasTrailingIcon()
+          child: buttonType.hasTrailingIcon
               ? Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
@@ -123,181 +127,5 @@ class LargeButtonWidget extends StatelessWidget {
         ),
       ),
     );
-  }
-
-  Color _defaultButtonColor(EnteColorScheme colorScheme) {
-    if (_isPrimary()) {
-      return colorScheme.primary500;
-    }
-    if (_isSecondary()) {
-      return colorScheme.fillFaint;
-    }
-    if (buttonType == ButtonType.neutral ||
-        buttonType == ButtonType.trailingIcon) {
-      return colorScheme.fillBase;
-    }
-    if (buttonType == ButtonType.critical) {
-      return colorScheme.warning700;
-    }
-    if (buttonType == ButtonType.tertiaryCritical) {
-      return Colors.transparent;
-    }
-    return Colors.transparent;
-  }
-
-  //Returning null to fallback to default color
-  Color? _pressedButtonColor(EnteColorScheme colorScheme) {
-    if (_isPrimary()) {
-      return colorScheme.primary700;
-    }
-    return null;
-  }
-
-  //Returning null to fallback to default color
-  Color? _disabledButtonColor(EnteColorScheme colorScheme) {
-    if (buttonType == ButtonType.primary || buttonType == ButtonType.critical) {
-      return colorScheme.fillFaint;
-    }
-    return null;
-  }
-
-  Color _defaultBorderColor(EnteColorScheme colorScheme) {
-    if (buttonType == ButtonType.tertiaryCritical) {
-      return colorScheme.warning700;
-    }
-    return Colors.transparent;
-  }
-
-  //Returning null to fallback to default color
-  Color? _pressedBorderColor(EnteColorScheme colorScheme) {
-    if (buttonType == ButtonType.primary) {
-      return colorScheme.strokeMuted;
-    }
-    if (buttonType == ButtonType.secondary ||
-        buttonType == ButtonType.critical ||
-        buttonType == ButtonType.tertiaryCritical) {
-      return colorScheme.strokeBase;
-    }
-    return null;
-  }
-
-  //Returning null to fallback to default color
-  Color? _disabledBorderColor(EnteColorScheme colorScheme) {
-    if (buttonType == ButtonType.primary ||
-        buttonType == ButtonType.secondary ||
-        buttonType == ButtonType.critical) {
-      return Colors.transparent;
-    }
-    if (buttonType == ButtonType.tertiaryCritical) {
-      return colorScheme.strokeMuted;
-    }
-    return null;
-  }
-
-  Color _defaultIconColor({
-    required EnteColorScheme colorScheme,
-    required EnteColorScheme inverseColorScheme,
-  }) {
-    if (_isPrimary() || buttonType == ButtonType.critical) {
-      return strokeBaseDark;
-    }
-    if (_isSecondary()) {
-      return colorScheme.strokeBase;
-    }
-    if (buttonType == ButtonType.neutral ||
-        buttonType == ButtonType.trailingIcon) {
-      return inverseColorScheme.strokeBase;
-    }
-    if (buttonType == ButtonType.tertiaryCritical) {
-      return colorScheme.warning500;
-    }
-    //fallback
-    return colorScheme.strokeBase;
-  }
-
-  //Returning null to fallback to default color
-  Color? _pressedIconColor(EnteColorScheme colorScheme) {
-    if (buttonType == ButtonType.tertiaryCritical) {
-      return colorScheme.strokeBase;
-    }
-    return null;
-  }
-
-  //Returning null to fallback to default color
-  Color? _disabledIconColor(EnteColorScheme colorScheme) {
-    if (buttonType == ButtonType.primary ||
-        buttonType == ButtonType.secondary) {
-      return colorScheme.strokeMuted;
-    }
-    if (buttonType == ButtonType.critical ||
-        buttonType == ButtonType.tertiaryCritical) {
-      return colorScheme.strokeFaint;
-    }
-    return null;
-  }
-
-  TextStyle _defaultLabelStyle({
-    required EnteTextTheme textTheme,
-    required EnteTextTheme inverseTextTheme,
-  }) {
-    if (_isPrimary() || buttonType == ButtonType.critical) {
-      return textTheme.bodyBold.copyWith(color: textBaseDark);
-    }
-    if (_isSecondary()) {
-      return textTheme.bodyBold;
-    }
-    if (buttonType == ButtonType.neutral ||
-        buttonType == ButtonType.trailingIcon) {
-      return inverseTextTheme.bodyBold;
-    }
-    if (buttonType == ButtonType.tertiaryCritical) {
-      return textTheme.bodyBold.copyWith(color: warning500);
-    }
-    //fallback
-    return textTheme.bodyBold;
-  }
-
-  //Returning null to fallback to default color
-  TextStyle? _pressedLabelStyle(
-      EnteTextTheme textTheme, EnteColorScheme colorScheme) {
-    if (buttonType == ButtonType.tertiaryCritical) {
-      return textTheme.bodyBold.copyWith(color: colorScheme.strokeBase);
-    }
-    return null;
-  }
-
-  //Returning null to fallback to default color
-  TextStyle? _disabledLabelStyle(
-    EnteTextTheme textTheme,
-    EnteColorScheme colorScheme,
-  ) {
-    if (buttonType == ButtonType.primary ||
-        buttonType == ButtonType.secondary ||
-        buttonType == ButtonType.critical ||
-        buttonType == ButtonType.tertiaryCritical) {
-      return textTheme.bodyBold.copyWith(color: colorScheme.textFaint);
-    }
-    return null;
-  }
-
-  bool _hasTrailingIcon() {
-    return (buttonType == ButtonType.trailingIcon ||
-        buttonType == ButtonType.trailingIconPrimary ||
-        buttonType == ButtonType.trailingIconSecondary);
-  }
-
-  bool _isPrimary() {
-    return (buttonType == ButtonType.primary ||
-        buttonType == ButtonType.trailingIconPrimary);
-  }
-
-  bool _isSecondary() {
-    return (buttonType == ButtonType.secondary ||
-        buttonType == ButtonType.trailingIconSecondary);
-  }
-
-  bool _isCritical() {
-    return (buttonType == ButtonType.critical ||
-        buttonType == ButtonType.tertiaryCritical);
   }
 }
