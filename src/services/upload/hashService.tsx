22adfb9539aa7d652f1388c94889067b074297f8
Abhinav <abhinavk.grd@gmail.com>
2022-12-13 09:40:33 +0530

    add chunk count check during file hashing to prevent possible infinite loop

--- src/services/upload/hashService.tsx
+++ src/services/upload/hashService.tsx
@@ -2,10 +2,13 @@ import { FILE_READER_CHUNK_SIZE } from 'constants/upload';
 import { getFileStream, getElectronFileStream } from 'services/readerService';
 import { ElectronFile, DataStream } from 'types/upload';
 import CryptoWorker from 'utils/crypto';
+import { CustomError } from 'utils/error';
+import { addLogLine, getFileNameSize } from 'utils/logging';
 import { logError } from 'utils/sentry';
 
 export async function getFileHash(file: File | ElectronFile) {
     try {
+        addLogLine(`getFileHash called for ${getFileNameSize(file)}`);
         let filedata: DataStream;
         if (file instanceof File) {
             filedata = getFileStream(file, FILE_READER_CHUNK_SIZE);
@@ -18,19 +21,28 @@ export async function getFileHash(file: File | ElectronFile) {
         const cryptoWorker = await new CryptoWorker();
         const hashState = await cryptoWorker.initChunkHashing();
 
-        const reader = filedata.stream.getReader();
-        // eslint-disable-next-line no-constant-condition
-        while (true) {
-            const { done, value: chunk } = await reader.read();
+        const streamReader = filedata.stream.getReader();
+        for (let i = 0; i < filedata.chunkCount; i++) {
+            const { done, value: chunk } = await streamReader.read();
             if (done) {
                 break;
             }
             await cryptoWorker.hashFileChunk(hashState, Uint8Array.from(chunk));
         }
+        const { done } = await streamReader.read();
+        if (!done) {
+            throw Error(CustomError.CHUNK_MORE_THAN_EXPECTED);
+        }
         const hash = await cryptoWorker.completeChunkHashing(hashState);
+        addLogLine(
+            `file hashing completed successfully ${getFileNameSize(file)}`
+        );
         return hash;
     } catch (e) {
         logError(e, 'getFileHash failed');
+        addLogLine(
+            `file hashing failed ${getFileNameSize(file)} ,${e.message} `
+        );
         throw e;
     }
 }
