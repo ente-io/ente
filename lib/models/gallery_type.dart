df1e0c3d105b1436f25945ba132161fa038ed693
Neeraj Gupta <254676+ua741@users.noreply.github.com>
2022-12-14 22:17:33 +0530

    GalleryType: add action method for addToAlbum option

--- lib/models/gallery_type.dart
+++ lib/models/gallery_type.dart
@@ -9,3 +9,21 @@ enum GalleryType {
   ownedCollection,
   searchResults
 }
+
+extension GalleyTypeExtension on GalleryType {
+  bool showAddToAlbum() {
+    switch (this) {
+      case GalleryType.homepage:
+      case GalleryType.archive:
+      case GalleryType.localFolder:
+      case GalleryType.ownedCollection:
+      case GalleryType.searchResults:
+        return true;
+
+      case GalleryType.hidden:
+      case GalleryType.trash:
+      case GalleryType.sharedCollection:
+        return false;
+    }
+  }
+}
