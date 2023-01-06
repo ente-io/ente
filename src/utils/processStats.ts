54fc0705cdc4db66eda24f6529801134ddfedce0
Abhinav <abhinavk.grd@gmail.com>
2023-01-06 14:58:23 +0530

    fix units and add spike logger

--- src/utils/processStats.ts
+++ src/utils/processStats.ts
@@ -1,7 +1,14 @@
 import ElectronLog from 'electron-log';
 import { webFrame } from 'electron/renderer';
+import { isPlatform } from './main';
 
-const LOGGING_INTERVAL_IN_MICROSECONDS = 1 * 1000;
+const LOGGING_INTERVAL_IN_MICROSECONDS = 30 * 1000; // 30 seconds
+
+const SPIKE_DETECTION_INTERVAL_IN_MICROSECONDS = 1 * 1000; // 1 seconds
+
+const HIGH_MEMORY_USAGE_THRESHOLD_IN_KILOBYTES = 1 * 1024 * 1024; // 1 GB
+
+const LOW_MEMORY_FREE_THRESHOLD_IN_KILOBYTES = 1 * 1024 * 1024; // 1 GB
 
 async function logMainProcessStats() {
     const processMemoryInfo = await process.getProcessMemoryInfo();
@@ -22,6 +29,30 @@ async function logMainProcessStats() {
     });
 }
 
+async function logSpikeMemoryUsage() {
+    const processMemoryInfo = await process.getProcessMemoryInfo();
+    const systemMemoryInfo = process.getSystemMemoryInfo();
+    if (
+        processMemoryInfo.residentSet >
+            HIGH_MEMORY_USAGE_THRESHOLD_IN_KILOBYTES ||
+        systemMemoryInfo.free < LOW_MEMORY_FREE_THRESHOLD_IN_KILOBYTES
+    ) {
+        const normalizedProcessMemoryInfo =
+            await getNormalizedProcessMemoryInfo(processMemoryInfo);
+        const normalizedSystemMemoryInfo =
+            getNormalizedSystemMemoryInfo(systemMemoryInfo);
+        const cpuUsage = process.getCPUUsage();
+        const heapStatistics = process.getHeapStatistics();
+
+        ElectronLog.log('main process stats', {
+            processMemoryInfo: normalizedProcessMemoryInfo,
+            systemMemoryInfo: normalizedSystemMemoryInfo,
+            heapStatistics,
+            cpuUsage,
+        });
+    }
+}
+
 async function logRendererProcessStats() {
     const blinkMemoryInfo = getNormalizedBlinkMemoryInfo();
     const heapStatistics = process.getHeapStatistics();
@@ -34,6 +65,7 @@ async function logRendererProcessStats() {
 }
 
 export function setupMainProcessStatsLogger() {
+    setInterval(logSpikeMemoryUsage, SPIKE_DETECTION_INTERVAL_IN_MICROSECONDS);
     setInterval(logMainProcessStats, LOGGING_INTERVAL_IN_MICROSECONDS);
 }
 
@@ -45,9 +77,11 @@ const getNormalizedProcessMemoryInfo = async (
     processMemoryInfo: Electron.ProcessMemoryInfo
 ) => {
     return {
-        residentSet: convertBytesToHumanReadable(processMemoryInfo.residentSet),
-        private: convertBytesToHumanReadable(processMemoryInfo.private),
-        shared: convertBytesToHumanReadable(processMemoryInfo.shared),
+        residentSet: !isPlatform('mac')
+            ? convertBytesToHumanReadable(processMemoryInfo.residentSet * 1024)
+            : 0,
+        private: convertBytesToHumanReadable(processMemoryInfo.private * 10124),
+        shared: convertBytesToHumanReadable(processMemoryInfo.shared * 1024),
     };
 };
 
@@ -55,17 +89,23 @@ const getNormalizedSystemMemoryInfo = (
     systemMemoryInfo: Electron.SystemMemoryInfo
 ) => {
     return {
-        total: convertBytesToHumanReadable(systemMemoryInfo.total),
-        free: convertBytesToHumanReadable(systemMemoryInfo.free),
-        swapTotal: convertBytesToHumanReadable(systemMemoryInfo.swapTotal),
-        swapFree: convertBytesToHumanReadable(systemMemoryInfo.swapFree),
+        total: convertBytesToHumanReadable(systemMemoryInfo.total * 1024),
+        free: convertBytesToHumanReadable(systemMemoryInfo.free * 1024),
+        swapTotal: !isPlatform('mac')
+            ? convertBytesToHumanReadable(systemMemoryInfo.swapTotal * 1024)
+            : 0,
+        swapFree: !isPlatform('mac')
+            ? convertBytesToHumanReadable(systemMemoryInfo.swapFree * 1024)
+            : 0,
     };
 };
 
 const getNormalizedBlinkMemoryInfo = () => {
     const blinkMemoryInfo = process.getBlinkMemoryInfo();
     return {
-        allocated: convertBytesToHumanReadable(blinkMemoryInfo.allocated),
+        allocated: convertBytesToHumanReadable(
+            blinkMemoryInfo.allocated * 1024
+        ),
         total: convertBytesToHumanReadable(blinkMemoryInfo.total),
     };
 };
