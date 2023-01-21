a826de6d9a42e45c3e2c1e4f04e54cf806d6d881
Abhinav <abhinavk.grd@gmail.com>
2023-01-21 21:34:14 +0530

    add per second log

--- src/utils/processStats.ts
+++ src/utils/processStats.ts
@@ -100,6 +100,12 @@ async function logSpikeRendererMemoryUsage() {
         previousRendererProcessMemoryInfo.private,
         previousRendererProcessMemoryInfo.residentSet ?? 0
     );
+
+    ElectronLog.log(
+        'renderer process memory usage',
+        convertBytesToHumanReadable(currentMemoryUsage * 1024)
+    );
+
     const isSpiking =
         currentMemoryUsage - previousMemoryUsage >=
         RENDERER_MEMORY_USAGE_DIFF_IN_KILOBYTES_CONSIDERED_AS_SPIKE;
