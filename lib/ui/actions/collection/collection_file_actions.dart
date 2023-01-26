25f00f65f0de5cddbb17046f94c7781752508e84
Neeraj Gupta <254676+ua741@users.noreply.github.com>
2023-01-26 10:56:33 +0530

    Fix copy

--- lib/ui/actions/collection/collection_file_actions.dart
+++ lib/ui/actions/collection/collection_file_actions.dart
@@ -50,6 +50,7 @@ extension CollectionFileActions on CollectionActions {
           isInAlert: true,
         ),
       ],
+      title: "Remove from album?",
       body: "Selected items will be removed from this album. Items which are "
           "only in this album will be moved to Uncategorized.",
       actionSheetType: ActionSheetType.defaultActionSheet,
