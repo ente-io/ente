8be077ae3a479c8edaa11d3eeb3f8a4b8b89d844
Abhinav <abhinavk.grd@gmail.com>
2023-01-27 16:48:42 +0530

    revert mui dialog paper box shadow

--- src/themes/darkThemeOptions.tsx
+++ src/themes/darkThemeOptions.tsx
@@ -131,7 +131,7 @@ const darkThemeOptions = createTheme({
                         backgroundColor: 'rgba(0,0,0,0.65)',
                     },
                     '& .MuiDialog-paper': {
-                        boxShadow: '0px 0px 10px 0px #252525',
+                        boxShadow: '0px 0px 10px 0px rgba(0, 0, 0, 0.25)',
                     },
                     '& .MuiDialogTitle-root': {
                         padding: '16px',
