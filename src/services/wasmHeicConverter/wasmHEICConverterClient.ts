1c68eba3605f334d008feffc6b3590c04c0ef76b
Abhinav <abhinavk.grd@gmail.com>
2023-01-30 16:34:36 +0530

    better name

--- src/services/wasmHeicConverter/wasmHEICConverterClient.ts
+++ src/services/wasmHeicConverter/wasmHEICConverterClient.ts
@@ -1,4 +1,4 @@
-import heicConvert from 'heic-convert';
+import HEICConvert from 'heic-convert';
 import { getUint8ArrayView } from 'services/readerService';
 
 export async function convertHEIC(
@@ -6,7 +6,7 @@ export async function convertHEIC(
     format: 'JPEG' | 'PNG'
 ): Promise<Blob> {
     const filedata = await getUint8ArrayView(fileBlob);
-    const result = await heicConvert({ buffer: filedata, format });
+    const result = await HEICConvert({ buffer: filedata, format });
     const convertedFileData = new Uint8Array(result);
     const convertedFileBlob = new Blob([convertedFileData]);
     return convertedFileBlob;
