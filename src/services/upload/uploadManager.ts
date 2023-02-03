8a4615abf84377054767de9369c4b740991b59be
Abhinav <abhinavk.grd@gmail.com>
2023-02-03 17:27:18 +0530

    remove the use of preservePhotoswipeProps

--- src/services/upload/uploadManager.ts
+++ src/services/upload/uploadManager.ts
@@ -2,7 +2,6 @@ import { getLocalFiles } from '../fileService';
 import { SetFiles } from 'types/gallery';
 import {
     sortFiles,
-    preservePhotoswipeProps,
     decryptFile,
     getUserOwnedNonTrashedFiles,
 } from 'utils/file';
@@ -414,7 +413,7 @@ class UploadManager {
     private updateUIFiles(decryptedFile: EnteFile) {
         this.existingFiles.push(decryptedFile);
         this.existingFiles = sortFiles(this.existingFiles);
-        this.setFiles(preservePhotoswipeProps(this.existingFiles));
+        this.setFiles((files) => [...files, decryptedFile]);
     }
 
     private updateElectronRemainingFiles(
