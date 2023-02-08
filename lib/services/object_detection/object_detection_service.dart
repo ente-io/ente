f407c0e8993ba7cc1051e75084fd993a6db29e95
vishnukvmd <vishnu@ente.io>
2023-02-08 19:10:18 +0530

    Implement ObjectDetectionService

---
+++ lib/services/object_detection/object_detection_service.dart
@@ -0,0 +1,57 @@
+import "dart:isolate";
+import "dart:typed_data";
+
+import "package:logging/logging.dart";
+import "package:photos/services/object_detection/models/predictions.dart";
+import 'package:photos/services/object_detection/models/recognition.dart';
+import "package:photos/services/object_detection/tflite/classifier.dart";
+import "package:photos/services/object_detection/utils/isolate_utils.dart";
+
+class ObjectDetectionService {
+  final _logger = Logger("ObjectDetectionService");
+
+  /// Instance of [ObjectClassifier]
+  late ObjectClassifier _classifier;
+
+  /// Instance of [IsolateUtils]
+  late IsolateUtils _isolateUtils;
+
+  ObjectDetectionService._privateConstructor();
+
+  Future<void> init() async {
+    _isolateUtils = IsolateUtils();
+    await _isolateUtils.start();
+    _classifier = ObjectClassifier();
+  }
+
+  static ObjectDetectionService instance =
+      ObjectDetectionService._privateConstructor();
+
+  Future<List<String>> predict(Uint8List bytes) async {
+    try {
+      final isolateData = IsolateData(
+        bytes,
+        _classifier.interpreter.address,
+        _classifier.labels,
+      );
+      final predictions = await _inference(isolateData);
+      final Set<String> results = {};
+      for (final Recognition result in predictions.recognitions) {
+        results.add(result.label);
+      }
+      return results.toList();
+    } catch (e, s) {
+      _logger.severe(e, s);
+      rethrow;
+    }
+  }
+
+  /// Runs inference in another isolate
+  Future<Predictions> _inference(IsolateData isolateData) async {
+    final responsePort = ReceivePort();
+    _isolateUtils.sendPort.send(
+      isolateData..responsePort = responsePort.sendPort,
+    );
+    return await responsePort.first;
+  }
+}
