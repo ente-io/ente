050a0c61399217170a10774e565c2562566dce41
Abhinav <abhinavk.grd@gmail.com>
2023-02-09 23:38:10 +0530

    revert heic type change

--- src/services/wasmHeicConverter/wasmHEICConverterClient.ts
+++ src/services/wasmHeicConverter/wasmHEICConverterClient.ts
@@ -1,12 +1,12 @@
-import HEICConvert from 'heic-convert';
+import * as HeicConvert from 'heic-convert';
 import { getUint8ArrayView } from 'services/readerService';
 
 export async function convertHEIC(
     fileBlob: Blob,
-    format: 'JPEG' | 'PNG'
+    format: string
 ): Promise<Blob> {
     const filedata = await getUint8ArrayView(fileBlob);
-    const result = await HEICConvert({ buffer: filedata, format });
+    const result = await HeicConvert({ buffer: filedata, format });
     const convertedFileData = new Uint8Array(result);
     const convertedFileBlob = new Blob([convertedFileData]);
     return convertedFileBlob;
--- src/worker/convert.worker.ts
+++ src/worker/convert.worker.ts
@@ -2,7 +2,7 @@ import * as Comlink from 'comlink';
 import { convertHEIC } from 'services/wasmHeicConverter/wasmHEICConverterClient';
 
 export class DedicatedConvertWorker {
-    async convertHEIC(fileBlob: Blob, format: 'JPEG' | 'PNG') {
+    async convertHEIC(fileBlob: Blob, format: string) {
         return convertHEIC(fileBlob, format);
     }
 }
