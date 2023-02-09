6d5387432dcd4a35c4bd03500808b08e2483974a
Abhinav <abhinavk.grd@gmail.com>
2023-02-09 22:15:27 +0530

    remove ml-debug page

--- src/constants/pages/index.ts
+++ src/constants/pages/index.ts
@@ -13,6 +13,6 @@ export enum PAGES {
     VERIFY = '/verify',
     ROOT = '/',
     SHARED_ALBUMS = '/shared-albums',
-    ML_DEBUG = '/ml-debug',
+    // ML_DEBUG = '/ml-debug',
     DEDUPLICATE = '/deduplicate',
 }
