733b36387f7726d2a6644607018ed8de810c29aa
ashilkn <ashilkn99@gmail.com>
2023-02-14 14:30:02 +0530

    Made EmptyStateItem component

---
+++ lib/ui/components/empty_state_item_widget.dart
@@ -0,0 +1,29 @@
+import "package:flutter/material.dart";
+import "package:photos/theme/ente_theme.dart";
+
+class EmptyStateItemWidget extends StatelessWidget {
+  final String textContent;
+  const EmptyStateItemWidget(this.textContent, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final colorScheme = getEnteColorScheme(context);
+    final textTheme = getEnteTextTheme(context);
+    return Row(
+      children: [
+        Icon(
+          Icons.check_outlined,
+          size: 17,
+          color: colorScheme.strokeFaint,
+        ),
+        const SizedBox(width: 6),
+        Text(
+          textContent,
+          style: textTheme.small.copyWith(
+            color: colorScheme.textFaint,
+          ),
+        ),
+      ],
+    );
+  }
+}
