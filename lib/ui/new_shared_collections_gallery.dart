6a09972c43d26fc9d30cb34492e000aca7956590
ashilkn <ashilkn99@gmail.com>
2023-02-14 15:21:18 +0530

    Handled possible render over flow error on small devices

--- lib/ui/new_shared_collections_gallery.dart
+++ lib/ui/new_shared_collections_gallery.dart
@@ -25,72 +25,75 @@ class EmptyStateWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final textTheme = getEnteTextTheme(context);
-    return Padding(
-      padding: const EdgeInsets.fromLTRB(16, 12, 16, 114),
+    return SingleChildScrollView(
       child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 10),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 4, vertical: 16),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "Private sharing",
-                    style: textTheme.h3Bold,
-                    textAlign: TextAlign.start,
-                  ),
-                  const SizedBox(height: 24),
-                  Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: const [
-                      EmptyStateItemWidget(
-                        "Share only with the people you want",
-                      ),
-                      SizedBox(height: 12),
-                      EmptyStateItemWidget(
-                        "Use public links for people not on ente",
-                      ),
-                      SizedBox(height: 12),
-                      EmptyStateItemWidget(
-                        "Allow people to add photos",
-                      ),
-                    ],
-                  ),
-                ],
+        padding: const EdgeInsets.fromLTRB(16, 12, 16, 114),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 10),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 4, vertical: 16),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Private sharing",
+                      style: textTheme.h3Bold,
+                      textAlign: TextAlign.start,
+                    ),
+                    const SizedBox(height: 24),
+                    Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: const [
+                        EmptyStateItemWidget(
+                          "Share only with the people you want",
+                        ),
+                        SizedBox(height: 12),
+                        EmptyStateItemWidget(
+                          "Use public links for people not on ente",
+                        ),
+                        SizedBox(height: 12),
+                        EmptyStateItemWidget(
+                          "Allow people to add photos",
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
-            ),
-            const SizedBox(height: 16),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 4),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: const [
-                  ButtonWidget(
-                    buttonType: ButtonType.trailingIconPrimary,
-                    labelText: "Share an album now",
-                    icon: Icons.arrow_forward_outlined,
-                  ),
-                  SizedBox(height: 6),
-                  ButtonWidget(
-                    buttonType: ButtonType.trailingIconSecondary,
-                    labelText: "Collect event photos",
-                    icon: Icons.add_photo_alternate_outlined,
-                  ),
-                  SizedBox(height: 6),
-                  ButtonWidget(
-                    buttonType: ButtonType.trailingIconSecondary,
-                    labelText: "Invite your friends",
-                    icon: Icons.ios_share_outlined,
-                  ),
-                ],
+              const SizedBox(height: 16),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 4),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: const [
+                    ButtonWidget(
+                      buttonType: ButtonType.trailingIconPrimary,
+                      labelText: "Share an album now",
+                      icon: Icons.arrow_forward_outlined,
+                    ),
+                    SizedBox(height: 6),
+                    ButtonWidget(
+                      buttonType: ButtonType.trailingIconSecondary,
+                      labelText: "Collect event photos",
+                      icon: Icons.add_photo_alternate_outlined,
+                    ),
+                    SizedBox(height: 6),
+                    ButtonWidget(
+                      buttonType: ButtonType.trailingIconSecondary,
+                      labelText: "Invite your friends",
+                      icon: Icons.ios_share_outlined,
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
