a149e661f5ade96d9602804b5f34200e4d727c9d
Neeraj Gupta <254676+ua741@users.noreply.github.com>
2023-02-15 10:49:32 +0530

    Referral: Add gateway and model classes

---
+++ lib/services/storage_bonus_service.dart
@@ -0,0 +1,18 @@
+import "package:photos/core/network/network.dart";
+import "package:photos/gateways/storage_bonus_gw.dart";
+import "package:shared_preferences/shared_preferences.dart";
+
+class StorageBonusService {
+  late StorageBonusGateway gateway;
+  late SharedPreferences prefs;
+
+  void init(SharedPreferences preferences) {
+    prefs = preferences;
+    gateway = StorageBonusGateway(NetworkClient.instance.enteDio);
+  }
+
+  StorageBonusService._privateConstructor();
+
+  static StorageBonusService instance =
+      StorageBonusService._privateConstructor();
+}
