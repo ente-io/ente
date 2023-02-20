35bcfbe201fb68541c84162c158da43afc3d2895
ashilkn <ashilkn99@gmail.com>
2023-02-20 14:17:10 +0530

    Added link of componnt in figma

--- lib/ui/components/empty_state_item_widget.dart
+++ lib/ui/components/empty_state_item_widget.dart
@@ -1,6 +1,8 @@
 import "package:flutter/material.dart";
 import "package:photos/theme/ente_theme.dart";
 
+///https://www.figma.com/file/SYtMyLBs5SAOkTbfMMzhqt/ente-Visual-Design?node-id=11379%3A67490&t=VI5KulbW3HMM5MVz-4
+
 class EmptyStateItemWidget extends StatelessWidget {
   final String textContent;
   const EmptyStateItemWidget(this.textContent, {super.key});
