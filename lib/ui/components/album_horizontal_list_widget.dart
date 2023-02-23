86e35e5cfc52fa3816d53bdd31ef55c87de56e9a
Neeraj Gupta <254676+ua741@users.noreply.github.com>
2023-02-23 06:45:29 +0530

    Refactor:create widget for horizonal album listing

---
+++ lib/ui/components/album_horizontal_list_widget.dart
@@ -0,0 +1,88 @@
+import "dart:async";
+
+import "package:flutter/cupertino.dart";
+import "package:logging/logging.dart";
+import "package:photos/core/event_bus.dart";
+import "package:photos/events/collection_updated_event.dart";
+import "package:photos/models/collection_items.dart";
+import "package:photos/ui/collections/collection_item_widget.dart";
+import "package:photos/ui/common/loading_widget.dart";
+
+class AlbumHorizontalListWidget extends StatefulWidget {
+  final Future<List<CollectionWithThumbnail>> future;
+  const AlbumHorizontalListWidget(
+    this.future, {
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<AlbumHorizontalListWidget> createState() =>
+      _AlbumHorizontalListWidgetState();
+}
+
+class _AlbumHorizontalListWidgetState extends State<AlbumHorizontalListWidget> {
+  late StreamSubscription<CollectionUpdatedEvent>
+      _collectionUpdatesSubscription;
+  late Logger _logger;
+
+  @override
+  void initState() {
+    _collectionUpdatesSubscription =
+        Bus.instance.on<CollectionUpdatedEvent>().listen((event) {
+      setState(() {});
+    });
+    _logger = Logger((_AlbumHorizontalListWidgetState).toString());
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _collectionUpdatesSubscription.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<List<CollectionWithThumbnail>>(
+      future: widget.future,
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          _logger.severe("failed to fetch albums", snapshot.error);
+          return const Text("Something went wrong");
+        } else if (snapshot.hasData) {
+          final collectionsWithThumbnail =
+              snapshot.data as List<CollectionWithThumbnail>;
+          return Align(
+            alignment: Alignment.centerLeft,
+            child: SizedBox(
+              height: 200,
+              child: ListView.builder(
+                shrinkWrap: true,
+                scrollDirection: Axis.horizontal,
+                itemCount: collectionsWithThumbnail.length,
+                padding: const EdgeInsets.fromLTRB(6, 6, 6, 6),
+                itemBuilder: (context, index) {
+                  final item = collectionsWithThumbnail[index];
+                  return GestureDetector(
+                    behavior: HitTestBehavior.opaque,
+                    onTap: () async {},
+                    child: Padding(
+                      padding: const EdgeInsets.all(2.0),
+                      child: CollectionItem(
+                        item,
+                        120,
+                        shouldRender: true,
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          );
+        } else {
+          return const EnteLoadingWidget();
+        }
+      },
+    );
+  }
+}
--- lib/ui/viewer/gallery/archive_page.dart
+++ lib/ui/viewer/gallery/archive_page.dart
@@ -1,17 +1,14 @@
 import 'package:collection/collection.dart' show IterableExtension;
 import 'package:flutter/material.dart';
-import "package:logging/logging.dart";
 import 'package:photos/core/configuration.dart';
 import 'package:photos/core/event_bus.dart';
 import 'package:photos/db/files_db.dart';
 import 'package:photos/events/files_updated_event.dart';
-import "package:photos/models/collection_items.dart";
 import 'package:photos/models/gallery_type.dart';
 import 'package:photos/models/magic_metadata.dart';
 import 'package:photos/models/selected_files.dart';
 import 'package:photos/services/collections_service.dart';
-import "package:photos/ui/collections/collection_item_widget.dart";
-import "package:photos/ui/common/loading_widget.dart";
+import "package:photos/ui/components/album_horizontal_list_widget.dart";
 import 'package:photos/ui/viewer/actions/file_selection_overlay_bar.dart';
 import "package:photos/ui/viewer/gallery/empty_state.dart";
 import 'package:photos/ui/viewer/gallery/gallery.dart';
@@ -22,7 +19,6 @@ class ArchivePage extends StatelessWidget {
   final GalleryType appBarType;
   final GalleryType overlayType;
   final _selectedFiles = SelectedFiles();
-  final Logger _logger = Logger("ArchivePage");
 
   ArchivePage({
     this.tagPrefix = "archived_page",
@@ -70,46 +66,8 @@ class ArchivePage extends StatelessWidget {
       emptyState: const EmptyState(
         text: "You don't have any archived items.",
       ),
-      header: FutureBuilder<List<CollectionWithThumbnail>>(
-        future: CollectionsService.instance.getArchivedCollectionWithThumb(),
-        builder: (context, snapshot) {
-          if (snapshot.hasError) {
-            _logger.severe("failed to fetch archived albums", snapshot.error);
-            return const Text("Something went wrong");
-          } else if (snapshot.hasData) {
-            final collectionsWithThumbnail =
-                snapshot.data as List<CollectionWithThumbnail>;
-            return Align(
-              alignment: Alignment.centerLeft,
-              child: SizedBox(
-                height: 200,
-                child: ListView.builder(
-                  shrinkWrap: true,
-                  scrollDirection: Axis.horizontal,
-                  itemCount: collectionsWithThumbnail.length,
-                  padding: const EdgeInsets.fromLTRB(6, 6, 6, 6),
-                  itemBuilder: (context, index) {
-                    final item = collectionsWithThumbnail[index];
-                    return GestureDetector(
-                      behavior: HitTestBehavior.opaque,
-                      onTap: () async {},
-                      child: Padding(
-                        padding: const EdgeInsets.all(2.0),
-                        child: CollectionItem(
-                          item,
-                          120,
-                          shouldRender: true,
-                        ),
-                      ),
-                    );
-                  },
-                ),
-              ),
-            );
-          } else {
-            return const EnteLoadingWidget();
-          }
-        },
+      header: AlbumHorizontalListWidget(
+        CollectionsService.instance.getArchivedCollectionWithThumb(),
       ),
     );
     return Scaffold(
