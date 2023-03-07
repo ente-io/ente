e29d682eef953e0318e622a0897704b825573078
vishnukvmd <vishnu@ente.io>
2023-03-07 17:25:18 +0530

    Add types for callback functions

--- lib/models/typedefs.dart
+++ lib/models/typedefs.dart
@@ -3,4 +3,6 @@ import 'dart:async';
 typedef FutureVoidCallback = Future<void> Function();
 typedef BoolCallBack = bool Function();
 typedef FutureVoidCallbackParamStr = Future<void> Function(String);
+typedef VoidCallbackParamStr = void Function(String);
 typedef FutureOrVoidCallback = FutureOr<void> Function();
+typedef VoidCallback = void Function();
