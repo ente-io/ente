4ed1b041a35aa916d7671c8e9a007ff77e5bf6ac
ashilkn <ashilkn99@gmail.com>
2023-03-10 15:49:29 +0530

    Added onTap() for ChipButton

--- lib/ui/components/buttons/chip_button_widget.dart
+++ lib/ui/components/buttons/chip_button_widget.dart
@@ -4,34 +4,39 @@ import "package:photos/theme/ente_theme.dart";
 class ChipButtonWidget extends StatelessWidget {
   final String label;
   final IconData? leadingIcon;
+  final VoidCallback? onTap;
   const ChipButtonWidget(
     this.label, {
     this.leadingIcon,
+    this.onTap,
     super.key,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          leadingIcon != null
-              ? Icon(
-                  leadingIcon,
-                  size: 17,
-                )
-              : const SizedBox.shrink(),
-          const SizedBox(width: 4),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 4),
-            child: Text(
-              label,
-              style: getEnteTextTheme(context).smallBold,
-            ),
-          )
-        ],
+    return GestureDetector(
+      onTap: onTap?.call,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            leadingIcon != null
+                ? Icon(
+                    leadingIcon,
+                    size: 17,
+                  )
+                : const SizedBox.shrink(),
+            const SizedBox(width: 4),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 4),
+              child: Text(
+                label,
+                style: getEnteTextTheme(context).smallBold,
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
