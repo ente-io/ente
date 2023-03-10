68a75de2927158f008faac43deea94adf02631e4
ashilkn <ashilkn99@gmail.com>
2023-03-10 18:03:43 +0530

    ChipButton tweaks

--- lib/ui/components/buttons/chip_button_widget.dart
+++ lib/ui/components/buttons/chip_button_widget.dart
@@ -2,7 +2,7 @@ import "package:flutter/material.dart";
 import "package:photos/theme/ente_theme.dart";
 
 class ChipButtonWidget extends StatelessWidget {
-  final String label;
+  final String? label;
   final IconData? leadingIcon;
   final VoidCallback? onTap;
   const ChipButtonWidget(
@@ -16,26 +16,33 @@ class ChipButtonWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: onTap?.call,
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            leadingIcon != null
-                ? Icon(
-                    leadingIcon,
-                    size: 17,
-                  )
-                : const SizedBox.shrink(),
-            const SizedBox(width: 4),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 4),
-              child: Text(
-                label,
-                style: getEnteTextTheme(context).smallBold,
-              ),
-            )
-          ],
+      child: Container(
+        decoration: BoxDecoration(
+          color: getEnteColorScheme(context).fillFaint,
+          borderRadius: const BorderRadius.all(Radius.circular(4)),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              leadingIcon != null
+                  ? Icon(
+                      leadingIcon,
+                      size: 17,
+                    )
+                  : const SizedBox.shrink(),
+              const SizedBox(width: 4),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 4),
+                child: Text(
+                  label ?? "",
+                  style: getEnteTextTheme(context).smallBold,
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
