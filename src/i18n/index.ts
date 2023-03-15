4c4d62a5296affa2dd86a462ce7d50cd6ca461a5
Abhinav <abhinavk.grd@gmail.com>
2023-03-15 16:06:22 +0530

    remove debug for prod

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -16,7 +16,6 @@ i18n
     // init i18next
     // for all options read: https://www.i18next.com/overview/configuration-options
     .init({
-        debug: true,
         fallbackLng: 'en',
         interpolation: {
             escapeValue: false, // not needed for react as it escapes by default
