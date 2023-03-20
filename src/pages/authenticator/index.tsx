937d114f7c220b8a75ad3f4adc71bf3f79952b2d
Neeraj Gupta <254676+ua741@users.noreply.github.com>
2023-03-20 10:23:14 +0530

    Add hook to download ente auth app

--- src/pages/authenticator/index.tsx
+++ src/pages/authenticator/index.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import OTPDisplay from 'components/Authenicator/OTPDisplay';
 import { getAuthCodes } from 'services/authenticator/authenticatorService';
+import { Button } from '@mui/material';
 
 const OTPPage = () => {
     const [codes, setCodes] = useState([]);
@@ -29,8 +30,31 @@ const OTPPage = () => {
                 .includes(searchTerm.toLowerCase())
     );
 
+    const DownloadApp = () => {
+        return (
+            <div
+                style={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                }}>
+                <p>Download our mobile app to add &amp; manage your secrets.</p>
+                <a href="https://github.com/ente-io/auth#-download" download>
+                    <Button
+                        style={{
+                            backgroundColor: 'green',
+                            padding: '12px 18px',
+                            color: 'white',
+                        }}>
+                        Download
+                    </Button>
+                </a>
+            </div>
+        );
+    };
+
     return (
-        // center the page
         <div
             style={{
                 display: 'flex',
@@ -55,15 +79,20 @@ const OTPPage = () => {
                         textAlign: 'center',
                         marginTop: '32px',
                     }}>
-                    <p>No results found. </p>
-                    {/* <p style={{ marginLeft: '4px' }}>Add a new secret to get started.</p> 
-                    <p>Download ente auth mobile app to manage your secrets</p> */}
+                    {searchTerm.length !== 0 ? (
+                        <p>No results found.</p>
+                    ) : (
+                        <div />
+                    )}
                 </div>
             ) : (
                 filteredCodes.map((code) => (
                     <OTPDisplay codeInfo={code} key={code.id} />
                 ))
             )}
+            <div style={{ marginBottom: '2rem' }} />
+            <DownloadApp />
+            <div style={{ marginBottom: '4rem' }} />
         </div>
     );
 };
