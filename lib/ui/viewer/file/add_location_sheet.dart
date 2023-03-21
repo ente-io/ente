b9fdb6d1e33ecf5aff791eb21812f7240f9697b6
ashilkn <ashilkn99@gmail.com>
2023-03-21 20:30:45 +0530

    Moved Radius Picker to a separate stateless widget

--- lib/ui/viewer/file/add_location_sheet.dart
+++ lib/ui/viewer/file/add_location_sheet.dart
@@ -134,104 +134,7 @@ class _AddLocationSheetState extends State<AddLocationSheet> {
                           borderRadius: 2,
                         ),
                         const SizedBox(height: 24),
-                        Row(
-                          children: [
-                            Container(
-                              height: 48,
-                              width: 48,
-                              decoration: BoxDecoration(
-                                color: colorScheme.fillFaint,
-                                borderRadius:
-                                    const BorderRadius.all(Radius.circular(2)),
-                              ),
-                              padding: const EdgeInsets.all(4),
-                              child: Column(
-                                mainAxisSize: MainAxisSize.min,
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                children: [
-                                  Expanded(
-                                    flex: 6,
-                                    child: Text(
-                                      _selectedRadius().toInt().toString(),
-                                      style: _selectedRadius() != 1200
-                                          ? textTheme.largeBold
-                                          : textTheme.bodyBold,
-                                      textAlign: TextAlign.center,
-                                    ),
-                                  ),
-                                  Expanded(
-                                    flex: 5,
-                                    child: Text(
-                                      "km",
-                                      style: textTheme.miniMuted,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                            const SizedBox(width: 4),
-                            Expanded(
-                              child: Padding(
-                                padding:
-                                    const EdgeInsets.symmetric(horizontal: 8),
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  mainAxisSize: MainAxisSize.min,
-                                  children: [
-                                    Text("Radius", style: textTheme.body),
-                                    const SizedBox(height: 10),
-                                    SizedBox(
-                                      height: 12,
-                                      child: SliderTheme(
-                                        data: SliderThemeData(
-                                          overlayColor: Colors.transparent,
-                                          thumbColor: strokeSolidMutedLight,
-                                          activeTrackColor:
-                                              strokeSolidMutedLight,
-                                          inactiveTrackColor:
-                                              colorScheme.strokeFaint,
-                                          activeTickMarkColor:
-                                              colorScheme.strokeMuted,
-                                          inactiveTickMarkColor:
-                                              strokeSolidMutedLight,
-                                          trackShape: CustomTrackShape(),
-                                          thumbShape:
-                                              const RoundSliderThumbShape(
-                                            enabledThumbRadius: 6,
-                                            pressedElevation: 0,
-                                            elevation: 0,
-                                          ),
-                                          tickMarkShape:
-                                              const RoundSliderTickMarkShape(
-                                            tickMarkRadius: 1,
-                                          ),
-                                        ),
-                                        child: RepaintBoundary(
-                                          child: Slider(
-                                            value: InheritedLocationTagData.of(
-                                              context,
-                                            ).selectedIndex.toDouble(),
-                                            onChanged: (value) {
-                                              InheritedLocationTagData.of(
-                                                context,
-                                              ).updateSelectedIndex(
-                                                value.toInt(),
-                                              );
-                                            },
-                                            min: 0,
-                                            max: radiusValues.length - 1,
-                                            divisions: radiusValues.length - 1,
-                                          ),
-                                        ),
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
+                        const RadiusPickerWidget(),
                         const SizedBox(height: 24),
                         Text(
                           "A location groups all photos that were taken within some radius of a photo",
@@ -366,5 +269,109 @@ class CustomTrackShape extends RoundedRectSliderTrackShape {
     const trackHeight = 2.0;
     final trackWidth = parentBox.size.width;
     return Rect.fromLTWH(0, 0, trackWidth, trackHeight);
+  }
+}
+
+class RadiusPickerWidget extends StatelessWidget {
+  const RadiusPickerWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final textTheme = getEnteTextTheme(context);
+    final colorScheme = getEnteColorScheme(context);
+    return Row(
+      children: [
+        Container(
+          height: 48,
+          width: 48,
+          decoration: BoxDecoration(
+            color: colorScheme.fillFaint,
+            borderRadius: const BorderRadius.all(Radius.circular(2)),
+          ),
+          padding: const EdgeInsets.all(4),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Expanded(
+                flex: 6,
+                child: Text(
+                  _selectedRadius(context).toInt().toString(),
+                  style: _selectedRadius(context) != 1200
+                      ? textTheme.largeBold
+                      : textTheme.bodyBold,
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              Expanded(
+                flex: 5,
+                child: Text(
+                  "km",
+                  style: textTheme.miniMuted,
+                ),
+              ),
+            ],
+          ),
+        ),
+        const SizedBox(width: 4),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text("Radius", style: textTheme.body),
+                const SizedBox(height: 10),
+                SizedBox(
+                  height: 12,
+                  child: SliderTheme(
+                    data: SliderThemeData(
+                      overlayColor: Colors.transparent,
+                      thumbColor: strokeSolidMutedLight,
+                      activeTrackColor: strokeSolidMutedLight,
+                      inactiveTrackColor: colorScheme.strokeFaint,
+                      activeTickMarkColor: colorScheme.strokeMuted,
+                      inactiveTickMarkColor: strokeSolidMutedLight,
+                      trackShape: CustomTrackShape(),
+                      thumbShape: const RoundSliderThumbShape(
+                        enabledThumbRadius: 6,
+                        pressedElevation: 0,
+                        elevation: 0,
+                      ),
+                      tickMarkShape: const RoundSliderTickMarkShape(
+                        tickMarkRadius: 1,
+                      ),
+                    ),
+                    child: RepaintBoundary(
+                      child: Slider(
+                        value: InheritedLocationTagData.of(
+                          context,
+                        ).selectedIndex.toDouble(),
+                        onChanged: (value) {
+                          InheritedLocationTagData.of(
+                            context,
+                          ).updateSelectedIndex(
+                            value.toInt(),
+                          );
+                        },
+                        min: 0,
+                        max: radiusValues.length - 1,
+                        divisions: radiusValues.length - 1,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  double _selectedRadius(BuildContext context) {
+    return radiusValues[InheritedLocationTagData.of(context).selectedIndex];
   }
 }
