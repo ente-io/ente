5c5a6a58139ec3668e58ba68702a9433c77bd059
Abhinav <abhinavk.grd@gmail.com>
2023-03-22 18:52:44 +0530

    added EnteMenuItemGroup

---
+++ src/components/Menu/menuItemGroup.tsx
@@ -0,0 +1,12 @@
+import { styled } from '@mui/material';
+
+export const EnteMenuItemGroup = styled('div')`
+    & :not(:last-of-type) {
+        border-bottom-left-radius: 0;
+        border-bottom-right-radius: 0;
+    }
+    & :not(:first-of-type) {
+        border-top-left-radius: 0;
+        border-top-right-radius: 0;
+    }
+`;
