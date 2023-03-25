2069fff499b2c17672a2d9ebfdc1ff0374195888
Neeraj Gupta <254676+ua741@users.noreply.github.com>
2023-03-25 07:38:54 +0530

    Convert pure data class to interface

--- src/types/authenticator/auth_entity.ts
+++ src/types/authenticator/auth_entity.ts
@@ -1,23 +1,8 @@
-export class AuthEntity {
+export interface AuthEntity {
     id: string;
     encryptedData: string | null;
     header: string | null;
     isDeleted: boolean;
     createdAt: number;
     updatedAt: number;
-    constructor(
-        id: string,
-        encryptedData: string | null,
-        header: string | null,
-        isDeleted: boolean,
-        createdAt: number,
-        updatedAt: number
-    ) {
-        this.id = id;
-        this.encryptedData = encryptedData;
-        this.header = header;
-        this.isDeleted = isDeleted;
-        this.createdAt = createdAt;
-        this.updatedAt = updatedAt;
-    }
 }
