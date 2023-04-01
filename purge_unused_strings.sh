796d94f1c635de9b41a5b822672860351126acc4
Abhinav <abhinavk.grd@gmail.com>
2023-04-01 10:44:06 +0530

    update script with variables

--- purge_unused_strings.sh
+++ purge_unused_strings.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Set the path to the JSON file and folder
+json_file_path="./public/locales/en/translation.json"
+folder_path="./src"
+
 # Check if jq and grep are installed
 if ! command -v jq &> /dev/null || ! command -v grep &> /dev/null
 then
@@ -8,17 +12,17 @@ then
 fi
 
 # Get the keys from the JSON file
-keys=$(jq -r 'keys[]' ./public/locales/en/translation.json)
+keys=$(jq -r 'keys[]' "$json_file_path")
 
 # Loop through the keys
 for key in $keys
 do
     # Check if the key is present in any of the files in the folder
-    grep -rqE "'$key'|\"$key\""  ./src
+    grep -rqE "'$key'|\"$key\""  "$folder_path"
     if [ $? -ne 0 ]
     then
         echo "$key is not present as a string in any of the files in the folder"
         # Remove the key from the JSON file
-        jq "del(.\"$key\")" ./public/locales/en/translation.json > /tmp/temp.json && mv /tmp/temp.json ./public/locales/en/translation.json
+        jq "del(.\"$key\")" "$json_file_path" > "$json_file_path.tmp" && mv "$json_file_path.tmp" "$json_file_path"
     fi
 done
