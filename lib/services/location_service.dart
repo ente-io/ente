9bf1c3ce53e530bed2844c9048bf77a718b55a0a
Neeraj Gupta <254676+ua741@users.noreply.github.com>
2023-04-03 16:39:57 +0530

    Remove unused methods

--- lib/services/location_service.dart
+++ lib/services/location_service.dart
@@ -1,4 +1,3 @@
-import "dart:collection";
 import "dart:convert";
 import "dart:math";
 
@@ -93,46 +92,10 @@ class LocationService {
     return false;
   }
 
-  Future<void> addFileToLocation(int locationId, int fileId) async {
-    final list = getFilesByLocation(locationId.toString());
-    list.add(fileId.toString());
-    await prefs.setStringList("location_$locationId", list);
-  }
-
   List<String> getFilesByLocation(String locationId) {
     var fileList = prefs.getStringList("location_$locationId");
     fileList ??= [];
     return fileList;
-  }
-
-  List<String> getLocationsByFileID(int fileId) {
-    final locationList = getAllLocationTags();
-    final locations = List<dynamic>.of([]);
-    for (String locationString in locationList) {
-      final locationJson = json.decode(locationString);
-      locations.add(locationJson);
-    }
-    final res = List<String>.of([]);
-    for (dynamic location in locations) {
-      final list = getFilesByLocation(location["id"].toString());
-      if (list.contains(fileId.toString())) {
-        res.add(location["name"]);
-      }
-    }
-    return res;
-  }
-
-  Map<String, List<String>> clusterFilesByLocation() {
-    final map = HashMap<String, List<String>>();
-    final locations = getAllLocationTags();
-    for (String locationData in locations) {
-      final locationJson = json.decode(locationData);
-      map.putIfAbsent(
-        locationData,
-        () => getFilesByLocation(locationJson['id'].toString()),
-      );
-    }
-    return map;
   }
 }
 
