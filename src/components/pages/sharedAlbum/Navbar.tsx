f9b82cbc313e0d4e4a6f6d6533110583329247b4
Abhinav <abhinavk.grd@gmail.com>
2023-04-03 19:39:09 +0530

    update collect Photos button icon

--- src/components/pages/sharedAlbum/Navbar.tsx
+++ src/components/pages/sharedAlbum/Navbar.tsx
@@ -5,6 +5,7 @@ import UploadButton from 'components/Upload/UploadButton';
 import React from 'react';
 import GoToEnte from './GoToEnte';
 import { t } from 'i18next';
+import AddPhotoAlternateOutlined from '@mui/icons-material/AddPhotoAlternateOutlined';
 
 export default function SharedAlbumNavbar({ showUploadButton, openUploader }) {
     return (
@@ -14,8 +15,8 @@ export default function SharedAlbumNavbar({ showUploadButton, openUploader }) {
             </FluidContainer>
             {showUploadButton ? (
                 <UploadButton
-                    disableShrink
                     openUploader={openUploader}
+                    icon={<AddPhotoAlternateOutlined />}
                     text={t('ADD_PHOTOS')}
                 />
             ) : (
