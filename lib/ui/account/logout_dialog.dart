c9ce4b662a64a9f287d8661c8b7658b1e967f558
Neeraj Gupta <254676+ua741@users.noreply.github.com>
2023-04-04 15:59:28 +0530

    l10n: extract strings

--- lib/ui/account/logout_dialog.dart
+++ lib/ui/account/logout_dialog.dart
@@ -1,11 +1,13 @@
 import 'package:ente_auth/core/configuration.dart';
+import 'package:ente_auth/l10n/l10n.dart';
 import 'package:ente_auth/utils/dialog_util.dart';
 import 'package:flutter/material.dart';
 
 Future<void> autoLogoutAlert(BuildContext context) async {
+  final l10n = context.l10n;
   final AlertDialog alert = AlertDialog(
-    title: const Text("Session expired"),
-    content: const Text("Please login again"),
+    title: Text(l10n.sessionExpired),
+    content: Text(l10n.pleaseLoginAgain),
     actions: [
       TextButton(
         child: Text(
@@ -17,7 +19,7 @@ Future<void> autoLogoutAlert(BuildContext context) async {
         onPressed: () async {
           Navigator.of(context, rootNavigator: true).pop('dialog');
           Navigator.of(context).popUntil((route) => route.isFirst);
-          final dialog = createProgressDialog(context, "Logging out...");
+          final dialog = createProgressDialog(context, l10n.loggingOut);
           await dialog.show();
           await Configuration.instance.logout();
           await dialog.hide();
