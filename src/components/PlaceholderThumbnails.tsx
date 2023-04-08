58723cc5346b9af9277f8c52c92192545e09eb18
Abhinav <abhinavk.grd@gmail.com>
2023-04-08 15:18:01 +0530

    added static thumbnail and loadingThumbnail and updated fileType indicator icons and positioning

---
+++ src/components/PlaceholderThumbnails.tsx
@@ -0,0 +1,54 @@
+import PhotoOutlined from '@mui/icons-material/PhotoOutlined';
+import PlayCircleOutlineOutlined from '@mui/icons-material/PlayCircleOutlineOutlined';
+import { styled } from '@mui/material';
+import { FILE_TYPE } from 'constants/file';
+import { Overlay } from './Container';
+
+interface Iprops {
+    hasStaticThumbnail: boolean;
+    fileType: FILE_TYPE;
+    src: string;
+}
+
+const CenteredOverlay = styled(Overlay)`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const StaticThumbnail = (props: Iprops) => {
+    return (
+        <CenteredOverlay
+            sx={(theme) => ({
+                backgroundColor: theme.palette.fill.dark,
+                borderWidth: '1px',
+                borderStyle: 'solid',
+                borderColor: theme.palette.stroke.faint,
+                borderRadius: '4px',
+                '& > svg': {
+                    color: theme.palette.stroke.muted,
+                    fontSize: '50px',
+                },
+            })}>
+            {props.fileType !== FILE_TYPE.VIDEO ? (
+                <PhotoOutlined />
+            ) : (
+                <PlayCircleOutlineOutlined />
+            )}
+        </CenteredOverlay>
+    );
+};
+
+export const LoadingThumbnail = () => {
+    return (
+        <Overlay
+            sx={(theme) => ({
+                backgroundColor: theme.palette.fill.dark,
+                borderWidth: '1px',
+                borderStyle: 'solid',
+                borderColor: theme.palette.stroke.faint,
+                borderRadius: '4px',
+            })}
+        />
+    );
+};
