af2b9f250dedd8df34e079c52a837ebb415bb189
Abhinav <abhinavk.grd@gmail.com>
2023-04-12 10:47:00 +0530

    changed back to using box as base for chip

--- src/components/Chip.tsx
+++ src/components/Chip.tsx
@@ -1,8 +1,8 @@
-import { Typography, TypographyProps, styled } from '@mui/material';
+import { Box, styled } from '@mui/material';
+import { CSSProperties } from '@mui/material/styles/createTypography';
 
-export const Chip = styled((props: TypographyProps) => (
-    <Typography variant="small" {...props} />
-))(({ theme }) => ({
+export const Chip = styled(Box)(({ theme }) => ({
+    ...(theme.typography.small as CSSProperties),
     padding: '8px 12px',
     borderRadius: '4px',
     backgroundColor: theme.colors.fill.faint,
