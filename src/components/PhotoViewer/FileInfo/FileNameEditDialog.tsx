0bce61f1ac6fee1c57f5ff451fab244dc5cb534d
Abhinav <abhinavk.grd@gmail.com>
2023-04-13 16:17:05 +0530

    updated File name edit dialog to v2

--- src/components/PhotoViewer/FileInfo/FileNameEditDialog.tsx
+++ src/components/PhotoViewer/FileInfo/FileNameEditDialog.tsx
@@ -1,10 +1,8 @@
-import React from 'react';
-import { DialogContent, DialogTitle } from '@mui/material';
-import DialogBoxBase from 'components/DialogBox/base';
 import SingleInputForm, {
     SingleInputFormProps,
 } from 'components/SingleInputForm';
 import { t } from 'i18next';
+import DialogBoxV2 from 'components/DialogBoxV2';
 
 export const FileNameEditDialog = ({
     isInEditMode,
@@ -25,23 +23,23 @@ export const FileNameEditDialog = ({
         }
     };
     return (
-        <DialogBoxBase
+        <DialogBoxV2
+            sx={{ zIndex: 1600 }}
             open={isInEditMode}
             onClose={closeEditMode}
-            sx={{ zIndex: 1600 }}>
-            <DialogTitle>{t('RENAME_FILE')}</DialogTitle>
-            <DialogContent>
-                <SingleInputForm
-                    initialValue={filename}
-                    callback={onSubmit}
-                    placeholder={t('ENTER_FILE_NAME')}
-                    buttonText={t('RENAME')}
-                    fieldType="text"
-                    caption={extension}
-                    secondaryButtonAction={closeEditMode}
-                    submitButtonProps={{ sx: { mt: 1, mb: 2 } }}
-                />
-            </DialogContent>
-        </DialogBoxBase>
+            attributes={{
+                title: t('RENAME_FILE'),
+            }}>
+            <SingleInputForm
+                initialValue={filename}
+                callback={onSubmit}
+                placeholder={t('ENTER_FILE_NAME')}
+                buttonText={t('RENAME')}
+                fieldType="text"
+                caption={extension}
+                secondaryButtonAction={closeEditMode}
+                submitButtonProps={{ sx: { mt: 1, mb: 2 } }}
+            />
+        </DialogBoxV2>
     );
 };
