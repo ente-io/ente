0d592634226d0c7da04806d53481b0011cfc6abb
Neeraj Gupta <254676+ua741@users.noreply.github.com>
2023-04-15 12:25:52 +0530

    Logout: Add warning for unsycned changes

--- lib/ui/account/logout_dialog.dart
+++ lib/ui/account/logout_dialog.dart
@@ -1,5 +1,6 @@
 import 'package:ente_auth/core/configuration.dart';
 import 'package:ente_auth/l10n/l10n.dart';
+import 'package:ente_auth/store/authenticator_db.dart';
 import 'package:ente_auth/utils/dialog_util.dart';
 import 'package:flutter/material.dart';
 
@@ -19,10 +20,22 @@ Future<void> autoLogoutAlert(BuildContext context) async {
         onPressed: () async {
           Navigator.of(context, rootNavigator: true).pop('dialog');
           Navigator.of(context).popUntil((route) => route.isFirst);
-          final dialog = createProgressDialog(context, l10n.loggingOut);
-          await dialog.show();
-          await Configuration.instance.logout();
-          await dialog.hide();
+          int pendingSyncCount =
+              await AuthenticatorDB.instance.getNeedSyncCount();
+          if (pendingSyncCount > 0) {
+            showChoiceActionSheet(
+              context,
+              title: l10n.pendingSyncs,
+              body: l10n.pendingSyncsWarningBody,
+              firstButtonLabel: context.l10n.yesLogout,
+              isCritical: true,
+              firstButtonOnTap: () async {
+                await _logout(context, l10n);
+              },
+            );
+          } else {
+            await _logout(context, l10n);
+          }
         },
       ),
     ],
@@ -33,4 +46,11 @@ Future<void> autoLogoutAlert(BuildContext context) async {
       return alert;
     },
   );
+}
+
+Future<void> _logout(BuildContext context, AppLocalizations l10n) async {
+  final dialog = createProgressDialog(context, l10n.loggingOut);
+  await dialog.show();
+  await Configuration.instance.logout();
+  await dialog.hide();
 }
