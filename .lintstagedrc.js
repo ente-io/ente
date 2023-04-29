2df2b312dba0e6a501dd5d7f20b893d539c44577
httpjamesm <github@httpjames.space>
2023-04-29 12:00:49 -0400

    fix: eslint issue

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,13 +1,15 @@
 const path = require('path');
 
 const buildEslintCommand = (filenames) =>
-    `yarn run lint --fix --file ${filenames
+    `yarn lint --fix --file ${filenames
         .map((f) => path.relative(process.cwd(), f))
-        .join(' --file ')}`;
+        .join(' --file ')} --`;
 
 const buildPrettierCommand = (filenames) =>
-    `yarn prettier --write --ignore-unknown ${filenames.join(' ')}`;
+    // `yarn prettier --write --ignore-unknown ${filenames.join(' ')}`;
+    `echo nothing`
 
 module.exports = {
-    'src/**/*.{js,jsx,ts,tsx}': [buildEslintCommand, buildPrettierCommand],
+    'apps/**/*.{js,jsx,ts,tsx}': [buildEslintCommand, buildPrettierCommand],
+    'packages/**/*.{js,jsx,ts,tsx}': [buildEslintCommand, buildPrettierCommand],
 };
