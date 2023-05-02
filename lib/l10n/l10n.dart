0eb346e2e5fc256b1cd0318432c9f56e93e9563b
Neeraj Gupta <254676+ua741@users.noreply.github.com>
2023-05-02 12:31:07 +0530

    l10n: save locale with country code

--- lib/l10n/l10n.dart
+++ lib/l10n/l10n.dart
@@ -27,11 +27,20 @@ Locale localResolutionCallBack(locales, supportedLocales) {
 }
 
 Future<Locale> getLocale() async {
-  final String? savedLocale =
+  final String? savedValue =
       (await SharedPreferences.getInstance()).getString('locale');
-  if (savedLocale != null &&
-      appSupportedLocales.contains(Locale(savedLocale))) {
-    return Locale(savedLocale);
+  // if savedLocale is not null and is supported by the app, return it
+  if (savedValue != null) {
+    late Locale savedLocale;
+    if (savedValue.contains('_')) {
+      final List<String> parts = savedValue.split('_');
+      savedLocale = Locale(parts[0], parts[1]);
+    } else {
+      savedLocale = Locale(savedValue);
+    }
+    if (appSupportedLocales.contains(savedLocale)) {
+      return savedLocale;
+    }
   }
   return const Locale('en');
 }
@@ -40,6 +49,11 @@ Future<void> setLocale(Locale locale) async {
   if (!appSupportedLocales.contains(locale)) {
     throw Exception('Locale $locale is not supported by the app');
   }
+  final StringBuffer out = StringBuffer(locale.languageCode);
+  if (locale.countryCode != null && locale.countryCode!.isNotEmpty) {
+    out.write('_');
+    out.write(locale.countryCode);
+  }
   await (await SharedPreferences.getInstance())
-      .setString('locale', locale.languageCode);
+      .setString('locale', out.toString());
 }
