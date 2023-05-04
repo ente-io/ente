d233075d94d33c750025e9521069a660fb1dbee0
Abhinav <abhinavk.grd@gmail.com>
2023-05-04 15:48:41 +0530

    remove unnecessary get

--- src/components/ExportModal.tsx
+++ src/components/ExportModal.tsx
@@ -124,7 +124,6 @@ export default function ExportModal(props: Props) {
     };
 
     const verifyExportFolderExists = () => {
-        const exportFolder = exportService.getExportSettings()?.folder;
         if (!exportFolder || !exportService.exists(exportFolder)) {
             appContext.setDialogMessage(
                 getExportDirectoryDoesNotExistMessage()
