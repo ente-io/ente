efcfa7d9f08fadf6e297f4b526c15869d44ba309
Abhinav <abhinavk.grd@gmail.com>
2023-05-11 12:50:08 +0530

    update RENDERER_OUTPUT_DIR

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,5 @@
 const PROD_HOST_URL: string = 'ente://app';
-const RENDERER_OUTPUT_DIR: string = './ui/out';
+const RENDERER_OUTPUT_DIR: string = './ui/app/photos/out';
 const LOG_FILENAME = 'ente.log';
 const MAX_LOG_SIZE = 50 * 1024 * 1024; // 50MB
 
