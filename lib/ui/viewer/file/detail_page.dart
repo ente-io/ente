002cd6b489ed00eca6a0281dd506754be8c950be
Neeraj Gupta <254676+ua741@users.noreply.github.com>
2023-05-18 23:45:19 +0530

    Dispose pageController in detailed view

--- lib/ui/viewer/file/detail_page.dart
+++ lib/ui/viewer/file/detail_page.dart
@@ -87,6 +87,7 @@ class _DetailPageState extends State<DetailPage> {
 
   @override
   void dispose() {
+    _pageController?.dispose();
     SystemChrome.setEnabledSystemUIMode(
       SystemUiMode.manual,
       overlays: SystemUiOverlay.values,
@@ -140,6 +141,8 @@ class _DetailPageState extends State<DetailPage> {
 
   Widget _buildPageView() {
     _logger.info("Building with " + _selectedIndex.toString());
+    // todo: perf.. fix this
+    _pageController?.dispose();
     _pageController = PageController(initialPage: _selectedIndex);
     return PageView.builder(
       itemBuilder: (context, index) {
