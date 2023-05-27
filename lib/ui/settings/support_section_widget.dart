378111fe241d744f93ed3f8f094ad5ec67193fa0
Neeraj Gupta <254676+ua741@users.noreply.github.com>
2023-05-27 11:06:42 +0530

    Launch github issues in external app

--- lib/ui/settings/support_section_widget.dart
+++ lib/ui/settings/support_section_widget.dart
@@ -4,13 +4,13 @@ import 'package:flutter/material.dart';
 import 'package:photos/core/constants.dart';
 import "package:photos/generated/l10n.dart";
 import 'package:photos/theme/ente_theme.dart';
-import 'package:photos/ui/common/web_page.dart';
 import 'package:photos/ui/components/captioned_text_widget.dart';
 import 'package:photos/ui/components/expandable_menu_item_widget.dart';
 import 'package:photos/ui/components/menu_item_widget/menu_item_widget.dart';
 import 'package:photos/ui/settings/about_section_widget.dart';
 import 'package:photos/ui/settings/common_settings.dart';
 import 'package:photos/utils/email_util.dart';
+import "package:url_launcher/url_launcher_string.dart";
 
 class SupportSectionWidget extends StatelessWidget {
   const SupportSectionWidget({Key? key}) : super(key: key);
@@ -55,16 +55,9 @@ class SupportSectionWidget extends StatelessWidget {
           trailingIcon: Icons.chevron_right_outlined,
           trailingIconIsMuted: true,
           onTap: () async {
-            Navigator.of(context).push(
-              MaterialPageRoute(
-                builder: (BuildContext context) {
-                  return WebPage(
-                    S.of(context).suggestFeatures,
-                    githubIssuesUrl,
-                    canOpenInBrowser: true,
-                  );
-                },
-              ),
+            launchUrlString(
+              githubIssuesUrl,
+              mode: LaunchMode.externalApplication,
             );
           },
         ),
