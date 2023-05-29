aad5346b86ffb46fd6c5d3bee5f145e540b49051
Neeraj Gupta <254676+ua741@users.noreply.github.com>
2023-05-29 10:13:02 +0530

    Fix: set root:true at app level

--- apps/photos/.eslintrc.js
+++ apps/photos/.eslintrc.js
@@ -1,4 +1,8 @@
 module.exports = {
+    // When root is set to true, ESLint will stop looking for configuration files in parent directories.
+    // This is required here to ensure desktop picks the right eslint config, where this app is
+    // backaged as a submodule.
+    root: true,
     extends: ['@ente/eslint-config'],
     parser: '@typescript-eslint/parser',
     parserOptions: {
