4531577c53a97c8e50380dd9fd52b518ca0ed735
Abhinav <abhinavk.grd@gmail.com>
2023-06-05 11:02:58 +0530

    implmented dontTimeout for electron ffmpeg API

--- src/constants/errors.ts
+++ src/constants/errors.ts
@@ -2,4 +2,5 @@ export const CustomErrors = {
     WINDOWS_NATIVE_IMAGE_PROCESSING_NOT_SUPPORTED:
         'Windows native image processing is not supported',
     INVALID_OS: (os: string) => `Invalid OS - ${os}`,
+    WAIT_TIME_EXCEEDED: 'Wait time exceeded',
 };
--- src/utils/common/index.ts
+++ src/utils/common/index.ts
@@ -1,2 +1,27 @@
+import { CustomErrors } from '../../constants/errors';
 import { app } from 'electron';
 export const isDev = !app.isPackaged;
+
+export const promiseWithTimeout = async <T>(
+    request: Promise<T>,
+    timeout: number
+): Promise<T> => {
+    const timeoutRef: {
+        current: NodeJS.Timeout;
+    } = { current: null };
+    const rejectOnTimeout = new Promise<null>((_, reject) => {
+        timeoutRef.current = setTimeout(
+            () => reject(Error(CustomErrors.WAIT_TIME_EXCEEDED)),
+            timeout
+        );
+    });
+    const requestWithTimeOutCancellation = async () => {
+        const resp = await request;
+        clearTimeout(timeoutRef.current);
+        return resp;
+    };
+    return await Promise.race([
+        requestWithTimeOutCancellation(),
+        rejectOnTimeout,
+    ]);
+};
