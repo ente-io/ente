fbbf24e0fb4aa8c779113cdb43b18969ba912553
Abhinav <abhinavk.grd@gmail.com>
2023-06-06 12:01:30 +0530

    fix type issues

--- apps/photos/src/services/upload/magicMetadataService.ts
+++ apps/photos/src/services/upload/magicMetadataService.ts
@@ -1,16 +1,13 @@
 import {
-    NEW_FILE_MAGIC_METADATA,
     FilePublicMagicMetadataProps,
     FilePublicMagicMetadata,
-} from 'types/magicMetadata';
+} from 'types/file';
 import { updateMagicMetadata } from 'utils/magicMetadata';
 
 export async function constructPublicMagicMetadata(
     publicMagicMetadataProps: FilePublicMagicMetadataProps
 ): Promise<FilePublicMagicMetadata> {
     const pubMagicMetadata = await updateMagicMetadata(
-        NEW_FILE_MAGIC_METADATA,
-        null,
         publicMagicMetadataProps
     );
     return pubMagicMetadata;
