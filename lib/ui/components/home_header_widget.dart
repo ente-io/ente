f0ac1f88bf1c4f8d253dade6eab7e543a48d5a8a
ashilkn <ashilkn99@gmail.com>
2023-06-09 21:38:59 +0530

    Remove PopupMenuButton that navigated to map

--- lib/ui/components/home_header_widget.dart
+++ lib/ui/components/home_header_widget.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import "package:photos/services/search_service.dart";
 import 'package:photos/ui/components/buttons/icon_button_widget.dart';
-import 'package:photos/ui/map/map_screen.dart';
 import 'package:photos/ui/viewer/search/search_widget.dart';
 
 class HomeHeaderWidget extends StatefulWidget {
@@ -19,67 +17,23 @@ class _HomeHeaderWidgetState extends State<HomeHeaderWidget> {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
-        Flexible(
-          flex: 0,
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              IconButtonWidget(
-                iconButtonType: IconButtonType.primary,
-                icon: Icons.menu_outlined,
-                onTap: () {
-                  Scaffold.of(context).openDrawer();
-                },
-              ),
-              const SizedBox(width: 24),
-            ],
-          ),
+        Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            IconButtonWidget(
+              iconButtonType: IconButtonType.primary,
+              icon: Icons.menu_outlined,
+              onTap: () {
+                Scaffold.of(context).openDrawer();
+              },
+            ),
+          ],
         ),
-        Flexible(
-          child: AnimatedSwitcher(
-            duration: const Duration(milliseconds: 250),
-            child: widget.centerWidget,
-          ),
+        AnimatedSwitcher(
+          duration: const Duration(milliseconds: 250),
+          child: widget.centerWidget,
         ),
-        Flexible(
-          flex: 0,
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              const SearchIconWidget(),
-              PopupMenuButton(
-                padding: const EdgeInsets.symmetric(horizontal: 0),
-                offset: const Offset(0, 40),
-                itemBuilder: (context) => [
-                  PopupMenuItem<int>(
-                    value: 0,
-                    child: Row(
-                      children: const [
-                        Icon(Icons.map_outlined),
-                        SizedBox(width: 16),
-                        Text(
-                          "Map",
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-                onSelected: (item) => {
-                  if (item == 0)
-                    {
-                      Navigator.of(context).push(
-                        MaterialPageRoute(
-                          builder: (context) => MapScreen(
-                            filesFutureFn: SearchService.instance.getAllFiles,
-                          ),
-                        ),
-                      )
-                    }
-                },
-              )
-            ],
-          ),
-        )
+        const SearchIconWidget(),
       ],
     );
   }
