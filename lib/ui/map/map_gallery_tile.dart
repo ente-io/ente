ed09b3306359498fbd7977e2b734d87ea7dc2230
ashilkn <ashilkn99@gmail.com>
2023-06-10 21:54:16 +0530

    Make map's marker look better

--- lib/ui/map/map_gallery_tile.dart
+++ lib/ui/map/map_gallery_tile.dart
@@ -1,4 +1,3 @@
-
 import "package:flutter/material.dart";
 import "package:photos/ui/map/image_marker.dart";
 import "package:photos/ui/map/marker_image.dart";
@@ -10,15 +9,11 @@ class MapGalleryTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: const EdgeInsets.all(10),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(4),
-        color: Colors.black,
-      ),
+      margin: const EdgeInsets.all(8),
       child: MarkerImage(
         key: key,
         file: imageMarker.imageFile,
-        seperator: 65,
+        seperator: 69,
       ),
     );
   }
