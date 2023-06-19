4878719268d0ff34d378ef0510dee67bb40d3d4f
Abhinav <abhinavk.grd@gmail.com>
2023-06-19 11:50:51 +0530

    update Chip style

--- apps/photos/src/components/Chip.tsx
+++ apps/photos/src/components/Chip.tsx
@@ -1,14 +1,9 @@
 import { Button, styled } from '@mui/material';
 import { CSSProperties } from '@mui/material/styles/createTypography';
 
-export const Chip = styled(Button)(({ theme }) => ({
-    ...(theme.typography.small as CSSProperties),
-    padding: '8px 12px',
-    borderRadius: '4px',
-    backgroundColor: theme.colors.fill.faint,
-    fontWeight: 'bold',
-    color: 'white',
-    '&:hover': {
-        color: 'black',
-    },
-}));
+export const Chip = styled((props) => <Button color="secondary" {...props} />)(
+    ({ theme }) => ({
+        ...(theme.typography.small as CSSProperties),
+        padding: '8px',
+    })
+);
