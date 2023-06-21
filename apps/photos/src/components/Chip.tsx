e586c321770c3c81f36994fcb6383f37db5ee772
Abhinav <abhinavk.grd@gmail.com>
2023-06-21 09:29:17 +0530

    fix build

--- apps/photos/src/components/Chip.tsx
+++ apps/photos/src/components/Chip.tsx
@@ -1,9 +1,9 @@
-import { Button, styled } from '@mui/material';
+import { Button, ButtonProps, styled } from '@mui/material';
 import { CSSProperties } from '@mui/material/styles/createTypography';
 
-export const Chip = styled((props) => <Button color="secondary" {...props} />)(
-    ({ theme }) => ({
-        ...(theme.typography.small as CSSProperties),
-        padding: '8px',
-    })
-);
+export const Chip = styled((props: ButtonProps) => (
+    <Button color="secondary" {...props} />
+))(({ theme }) => ({
+    ...(theme.typography.small as CSSProperties),
+    padding: '8px',
+}));
