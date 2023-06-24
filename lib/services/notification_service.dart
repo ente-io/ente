c251c7d007c8a5a1dba0e16e715eb5b7d5274c83
vishnukvmd <vishnu@ente.io>
2023-06-24 11:32:26 +0530

    Enable notifications on iOS

--- lib/services/notification_service.dart
+++ lib/services/notification_service.dart
@@ -1,26 +1,47 @@
 import 'dart:io';
 
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import "package:shared_preferences/shared_preferences.dart";
 
 class NotificationService {
   static final NotificationService instance =
       NotificationService._privateConstructor();
+  static const String keyGrantedNotificationPermission =
+      "notification_permission_granted";
 
   NotificationService._privateConstructor();
+
+  late SharedPreferences _preferences;
   final FlutterLocalNotificationsPlugin _flutterLocalNotificationsPlugin =
       FlutterLocalNotificationsPlugin();
 
   Future<void> init() async {
-    if (!Platform.isAndroid) {
-      return;
-    }
-    const AndroidInitializationSettings initializationSettingsAndroid =
-        AndroidInitializationSettings('notification_icon');
+    _preferences = await SharedPreferences.getInstance();
+    const androidSettings = AndroidInitializationSettings('notification_icon');
+    const iosSettings = DarwinInitializationSettings();
     const InitializationSettings initializationSettings =
         InitializationSettings(
-      android: initializationSettingsAndroid,
+      android: androidSettings,
+      iOS: iosSettings,
     );
     await _flutterLocalNotificationsPlugin.initialize(initializationSettings);
+    if (Platform.isIOS && !_hasGrantedPermissions()) {
+      final result = await _flutterLocalNotificationsPlugin
+          .resolvePlatformSpecificImplementation<
+              IOSFlutterLocalNotificationsPlugin>()
+          ?.requestPermissions(
+            sound: true,
+            alert: true,
+          );
+      if (result != null && result) {
+        _preferences.setBool(keyGrantedNotificationPermission, true);
+      }
+    }
+  }
+
+  bool _hasGrantedPermissions() {
+    final result = _preferences.getBool(keyGrantedNotificationPermission);
+    return result ?? false;
   }
 
   Future<void> showNotification(String title, String message) async {
