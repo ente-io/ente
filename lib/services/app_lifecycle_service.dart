f1b36149b59246466c77e0e3085903bb885ec01e
vishnukvmd <vishnu@ente.io>
2023-06-25 14:22:33 +0530

    Save last app open time

--- lib/services/app_lifecycle_service.dart
+++ lib/services/app_lifecycle_service.dart
@@ -1,17 +1,25 @@
 import 'package:logging/logging.dart';
 import 'package:media_extension/media_extension_action_types.dart';
+import "package:shared_preferences/shared_preferences.dart";
 
 class AppLifecycleService {
+  static const String keyLastAppOpenTime = "last_app_open_time";
+
   final _logger = Logger("AppLifecycleService");
 
   bool isForeground = false;
   MediaExtentionAction mediaExtensionAction =
       MediaExtentionAction(action: IntentAction.main);
+  late SharedPreferences _preferences;
 
   static final AppLifecycleService instance =
       AppLifecycleService._privateConstructor();
 
   AppLifecycleService._privateConstructor();
+
+  Future<void> init() async {
+    _preferences = await SharedPreferences.getInstance();
+  }
 
   void setMediaExtensionAction(MediaExtentionAction mediaExtensionAction) {
     _logger.info("App invoked via ${mediaExtensionAction.action}");
@@ -20,11 +28,19 @@ class AppLifecycleService {
 
   void onAppInForeground(String reason) {
     _logger.info("App in foreground via $reason");
+    _preferences.setInt(
+      keyLastAppOpenTime,
+      DateTime.now().microsecondsSinceEpoch,
+    );
     isForeground = true;
   }
 
   void onAppInBackground(String reason) {
     _logger.info("App in background $reason");
     isForeground = false;
+  }
+
+  int getLastAppOpenTime() {
+    return _preferences.getInt(keyLastAppOpenTime) ?? 0;
   }
 }
