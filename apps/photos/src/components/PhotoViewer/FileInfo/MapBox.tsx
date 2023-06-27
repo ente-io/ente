dbff5b3e260ce966fc3c10f392f77f75c9ffccb9
Abhinav <abhinavk.grd@gmail.com>
2023-06-27 15:07:38 +0530

    refactored MapBox component

--- apps/photos/src/components/PhotoViewer/FileInfo/MapBox.tsx
+++ apps/photos/src/components/PhotoViewer/FileInfo/MapBox.tsx
@@ -1,52 +1,38 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import dynamic from 'next/dynamic';
 import { runningInBrowser } from 'utils/common';
 import 'leaflet/dist/leaflet.css';
 import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css'; // Re-uses images from ~leaflet package
-const L = runningInBrowser() ? require('leaflet') : null;
+import * as L from 'leaflet';
 runningInBrowser() ? require('leaflet-defaulticon-compatibility') : null;
+
+const LAYER_TILE_URL = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+const LAYER_TILE_ATTRIBUTION =
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+const MAP_CONTAINER_ID = 'map-container';
+const ZOOM_LEVEL = 16;
 
 interface MapBoxProps {
     location: { latitude: number; longitude: number };
 }
 
 const MapBox: React.FC<MapBoxProps> = ({ location }) => {
-    const [isClient, setIsClient] = useState(false);
-    const position: [number, number] = [location.latitude, location.longitude];
-
     useEffect(() => {
-        setIsClient(true);
-    }, []);
+        const position: L.LatLngTuple = [location.latitude, location.longitude];
+        const mapContainer = document.getElementById(MAP_CONTAINER_ID);
 
-    useEffect(() => {
-        if (isClient) {
-            const mapContainer = document.getElementById('map-container');
-            if (mapContainer && !mapContainer.hasChildNodes()) {
-                const map = L.map(mapContainer).setView(position, 16);
-
-                L.tileLayer(
-                    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-                    {
-                        attribution:
-                            '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-                    }
-                ).addTo(map);
-
-                L.marker(position)
-                    .addTo(map)
-
-                    .openPopup();
-            }
+        if (mapContainer && !mapContainer.hasChildNodes()) {
+            const map = L.map(mapContainer).setView(position, ZOOM_LEVEL);
+            L.tileLayer(LAYER_TILE_URL, {
+                attribution: LAYER_TILE_ATTRIBUTION,
+            }).addTo(map);
+            L.marker(position).addTo(map).openPopup();
         }
-    }, [isClient, position]);
-
-    if (!isClient) {
-        return null; // Render nothing on the server-side
-    }
+    }, []);
 
     return (
         <div
-            id="map-container"
+            id={MAP_CONTAINER_ID}
             style={{ height: '200px', width: '100%' }}></div>
     );
 };
