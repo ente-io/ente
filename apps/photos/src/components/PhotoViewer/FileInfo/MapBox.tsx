6a0df3762b93170b34edf94c783b3896dd87919c
Abhinav <abhinavk.grd@gmail.com>
2023-06-27 15:34:31 +0530

    fix build

--- apps/photos/src/components/PhotoViewer/FileInfo/MapBox.tsx
+++ apps/photos/src/components/PhotoViewer/FileInfo/MapBox.tsx
@@ -1,9 +1,10 @@
 import { useEffect } from 'react';
+import { styled } from '@mui/material';
+import { runningInBrowser } from 'utils/common';
+
 import 'leaflet/dist/leaflet.css';
 import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css'; // Re-uses images from ~leaflet package
-import * as L from 'leaflet';
-import 'leaflet-defaulticon-compatibility';
-import { styled } from '@mui/material';
+runningInBrowser() && require('leaflet-defaulticon-compatibility');
 
 const LAYER_TILE_URL = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 const LAYER_TILE_ATTRIBUTION =
@@ -22,16 +23,23 @@ interface MapBoxProps {
 
 const MapBox: React.FC<MapBoxProps> = ({ location }) => {
     useEffect(() => {
-        const position: L.LatLngTuple = [location.latitude, location.longitude];
-        const mapContainer = document.getElementById(MAP_CONTAINER_ID);
+        const main = async () => {
+            const L = await import('leaflet');
+            const position: L.LatLngTuple = [
+                location.latitude,
+                location.longitude,
+            ];
+            const mapContainer = document.getElementById(MAP_CONTAINER_ID);
 
-        if (mapContainer && !mapContainer.hasChildNodes()) {
-            const map = L.map(mapContainer).setView(position, ZOOM_LEVEL);
-            L.tileLayer(LAYER_TILE_URL, {
-                attribution: LAYER_TILE_ATTRIBUTION,
-            }).addTo(map);
-            L.marker(position).addTo(map).openPopup();
-        }
+            if (mapContainer && !mapContainer.hasChildNodes()) {
+                const map = L.map(mapContainer).setView(position, ZOOM_LEVEL);
+                L.tileLayer(LAYER_TILE_URL, {
+                    attribution: LAYER_TILE_ATTRIBUTION,
+                }).addTo(map);
+                L.marker(position).addTo(map).openPopup();
+            }
+        };
+        main();
     }, []);
 
     return <MapBoxContainer id={MAP_CONTAINER_ID} />;
