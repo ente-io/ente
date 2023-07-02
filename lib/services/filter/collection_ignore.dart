a69037bc83e2e6d89543b9361a716f08712dbd9b
Neeraj Gupta <254676+ua741@users.noreply.github.com>
2023-07-02 19:41:43 +0530

    Fix: Show files queued for upload on home gallery

--- lib/services/filter/collection_ignore.dart
+++ lib/services/filter/collection_ignore.dart
@@ -1,6 +1,9 @@
 import "package:photos/models/file.dart";
 import "package:photos/services/filter/filter.dart";
 
+// CollectionsIgnoreFilter will filter out files that are in present in the
+// given collections. This is useful for filtering out files that are in archive
+// or hidden collections from home page and other places
 class CollectionsIgnoreFilter extends Filter {
   final Set<int> collectionIDs;
 
@@ -24,7 +27,16 @@ class CollectionsIgnoreFilter extends Filter {
 
   @override
   bool filter(File file) {
-    return file.isUploaded &&
-        !_ignoredUploadIDs!.contains(file.uploadedFileID!);
+    if (!file.isUploaded) {
+      // if file is in one of the ignored collections, filter it out. This check
+      // avoids showing un-uploaded files that are going to be uploaded to one of
+      // the ignored collections
+      if (file.collectionID != null &&
+          collectionIDs.contains(file.collectionID!)) {
+        return false;
+      }
+      return true;
+    }
+    return !_ignoredUploadIDs!.contains(file.uploadedFileID!);
   }
 }
