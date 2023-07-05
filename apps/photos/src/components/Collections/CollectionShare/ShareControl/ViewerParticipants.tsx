2be02d880e9688cf2803f2c0df63e3d560bbda32
jubitjohn <jubitjohn1@gmail.com>
2023-07-05 10:51:39 +0530

    ViewerList Added

--- apps/photos/src/components/Collections/CollectionShare/ShareControl/ViewerParticipants.tsx
+++ apps/photos/src/components/Collections/CollectionShare/ShareControl/ViewerParticipants.tsx
@@ -1,11 +1,12 @@
 import { Box } from '@mui/material';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { t } from 'i18next';
 import { Collection } from 'types/collection';
 import { MenuItemGroup } from 'components/Menu/MenuItemGroup';
 import { EnteMenuItem } from 'components/Menu/EnteMenuItem';
 import MenuItemDivider from 'components/Menu/MenuItemDivider';
 import MenuSectionTitle from 'components/Menu/MenuSectionTitle';
+import AvatarCollectionShare from '../AvatarCollectionShare';
 
 interface Iprops {
     collection: Collection;
@@ -24,28 +25,27 @@ export function ViewerParticipants({ collection }: Iprops) {
         'Custom',
     ];
 
-    // const [Viewers, setViewers] = useState([]);
+    const [Viewers, setViewers] = useState([]);
 
-    // useEffect(() => {
-    //     collection.sharees?.map((item) => {
-    //         if (item.role === 'viewer')
-    //             setViewers((prevViewers) => [...prevViewers, item.email]);
-    //     });
-    // }, [collection.sharees]);
-
-    // console.log(Viewers);
+    useEffect(() => {
+        collection.sharees?.map((sharee) => {
+            if (sharee.role === 'VIEWER')
+                setViewers((prevViewers) => [...prevViewers, sharee.email]);
+        });
+    }, [collection.sharees]);
 
     return (
         <Box mb={3}>
             <MenuSectionTitle title={t('Viewers')} />
             <MenuItemGroup>
-                {shareExpireOption.map((item, index) => (
+                {Viewers.map((item, index) => (
                     <>
                         <EnteMenuItem
                             fontWeight="normal"
                             key={item}
                             onClick={() => console.log('clicked')}
                             label={item}
+                            startIcon={<AvatarCollectionShare email={item} />}
                         />
                         {index !== shareExpireOption.length - 1 && (
                             <MenuItemDivider />
