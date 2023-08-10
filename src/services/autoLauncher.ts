29e8bd9dcb56e4bb5328115435afcf28c2801b80
Abhinav <abhinavk.grd@gmail.com>
2023-08-10 14:27:20 +0530

    add migration for already turned on auto-launch for windows

--- src/services/autoLauncher.ts
+++ src/services/autoLauncher.ts
@@ -5,29 +5,34 @@ import macAutoLauncher from './autoLauncherClients/macAutoLauncher';
 
 class AutoLauncher {
     private client: AutoLauncherClient;
-    init() {
+    async init() {
         if (isPlatform('linux') || isPlatform('windows')) {
             this.client = linuxAndWinAutoLauncher;
         } else {
             this.client = macAutoLauncher;
         }
+        // migrate old auto launch settings for windows from mac auto launcher to linux and windows auto launcher
+        if (isPlatform('windows') && (await macAutoLauncher.isEnabled())) {
+            await macAutoLauncher.toggleAutoLaunch();
+            await linuxAndWinAutoLauncher.toggleAutoLaunch();
+        }
     }
     async isEnabled() {
         if (!this.client) {
-            this.init();
+            await this.init();
         }
         return await this.client.isEnabled();
     }
     async toggleAutoLaunch() {
         if (!this.client) {
-            this.init();
+            await this.init();
         }
         await this.client.toggleAutoLaunch();
     }
 
-    wasAutoLaunched() {
+    async wasAutoLaunched() {
         if (!this.client) {
-            this.init();
+            await this.init();
         }
         return this.client.wasAutoLaunched();
     }
