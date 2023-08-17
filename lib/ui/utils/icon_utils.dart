041e90a8ed024d7c2f3a8837c3099b6cbdb5817b
vishnukvmd <vishnu@ente.io>
2023-08-17 23:16:10 +0530

    Setup custom icons

--- lib/ui/utils/icon_utils.dart
+++ lib/ui/utils/icon_utils.dart
@@ -4,14 +4,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_svg/svg.dart';
-import 'package:logging/logging.dart';
 
 class IconUtils {
   IconUtils._privateConstructor();
 
   static final IconUtils instance = IconUtils._privateConstructor();
 
+  // Map of icon-title to the color code in HEX
   final Map<String, String> _simpleIcons = {};
+  final Map<String, String> _customIcons = {};
 
   Future<void> init() async {
     await _loadJson();
@@ -20,33 +21,47 @@ class IconUtils {
   Widget getIcon(String provider) {
     final title = _getProviderTitle(provider);
     if (_simpleIcons.containsKey(title)) {
-      return SvgPicture.asset(
-        _getIconPath(provider),
-        semanticsLabel: title,
-        colorFilter: ColorFilter.mode(
-          Color(int.parse("0xFF" + _simpleIcons[title]!)),
-          BlendMode.srcIn,
-        ),
+      return _getSVGIcon(
+        "assets/simple-icons/icons/$title.svg",
+        title,
+        _simpleIcons[title]!,
+      );
+    } else if (_customIcons.containsKey(title)) {
+      return _getSVGIcon(
+        "assets/custom-icons/icons/$title.svg",
+        title,
+        _customIcons[title]!,
       );
     } else {
       return Text(title);
     }
   }
 
-  Future<void> _loadJson() async {
-    final data = await rootBundle
-        .loadString('assets/simple-icons/_data/simple-icons.json');
-    final result = json.decode(data);
-    Logger("IconUtils").info(result["icons"].length);
-    for (final icon in result["icons"]) {
-      _simpleIcons[icon["title"].toString().toLowerCase()] = icon["hex"];
-    }
-    Logger("IconUtils").info(_simpleIcons);
+  Widget _getSVGIcon(String path, String title, String color) {
+    return SvgPicture.asset(
+      path,
+      width: 24,
+      semanticsLabel: title,
+      colorFilter: ColorFilter.mode(
+        Color(int.parse("0xFF" + color)),
+        BlendMode.srcIn,
+      ),
+    );
   }
 
-  String _getIconPath(String provider) {
-    final title = _getProviderTitle(provider);
-    return "assets/simple-icons/icons/$title.svg";
+  Future<void> _loadJson() async {
+    final simpleIconData = await rootBundle
+        .loadString('assets/simple-icons/_data/simple-icons.json');
+    final simpleIcons = json.decode(simpleIconData);
+    for (final icon in simpleIcons["icons"]) {
+      _simpleIcons[icon["title"].toString().toLowerCase()] = icon["hex"];
+    }
+    final customIconData = await rootBundle
+        .loadString('assets/custom-icons/_data/custom-icons.json');
+    final customIcons = json.decode(customIconData);
+    for (final icon in customIcons["icons"]) {
+      _customIcons[icon["title"].toString().toLowerCase()] = icon["hex"];
+    }
   }
 
   String _getProviderTitle(String provider) {
