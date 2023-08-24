b50fde47531310c6bbcfca3f8b320736a0dbabf2
Neeraj Gupta <254676+ua741@users.noreply.github.com>
2023-08-24 22:26:00 +0530

    Rename: File -> EnteFile

--- lib/models/ente_file.dart
+++
@@ -1,8 +0,0 @@
-// EnteFile is base file entry for various type of files
-// like DeviceFile,RemoteFile or TrashedFile
-
-abstract class EnteFile {
-  // returns cacheKey which should be used while caching entry related to
-  // this file.
-  String cacheKey();
-}
