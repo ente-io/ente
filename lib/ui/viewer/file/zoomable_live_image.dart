f81c92bfeeb4a85b0f291160f758c7fe902fe01d
Neeraj Gupta <254676+ua741@users.noreply.github.com>
2023-08-25 10:46:16 +0530

    Fix type mistmatch issues

--- lib/ui/viewer/file/zoomable_live_image.dart
+++ lib/ui/viewer/file/zoomable_live_image.dart
@@ -9,7 +9,6 @@ import 'package:photos/core/constants.dart';
 import "package:photos/generated/l10n.dart";
 import "package:photos/models/file/extensions/file_props.dart";
 import 'package:photos/models/file/file.dart';
-import 'package:photos/models/file/file_type.dart';
 import "package:photos/models/metadata/file_magic.dart";
 import "package:photos/services/file_magic_service.dart";
 import 'package:photos/ui/viewer/file/zoomable_image.dart';
@@ -19,13 +18,13 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:video_player/video_player.dart';
 
 class ZoomableLiveImage extends StatefulWidget {
-  final EnteFile file;
+  final EnteFile enteFile;
   final Function(bool)? shouldDisableScroll;
   final String? tagPrefix;
   final Decoration? backgroundDecoration;
 
   const ZoomableLiveImage(
-    this.file, {
+    this.enteFile, {
     Key? key,
     this.shouldDisableScroll,
     required this.tagPrefix,
@@ -39,7 +38,7 @@ class ZoomableLiveImage extends StatefulWidget {
 class _ZoomableLiveImageState extends State<ZoomableLiveImage>
     with SingleTickerProviderStateMixin {
   final Logger _logger = Logger("ZoomableLiveImage");
-  late EnteFile _file;
+  late EnteFile _enteFile;
   bool _showVideo = false;
   bool _isLoadingVideoPlayer = false;
 
@@ -48,7 +47,7 @@ class _ZoomableLiveImageState extends State<ZoomableLiveImage>
 
   @override
   void initState() {
-    _file = widget.file;
+    _enteFile = widget.enteFile;
     Future.microtask(() => _showHintForMotionPhotoPlay).ignore();
     super.initState();
   }
@@ -77,7 +76,7 @@ class _ZoomableLiveImageState extends State<ZoomableLiveImage>
       content = _getVideoPlayer();
     } else {
       content = ZoomableImage(
-        _file,
+        _enteFile,
         tagPrefix: widget.tagPrefix,
         shouldDisableScroll: widget.shouldDisableScroll,
         backgroundDecoration: widget.backgroundDecoration,
@@ -125,27 +124,30 @@ class _ZoomableLiveImageState extends State<ZoomableLiveImage>
       return;
     }
     _isLoadingVideoPlayer = true;
-    final File? videoFile = _file.fileType == FileType.livePhoto
+    // For non-live photo, with fileType as Image, we still call _getMotionPhoto
+    // to check if it is a motion photo. This is needed to handle earlier
+    // uploads and upload from desktop
+    final File? videoFile = _enteFile.isLivePhoto
         ? await _getLivePhotoVideo()
         : await _getMotionPhotoVideo();
 
     if (videoFile != null && videoFile.existsSync()) {
       _setVideoPlayerController(file: videoFile);
-    } else if (_file.fileType == FileType.livePhoto) {
+    } else if (_enteFile.isLivePhoto) {
       showShortToast(context, S.of(context).downloadFailed);
     }
     _isLoadingVideoPlayer = false;
   }
 
   Future<File?> _getLivePhotoVideo() async {
-    if (_file.isRemoteFile && !(await isFileCached(_file, liveVideo: true))) {
+    if (_enteFile.isRemoteFile && !(await isFileCached(_enteFile, liveVideo: true))) {
       showShortToast(context, S.of(context).downloading);
     }
 
-    File? videoFile = await getFile(widget.file, liveVideo: true)
+    File? videoFile = await getFile(widget.enteFile, liveVideo: true)
         .timeout(const Duration(seconds: 15))
         .onError((dynamic e, s) {
-      _logger.info("getFile failed ${_file.tag}", e);
+      _logger.info("getFile failed ${_enteFile.tag}", e);
       return null;
     });
 
@@ -153,11 +155,11 @@ class _ZoomableLiveImageState extends State<ZoomableLiveImage>
     // getFile with liveVideo as true can fail for file with localID when
     // the live photo was downloaded from remote.
     if ((videoFile == null || !videoFile.existsSync()) &&
-        _file.uploadedFileID != null) {
-      videoFile = await getFileFromServer(widget.file, liveVideo: true)
+        _enteFile.uploadedFileID != null) {
+      videoFile = await getFileFromServer(widget.enteFile, liveVideo: true)
           .timeout(const Duration(seconds: 15))
           .onError((dynamic e, s) {
-        _logger.info("getRemoteFile failed ${_file.tag}", e);
+        _logger.info("getRemoteFile failed ${_enteFile.tag}", e);
         return null;
       });
     }
@@ -165,25 +167,25 @@ class _ZoomableLiveImageState extends State<ZoomableLiveImage>
   }
 
   Future<File?> _getMotionPhotoVideo() async {
-    if (_file.isRemoteFile && !(await isFileCached(_file))) {
+    if (_enteFile.isRemoteFile && !(await isFileCached(_enteFile))) {
       showShortToast(context, S.of(context).downloading);
     }
 
     final File? imageFile = await getFile(
-      widget.file,
+      widget.enteFile,
       isOrigin: !Platform.isAndroid,
     ).timeout(const Duration(seconds: 15)).onError((dynamic e, s) {
-      _logger.info("getFile failed ${_file.tag}", e);
+      _logger.info("getFile failed ${_enteFile.tag}", e);
       return null;
     });
     if (imageFile != null) {
       final motionPhoto = MotionPhotos(imageFile.path);
       final index = await motionPhoto.getMotionVideoIndex();
       if (index != null) {
-        if (widget.file.pubMagicMetadata?.mvi == null &&
-            (widget.file.ownerID ?? 0) == Configuration.instance.getUserID()!) {
+        if (widget.enteFile.pubMagicMetadata?.mvi == null &&
+            (widget.enteFile.ownerID ?? 0) == Configuration.instance.getUserID()!) {
           FileMagicService.instance.updatePublicMagicMetadata(
-            [widget.file],
+            [widget.enteFile],
             {motionVideoIndexKey: index.start},
           ).ignore();
         }
@@ -209,7 +211,7 @@ class _ZoomableLiveImageState extends State<ZoomableLiveImage>
   }
 
   void _showHintForMotionPhotoPlay() async {
-    if (!_file.isLiveOrMotionPhoto) {
+    if (!_enteFile.isLiveOrMotionPhoto) {
       return;
     }
     final preferences = await SharedPreferences.getInstance();
