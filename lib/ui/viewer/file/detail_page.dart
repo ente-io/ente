5c9c3531ab722d978340a8f20ee91751584e8a60
Neeraj Gupta <254676+ua741@users.noreply.github.com>
2023-08-30 09:49:53 +0530

    Fix: Increase page scroll speed
    Signed-off-by: Neeraj Gupta <254676+ua741@users.noreply.github.com>

--- lib/ui/viewer/file/detail_page.dart
+++ lib/ui/viewer/file/detail_page.dart
@@ -7,6 +7,7 @@ import 'package:photos/core/constants.dart';
 import 'package:photos/core/errors.dart';
 import "package:photos/generated/l10n.dart";
 import 'package:photos/models/file/file.dart';
+import "package:photos/ui/common/fast_scroll_physics.dart";
 import 'package:photos/ui/tools/editor/image_editor_page.dart';
 import "package:photos/ui/viewer/file/file_app_bar.dart";
 import "package:photos/ui/viewer/file/file_bottom_bar.dart";
@@ -200,7 +201,7 @@ class _DetailPageState extends State<DetailPage> {
       },
       physics: _shouldDisableScroll
           ? const NeverScrollableScrollPhysics()
-          : const PageScrollPhysics(),
+          : const FastScrollPhysics(speedFactor: 4.0),
       controller: _pageController,
       itemCount: _files!.length,
     );
