14434a25c3b78965f4d3c1526631d7ab84f79ae5
ashilkn <ashilkn99@gmail.com>
2023-08-31 12:34:51 +0530

    Media_kit: Show loading states when downloading and decrypting video

--- lib/ui/viewer/file/video_widget_new.dart
+++ lib/ui/viewer/file/video_widget_new.dart
@@ -1,5 +1,6 @@
 import "dart:io";
 
+import "package:flutter/cupertino.dart";
 import "package:flutter/material.dart";
 import "package:media_kit/media_kit.dart";
 import "package:media_kit_video/media_kit_video.dart";
@@ -7,8 +8,10 @@ import "package:photos/generated/l10n.dart";
 import "package:photos/models/file/extensions/file_props.dart";
 import "package:photos/models/file/file.dart";
 import "package:photos/services/files_service.dart";
+import "package:photos/ui/viewer/file/thumbnail_widget.dart";
 import "package:photos/utils/dialog_util.dart";
 import "package:photos/utils/file_util.dart";
+import "package:photos/utils/toast_util.dart";
 
 class VideoWidgetNew extends StatefulWidget {
   final EnteFile file;
@@ -22,7 +25,8 @@ class _VideoWidgetNewState extends State<VideoWidgetNew> {
   // Create a [Player] to control playback.
   late final player = Player();
   // Create a [VideoController] to handle video output from [Player].
-  late final controller = VideoController(player);
+  VideoController? controller;
+  final _progressNotifier = ValueNotifier<double?>(null);
 
   @override
   void initState() {
@@ -33,11 +37,7 @@ class _VideoWidgetNewState extends State<VideoWidgetNew> {
     } else if (widget.file.isSharedMediaToAppSandbox) {
       final localFile = File(getSharedMediaFilePath(widget.file));
       if (localFile.existsSync()) {
-        player.open(
-          Media(
-            localFile.path,
-          ),
-        );
+        _setVideoController(localFile.path);
       } else if (widget.file.uploadedFileID != null) {
         _loadNetworkVideo();
       }
@@ -49,12 +49,9 @@ class _VideoWidgetNewState extends State<VideoWidgetNew> {
           }
         } else {
           asset.getMediaUrl().then((url) {
-            player.open(
-              Media(
-                //falling back to a default video to know when url is null
-                url ??
-                    'https://user-images.githubusercontent.com/28951144/229373695-22f88f13-d18f-4288-9bf1-c3e078d83722.mp4',
-              ),
+            _setVideoController(
+              url ??
+                  'https://user-images.githubusercontent.com/28951144/229373695-22f88f13-d18f-4288-9bf1-c3e078d83722.mp4',
             );
           });
         }
@@ -65,26 +62,33 @@ class _VideoWidgetNewState extends State<VideoWidgetNew> {
   @override
   void dispose() {
     player.dispose();
+    // _progressNotifier.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: SizedBox(
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.width * 9.0 / 16.0,
-        child: Video(controller: controller),
-      ),
+      child: controller != null
+          ? Video(controller: controller!)
+          : _getLoadingWidget(),
     );
   }
 
   void _loadNetworkVideo() {
     getFileFromServer(
       widget.file,
+      progressCallback: (count, total) {
+        _progressNotifier.value = count / (widget.file.fileSize ?? total);
+        if (_progressNotifier.value == 1) {
+          if (mounted) {
+            showShortToast(context, S.of(context).decryptingVideo);
+          }
+        }
+      },
     ).then((file) {
       if (file != null) {
-        player.open(Media(file.path));
+        _setVideoController(file.path);
       }
     }).onError((error, stackTrace) {
       showErrorDialog(context, "Error", S.of(context).failedToDownloadVideo);
@@ -100,6 +104,59 @@ class _VideoWidgetNewState extends State<VideoWidgetNew> {
         if (mounted) {
           setState(() {});
         }
+      });
+    }
+  }
+
+  Widget _getLoadingWidget() {
+    return Stack(
+      children: [
+        _getThumbnail(),
+        Container(
+          color: Colors.black12,
+          constraints: const BoxConstraints.expand(),
+        ),
+        Center(
+          child: SizedBox.fromSize(
+            size: const Size.square(20),
+            child: ValueListenableBuilder(
+              valueListenable: _progressNotifier,
+              builder: (BuildContext context, double? progress, _) {
+                return progress == null || progress == 1
+                    ? const CupertinoActivityIndicator(
+                        color: Colors.white,
+                      )
+                    : CircularProgressIndicator(
+                        backgroundColor: Colors.black,
+                        value: progress,
+                        valueColor: const AlwaysStoppedAnimation<Color>(
+                          Color.fromRGBO(45, 194, 98, 1.0),
+                        ),
+                      );
+              },
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _getThumbnail() {
+    return Container(
+      color: Colors.black,
+      constraints: const BoxConstraints.expand(),
+      child: ThumbnailWidget(
+        widget.file,
+        fit: BoxFit.contain,
+      ),
+    );
+  }
+
+  void _setVideoController(String url) {
+    if (mounted) {
+      setState(() {
+        controller = VideoController(player);
+        player.open(Media(url));
       });
     }
   }
