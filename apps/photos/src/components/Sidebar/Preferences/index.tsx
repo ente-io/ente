c07067796e9738c21989ebb6c378e16446541aee
Abhinav <abhinavk.grd@gmail.com>
2023-09-06 12:20:50 +0530

    adds Sidebar Preferences setting to opt out of crash reporting

--- apps/photos/src/components/Sidebar/Preferences/index.tsx
+++ apps/photos/src/components/Sidebar/Preferences/index.tsx
@@ -10,10 +10,17 @@ import AdvancedSettings from '../AdvancedSettings';
 import MapSettings from '../MapSetting';
 import { LanguageSelector } from './LanguageSelector';
 import { EnteMenuItem } from 'components/Menu/EnteMenuItem';
+import { LS_KEYS } from 'utils/storage/localStorage';
+import { useLocalState } from 'hooks/useLocalState';
+import ElectronService from 'services/electron/common';
 
 export default function Preferences({ open, onClose, onRootClose }) {
     const [advancedSettingsView, setAdvancedSettingsView] = useState(false);
     const [mapSettingsView, setMapSettingsView] = useState(false);
+    const [optOutOfCrashReports, setOptOutOfCrashReports] = useLocalState(
+        LS_KEYS.OPT_OUT_OF_CRASH_REPORTS,
+        false
+    );
 
     const openAdvancedSettings = () => setAdvancedSettingsView(true);
     const closeAdvancedSettings = () => setAdvancedSettingsView(false);
@@ -32,6 +39,13 @@ export default function Preferences({ open, onClose, onRootClose }) {
         } else {
             onClose();
         }
+    };
+
+    const toggleOptOutOfCrashReports = () => {
+        if (isElectron()) {
+            ElectronService.updateOptOutOfCrashReports(!optOutOfCrashReports);
+        }
+        setOptOutOfCrashReports(!optOutOfCrashReports);
     };
 
     return (
@@ -62,6 +76,12 @@ export default function Preferences({ open, onClose, onRootClose }) {
                             onClick={openMapSettings}
                             endIcon={<ChevronRight />}
                             label={t('MAP')}
+                        />
+                        <EnteMenuItem
+                            variant="toggle"
+                            checked={!optOutOfCrashReports}
+                            onClick={toggleOptOutOfCrashReports}
+                            label={t('CRASH_REPORTING')}
                         />
                     </Stack>
                 </Box>
