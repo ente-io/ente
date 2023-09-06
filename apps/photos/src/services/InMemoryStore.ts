db8f76987472548b1ed57929c6f9fc68a776a083
Abhinav <abhinavk.grd@gmail.com>
2023-09-06 13:02:18 +0530

    adds InMemoryStore

---
+++ apps/photos/src/services/InMemoryStore.ts
@@ -0,0 +1,23 @@
+export enum MS_KEYS {
+    OPT_OUT_OF_CRASH_REPORTS = 'optOutOfCrashReports',
+}
+
+type StoreType = Map<Partial<MS_KEYS>, any>;
+
+class InMemoryStore {
+    private store: StoreType = new Map();
+
+    get(key: MS_KEYS) {
+        return this.store.get(key);
+    }
+
+    set(key: MS_KEYS, value: any) {
+        this.store.set(key, value);
+    }
+
+    has(key: MS_KEYS) {
+        return this.store.has(key);
+    }
+}
+
+export default new InMemoryStore();
