539e4084fc82dac4d904de5f37652fc14e7818a2
Abhinav <abhinavk.grd@gmail.com>
2023-09-07 09:48:13 +0530

    reset optOutOfCrashReports as clearElectronStore resets items specific to user

--- src/api/electronStore.ts
+++ src/api/electronStore.ts
@@ -2,12 +2,14 @@ import { keysStore } from '../stores/keys.store';
 import { safeStorageStore } from '../stores/safeStorage.store';
 import { uploadStatusStore } from '../stores/upload.store';
 import { logError } from '../services/logging';
+import { userPreferencesStore } from '../stores/userPreferences.store';
 
 export const clearElectronStore = () => {
     try {
         uploadStatusStore.clear();
         keysStore.clear();
         safeStorageStore.clear();
+        userPreferencesStore.delete('optOutOfCrashReports');
     } catch (e) {
         logError(e, 'error while clearing electron store');
         throw e;
