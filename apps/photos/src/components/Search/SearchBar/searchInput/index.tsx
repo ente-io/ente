77852c5e8095a1153974fff8023fe702b29dcc95
Abhinav <abhinavk.grd@gmail.com>
2023-09-09 14:57:25 +0530

    add file type search

--- apps/photos/src/components/Search/SearchBar/searchInput/index.tsx
+++ apps/photos/src/components/Search/SearchBar/searchInput/index.tsx
@@ -27,6 +27,7 @@ import { Person, Thing, WordGroup } from 'types/machineLearning';
 import { t } from 'i18next';
 import memoize from 'memoize-one';
 import { LocationTagData } from 'types/entity';
+import { FILE_TYPE } from 'constants/file';
 
 interface Iprops {
     isOpen: boolean;
@@ -117,6 +118,10 @@ export default function SearchInput(props: Iprops) {
                 break;
             case SuggestionType.TEXT:
                 search = { text: selectedOption.value as WordGroup };
+                break;
+            case SuggestionType.FILE_TYPE:
+                search = { fileType: selectedOption.value as FILE_TYPE };
+                break;
         }
         props.updateSearch(search, {
             optionName: selectedOption.label,
