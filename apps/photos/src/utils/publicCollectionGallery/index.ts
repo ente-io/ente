66f918619b53a7485d0910406cb1e926c66f03d5
Abhinav <abhinavk.grd@gmail.com>
2023-09-12 12:19:13 +0530

    fix build

--- apps/photos/src/utils/publicCollectionGallery/index.ts
+++ apps/photos/src/utils/publicCollectionGallery/index.ts
@@ -9,6 +9,8 @@ const defaultPublicCollectionGalleryContext: PublicCollectionGalleryContextType 
         accessedThroughSharedURL: false,
         photoListHeader: null,
         photoListFooter: null,
+        files: new Map(),
+        thumbs: new Map(),
     };
 
 export const PublicCollectionGalleryContext =
