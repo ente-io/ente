b9a8cf95fd89bc251aee26007eb894afcc0c97ed
Abhinav <abhinavk.grd@gmail.com>
2023-09-12 14:41:06 +0530

    remove console log

--- apps/photos/src/components/PhotoViewer/index.tsx
+++ apps/photos/src/components/PhotoViewer/index.tsx
@@ -136,10 +136,6 @@ function PhotoViewer(props: Iprops) {
     }, []);
 
     useEffect(() => {
-        console.log(fileDownloadProgress);
-    }, [fileDownloadProgress]);
-
-    useEffect(() => {
         if (!pswpElement) return;
         if (isOpen) {
             openPhotoSwipe();
