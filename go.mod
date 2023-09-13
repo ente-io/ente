f34be1f3c01544542e4c417636b378fb68ae1221
Neeraj Gupta <254676+ua741@users.noreply.github.com>
2023-09-13 18:09:54 +0530

    Add colors to http log

--- go.mod
+++ go.mod
@@ -10,6 +10,12 @@ require (
 )
 
 require (
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.17 // indirect
+)
+
+require (
+	github.com/fatih/color v1.15.0
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
--- internal/api/log.go
+++ internal/api/log.go
@@ -2,49 +2,57 @@ package api
 
 import (
 	"fmt"
-	"github.com/go-resty/resty/v2"
 	"strings"
+
+	"github.com/fatih/color"
+	"github.com/go-resty/resty/v2"
 )
 
 func logRequest(req *resty.Request) {
-	fmt.Println("Request:")
-	fmt.Printf("Method: %s\n", req.Method)
-	fmt.Printf("URL: %s\n", req.URL)
-	fmt.Println("Headers:")
+	fmt.Println(color.GreenString("Request:"))
+	fmt.Printf("%s %s\n", color.CyanString(req.Method), color.YellowString(req.URL))
+	fmt.Println(color.GreenString("Headers:"))
 	for k, v := range req.Header {
 		redacted := false
 		for _, rh := range RedactedHeaders {
-			if strings.ToLower(k) == strings.ToLower(rh) {
+			if strings.EqualFold(strings.ToLower(k), strings.ToLower(rh)) {
 				redacted = true
 				break
 			}
 		}
 		if redacted {
-			fmt.Printf("%s: %s\n", k, "REDACTED")
+			fmt.Printf("%s: %s\n", color.CyanString(k), color.RedString("REDACTED"))
 		} else {
-			fmt.Printf("%s: %s\n", k, v)
+			if len(v) == 1 {
+				fmt.Printf("%s: %s\n", color.CyanString(k), color.YellowString(v[0]))
+			} else {
+				fmt.Printf("%s: %s\n", color.CyanString(k), color.YellowString(strings.Join(v, ",")))
+			}
 		}
 	}
 }
 
 func logResponse(resp *resty.Response) {
-	fmt.Println("Response:")
-	fmt.Printf("Status Code: %d\n", resp.StatusCode())
-	fmt.Printf("Protocol: %s\n", resp.Proto())
+	fmt.Println(color.GreenString("Response:"))
+	if resp.StatusCode() < 200 || resp.StatusCode() >= 300 {
+		fmt.Printf("%s %s\n", color.CyanString(resp.Proto()), color.RedString(resp.Status()))
+	} else {
+		fmt.Printf("%s %s\n", color.CyanString(resp.Proto()), color.YellowString(resp.Status()))
+	}
 	fmt.Printf("Time Duration: %s\n", resp.Time())
-	fmt.Println("Headers:")
+	fmt.Println(color.GreenString("Headers:"))
 	for k, v := range resp.Header() {
 		redacted := false
 		for _, rh := range RedactedHeaders {
-			if strings.ToLower(k) == strings.ToLower(rh) {
+			if strings.EqualFold(strings.ToLower(k), strings.ToLower(rh)) {
 				redacted = true
 				break
 			}
 		}
 		if redacted {
-			fmt.Printf("%s: %s\n", k, "REDACTED")
+			fmt.Printf("%s: %s\n", color.CyanString(k), color.RedString("REDACTED"))
 		} else {
-			fmt.Printf("%s: %s\n", k, v)
+			fmt.Printf("%s: %s\n", color.CyanString(k), color.YellowString(strings.Join(v, ",")))
 		}
 	}
 }
