33273bc796cd1e79b34ba2cec0c0c13c55ed0b7c
Abhinav <abhinavk.grd@gmail.com>
2023-09-14 14:38:53 +0530

    fix default subtext color

--- apps/photos/src/components/CaptionedText.tsx
+++ apps/photos/src/components/CaptionedText.tsx
@@ -8,34 +8,32 @@ interface Iprops {
     color?: ButtonProps['color'];
 }
 
+const getSubTextColor = (color: ButtonProps['color']) => {
+    switch (color) {
+        case 'critical':
+            return 'critical.main';
+        default:
+            return 'text.faint';
+    }
+};
+
 export const CaptionedText = (props: Iprops) => {
     return (
         <VerticallyCenteredFlex gap={'4px'}>
             <Typography> {props.mainText}</Typography>
-            <Typography
-                variant="small"
-                sx={{
-                    color: (theme) =>
-                        theme.palette[props.color].main ?? 'text.faint',
-                }}>
+            <Typography variant="small" color={getSubTextColor(props.color)}>
                 {'•'}
             </Typography>
             {props.subText ? (
                 <Typography
                     variant="small"
-                    sx={{
-                        color: (theme) =>
-                            theme.palette[props.color].main ?? 'text.faint',
-                    }}>
+                    color={getSubTextColor(props.color)}>
                     {props.subText}
                 </Typography>
             ) : (
                 <Typography
                     variant="small"
-                    sx={{
-                        color: (theme) =>
-                            theme.palette[props.color].main ?? 'text.faint',
-                    }}>
+                    color={getSubTextColor(props.color)}>
                     {props.subIcon}
                 </Typography>
             )}
