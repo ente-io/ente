4492154ba5ddcbb4ef9908fe07027d2bfb300188
Abhinav <abhinavk.grd@gmail.com>
2023-09-14 14:06:51 +0530

    update CaptionedText to support button type color

--- apps/photos/src/components/CaptionedText.tsx
+++ apps/photos/src/components/CaptionedText.tsx
@@ -1,25 +1,41 @@
-import { Typography } from '@mui/material';
+import { ButtonProps, Typography } from '@mui/material';
 import { VerticallyCenteredFlex } from './Container';
 
 interface Iprops {
     mainText: string;
     subText?: string;
     subIcon?: React.ReactNode;
+    color?: ButtonProps['color'];
 }
 
 export const CaptionedText = (props: Iprops) => {
     return (
         <VerticallyCenteredFlex gap={'4px'}>
             <Typography> {props.mainText}</Typography>
-            <Typography color="text.faint" variant="small">
+            <Typography
+                variant="small"
+                sx={{
+                    color: (theme) =>
+                        theme.palette[props.color].main ?? 'text.faint',
+                }}>
                 {'•'}
             </Typography>
             {props.subText ? (
-                <Typography variant="small" color="text.faint">
+                <Typography
+                    variant="small"
+                    sx={{
+                        color: (theme) =>
+                            theme.palette[props.color].main ?? 'text.faint',
+                    }}>
                     {props.subText}
                 </Typography>
             ) : (
-                <Typography variant="small" color="text.faint">
+                <Typography
+                    variant="small"
+                    sx={{
+                        color: (theme) =>
+                            theme.palette[props.color].main ?? 'text.faint',
+                    }}>
                     {props.subIcon}
                 </Typography>
             )}
