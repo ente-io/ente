ff099d0c1ae2abac67cc32cd54c59319908425c4
Abhinav <abhinavk.grd@gmail.com>
2023-09-19 11:42:30 +0530

    add unit test for parseDateTimeFromFileName

--- apps/photos/tests/upload.test.ts
+++ apps/photos/tests/upload.test.ts
@@ -3,6 +3,57 @@ import { getLocalCollections } from 'services/collectionService';
 import { getUserDetailsV2 } from 'services/userService';
 import { groupFilesBasedOnCollectionID } from 'utils/file';
 import { FILE_TYPE } from 'constants/file';
+import { tryToParseDateTime } from 'utils/time';
+
+const DATE_TIME_PARSING_TEST_FILE_NAMES = [
+    {
+        fileName: 'Screenshot_20220807-195908_Firefox',
+        expectedDateTime: '2022-08-07 19:59:08',
+    },
+    {
+        fileName: 'Screenshot_20220507-195908',
+        expectedDateTime: '2022-05-07 19:59:08',
+    },
+    {
+        fileName: '2022-02-18 16.00.12-DCMX.png',
+        expectedDateTime: '2022-02-18 16:00:12',
+    },
+    {
+        fileName: '20221107_231730',
+        expectedDateTime: '2022-11-07 23:17:30',
+    },
+    {
+        fileName: '2020-11-01 02.31.02',
+        expectedDateTime: '2020-11-01 02:31:02',
+    },
+    {
+        fileName: 'IMG_20210921_144423',
+        expectedDateTime: '2021-09-21 14:44:23',
+    },
+    {
+        // we don't parse the time from this format, will improve later
+        fileName: '2019-10-31 155703',
+        expectedDateTime: '2019-10-31 00:00:00',
+        correctExpectedDateTime: '2019-10-31 15:57:03',
+    },
+    {
+        fileName: 'IMG_20210921_144423_783',
+        expectedDateTime: '2021-09-21 14:44:23',
+    },
+    {
+        fileName: 'Screenshot_2022-06-21-16-51-29-164_newFormat.heic',
+        expectedDateTime: '2022-06-21 16:51:29',
+    },
+    {
+        fileName:
+            'Screenshot 20221106 211633.com.google.android.apps.nbu.paisa.user.jpg',
+        expectedDateTime: '2022-11-06 21:16:33',
+    },
+    {
+        fileName: 'signal-2022-12-17-15-16-04-718.jpg',
+        expectedDateTime: '2022-12-17 15:16:04',
+    },
+];
 
 export async function testUpload() {
     if (!process.env.NEXT_PUBLIC_EXPECTED_JSON_PATH) {
@@ -26,6 +77,7 @@ export async function testUpload() {
         await fileDimensionExtractionCheck(expectedState);
         await googleMetadataReadingCheck(expectedState);
         await totalFileCountCheck(expectedState);
+        parseDateTimeFromFileNameTest();
     } catch (e) {
         console.log(e);
     }
@@ -281,4 +333,37 @@ async function googleMetadataReadingCheck(expectedState) {
         }
     );
     console.log('googleMetadataJSON reading check passed ✅');
+}
+
+function parseDateTimeFromFileNameTest() {
+    DATE_TIME_PARSING_TEST_FILE_NAMES.forEach(
+        ({ fileName, expectedDateTime }) => {
+            const dateTime = tryToParseDateTime(fileName);
+            const formattedDateTime = getFormattedDateTime(dateTime);
+            if (formattedDateTime !== expectedDateTime) {
+                throw Error(
+                    `parseDateTimeFromFileNameTest failed ❌ ,
+                    for ${fileName}
+                    expected: ${expectedDateTime} got: ${formattedDateTime}`
+                );
+            }
+        }
+    );
+    console.log('parseDateTimeFromFileNameTest passed ✅');
+}
+
+// format: YYYY-MM-DD HH:MM:SS
+function getFormattedDateTime(date: Date) {
+    const year = date.getFullYear();
+    const month = padZero(date.getMonth() + 1);
+    const day = padZero(date.getDate());
+    const hour = padZero(date.getHours());
+    const minute = padZero(date.getMinutes());
+    const second = padZero(date.getSeconds());
+
+    return `${year}-${month}-${day} ${hour}:${minute}:${second}`;
+}
+
+function padZero(number: number) {
+    return number < 10 ? `0${number}` : number;
 }
