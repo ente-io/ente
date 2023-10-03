dedbca36f52a9d9c8d9dd06a42e2314146a93a26
vishnukvmd <vishnu@ente.io>
2023-10-03 22:54:29 +0530

    Fetch embeddings from server

--- lib/services/semantic_search/embedding_store.dart
+++ lib/services/semantic_search/embedding_store.dart
@@ -1,3 +1,5 @@
+import "package:logging/logging.dart";
+import "package:photos/core/network/network.dart";
 import "package:photos/services/semantic_search/remote_embedding.dart";
 import "package:shared_preferences/shared_preferences.dart";
 
@@ -6,13 +8,36 @@ class EmbeddingStore {
 
   static final EmbeddingStore instance = EmbeddingStore._privateConstructor();
 
+  static const kEmbeddingsSyncTimeKey = "embeddings_sync_time";
+
+  final _logger = Logger("EmbeddingStore");
+  final _dio = NetworkClient.instance.enteDio;
+
   late SharedPreferences _preferences;
 
   Future<void> init(SharedPreferences preferences) async {
     _preferences = preferences;
   }
 
-  Future<List<RemoteEmbedding>> fetchEmbeddings() async {
+  Future<List<RemoteEmbedding>> fetchEmbeddings({int limit = 500}) async {
+    try {
+      final response = await _dio.get(
+        "/embeddings/diff",
+        queryParameters: {
+          "sinceTime": _preferences.getInt(kEmbeddingsSyncTimeKey) ?? 0,
+          "limit": limit,
+        },
+      );
+      final remoteEmbeddings = <RemoteEmbedding>[];
+      final diff = response.data["diff"] as List;
+      for (var entry in diff) {
+        final embedding = RemoteEmbedding.fromMap(entry);
+        remoteEmbeddings.add(embedding);
+      }
+      return remoteEmbeddings;
+    } catch (e, s) {
+      _logger.severe(e, s);
+    }
     return [];
   }
 }
