78bec602623d78558d77241f28cef0e5d24f802f
Neeraj Gupta <254676+ua741@users.noreply.github.com>
2023-10-16 16:48:23 +0530

    Download: return path to decrypted file

--- pkg/download.go
+++ pkg/download.go
@@ -1,28 +1,34 @@
 package pkg
 
 import (
+	"cli-go/internal/crypto"
+	"cli-go/pkg/model"
+	"cli-go/utils/encoding"
 	"context"
 	"fmt"
+	"log"
 	"os"
 )
 
-func (c *ClICtrl) initiateDownload(ctx context.Context) error {
-	files, err := c.getRemoteFiles(ctx)
+func (c *ClICtrl) downloadAndDecrypt(
+	ctx context.Context,
+	file model.RemoteFile,
+	deviceKey []byte,
+) (*string, error) {
+	dir, err := os.MkdirTemp("", "ente-cli-download/*")
 	if err != nil {
-		return err
+		return nil, err
 	}
-	dir, err := os.MkdirTemp("", "photos-download")
+	downloadPath := fmt.Sprintf("%s/%d", dir, file.ID)
+	log.Printf("Downloading file %d to %s", file.ID, downloadPath)
+	err = c.Client.DownloadFile(ctx, file.ID, downloadPath)
 	if err != nil {
-		return err
+		return nil, err
 	}
-	fmt.Println("total files to download:  in diroector", len(files), dir)
-	//for _, file := range files {
-	//	//downloadPath := fmt.Sprintf("%s/%d", dir, file.ID)
-	//	//log.Printf("Downloading file %d to %s", file.ID, downloadPath)
-	//	//err = c.Client.DownloadFile(ctx, file.ID, downloadPath)
-	//	//if err != nil {
-	//	//	return err
-	//	//}
-	//}
-	return nil
+	decryptedPath := fmt.Sprintf("%s/%d.decrypted", dir, file.ID)
+	err = crypto.DecryptFile(downloadPath, decryptedPath, file.Key.MustDecrypt(deviceKey), encoding.DecodeBase64(file.FileNonce))
+	if err != nil {
+		return nil, err
+	}
+	return &decryptedPath, nil
 }
