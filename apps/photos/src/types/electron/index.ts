4f5d19de77f2be1b05550545ea378fd64bce7345
Abhinav <abhinavk.grd@gmail.com>
2023-10-18 12:58:06 +0530

    added electron APIs for clipEmbedding extraction

--- apps/photos/src/types/electron/index.ts
+++ apps/photos/src/types/electron/index.ts
@@ -94,4 +94,6 @@ export interface ElectronAPIs {
     deleteFile: (path: string) => void;
     rename: (oldPath: string, newPath: string) => Promise<void>;
     updateOptOutOfCrashReports: (optOut: boolean) => Promise<void>;
+    computeImageEmbedding: (imageData: Uint8Array) => Promise<Float32Array>;
+    computeTextEmbedding: (text: string) => Promise<Float32Array>;
 }
