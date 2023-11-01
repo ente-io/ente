b1772dff5935deddc69d24dce417bce67d5536f1
ashilkn <ashilkn99@gmail.com>
2023-11-01 15:16:38 +0530

    limit number of recents shown

--- lib/models/search/recent_searches.dart
+++ lib/models/search/recent_searches.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import "package:photos/core/constants.dart";
 import "package:photos/models/search/search_result.dart";
 
 class RecentSearches with ChangeNotifier {
@@ -12,6 +13,9 @@ class RecentSearches with ChangeNotifier {
 
   void add(SearchResult result) {
     searches.add(result);
+    while (searches.length > searchSectionLimit) {
+      searches.remove(searches.first);
+    }
     //buffer for not surfacing a new recent search before going to the next
     //screen
     Future.delayed(const Duration(seconds: 1), () {
