0eb239508008a325cd0518d27f101967df13f2d2
Abhinav <abhinavk.grd@gmail.com>
2023-11-01 16:00:38 +0530

    add mui config package with mui theme type file

---
+++ packages/mui-config/.eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    extends: ['@ente/eslint-config'],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        tsconfigRootDir: __dirname,
+        project: './tsconfig.json',
+    },
+    ignorePatterns: ['.eslintrc.js'],
+};
