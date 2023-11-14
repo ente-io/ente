993dd1e7f49ca9f0160208f6b23c2cde24ef8553
httpjamesm <github@httpjames.space>
2023-11-13 20:00:54 -0500

    fix: config utils

--- apps/cast/next.config.js
+++ apps/cast/next.config.js
@@ -1,6 +1,84 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true,
-};
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+    enabled: process.env.ANALYZE === 'true',
+});
 
-module.exports = nextConfig;
+const { withSentryConfig } = require('@sentry/nextjs');
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
+
+const {
+    getGitSha,
+    convertToNextHeaderFormat,
+    buildCSPHeader,
+    WEB_SECURITY_HEADERS,
+    CSP_DIRECTIVES,
+    ALL_ROUTES,
+    getIsSentryEnabled,
+} = require('./configUtil');
+
+const GIT_SHA = getGitSha();
+
+const IS_SENTRY_ENABLED = getIsSentryEnabled();
+
+module.exports = (phase) =>
+    withSentryConfig(
+        withBundleAnalyzer({
+            sentry: {
+                hideSourceMaps: false,
+                widenClientFileUpload: true,
+            },
+            compiler: {
+                emotion: {
+                    importMap: {
+                        '@mui/material': {
+                            styled: {
+                                canonicalImport: ['@emotion/styled', 'default'],
+                                styledBaseImport: ['@mui/material', 'styled'],
+                            },
+                        },
+                        '@mui/material/styles': {
+                            styled: {
+                                canonicalImport: ['@emotion/styled', 'default'],
+                                styledBaseImport: [
+                                    '@mui/material/styles',
+                                    'styled',
+                                ],
+                            },
+                        },
+                    },
+                },
+            },
+            transpilePackages: [
+                '@mui/material',
+                '@mui/system',
+                '@mui/icons-material',
+            ],
+            env: {
+                SENTRY_RELEASE: GIT_SHA,
+                NEXT_PUBLIC_IS_TEST_APP: process.env.IS_TEST_RELEASE,
+            },
+
+            headers() {
+                return [
+                    {
+                        // Apply these headers to all routes in your application....
+                        source: ALL_ROUTES,
+                        headers: convertToNextHeaderFormat({
+                            ...WEB_SECURITY_HEADERS,
+                            ...buildCSPHeader(CSP_DIRECTIVES),
+                        }),
+                    },
+                ];
+            },
+            // https://dev.to/marcinwosinek/how-to-add-resolve-fallback-to-webpack-5-in-nextjs-10-i6j
+            webpack: (config, { isServer }) => {
+                if (!isServer) {
+                    config.resolve.fallback.fs = false;
+                }
+                return config;
+            },
+        }),
+        {
+            release: GIT_SHA,
+            dryRun: phase === PHASE_DEVELOPMENT_SERVER || !IS_SENTRY_ENABLED,
+        }
+    );
--- apps/cast/src/pages/index.tsx
+++ apps/cast/src/pages/index.tsx
@@ -1,10 +1,32 @@
 // import { Inter } from 'next/font/google';
-// import { useEffect } from 'react';
-// import { syncCollections } from 'services/collectionService';
-// import { syncFiles } from 'services/fileService';
+import { useEffect } from 'react';
+import { syncCollections } from 'services/collectionService';
+import { syncFiles } from 'services/fileService';
 
 // const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
+    const init = async () => {
+        const collections = await syncCollections();
+
+        // get requested collection id from fragment (this is temporary and will be changed during cast)
+        const requestedCollectionID = window.location.hash.slice(1);
+
+        const files = await syncFiles('normal', collections, () => {});
+
+        console.log(files);
+
+        if (requestedCollectionID) {
+            const collectionFiles = files.filter(
+                (file) => file.collectionID === Number(requestedCollectionID)
+            );
+
+            console.log('collectionFiles', collectionFiles);
+        }
+    };
+
+    useEffect(() => {
+        init();
+    }, []);
     return <></>;
 }
