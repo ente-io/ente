db3952bb88f1b20dee0fc94fb932dbbd5d3308ce
vishnukvmd <vishnu@ente.io>
2023-11-14 19:26:45 +0530

    Disable search by default

--- lib/utils/local_settings.dart
+++ lib/utils/local_settings.dart
@@ -45,7 +45,7 @@ class LocalSettings {
     if (_prefs.containsKey(kEnableMagicSearch)) {
       return _prefs.getBool(kEnableMagicSearch)!;
     }
-    return true;
+    return false;
   }
 
   Future<void> setShouldEnableMagicSearch(bool value) async {
