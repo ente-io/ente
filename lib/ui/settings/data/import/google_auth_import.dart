d838e2836cf7e17795134fc17f646f455b41b0bc
Muhammed Ayimen <aiymenlatheef03@gmail.com>
2023-11-17 08:05:14 +0900

    Removed: New Qr code analyzing screen and added the old one

--- lib/ui/settings/data/import/google_auth_import.dart
+++ lib/ui/settings/data/import/google_auth_import.dart
@@ -11,7 +11,7 @@ import 'package:ente_auth/store/code_store.dart';
 import 'package:ente_auth/ui/components/buttons/button_widget.dart';
 import 'package:ente_auth/ui/components/dialog_widget.dart';
 import 'package:ente_auth/ui/components/models/button_type.dart';
-import 'package:ente_auth/ui/settings/data/import/analyze_qr_code.dart';
+import 'package:ente_auth/ui/scanner_gauth_page.dart';
 import 'package:ente_auth/ui/settings/data/import/import_success.dart';
 import 'package:flutter/material.dart';
 import 'package:logging/logging.dart';
@@ -46,7 +46,7 @@ Future<void> showGoogleAuthInstruction(BuildContext context) async {
       final List<Code>? codes = await Navigator.of(context).push(
         MaterialPageRoute(
           builder: (BuildContext context) {
-            return const QrScanner();
+            return const ScannerGoogleAuthPage();
           },
         ),
       );
