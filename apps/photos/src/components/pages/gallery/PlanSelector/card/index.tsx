6a3e83134be198c73a3c991d0dcaf7d12fdfd5c3
Neeraj Gupta <254676+ua741@users.noreply.github.com>
2023-11-20 11:02:43 +0530

    Add missing import

--- apps/photos/src/components/pages/gallery/PlanSelector/card/index.tsx
+++ apps/photos/src/components/pages/gallery/PlanSelector/card/index.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useMemo, useState } from 'react';
+import React, { useContext, useEffect, useMemo, useState } from 'react';
 import { Plan } from 'types/billing';
 import {
     isUserSubscribedPlan,
