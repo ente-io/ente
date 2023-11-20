e44310a5a633a85013aa93f4a24f8a0f9c59433e
Neeraj Gupta <254676+ua741@users.noreply.github.com>
2023-11-20 15:52:09 +0530

    remove redundant comment

--- lib/ui/tools/lock_screen.dart
+++ lib/ui/tools/lock_screen.dart
@@ -72,10 +72,7 @@ class _LockScreenState extends State<LockScreen> with WidgetsBindingObserver {
     if (Platform.isAndroid) {
       return false;
     }
-// The equivalent of the "smallestWidth" qualifier on Android.
     var shortestSide = MediaQuery.of(context).size.shortestSide;
-    // Determine if we should use mobile layout or not, 600 here is
-// a common breakpoint for a typical 7-inch tablet.
     return shortestSide > 600 ? true : false;
   }
 
