20a6113562b65575001bdf61a934a19d7fd0fe57
Muhammed Ayimen <aiymenlatheef03@gmail.com>
2023-11-20 20:38:10 +0900

    Removed: scale state controller - pt2

--- lib/ui/viewer/file/zoomable_image.dart
+++ lib/ui/viewer/file/zoomable_image.dart
@@ -54,8 +54,6 @@ class _ZoomableImageState extends State<ZoomableImage>
   bool _loadedLargeThumbnail = false;
   bool _loadingFinalImage = false;
   bool _loadedFinalImage = false;
-  // ValueChanged<PhotoViewScaleState>? _scaleStateChangedCallback;
-  final bool _isZooming = false;
   PhotoViewController _photoViewController = PhotoViewController();
 
   @override
@@ -63,14 +61,6 @@ class _ZoomableImageState extends State<ZoomableImage>
     _photo = widget.photo;
     _logger = Logger("ZoomableImage");
     _logger.info('initState for ${_photo.generatedID} with tag ${_photo.tag}');
-    // _scaleStateChangedCallback = (value) {
-    //   if (widget.shouldDisableScroll != null) {
-    //     widget.shouldDisableScroll!(value != PhotoViewScaleState.initial);
-    //   }
-    //   _isZooming = value != PhotoViewScaleState.initial;
-    //   debugPrint("isZooming = $_isZooming, currentState $value");
-    //   // _logger.info('is reakky zooming $_isZooming with state $value');
-    // };
     super.initState();
   }
 
@@ -111,21 +101,16 @@ class _ZoomableImageState extends State<ZoomableImage>
       content = const EnteLoadingWidget();
     }
 
-    final GestureDragUpdateCallback? verticalDragCallback = _isZooming
-        ? null
-        : (d) => {
-              if (!_isZooming)
-                {
-                  if (d.delta.dy > dragSensitivity)
-                    {
-                      {Navigator.of(context).pop()},
-                    }
-                  else if (d.delta.dy < (dragSensitivity * -1))
-                    {
-                      showDetailsSheet(context, widget.photo),
-                    },
-                },
-            };
+    verticalDragCallback(d) => {
+          if (d.delta.dy > dragSensitivity)
+            {
+              {Navigator.of(context).pop()},
+            }
+          else if (d.delta.dy < (dragSensitivity * -1))
+            {
+              showDetailsSheet(context, widget.photo),
+            },
+        };
     return GestureDetector(
       onVerticalDragUpdate: verticalDragCallback,
       child: content,
@@ -266,15 +251,14 @@ class _ZoomableImageState extends State<ZoomableImage>
     required ImageProvider? previewImageProvider,
     required ImageProvider finalImageProvider,
   }) async {
-    final bool shouldFixPosition = previewImageProvider != null &&
-        _isZooming &&
-        _photoViewController.scale != null;
+    final bool shouldFixPosition =
+        previewImageProvider != null && _photoViewController.scale != null;
     ImageInfo? finalImageInfo;
     if (shouldFixPosition) {
       if (kDebugMode) {
         showToast(
           context,
-          'Updating photo scale zooming: $_isZooming and scale: ${_photoViewController.scale}',
+          'Updating photo scale zooming and scale: ${_photoViewController.scale}',
         );
       }
       final prevImageInfo = await getImageInfo(previewImageProvider);
