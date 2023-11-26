17f5a7996ae326c496b02820a6708361ed2b1259
Neeraj Gupta <neeraj.gupta983@gmail.com>
2023-11-26 16:27:25 +0530

    Change Password: Confirm before signing out from other devices (#374)

--- lib/services/user_service.dart
+++ lib/services/user_service.dart
@@ -441,6 +441,7 @@ class UserService {
   Future<void> registerOrUpdateSrp(
     Uint8List loginKey, {
     SetKeysRequest? setKeysRequest,
+    bool logOutOtherDevices = false,
   }) async {
     try {
       final String username = const Uuid().v4().toString();
@@ -496,6 +497,7 @@ class UserService {
               'setupID': setupSRPResponse.setupID,
               'srpM1': base64Encode(SRP6Util.encodeBigInt(clientM!)),
               'updatedKeyAttr': setKeysRequest.toMap(),
+              'logOutOtherDevices': logOutOtherDevices,
             },
           );
         }
@@ -608,8 +610,9 @@ class UserService {
 
   Future<void> updateKeyAttributes(
     KeyAttributes keyAttributes,
-    Uint8List loginKey,
-  ) async {
+    Uint8List loginKey, {
+    required bool logoutOtherDevices,
+  }) async {
     try {
       final setKeyRequest = SetKeysRequest(
         kekSalt: keyAttributes.kekSalt,
@@ -618,11 +621,11 @@ class UserService {
         memLimit: keyAttributes.memLimit,
         opsLimit: keyAttributes.opsLimit,
       );
-      await registerOrUpdateSrp(loginKey, setKeysRequest: setKeyRequest);
-      // await _enteDio.put(
-      //   "/users/keys",
-      //   data: setKeyRequest.toMap(),
-      // );
+      await registerOrUpdateSrp(
+        loginKey,
+        setKeysRequest: setKeyRequest,
+        logOutOtherDevices: logoutOtherDevices,
+      );
       await _config.setKeyAttributes(keyAttributes);
     } catch (e) {
       _logger.severe(e);
