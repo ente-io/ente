ff29ac33fb12d757c942afa1bef501860fcb5b52
Abhinav <abhinavk.grd@gmail.com>
2023-11-27 10:22:47 +0530

    fix next font issue

--- apps/cast/src/pages/_app.tsx
+++ apps/cast/src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import type { AppProps } from 'next/app';
-import { Inter } from 'next/font/google';
+import { Inter } from '@next/font/google';
 import 'styles/global.css';
 
 const inter = Inter({ subsets: ['latin'] });
