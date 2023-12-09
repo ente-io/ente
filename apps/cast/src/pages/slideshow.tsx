0d33b133cbe88e7fc422492628be5b5fe6f8f289
httpjamesm <github@httpjames.space>
2023-12-09 16:57:06 -0500

    fix: remove embrassing debug console.log

--- apps/cast/src/pages/slideshow.tsx
+++ apps/cast/src/pages/slideshow.tsx
@@ -35,10 +35,7 @@ export default function Slideshow() {
 
             options.disableIdleTimeout = true;
 
-            console.log('starting now');
             cast.framework.CastReceiverContext.getInstance().start(options);
-
-            console.log('adfslasdfasdf');
 
             // get requested collection id from localStorage
             const requestedCollectionID =
