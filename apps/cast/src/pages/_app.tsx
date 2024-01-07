a26ad05fac84d298d2540821885db7cf85201ad4
httpjamesm <github@httpjames.space>
2024-01-06 23:11:35 -0500

    fix: next font dep

--- apps/cast/src/pages/_app.tsx
+++ apps/cast/src/pages/_app.tsx
@@ -2,7 +2,7 @@ import type { AppProps } from 'next/app';
 import { Inter } from '@next/font/google';
 import 'styles/global.css';
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ['latin'], display: 'swap' });
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
