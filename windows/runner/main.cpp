318ba2e1ed62c97e25ae55dae1040f145f766e56
Prateek Sunal <prtksunal@gmail.com>
2024-01-26 19:22:41 +0530

    fix: ente Auth everywhere

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -10,7 +10,7 @@
 int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
                       _In_ wchar_t *command_line, _In_ int show_command)
 {
-  HWND hwnd = ::FindWindow(L"FLUTTER_RUNNER_WIN32_WINDOW", L"Auth");
+  HWND hwnd = ::FindWindow(L"FLUTTER_RUNNER_WIN32_WINDOW", L"ente Auth");
   if (hwnd != NULL)
   {
     DispatchToUniLinksDesktop(hwnd);
@@ -39,7 +39,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
   Win32Window::Size size(1280, 720);
-  if (!window.CreateAndShow(L"Auth", origin, size))
+  if (!window.CreateAndShow(L"ente Auth", origin, size))
   {
     return EXIT_FAILURE;
   }
