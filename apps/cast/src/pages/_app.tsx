f5d476df53fba49d4e2d67e794f45aef48215393
Neeraj Gupta <254676+ua741@users.noreply.github.com>
2024-01-30 11:01:44 +0530

    Remove dependency from next/font

--- apps/cast/src/pages/_app.tsx
+++ apps/cast/src/pages/_app.tsx
@@ -1,12 +1,10 @@
 import type { AppProps } from 'next/app';
-import { Inter } from '@next/font/google';
+
 import 'styles/global.css';
 import { ThemeProvider, CssBaseline } from '@mui/material';
 import { getTheme } from '@ente/shared/themes';
 import { THEME_COLOR } from '@ente/shared/themes/constants';
 import { APPS } from '@ente/shared/apps/constants';
-
-const inter = Inter({ subsets: ['latin'], display: 'swap' });
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
@@ -14,7 +12,6 @@ export default function App({ Component, pageProps }: AppProps) {
             <CssBaseline enableColorScheme />
 
             <main
-                className={inter.className}
                 style={{
                     display: 'contents',
                 }}>
