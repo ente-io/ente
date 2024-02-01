41e049e23860531f4fd858c6ede014762ade69c8
ashilkn <ashilkn99@gmail.com>
2024-02-01 11:14:22 +0530

    feat(map in info): fix france coming up as center point initially when opening maps

--- lib/ui/map/map_screen.dart
+++ lib/ui/map/map_screen.dart
@@ -48,7 +48,7 @@ class _MapScreenState extends State<MapScreen> {
   double maxZoom = 18.0;
   double minZoom = 2.8;
   int debounceDuration = 500;
-  LatLng center = const LatLng(46.7286, 4.8614);
+  late LatLng center;
   final Logger _logger = Logger("_MapScreenState");
   StreamSubscription? _mapMoveSubscription;
   Isolate? isolate;
@@ -70,6 +70,7 @@ class _MapScreenState extends State<MapScreen> {
 
   Future<void> initialize() async {
     try {
+      center = widget.center ?? const LatLng(46.7286, 4.8614);
       allImages = await widget.filesFutureFn();
       unawaited(processFiles(allImages));
     } catch (e, s) {
