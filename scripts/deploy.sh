e083c7caf2cab76d9371e8f7ccc211921dcc942a
Manav Rathi <manav@mrmr.io>
2024-02-03 16:49:50 +0530

    Copy the out directory
    The symlink does not seem to be working. In the build logs there is nothing
    wrong, but at the end CF gives up by saying:
    
        + ln -sf apps/auth/out
        Finished
        Note: No functions dir at /functions found. Skipping.
        Validating asset output directory
        Failed: build output directory contains links to files that can't be accessed

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,16 +7,18 @@
 # Ref: https://developers.cloudflare.com/pages/how-to/build-commands-branches/
 #
 # The CF Pages configuration is set to use `out/` as the build output directory,
-# so once we're done building we symlink `out/` to the app specific output.
+# so once we're done building we copy the app specific output to `out/`.
 
 set -o errexit
 set -o xtrace
 
+rm -rf out
+
 if test "$CF_PAGES_BRANCH" = "auth-release"
 then
     yarn export:auth
-    ln -sf apps/auth/out
+    cp -R apps/auth/out .
 else
     yarn export:photos
-    ln -sf apps/photos/out
+    cp -r apps/photos/out .
 fi
