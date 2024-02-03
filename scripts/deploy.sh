67624839f3c9235e5896605695938c2b16fdd46a
Manav Rathi <manav@mrmr.io>
2024-02-03 15:54:45 +0530

    Handle the single output directory by creating a symlink

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,12 +5,17 @@
 # CF_PAGES_BRANCH environment variable.
 #
 # Ref: https://developers.cloudflare.com/pages/how-to/build-commands-branches/
+#
+# The CF Pages configuration is set to use `out/` as the build output directory,
+# so once we're done building we symlink `out/` to the app specific output.
 
 set -o errexit
 
 if test "$CF_PAGES_BRANCH" == "auth-release"
 then
     yarn export:auth
+    ln -sf apps/auth/out
 else
     yarn export:photos
+    ln -sf apps/photos/out
 fi
