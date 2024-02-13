d3e8463cb7fd70a1934e762731b5206e1a2dda65
Prateek Sunal <prtksunal@gmail.com>
2024-02-13 17:02:40 +0530

    chore: lint fix

--- lib/onboarding/view/onboarding_page.dart
+++ lib/onboarding/view/onboarding_page.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-import 'dart:io';
 
 import 'package:ente_auth/app/view/app.dart';
 import 'package:ente_auth/core/configuration.dart';
@@ -8,21 +7,15 @@ import 'package:ente_auth/ente_theme_data.dart';
 import 'package:ente_auth/events/trigger_logout_event.dart';
 import "package:ente_auth/l10n/l10n.dart";
 import 'package:ente_auth/locale.dart';
-import 'package:ente_auth/ui/account/email_entry_page.dart';
 import 'package:ente_auth/ui/account/login_page.dart';
 import 'package:ente_auth/ui/account/logout_dialog.dart';
 import 'package:ente_auth/ui/account/password_entry_page.dart';
 import 'package:ente_auth/ui/account/password_reentry_page.dart';
-import 'package:ente_auth/ui/components/buttons/button_widget.dart';
-import 'package:ente_auth/ui/components/models/button_result.dart';
 import 'package:ente_auth/ui/home_page.dart';
 import 'package:ente_auth/ui/settings/language_picker.dart';
-import 'package:ente_auth/utils/dialog_util.dart';
 import 'package:ente_auth/utils/navigation_util.dart';
-import 'package:ente_auth/utils/toast_util.dart';
 import 'package:flutter/foundation.dart';
 import "package:flutter/material.dart";
-import 'package:local_auth/local_auth.dart';
 
 class OnboardingPage extends StatefulWidget {
   const OnboardingPage({super.key});
@@ -183,68 +176,69 @@ class _OnboardingPageState extends State<OnboardingPage> {
     );
   }
 
-  Future<void> _optForOfflineMode() async {
-    final canContinue = Platform.isMacOS || Platform.isLinux
-        ? true
-        : await LocalAuthentication().canCheckBiometrics;
+  // TODO: Remove After Stable
+  // Future<void> _optForOfflineMode() async {
+  //   final canContinue = Platform.isMacOS || Platform.isLinux
+  //       ? true
+  //       : await LocalAuthentication().canCheckBiometrics;
 
-    if (!canContinue) {
-      showToast(
-        context,
-        "Sorry, biometric authentication is not supported on this device.",
-      );
-      return;
-    }
-    final bool hasOptedBefore = Configuration.instance.hasOptedForOfflineMode();
-    ButtonResult? result;
-    if (!hasOptedBefore) {
-      result = await showChoiceActionSheet(
-        context,
-        title: context.l10n.warning,
-        body: context.l10n.offlineModeWarning,
-        secondButtonLabel: context.l10n.cancel,
-        firstButtonLabel: context.l10n.ok,
-      );
-    }
-    if (hasOptedBefore || result?.action == ButtonAction.first) {
-      await Configuration.instance.optForOfflineMode();
-      Navigator.of(context).push(
-        MaterialPageRoute(
-          builder: (BuildContext context) {
-            return const HomePage();
-          },
-        ),
-      );
-    }
-  }
+  //   if (!canContinue) {
+  //     showToast(
+  //       context,
+  //       "Sorry, biometric authentication is not supported on this device.",
+  //     );
+  //     return;
+  //   }
+  //   final bool hasOptedBefore = Configuration.instance.hasOptedForOfflineMode();
+  //   ButtonResult? result;
+  //   if (!hasOptedBefore) {
+  //     result = await showChoiceActionSheet(
+  //       context,
+  //       title: context.l10n.warning,
+  //       body: context.l10n.offlineModeWarning,
+  //       secondButtonLabel: context.l10n.cancel,
+  //       firstButtonLabel: context.l10n.ok,
+  //     );
+  //   }
+  //   if (hasOptedBefore || result?.action == ButtonAction.first) {
+  //     await Configuration.instance.optForOfflineMode();
+  //     Navigator.of(context).push(
+  //       MaterialPageRoute(
+  //         builder: (BuildContext context) {
+  //           return const HomePage();
+  //         },
+  //       ),
+  //     );
+  //   }
+  // }
 
-  void _navigateToSignUpPage() {
-    Widget page;
-    if (Configuration.instance.getEncryptedToken() == null) {
-      page = const EmailEntryPage();
-    } else {
-      // No key
-      if (Configuration.instance.getKeyAttributes() == null) {
-        // Never had a key
-        page = const PasswordEntryPage(
-          mode: PasswordEntryMode.set,
-        );
-      } else if (Configuration.instance.getKey() == null) {
-        // Yet to decrypt the key
-        page = const PasswordReentryPage();
-      } else {
-        // All is well, user just has not subscribed
-        page = const HomePage();
-      }
-    }
-    Navigator.of(context).push(
-      MaterialPageRoute(
-        builder: (BuildContext context) {
-          return page;
-        },
-      ),
-    );
-  }
+  // void _navigateToSignUpPage() {
+  //   Widget page;
+  //   if (Configuration.instance.getEncryptedToken() == null) {
+  //     page = const EmailEntryPage();
+  //   } else {
+  //     // No key
+  //     if (Configuration.instance.getKeyAttributes() == null) {
+  //       // Never had a key
+  //       page = const PasswordEntryPage(
+  //         mode: PasswordEntryMode.set,
+  //       );
+  //     } else if (Configuration.instance.getKey() == null) {
+  //       // Yet to decrypt the key
+  //       page = const PasswordReentryPage();
+  //     } else {
+  //       // All is well, user just has not subscribed
+  //       page = const HomePage();
+  //     }
+  //   }
+  //   Navigator.of(context).push(
+  //     MaterialPageRoute(
+  //       builder: (BuildContext context) {
+  //         return page;
+  //       },
+  //     ),
+  //   );
+  // }
 
   void _navigateToSignInPage() {
     Widget page;
