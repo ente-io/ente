f30c1390976c83940cbd2e6e45908382235b77ce
Neeraj Gupta <254676+ua741@users.noreply.github.com>
2024-02-15 15:56:58 +0530

    Return empty result for unused sections

--- lib/models/search/search_types.dart
+++ lib/models/search/search_types.dart
@@ -9,6 +9,7 @@ import "package:photos/events/location_tag_updated_event.dart";
 import "package:photos/generated/l10n.dart";
 import "package:photos/models/collection/collection.dart";
 import "package:photos/models/collection/collection_items.dart";
+import "package:photos/models/search/generic_search_result.dart";
 import "package:photos/models/search/search_result.dart";
 import "package:photos/models/typedefs.dart";
 import "package:photos/services/collections_service.dart";
@@ -243,10 +244,10 @@ extension SectionTypeExtensions on SectionType {
   }) {
     switch (this) {
       case SectionType.face:
-        return SearchService.instance.getAllLocationTags(limit);
+        return Future.value(List<GenericSearchResult>.empty());
 
       case SectionType.content:
-        return SearchService.instance.getAllLocationTags(limit);
+        return Future.value(List<GenericSearchResult>.empty());
 
       case SectionType.moment:
         return SearchService.instance.getRandomMomentsSearchResults(context);
