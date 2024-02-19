4935695a6f85b008463f0075db1e677bb71ce3b0
Prateek Sunal <prtksunal@gmail.com>
2024-02-19 09:04:31 +0530

    fix: windows build issue

--- windows/runner/win32_window.cpp
+++ windows/runner/win32_window.cpp
@@ -142,10 +142,6 @@ bool Win32Window::CreateAndShow(const std::wstring &title,
       Scale(size.width, scale_factor), Scale(size.height, scale_factor),
       nullptr, nullptr, GetModuleHandle(nullptr), this);
 
-      Scale(origin.x, scale_factor), Scale(origin.y, scale_factor),
-      Scale(size.width, scale_factor), Scale(size.height, scale_factor),
-      nullptr, nullptr, GetModuleHandle(nullptr), this);
-
       if (!window)
       {
         return false;
