d12878cc3b4cdf952be2f56dc7d4fd0aa1372170
Manav Rathi <manav@mrmr.io>
2024-02-20 14:49:59 +0530

    Remove dangling semicolon
    Fixes https://github.com/ente-io/auth/issues/462

--- apps/auth/src/pages/auth/index.tsx
+++ apps/auth/src/pages/auth/index.tsx
@@ -55,7 +55,6 @@ const AuthenticatorCodesPage = () => {
                 <VerticallyCentered>
                     <EnteSpinner></EnteSpinner>
                 </VerticallyCentered>
-                ;
             </>
         );
     }
