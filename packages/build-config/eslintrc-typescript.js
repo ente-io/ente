44703ff3df80fecbd0734c39aa3c03bd434ea99e
Manav Rathi <manav@mrmr.io>
2024-02-22 11:31:14 +0530

    Use the strict variant of recommended configs
    Ref: https://typescript-eslint.io/users/configs/

--- packages/build-config/eslintrc-typescript.js
+++ packages/build-config/eslintrc-typescript.js
@@ -1,13 +1,14 @@
 module.exports = {
     extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended-type-checked',
+        "eslint:recommended",
+        "plugin:@typescript-eslint/strict-type-checked",
+        "plugin:@typescript-eslint/stylistic-type-checked",
     ],
-    plugins: ['@typescript-eslint'],
-    parser: '@typescript-eslint/parser',
+    plugins: ["@typescript-eslint"],
+    parser: "@typescript-eslint/parser",
     parserOptions: {
         project: true,
     },
-    ignorePatterns: ['.eslintrc.js'],
+    ignorePatterns: [".eslintrc.js"],
     root: true,
 };
