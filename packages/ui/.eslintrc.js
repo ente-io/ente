666cd002c7014f7fdca703d9564c5281f2d6bfe4
Manav Rathi <manav@mrmr.io>
2024-02-24 15:25:31 +0530

    Split react and non-react lints

--- packages/ui/.eslintrc.js
+++ packages/ui/.eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-    extends: ["@/build-config/eslintrc-typescript"],
+    extends: ["@/build-config/eslintrc-typescript-react"],
     parserOptions: {
         tsconfigRootDir: __dirname,
     },
