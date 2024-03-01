d460584808d19c6ba6a90b733584e9cddbd58948
Manav Rathi <manav@mrmr.io>
2024-03-01 10:49:09 +0530

    Tweak config

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,12 +2,17 @@ import { defineConfig } from "vitepress";
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-    title: "Ente Docs",
-    description: "Help and documentation for Ente's products",
+    title: "Ente Help",
+    description: "Documentation and help for Ente's products",
     head: [["link", { rel: "icon", type: "image/png", href: "/favicon.png" }]],
     cleanUrls: true,
     themeConfig: {
+        // We use the default theme (with some CSS color overrides). This
+        // themeConfig block can be used to further customize the default theme.
+        //
         // https://vitepress.dev/reference/default-theme-config
+        logo: "/logo.png",
+        externalLinkIcon: true,
         editLink: {
             pattern:
                 "https://github.com/ente-io/ente/edit/main/docs/docs/:path",
@@ -30,6 +35,8 @@ export default defineConfig({
         },
         socialLinks: [
             { icon: "github", link: "https://github.com/ente-io/ente/" },
+            { icon: "twitter", link: "https://twitter.com/enteio" },
+            { icon: "discord", link: "https://discord.gg/z2YVKkycX3" },
         ],
     },
 });
