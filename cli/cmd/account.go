d0bc26fa4008152e5f8728e141c94a856a15cabf
Neeraj Gupta <254676+ua741@users.noreply.github.com>
2024-03-08 17:35:16 +0530

    [cli] Add command to get-token

--- cli/cmd/account.go
+++ cli/cmd/account.go
@@ -62,10 +62,45 @@ var updateAccCmd = &cobra.Command{
 			fmt.Printf("invalid app. Accepted values are 'photos', 'locker', 'auth'")
 
 		}
-		err := ctrl.UpdateAccount(context.Background(), model.UpdateAccountParams{
+		err := ctrl.UpdateAccount(context.Background(), model.AccountCommandParams{
 			Email:     email,
 			App:       api.StringToApp(app),
 			ExportDir: &exportDir,
+		})
+		if err != nil {
+			fmt.Printf("Error updating account: %v\n", err)
+		}
+	},
+}
+
+// Subcommand for 'account update'
+var getTokenCmd = &cobra.Command{
+	Use:   "get-token",
+	Short: "Get token for an account for a specific app",
+	Run: func(cmd *cobra.Command, args []string) {
+		recoverWithLog()
+		app, _ := cmd.Flags().GetString("app")
+		email, _ := cmd.Flags().GetString("email")
+		if email == "" {
+
+			fmt.Println("email must be specified, use --help for more information")
+			// print help
+			return
+		}
+
+		validApps := map[string]bool{
+			"photos": true,
+			"locker": true,
+			"auth":   true,
+		}
+
+		if !validApps[app] {
+			fmt.Printf("invalid app. Accepted values are 'photos', 'locker', 'auth'")
+
+		}
+		err := ctrl.GetToken(context.Background(), model.AccountCommandParams{
+			Email: email,
+			App:   api.StringToApp(app),
 		})
 		if err != nil {
 			fmt.Printf("Error updating account: %v\n", err)
@@ -78,7 +113,9 @@ func init() {
 	rootCmd.AddCommand(accountCmd)
 	// Add 'config' subcommands to the 'config' command
 	updateAccCmd.Flags().String("dir", "", "update export directory")
-	updateAccCmd.Flags().String("email", "", "email address of the account to update")
+	updateAccCmd.Flags().String("email", "", "email address of the account")
 	updateAccCmd.Flags().String("app", "photos", "Specify the app, default is 'photos'")
-	accountCmd.AddCommand(listAccCmd, addAccCmd, updateAccCmd)
+	getTokenCmd.Flags().String("email", "", "email address of the account")
+	getTokenCmd.Flags().String("app", "photos", "Specify the app, default is 'photos'")
+	accountCmd.AddCommand(listAccCmd, addAccCmd, updateAccCmd, getTokenCmd)
 }
