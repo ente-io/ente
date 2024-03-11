048162ba1f2e22515d372e973baceeb643fe63d1
httpjamesm <github@httpjames.space>
2024-03-11 13:09:27 -0400

    fix: resp data

--- web/packages/accounts/services/passkey.ts
+++ web/packages/accounts/services/passkey.ts
@@ -1,27 +1,30 @@
-import HTTPService from "@ente/shared/network/HTTPService";
-import { getToken } from "@ente/shared/storage/localStorage/helpers"
-import { logError } from "@ente/shared/sentry";
 import { CustomError } from "@ente/shared/error";
+import HTTPService from "@ente/shared/network/HTTPService";
+import { logError } from "@ente/shared/sentry";
+import { getToken } from "@ente/shared/storage/localStorage/helpers";
 
 export const isPasskeyRecoveryEnabled = async () => {
     try {
         const token = getToken();
 
-        const resp = await HTTPService.get("/users/two-factor/recovery-status", {}, {
-            "X-Auth-Token": token,
-        });
+        const resp = await HTTPService.get(
+            "/users/two-factor/recovery-status",
+            {},
+            {
+                "X-Auth-Token": token,
+            },
+        );
 
         if (typeof resp.data === "undefined") {
             throw Error(CustomError.REQUEST_FAILED);
         }
 
-        return resp.isPasskeyRecoveryEnabled as boolean;
-
+        return resp.data["isPasskeyRecoveryEnabled"] as boolean;
     } catch (e) {
         logError(e, "failed to get passkey recovery status");
-        throw e
+        throw e;
     }
-}
+};
 
 export const configurePasskeyRecovery = async (
     secret: string,
@@ -31,19 +34,23 @@ export const configurePasskeyRecovery = async (
     try {
         const token = getToken();
 
-        const resp = await HTTPService.post("/users/two-factor/passkeys/configure-recovery", {
-            secret,
-            userEncryptedSecret,
-            userSecretNonce,
-        }, {
-            "X-Auth-Token": token,
-        });
+        const resp = await HTTPService.post(
+            "/users/two-factor/passkeys/configure-recovery",
+            {
+                secret,
+                userEncryptedSecret,
+                userSecretNonce,
+            },
+            {
+                "X-Auth-Token": token,
+            },
+        );
 
         if (typeof resp.data === "undefined") {
             throw Error(CustomError.REQUEST_FAILED);
         }
     } catch (e) {
         logError(e, "failed to configure passkey recovery");
-        throw e
+        throw e;
     }
-}
+};
