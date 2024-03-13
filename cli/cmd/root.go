c5bdc5249f99589a4e687d83b9ba06d040e7b5f9
Neeraj Gupta <254676+ua741@users.noreply.github.com>
2024-03-13 11:38:30 +0530

    [cli] Pick version from the github tag

--- cli/cmd/root.go
+++ cli/cmd/root.go
@@ -12,7 +12,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-const AppVersion = "0.1.11"
+var version string
 
 var ctrl *pkg.ClICtrl
 
@@ -34,8 +34,9 @@ func GenerateDocs() error {
 
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
-func Execute(controller *pkg.ClICtrl) {
+func Execute(controller *pkg.ClICtrl, ver string) {
 	ctrl = controller
+	version = ver
 	err := rootCmd.Execute()
 	if err != nil {
 		os.Exit(1)
