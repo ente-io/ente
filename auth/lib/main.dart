af22c48a99425d996b5b37cc07254b6d8c1e40c5
Prateek Sunal <prtksunal@gmail.com>
2024-03-23 20:01:56 +0530

    fix: remove unwanted debug print

--- auth/lib/main.dart
+++ auth/lib/main.dart
@@ -61,7 +61,6 @@ Future<void> initSystemTray() async {
   const kSystemTrayEventRightClick = 'rightMouseDown';
   // // handle system tray event
   systemTray.registerSystemTrayEventHandler((eventName) {
-    debugPrint("eventName: $eventName");
     if (eventName == kSystemTrayEventClick) {
       Platform.isWindows ? appWindow.show() : systemTray.popUpContextMenu();
     } else if (eventName == kSystemTrayEventRightClick) {
