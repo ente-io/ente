d5f0334a344418de2d94d60d8e1a56d18cc1d142
Vishal <vs704.vs@gmail.com>
2024-03-28 17:44:08 +0530

    Remove isEnabled from config

--- server/pkg/utils/email/email.go
+++ server/pkg/utils/email/email.go
@@ -21,88 +21,12 @@ import (
 
 // Send sends an email
 func Send(toEmails []string, fromName string, fromEmail string, subject string, htmlBody string, inlineImages []map[string]interface{}) error {
-	if len(toEmails) == 0 {
-		return ente.ErrBadRequest
-	}
-
-	authKey := viper.GetString("transmail.key")
-	silent := viper.GetBool("internal.silent")
-	if authKey == "" || silent {
-		log.Infof("Skipping sending email to %s: %s", toEmails[0], subject)
-		return nil
-	}
-
-	var to []ente.ToEmailAddress
-	for _, toEmail := range toEmails {
-		to = append(to, ente.ToEmailAddress{EmailAddress: ente.EmailAddress{Address: toEmail}})
-	}
-	mail := &ente.Mail{
-		BounceAddress: ente.TransmailEndBounceAddress,
-		From:          ente.EmailAddress{Address: fromEmail, Name: fromName},
-		Subject:       subject,
-		Htmlbody:      htmlBody,
-		InlineImages:  inlineImages,
-	}
-	if len(toEmails) == 1 {
-		mail.To = to
+	smtpHost := viper.GetString("smtp.host")
+	if smtpHost != "" {
+		return sendViaSMTP(toEmails, fromName, fromEmail, subject, htmlBody, inlineImages)
 	} else {
-		mail.Bcc = to
+		return sendViaTransmail(toEmails, fromName, fromEmail, subject, htmlBody, inlineImages)
 	}
-	postBody, err := json.Marshal(mail)
-	if err != nil {
-		return stacktrace.Propagate(err, "")
-	}
-	reqBody := bytes.NewBuffer(postBody)
-	client := &http.Client{}
-	req, err := http.NewRequest("POST", ente.TransmailEndPoint, reqBody)
-	if err != nil {
-		return stacktrace.Propagate(err, "")
-	}
-	req.Header.Set("accept", "application/json")
-	req.Header.Set("content-type", "application/json")
-	req.Header.Set("authorization", authKey)
-	_, err = client.Do(req)
-	return stacktrace.Propagate(err, "")
-}
-
-func SendTemplatedEmail(to []string, fromName string, fromEmail string, subject string, templateName string, templateData map[string]interface{}, inlineImages []map[string]interface{}) error {
-	body, err := getMailBody(templateName, templateData)
-	if err != nil {
-		return stacktrace.Propagate(err, "")
-	}
-
-	isSESEnabled := viper.GetBool("smtp.isEnabled")
-	if isSESEnabled {
-		return sendViaSMTP(to, fromName, fromEmail, subject, body, inlineImages)
-	} else {
-		return Send(to, fromName, fromEmail, subject, body, inlineImages)
-	}
-}
-
-func GetMaskedEmail(email string) string {
-	at := strings.LastIndex(email, "@")
-	if at >= 0 {
-		username, domain := email[:at], email[at+1:]
-		maskedUsername := ""
-		for i := 0; i < len(username); i++ {
-			maskedUsername += "*"
-		}
-		return maskedUsername + "@" + domain
-	} else {
-		// Should ideally never happen, there should always be an @ symbol
-		return "[invalid_email]"
-	}
-}
-
-// getMailBody generates the mail html body from provided template and data
-func getMailBody(templateName string, templateData map[string]interface{}) (string, error) {
-	htmlbody := new(bytes.Buffer)
-	t := template.Must(template.New(templateName).ParseFiles("mail-templates/" + templateName))
-	err := t.Execute(htmlbody, templateData)
-	if err != nil {
-		return "", stacktrace.Propagate(err, "")
-	}
-	return htmlbody.String(), nil
 }
 
 func sendViaSMTP(toEmails []string, fromName string, fromEmail string, subject string, htmlBody string, inlineImages []map[string]interface{}) error {
@@ -166,4 +90,84 @@ func sendViaSMTP(toEmails []string, fromName string, fromEmail string, subject s
 	}
 
 	return nil
+}
+
+func sendViaTransmail(toEmails []string, fromName string, fromEmail string, subject string, htmlBody string, inlineImages []map[string]interface{}) error {
+	if len(toEmails) == 0 {
+		return ente.ErrBadRequest
+	}
+
+	authKey := viper.GetString("transmail.key")
+	silent := viper.GetBool("internal.silent")
+	if authKey == "" || silent {
+		log.Infof("Skipping sending email to %s: %s", toEmails[0], subject)
+		return nil
+	}
+
+	var to []ente.ToEmailAddress
+	for _, toEmail := range toEmails {
+		to = append(to, ente.ToEmailAddress{EmailAddress: ente.EmailAddress{Address: toEmail}})
+	}
+	mail := &ente.Mail{
+		BounceAddress: ente.TransmailEndBounceAddress,
+		From:          ente.EmailAddress{Address: fromEmail, Name: fromName},
+		Subject:       subject,
+		Htmlbody:      htmlBody,
+		InlineImages:  inlineImages,
+	}
+	if len(toEmails) == 1 {
+		mail.To = to
+	} else {
+		mail.Bcc = to
+	}
+	postBody, err := json.Marshal(mail)
+	if err != nil {
+		return stacktrace.Propagate(err, "")
+	}
+	reqBody := bytes.NewBuffer(postBody)
+	client := &http.Client{}
+	req, err := http.NewRequest("POST", ente.TransmailEndPoint, reqBody)
+	if err != nil {
+		return stacktrace.Propagate(err, "")
+	}
+	req.Header.Set("accept", "application/json")
+	req.Header.Set("content-type", "application/json")
+	req.Header.Set("authorization", authKey)
+	_, err = client.Do(req)
+	return stacktrace.Propagate(err, "")
+}
+
+func SendTemplatedEmail(to []string, fromName string, fromEmail string, subject string, templateName string, templateData map[string]interface{}, inlineImages []map[string]interface{}) error {
+	body, err := getMailBody(templateName, templateData)
+	if err != nil {
+		return stacktrace.Propagate(err, "")
+	}
+
+	return Send(to, fromName, fromEmail, subject, body, inlineImages)
+}
+
+func GetMaskedEmail(email string) string {
+	at := strings.LastIndex(email, "@")
+	if at >= 0 {
+		username, domain := email[:at], email[at+1:]
+		maskedUsername := ""
+		for i := 0; i < len(username); i++ {
+			maskedUsername += "*"
+		}
+		return maskedUsername + "@" + domain
+	} else {
+		// Should ideally never happen, there should always be an @ symbol
+		return "[invalid_email]"
+	}
+}
+
+// getMailBody generates the mail html body from provided template and data
+func getMailBody(templateName string, templateData map[string]interface{}) (string, error) {
+	htmlbody := new(bytes.Buffer)
+	t := template.Must(template.New(templateName).ParseFiles("mail-templates/" + templateName))
+	err := t.Execute(htmlbody, templateData)
+	if err != nil {
+		return "", stacktrace.Propagate(err, "")
+	}
+	return htmlbody.String(), nil
 }
