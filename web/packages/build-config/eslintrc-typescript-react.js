14fde54d87ddb3cae6994750ba08919488b67817
Manav Rathi <manav@mrmr.io>
2024-04-02 20:04:23 +0530

    Not really

--- web/packages/build-config/eslintrc-typescript-react.js
+++ web/packages/build-config/eslintrc-typescript-react.js
@@ -6,19 +6,11 @@ module.exports = {
         "plugin:@typescript-eslint/strict-type-checked",
         "plugin:@typescript-eslint/stylistic-type-checked",
     ],
-    plugins: ["@typescript-eslint", "react-namespace-import"],
+    plugins: ["@typescript-eslint"],
     parser: "@typescript-eslint/parser",
     parserOptions: {
         project: true,
     },
     root: true,
     ignorePatterns: [".eslintrc.js"],
-    rules: {
-        // The recommended way to import React is:
-        //
-        //     import * as React from "react";
-        //
-        // This rule enforces that.
-        "react-namespace-import/no-namespace-import": "error",
-    },
 };
