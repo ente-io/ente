87dc7d76cafc01cd81227a0d6e54d7515eb35bb7
Manav Rathi <manav@mrmr.io>
2024-04-04 11:25:34 +0530

    Remove middleman package, to get it to compile

--- web/apps/payments/vite.config.ts
+++ web/apps/payments/vite.config.ts
@@ -1,3 +1,7 @@
-import defineConfig from "@/vite/vite.config.ts";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
-export default defineConfig;
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [react()],
+});
