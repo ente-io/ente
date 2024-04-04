756050ae8cbae8bfb24dd930296af08460d08f0e
Manav Rathi <manav@mrmr.io>
2024-04-04 13:00:02 +0530

    Fix compilation

--- web/apps/payments/src/App.tsx
+++ web/apps/payments/src/App.tsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { Container } from "./components/Container";
+import { parseAndHandleRequest } from "./services/billing-service";
+import S from "./utils/strings";
 
 const App: React.FC = () => {
-    return <h1>Hello</h1>;
+    const [failed, setFailed] = React.useState(false);
+
+    useEffect(() => {
+        parseAndHandleRequest().catch(() => {
+            setFailed(true);
+        });
+    }, []);
+
+    return <Container>{failed ? S.error_generic : <Spinner />}</Container>;
 };
 
 export default App;
+
+const Spinner: React.FC = () => <div className="loading-spinner"></div>;
--- web/apps/payments/src/main.tsx
+++ web/apps/payments/src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.js";
-import "./index.css";
+import App from "./App";
+import "./styles/globals.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
