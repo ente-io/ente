712b99b8f35d5ecc1076ed4e8cdf4c495f9ee8d9
Manav Rathi <manav@mrmr.io>
2024-04-04 14:15:42 +0530

    Fix lint issue

--- web/apps/payments/src/main.tsx
+++ web/apps/payments/src/main.tsx
@@ -3,7 +3,10 @@ import ReactDOM from "react-dom/client";
 import { App } from "./App";
 import "./styles/globals.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+const root = document.getElementById("root");
+if (!root) throw new Error("Could not load root element to render onto");
+
+ReactDOM.createRoot(root).render(
     <React.StrictMode>
         <App />
     </React.StrictMode>,
