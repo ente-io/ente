791cc61ca7a6b40ae1eb7fbc76337a340f1f989e
Manav Rathi <manav@mrmr.io>
2024-04-05 21:12:03 +0530

    Fix more leftover issues

--- web/apps/accounts/src/pages/two-factor/recover/index.tsx
+++ web/apps/accounts/src/pages/two-factor/recover/index.tsx
@@ -1,12 +1,10 @@
 import TwoFactorRecoverPage from "@ente/accounts/pages/two-factor/recover";
 import { APPS } from "@ente/shared/apps/constants";
-import { useRouter } from "next/router";
 import { AppContext } from "pages/_app";
 import { useContext } from "react";
 
 export default function TwoFactorRecover() {
     const appContext = useContext(AppContext);
-    const router = useRouter();
     return (
         <TwoFactorRecoverPage appContext={appContext} appName={APPS.ACCOUNTS} />
     );
