356ad6f00408d722a4fcd71674ae61103eefca45
Manav Rathi <manav@mrmr.io>
2024-04-05 14:02:21 +0530

    Use regular link
    Removing unnecessary dependencies on next specific components

--- web/packages/shared/components/Navbar/EnteLinkLogo.tsx
+++ web/packages/shared/components/Navbar/EnteLinkLogo.tsx
@@ -1,11 +1,10 @@
 import { ENTE_WEBSITE_LINK } from "@ente/shared/constants/urls";
 import { Box } from "@mui/material";
-import Link from "next/link";
 import Ente from "../../components/icons/ente";
 
 export function EnteLinkLogo() {
     return (
-        <Link href={ENTE_WEBSITE_LINK}>
+        <a href={ENTE_WEBSITE_LINK}>
             <Box
                 sx={(theme) => ({
                     ":hover": {
@@ -18,6 +17,6 @@ export function EnteLinkLogo() {
             >
                 <Ente />
             </Box>
-        </Link>
+        </a>
     );
 }
