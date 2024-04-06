d00211964b4670396bc749c39fb3b8892d5e1e5b
Manav Rathi <manav@mrmr.io>
2024-04-06 11:44:27 +0530

    Improve heuristic

--- web/scripts/find-unused-tr-keys.sh
+++ web/scripts/find-unused-tr-keys.sh
@@ -4,9 +4,9 @@
 #
 # Caveat emptor. Uses a heuristic grep, not perfect.
 
-jq -r 'keys[]' packages/next/locales/en-US/translation.json | head | while read key
+jq -r 'keys[]' packages/next/locales/en-US/translation.json | while read key
 do
-    if ! git grep --quiet --fixed 't("'$key'")' -- :^'**/translation.json'
+    if ! git grep --quiet --fixed '"'$key'"' -- :^'**/translation.json'
     then
         echo "possibly unused key ${key}"
     fi
