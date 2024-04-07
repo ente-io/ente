99579fbf51026f3a79040a0ac533a447a6adfbe8
Manav Rathi <manav@mrmr.io>
2024-04-07 10:56:15 +0530

    Inline

--- web/packages/shared/constants/urls.ts
+++ web/packages/shared/constants/urls.ts
@@ -1,10 +1,5 @@
 export const ENTE_WEBSITE_LINK = "https://ente.io";
 
-export const ML_BLOG_LINK = "https://ente.io/blog/desktop-ml-beta";
-
-export const FACE_SEARCH_PRIVACY_POLICY_LINK =
-    "https://ente.io/privacy#8-biometric-information-privacy-policy";
-
 export const SUPPORT_EMAIL = "support@ente.io";
 
 export const FEEDBACK_EMAIL = "feedback@ente.io";
