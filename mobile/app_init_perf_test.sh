504f23fe4e90b763e032a46a0dd0dcf397664421
ashilkn <ashilkn99@gmail.com>
2024-04-08 13:30:52 +0530

    add comments to app_init_test script

--- mobile/app_init_perf_test.sh
+++ mobile/app_init_perf_test.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-# Make sure to go through home_gallery_scroll_test.dart and 
+# Make sure to go through app_init_test.dart and 
 # fill in email and password.
-# Specify destination directory for the perf results in perf_driver.dart.
 
+# Specify destination directory for the perf results in perf_driver.dart.
+# Specify the report_key of the test in perf_driver.dart. `report_key`` of
+# `traceAction`` in app_init_test.dart.
+
+# On first run, app will start from login page. from second run onwards, 
+# app will start from home page. --keep-app-running is for starting the 
+# app from home page instead of logging in on every run.
 
 export ENDPOINT="https://api.ente.io"
 
