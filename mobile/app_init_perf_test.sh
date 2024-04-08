f81fb5b626a9e15454d19475b05c8d59c8f852f1
ashilkn <ashilkn99@gmail.com>
2024-04-08 13:21:29 +0530

    write perf test for app init

---
+++ mobile/app_init_perf_test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Make sure to go through home_gallery_scroll_test.dart and 
+# fill in email and password.
+# Specify destination directory for the perf results in perf_driver.dart.
+
+
+export ENDPOINT="https://api.ente.io"
+
+flutter drive \
+  --driver=test_driver/perf_driver.dart \
+  --target=integration_test/app_init_test.dart \
+  --dart-define=endpoint=$ENDPOINT \
+  --profile --flavor independent \
+  --no-dds \
+  --keep-app-running
+
+exit $?
