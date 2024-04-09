54a973c457b7e40c992102012f49430073f1ba03
Manav Rathi <manav@mrmr.io>
2024-04-09 09:37:22 +0530

    Expose on the globalThis
    Note that the filename of the .d.ts has to be different from any existing file!
    https://stackoverflow.com/questions/59728371/typescript-d-ts-file-not-recognized

--- web/packages/next/electron.ts
+++ web/packages/next/electron.ts
@@ -1,10 +1,22 @@
-import type { ElectronAPIsType } from "./types/ipc";
+import type { Electron } from "./types/ipc";
 
+// type ElectronAPIsType =
 // TODO (MR):
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const ElectronAPIs = (globalThis as unknown as any)[
     // eslint-disable-next-line @typescript-eslint/dot-notation, @typescript-eslint/no-unsafe-member-access
     "ElectronAPIs"
-] as ElectronAPIsType;
+] as Electron;
+
+// /**
+//  * Extend the global object's (`globalThis`) interface to state that it can
+//  * potentially hold a property called `electron`. It will be injected by our
+//  * preload.js script when we're running in the context of our desktop app.
+//  */
+// declare global {
+//     const electron: Electron | undefined;
+// }
+
+// export const globalElectron = globalThis.electron;
 
 export default ElectronAPIs;
