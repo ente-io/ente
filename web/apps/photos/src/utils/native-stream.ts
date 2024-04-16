81d1563c3d2c806da58920a26ad96e842e02c817
Manav Rathi <manav@mrmr.io>
2024-04-16 10:17:34 +0530

    Also reproducible on latest beta

--- web/apps/photos/src/utils/native-stream.ts
+++ web/apps/photos/src/utils/native-stream.ts
@@ -15,7 +15,35 @@
  * @param stream The stream which should be written into the file.
  *  */
 export const writeStream = async (path: string, stream: ReadableStream) => {
+    // return writeStreamOneShot(path, stream)
 
+    // The duplex parameter needs to be set to 'half' when streaming requests.
+    //
+    // Currently browsers, and specifically in our case, since this code runs
+    // only within our desktop (Electron) app, Chromium, don't support 'full'
+    // duplex mode (i.e. streaming both the request and the response).
+    // https://developer.chrome.com/docs/capabilities/web-apis/fetch-streaming-requests
+    const req = new Request(`stream://write${path}`, {
+        // GET can't have a body
+        method: "POST",
+        headers: { "Content-Type": "application/octet-stream" },
+        body: stream,
+        // @ts-expect-error TypeScript's libdom.d.ts does not include the
+        // "duplex" parameter, e.g. see
+        // https://github.com/node-fetch/node-fetch/issues/1769.
+        duplex: "half",
+    });
+    const res = await fetch(req);
+    if (!res.ok)
+        throw new Error(
+            `Failed to write stream to ${path}: HTTP ${res.status}`,
+        );
+};
+
+export const writeStreamOneShot = async (
+    path: string,
+    stream: ReadableStream,
+) => {
     const response = new Response(stream);
     const blob = await response.blob();
     // const ReadableStream()
