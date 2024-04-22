5b4d66933e409a539fde1110f94e8cfc09797fc2
Neeraj Gupta <254676+ua741@users.noreply.github.com>
2024-04-22 16:44:12 +0530

    [server] Add req validation for remoteStore insertOrUpdate

--- server/pkg/controller/remotestore/controller.go
+++ server/pkg/controller/remotestore/controller.go
@@ -3,6 +3,7 @@ package remotestore
 import (
 	"database/sql"
 	"errors"
+	"fmt"
 
 	"github.com/ente-io/museum/ente"
 	"github.com/ente-io/museum/pkg/repo/remotestore"
@@ -11,13 +12,34 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type FlagKey string
+
+const (
+	RecoveryKeyVerified FlagKey = "recoveryKeyVerified"
+	MapEnabled          FlagKey = "mapEnabled"
+	FaceSearchEnabled   FlagKey = "faceSearchEnabled"
+	PassKeyEnabled      FlagKey = "passKeyEnabled"
+)
+
+var (
+	_allowKeys = map[FlagKey]*bool{
+		RecoveryKeyVerified: nil,
+		MapEnabled:          nil,
+		FaceSearchEnabled:   nil,
+		PassKeyEnabled:      nil,
+	}
+)
+
 // Controller is interface for exposing business logic related to for remote store
 type Controller struct {
 	Repo *remotestore.Repository
 }
 
-// Insert of update the key's value
+// InsertOrUpdate the key's value
 func (c *Controller) InsertOrUpdate(ctx *gin.Context, request ente.UpdateKeyValueRequest) error {
+	if err := _validateRequest(request); err != nil {
+		return err
+	}
 	userID := auth.GetUserID(ctx.Request.Header)
 	return c.Repo.InsertOrUpdate(ctx, userID, request.Key, request.Value)
 }
@@ -33,4 +55,14 @@ func (c *Controller) Get(ctx *gin.Context, req ente.GetValueRequest) (*ente.GetV
 		}
 	}
 	return &ente.GetValueResponse{Value: value}, nil
+}
+
+func _validateRequest(request ente.UpdateKeyValueRequest) error {
+	if _, ok := _allowKeys[FlagKey(request.Key)]; !ok {
+		return stacktrace.Propagate(ente.NewBadRequestWithMessage(fmt.Sprintf("key %s is not allowed", request.Key)), "key not allowed")
+	}
+	if request.Value != "true" && request.Value != "false" {
+		return stacktrace.Propagate(ente.NewBadRequestWithMessage(fmt.Sprintf("value %s is not allowed", request.Value)), "value not allowed")
+	}
+	return nil
 }
