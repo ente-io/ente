811ffe0117637e94c4211fc4fe2da8ba179c404e
laurenspriem <github_uni.fishers@aleeas.com>
2024-04-26 14:13:00 +0530

    [mob][photos] Create new cluster when tapping unassigned face

--- mobile/lib/face/db.dart
+++ mobile/lib/face/db.dart
@@ -348,6 +348,18 @@ class FaceMLDataDB {
     return result;
   }
 
+  Future<int?> getClusterIDForFaceID(String faceID) async {
+    final db = await instance.sqliteAsyncDB;
+    final List<Map<String, dynamic>> maps = await db.getAll(
+      'SELECT $fcClusterID FROM $faceClustersTable WHERE $fcFaceId = ?',
+      [faceID],
+    );
+    if (maps.isEmpty) {
+      return null;
+    }
+    return maps.first[fcClusterID] as int;
+  }
+
   Future<Map<int, Iterable<String>>> getAllClusterIdToFaceIDs() async {
     final db = await instance.sqliteAsyncDB;
     final Map<int, List<String>> result = {};
