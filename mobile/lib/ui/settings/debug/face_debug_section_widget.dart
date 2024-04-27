968eaaf5f6b8b47e203d1196163b9c352960b997
laurenspriem <github_uni.fishers@aleeas.com>
2024-04-27 09:39:12 +0530

    [mob][photos] Better error logging

--- mobile/lib/ui/settings/debug/face_debug_section_widget.dart
+++ mobile/lib/ui/settings/debug/face_debug_section_widget.dart
@@ -216,9 +216,14 @@ class _FaceDebugSectionWidgetState extends State<FaceDebugSectionWidget> {
           trailingIcon: Icons.chevron_right_outlined,
           trailingIconIsMuted: true,
           onTap: () async {
-            await FaceMLDataDB.instance.dropFeedbackTables();
-            Bus.instance.fire(PeopleChangedEvent());
-            showShortToast(context, "Done");
+            try {
+              await FaceMLDataDB.instance.dropFeedbackTables();
+              Bus.instance.fire(PeopleChangedEvent());
+              showShortToast(context, "Done");
+            } catch (e, s) {
+              _logger.warning('reset feedback failed ', e, s);
+              await showGenericErrorDialog(context: context, error: e);
+            }
           },
         ),
         sectionOptionSpacing,
