31a70674ff3c875a223016a2c7f76ed0ede18b3b
Manav Rathi <manav@mrmr.io>
2024-04-27 16:49:22 +0530

    Revert "Read in 4 MB blocks"
    This reverts commit db30d8d81e7082562fdb645a8f9ac49d332be3d2.

--- web/apps/photos/src/services/upload/uploadService.ts
+++ web/apps/photos/src/services/upload/uploadService.ts
@@ -774,18 +774,9 @@ const computeHash = async (
     console.log("got stream and chunks", stream, chunkCount);
     const hashState = await worker.initChunkHashing();
 
-    const chunkedStream = stream.pipeThrough(
-        new TransformStream(
-            undefined,
-            new ByteLengthQueuingStrategy({
-                highWaterMark: ENCRYPTION_CHUNK_SIZE,
-            }),
-        ),
-    );
-    const streamReader = chunkedStream.getReader();
+    const streamReader = stream.getReader();
     for (let i = 0; i < chunkCount; i++) {
         const { done, value: chunk } = await streamReader.read();
-        console.log("chunk size", chunk.length);
         if (done) throw new Error("Less chunks than expected");
         await worker.hashFileChunk(hashState, Uint8Array.from(chunk));
     }
