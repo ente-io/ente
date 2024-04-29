aa111b2a245e2a5b7f64c41d673d924e6bf16711
Manav Rathi <manav@mrmr.io>
2024-04-29 10:12:53 +0530

    Outline the plan

--- desktop/src/main/stores/upload-status.ts
+++ desktop/src/main/stores/upload-status.ts
@@ -1,12 +1,31 @@
 import Store, { Schema } from "electron-store";
 
 export interface UploadStatusStore {
-    filePaths: string[];
-    zipPaths: string[];
+    /**
+     * The name of the collection (when uploading to a singular collection) or
+     * the root collection (when uploading to separate * albums) to which we
+     * these uploads are meant to go to.
+     */
     collectionName: string;
+    /**
+     * Paths of regular files that need to be uploaded.
+     */
+    filePaths: string[];
+    /**
+     * Paths of zip files that need to be uploaded.
+     */
+    zipPaths: string[];
+    /**
+     * For each zip file, which of its entries (paths) within the zip file that
+     * need to be uploaded.
+     */
+    zipEntries: Record<string, string[]>;
 }
 
 const uploadStatusSchema: Schema<UploadStatusStore> = {
+    collectionName: {
+        type: "string",
+    },
     filePaths: {
         type: "array",
         items: {
@@ -19,8 +38,8 @@ const uploadStatusSchema: Schema<UploadStatusStore> = {
             type: "string",
         },
     },
-    collectionName: {
-        type: "string",
+    zipEntries: {
+        type: "object",
     },
 };
 
