b7842765e883735f5b8c14eb2e86b741081946b3
Manav Rathi <manav@mrmr.io>
2024-05-03 19:23:54 +0530

    Document

--- web/apps/cast/src/utils/useCastReceiver.tsx
+++ web/apps/cast/src/utils/useCastReceiver.tsx
@@ -5,21 +5,19 @@ type Receiver = {
     cast: typeof cast;
 };
 
+/**
+ * Load the Chromecast Web Receiver SDK and return a reference to the `cast`
+ * global object that the SDK attaches to the window.
+ */
 export const useCastReceiver = () => {
-    const [receiver, setReceiver] = useState<Receiver | null>({
-        cast: null,
-    });
+    const [receiver, setReceiver] = useState<Receiver | undefined>();
 
     useEffect(() => {
         const script = document.createElement("script");
         script.src =
             "https://www.gstatic.com/cast/sdk/libs/caf_receiver/v3/cast_receiver_framework.js";
 
-        script.addEventListener("load", () => {
-            setReceiver({
-                cast,
-            });
-        });
+        script.addEventListener("load", () => setReceiver({ cast }));
         document.body.appendChild(script);
     }, []);
 
