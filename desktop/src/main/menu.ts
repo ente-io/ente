5f964e533c1e9d224c27d89be794e9d24e78d020
Manav Rathi <manav@mrmr.io>
2024-05-06 16:07:35 +0530

    Show always (for temp debugging)

--- desktop/src/main/menu.ts
+++ desktop/src/main/menu.ts
@@ -10,7 +10,7 @@ import { forceCheckForAppUpdates } from "./services/app-update";
 import autoLauncher from "./services/auto-launcher";
 import { openLogDirectory } from "./services/dir";
 import { userPreferences } from "./stores/user-preferences";
-import { isDev } from "./utils/electron";
+// import { isDev } from "./utils/electron";
 
 /** Create and return the entries in the app's main menu bar */
 export const createApplicationMenu = async (mainWindow: BrowserWindow) => {
@@ -24,9 +24,10 @@ export const createApplicationMenu = async (mainWindow: BrowserWindow) => {
     const macOSOnly = (options: MenuItemConstructorOptions[]) =>
         process.platform == "darwin" ? options : [];
 
-    const devOnly = (options: MenuItemConstructorOptions[]) =>
-        // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-        isDev ?? true ? options : [];
+    // TODO(MR): Desktop-release
+    // const devOnly = (options: MenuItemConstructorOptions[]) =>
+    //     // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+    //     isDev ?? true ? options : [];
 
     const handleCheckForUpdates = () => forceCheckForAppUpdates(mainWindow);
 
@@ -146,9 +147,9 @@ export const createApplicationMenu = async (mainWindow: BrowserWindow) => {
             label: "View",
             submenu: [
                 { label: "Reload", role: "reload" },
-                ...devOnly([
-                    { label: "Toggle Dev Tools", role: "toggleDevTools" },
-                ]),
+                // ...devOnly([
+                { label: "Toggle Dev Tools", role: "toggleDevTools" },
+                // ]),
                 { type: "separator" },
                 { label: "Toggle Full Screen", role: "togglefullscreen" },
             ],
