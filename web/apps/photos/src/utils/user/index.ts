e934cc8cb4c3fd5c9df1eca763f638e5bc5fabc1
Manav Rathi <manav@mrmr.io>
2024-05-06 20:11:27 +0530

    Allow UID 1 to re-index

--- web/apps/photos/src/utils/user/index.ts
+++ web/apps/photos/src/utils/user/index.ts
@@ -1,4 +1,5 @@
 import { getData, LS_KEYS } from "@ente/shared/storage/localStorage";
+import type { User } from "@ente/shared/user/types";
 import { UserDetails } from "types/user";
 
 export function getLocalUserDetails(): UserDetails {
@@ -9,7 +10,12 @@ export const isInternalUser = () => {
     const userEmail = getData(LS_KEYS.USER)?.email;
     if (!userEmail) return false;
 
-    return (
-        userEmail.endsWith("@ente.io") || userEmail === "kr.anand619@gmail.com"
-    );
+    return userEmail.endsWith("@ente.io");
+};
+
+export const isInternalUserForML = () => {
+    const userId = (getData(LS_KEYS.USER) as User)?.id;
+    if (userId == 1) return true;
+
+    return isInternalUser();
 };
