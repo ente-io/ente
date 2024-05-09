bedb515dc2c26c52c63791a8c410f1b7889c0a66
Manav Rathi <manav@mrmr.io>
2024-05-09 09:54:06 +0530

    Cleanup

--- web/apps/photos/src/components/UploadSelectorInputs.tsx
+++ web/apps/photos/src/components/UploadSelectorInputs.tsx
@@ -1,9 +1,24 @@
-export default function UploadSelectorInputs({
+type GetInputProps = () => React.HTMLAttributes<HTMLInputElement>;
+
+interface UploadSelectorInputsProps {
+    getDragAndDropInputProps: GetInputProps;
+    getFileSelectorInputProps: GetInputProps;
+    getFolderSelectorInputProps: GetInputProps;
+    getZipFileSelectorInputProps?: GetInputProps;
+}
+
+/**
+ * Create a bunch of HTML inputs elements, one each for the given props.
+ *
+ * These hidden input element serve as the way for us to show various file /
+ * folder Selector dialogs and handle drag and drop inputs.
+ */
+export const UploadSelectorInputs: React.FC<UploadSelectorInputsProps> = ({
     getDragAndDropInputProps,
     getFileSelectorInputProps,
     getFolderSelectorInputProps,
     getZipFileSelectorInputProps,
-}) {
+}) => {
     return (
         <>
             <input {...getDragAndDropInputProps()} />
@@ -14,4 +29,4 @@ export default function UploadSelectorInputs({
             )}
         </>
     );
-}
+};
