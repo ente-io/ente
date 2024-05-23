3e79c8cf28ed58a6c54712ead3638a6270d9e32b
laurenspriem <github_uni.fishers@aleeas.com>
2024-05-23 18:12:41 +0530

    [mob][photos] Decrypt remote embeddings in computer

--- mobile/lib/services/machine_learning/file_ml/remote_fileml_service.dart
+++ mobile/lib/services/machine_learning/file_ml/remote_fileml_service.dart
@@ -1,6 +1,7 @@
 import "dart:async";
 import "dart:convert";
 
+import "package:computer/computer.dart";
 import "package:logging/logging.dart";
 import "package:photos/core/network/network.dart";
 import "package:photos/db/files_db.dart";
@@ -15,6 +16,8 @@ import "package:shared_preferences/shared_preferences.dart";
 
 class RemoteFileMLService {
   RemoteFileMLService._privateConstructor();
+
+  static final Computer _computer = Computer.shared();
 
   static final RemoteFileMLService instance =
       RemoteFileMLService._privateConstructor();
@@ -107,15 +110,17 @@ class RemoteFileMLService {
       final input = EmbeddingsDecoderInput(embedding, fileKey);
       inputs.add(input);
     }
-    // todo: use compute or isolate
-    return decryptFileMLComputer(
-      {
+    return _computer.compute<Map<String, dynamic>, Map<int, FileMl>>(
+      _decryptFileMLComputer,
+      param: {
         "inputs": inputs,
       },
     );
   }
 
-  Future<Map<int, FileMl>> decryptFileMLComputer(
+}
+
+Future<Map<int, FileMl>> _decryptFileMLComputer(
     Map<String, dynamic> args,
   ) async {
     final result = <int, FileMl>{};
@@ -135,4 +140,3 @@ class RemoteFileMLService {
     }
     return result;
   }
-}
