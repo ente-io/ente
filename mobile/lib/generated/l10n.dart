1ec7e026950ea1be4d7d50879009d0b461cda739
laurenspriem <github_uni.fishers@aleeas.com>
2024-05-25 12:03:34 +0530

    [mob][photos] Copy change

--- mobile/lib/generated/l10n.dart
+++ mobile/lib/generated/l10n.dart
@@ -8794,10 +8794,10 @@ class S {
     );
   }
 
-  /// `Indexing is paused, will automatically resume when device is ready`
+  /// `Indexing is paused. It will automatically resume when device is ready`
   String get indexingIsPaused {
     return Intl.message(
-      'Indexing is paused, will automatically resume when device is ready',
+      'Indexing is paused. It will automatically resume when device is ready',
       name: 'indexingIsPaused',
       desc: '',
       args: [],
