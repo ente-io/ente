e836ada0d6c28294deb7c8798b53e3c4a156cd76
Manav Rathi <manav@mrmr.io>
2024-05-26 20:01:23 +0530

    Refactor

---
+++ web/apps/photos/src/pages/verify.tsx
@@ -0,0 +1,8 @@
+import { ensure } from "@/utils/ensure";
+import Page_ from "@ente/accounts/pages/verify";
+import { useContext } from "react";
+import { AppContext } from "./_app";
+
+const Page = () => <Page_ appContext={ensure(useContext(AppContext))} />;
+
+export default Page;
