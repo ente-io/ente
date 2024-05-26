4f31bd625d763f0d683f63db1a49b5b43a74f2ed
Manav Rathi <manav@mrmr.io>
2024-05-26 18:05:04 +0530

    Context

--- web/packages/shared/apps/types.ts
+++ web/packages/shared/apps/types.ts
@@ -1,14 +1,9 @@
+import type { BaseAppContextT } from "@/next/types/app";
 import { TwoFactorType } from "@ente/accounts/constants/twofactor";
-import type { DialogBoxAttributesV2 } from "@ente/shared/components/DialogBoxV2/types";
 import { APPS } from "./constants";
 
 export interface PageProps {
-    appContext: {
-        showNavBar: (show: boolean) => void;
-        isMobile: boolean;
-        setDialogBoxAttributesV2: (attrs: DialogBoxAttributesV2) => void;
-        logout: () => void;
-    };
+    appContext: BaseAppContextT;
     appName: APPS;
     twoFactorType?: TwoFactorType;
 }
