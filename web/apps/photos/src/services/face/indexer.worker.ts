daf72d8ac6d72aeaf8b832be8e339e7b71b69962
Manav Rathi <manav@mrmr.io>
2024-05-29 15:39:13 +0530

    Tweak

--- web/apps/photos/src/services/face/indexer.worker.ts
+++ web/apps/photos/src/services/face/indexer.worker.ts
@@ -7,16 +7,16 @@ import { indexFaces } from "./f-index";
 /**
  * Face indexing orchestrator.
  *
- * This is class that drives the face indexing process, across all files that
- * need to still be indexed. This usually runs in a Web Worker so as to not get
- * in the way of the main thread.
+ * This is class that drives the face indexing process across all files that
+ * need to still be indexed. It runs in a Web Worker so as to not get in the way
+ * of the main thread.
  *
  * It operates in two modes - live indexing and backfill.
  *
  * In live indexing, any files that are being uploaded from the current client
- * are provided to the indexer, which indexes them. This is more efficient since
- * we already have the file's content at hand and do not have to download and
- * decrypt it.
+ * are provided to the indexer, which puts them in a queue and indexes them one
+ * by one. This is more efficient since we already have the file's content at
+ * hand and do not have to download and decrypt it.
  *
  * In backfill, the indexer figures out if any of the user's files (irrespective
  * of where they were uploaded from) still need to be indexed, and if so,
