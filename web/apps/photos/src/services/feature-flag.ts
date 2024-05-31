3ad8f732893c4bf34271cbc5149eb753adfb9023
Manav Rathi <manav@mrmr.io>
2024-05-31 14:06:06 +0530

    mandate

---
+++ web/apps/photos/src/services/feature-flag.ts
@@ -0,0 +1,15 @@
+/**
+ * Fetch feature flags (potentially user specific) from remote and save them in
+ * local storage for subsequent lookup.
+ */
+export const fetchAndSaveFeatureFlags = async () => {};
+
+/**
+ * Return `true` if the current user is marked as an "internal" user.
+ */
+export const isInternalUser = async () => {};
+
+/**
+ * Return `true` if the current user is marked as an "beta" user.
+ */
+export const isBetaUser = async () => {};
