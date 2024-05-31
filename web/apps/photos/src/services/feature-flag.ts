c7c50293df0605ce72971c32a977ca482ebe18b1
Manav Rathi <manav@mrmr.io>
2024-05-31 15:30:58 +0530

    Reduce delay

--- web/apps/photos/src/services/feature-flag.ts
+++ web/apps/photos/src/services/feature-flag.ts
@@ -24,7 +24,7 @@ export const fetchAndSaveFeatureFlagsIfNeeded = () => {
         void fetchAndSaveFeatureFlags().then(() => {
             _haveFetched = true;
         });
-    }, 5000);
+    }, 2000);
 };
 
 export const clearFeatureFlagSessionState = () => {
