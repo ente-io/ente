176289d0a81b3524e116e802f463befb3f040be4
Manav Rathi <manav@mrmr.io>
2024-06-02 16:50:30 +0530

    zod flags

--- web/packages/new/photos/services/feature-flags.ts
+++ web/packages/new/photos/services/feature-flags.ts
@@ -1,5 +1,6 @@
 import log from "@/next/log";
 import { ensure } from "@/utils/ensure";
+import { nullToUndefined } from "@/utils/transform";
 import { apiOrigin } from "@ente/shared/network/api";
 import { getToken } from "@ente/shared/storage/localStorage/helpers";
 import { z } from "zod";
@@ -13,8 +14,25 @@ let _haveFetched = false;
  *
  * It fetches only once per session, and so is safe to call as arbitrarily many
  * times. Remember to call {@link clearFeatureFlagSessionState} on logout to
- * forget that we've already fetched so that these can be fetched again on the
+ * clear any in memory state so that these can be fetched again on the
  * subsequent login.
+ *
+ * [Note: Feature Flags]
+ *
+ * The workflow with feature flags is:
+ *
+ * 1. On app start feature flags are fetched once and saved in local storage. If
+ *    this fetch fails, we try again periodically (on every "sync") until
+ *    success.
+ *
+ * 2. Attempts to access any individual feature flage (e.g.
+ *    {@link isInternalUser}) returns the corresponding value from local storage
+ *    (substituting a default if needed).
+ *
+ * 3. However, if perchance the fetch-on-app-start hasn't completed yet (or had
+ *    failed), then a new fetch is tried. If even this fetch fails, we return
+ *    the default. Otherwise the now fetched result is saved to local storage
+ *    and the corresponding value returned.
  */
 export const fetchAndSaveFeatureFlagsIfNeeded = () => {
     if (_haveFetched) return;
@@ -62,31 +80,25 @@ const saveFlagJSONString = (s: string) =>
 const remoteFeatureFlags = () => {
     const s = localStorage.getItem("remoteFeatureFlags");
     if (!s) return undefined;
-    return JSON.parse(s);
+    return FeatureFlags.parse(s);
 };
 
 const FeatureFlags = z.object({
-    internalUser: z
-        .boolean()
-        .nullish()
-        .transform((v) => (v === null ? undefined : v)),
-    betaUser: z
-        .boolean()
-        .nullish()
-        .transform((v) => (v === null ? undefined : v)),
+    internalUser: z.boolean().nullish().transform(nullToUndefined),
+    betaUser: z.boolean().nullish().transform(nullToUndefined),
 });
 
-type FeatureFlags = 
+type FeatureFlags = z.infer<typeof FeatureFlags>;
 
 const remoteFeatureFlagsFetchingIfNeeded = async () => {
-    let ff = await remoteFeatureFlags();
+    let ff = remoteFeatureFlags();
     if (!ff) {
         try {
             await fetchAndSaveFeatureFlags();
-            ff = await remoteFeatureFlags();
         } catch (e) {
             log.warn("Ignoring error when fetching feature flags", e);
         }
+        ff = remoteFeatureFlags();
     }
     return ff;
 };
@@ -95,17 +107,8 @@ const remoteFeatureFlagsFetchingIfNeeded = async () => {
  * Return `true` if the current user is marked as an "internal" user.
  */
 export const isInternalUser = async () => {
-    // TODO: Dedup
     const flags = await remoteFeatureFlagsFetchingIfNeeded();
-    // TODO(MR): Use Yup here
-    if (
-        flags &&
-        typeof flags === "object" &&
-        "internalUser" in flags &&
-        typeof flags.internalUser == "boolean"
-    )
-        return flags.internalUser;
-    return false;
+    return flags?.internalUser ?? false;
 };
 
 /**
@@ -113,13 +116,5 @@ export const isInternalUser = async () => {
  */
 export const isBetaUser = async () => {
     const flags = await remoteFeatureFlagsFetchingIfNeeded();
-    // TODO(MR): Use Yup here
-    if (
-        flags &&
-        typeof flags === "object" &&
-        "betaUser" in flags &&
-        typeof flags.betaUser == "boolean"
-    )
-        return flags.betaUser;
-    return false;
+    return flags?.betaUser ?? false;
 };
