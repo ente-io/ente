ebab8568b99835aeecf4dc442759496e78d30159
Manav Rathi <manav@mrmr.io>
2024-06-02 14:31:01 +0530

    Prune old config

--- web/packages/eslint-config/index.js
+++ web/packages/eslint-config/index.js
@@ -4,7 +4,6 @@ module.exports = {
         "eslint:recommended",
         "plugin:@typescript-eslint/recommended",
         "plugin:@typescript-eslint/recommended-requiring-type-checking",
-        "prettier",
     ],
     parserOptions: {
         tsconfigRootDir: __dirname,
