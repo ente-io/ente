50b032654cf8da0a5ef99cc49db72013e3368bf4
Manav Rathi <manav@mrmr.io>
2024-06-05 14:16:27 +0530

    Prune

--- web/packages/shared/apps/constants.ts
+++ web/packages/shared/apps/constants.ts
@@ -1,18 +1,5 @@
-import type { AppName } from "@/next/types/app";
-
 export enum APPS {
     PHOTOS = "PHOTOS",
     AUTH = "AUTH",
     ACCOUNTS = "ACCOUNTS",
 }
-
-export const appNameToAppNameOld = (appName: AppName): APPS => {
-    switch (appName) {
-        case "accounts":
-            return APPS.ACCOUNTS;
-        case "photos":
-            return APPS.PHOTOS;
-        case "auth":
-            return APPS.AUTH;
-    }
-};
