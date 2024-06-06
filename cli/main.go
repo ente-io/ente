f4d44e484dacef378f5a9193f35eb8c6fdec98d4
Neeraj Gupta <254676+ua741@users.noreply.github.com>
2024-06-06 08:51:12 +0530

    [cli] Update README

--- cli/main.go
+++ cli/main.go
@@ -23,7 +23,7 @@ func main() {
 		cliDBPath = constants.CliDataPath
 		_, err := internal.ValidateDirForWrite(cliDBPath)
 		if err != nil {
-			log.Fatalf("Please mount a volume to %s to persist cli data\n%v\n", cliDBPath, err)
+			log.Fatalf("Please mount a volume to %s\n%v\n", cliDBPath, err)
 		}
 	}
 	if err != nil {
