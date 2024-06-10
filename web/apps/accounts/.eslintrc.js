0b4d47933719760d556b82e589da787957667318
Manav Rathi <manav@mrmr.io>
2024-06-10 15:58:07 +0530

    Move towards new ruleset

--- web/apps/accounts/.eslintrc.js
+++ web/apps/accounts/.eslintrc.js
@@ -1,3 +1,4 @@
+/*
 module.exports = {
     // When root is set to true, ESLint will stop looking for configuration files in parent directories.
     // This is required here to ensure desktop picks the right eslint config, where this app is
@@ -10,4 +11,24 @@ module.exports = {
         project: "./tsconfig.json",
     },
     ignorePatterns: [".eslintrc.js", "out", "next.config.js", "next-env.d.ts"],
+};
+*/
+module.exports = {
+    extends: ["@/build-config/eslintrc-next"],
+    ignorePatterns: ["next.config.base.js"],
+    /* TODO: Temporary overrides */
+    rules: {
+        "react/react-in-jsx-scope": "off",
+        "react/prop-types": "off",
+        "react-hooks/exhaustive-deps": "off",
+        "@typescript-eslint/no-unsafe-assignment": "off",
+        "@typescript-eslint/no-misused-promises": "off",
+        "@typescript-eslint/no-floating-promises": "off",
+        "@typescript-eslint/no-unsafe-member-access": "off",
+        "@typescript-eslint/no-unsafe-assignment": "off",
+        "@typescript-eslint/no-explicit-any": "off",
+        "@typescript-eslint/no-unsafe-argument": "off",
+        "@typescript-eslint/restrict-template-expressions": "off",
+        "react-refresh/only-export-components": "off",
+    },
 };
