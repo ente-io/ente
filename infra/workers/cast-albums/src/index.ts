eaccba5f22312179ba005d7e0c30062cfe85a207
Manav Rathi <manav@mrmr.io>
2024-06-15 12:02:29 +0530

    Explicit header whitelist

--- infra/workers/cast-albums/src/index.ts
+++ infra/workers/cast-albums/src/index.ts
@@ -1,17 +1,28 @@
-/** Proxy file and thumbnail requests from the cast web app */
+/** Proxy file and thumbnail requests for the cast web app */
 
 export default {
     async fetch(request: Request) {
         switch (request.method) {
+            case "OPTIONS":
+                return handleOPTIONS();
             case "GET":
                 return handleGET(request);
-            case "OPTIONS":
-                return handleOPTIONS(request);
             default:
                 throw new Error(`Unsupported HTTP method ${request.method}`);
         }
     },
 } satisfies ExportedHandler;
+
+const handleOPTIONS = () => {
+    return new Response("", {
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET, OPTIONS",
+            "Access-Control-Max-Age": "86400",
+            "Access-Control-Allow-Headers": "X-Cast-Access-Token",
+        },
+    });
+};
 
 const handleGET = async (request: Request) => {
     const url = new URL(request.url);
@@ -31,19 +42,4 @@ const handleGET = async (request: Request) => {
     response = new Response(response.body, response);
     response.headers.set("Access-Control-Allow-Origin", "*");
     return response;
-};
-
-const handleOPTIONS = (request: Request) => {
-    let corsHeaders: Record<string, string> = {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "GET, OPTIONS",
-        "Access-Control-Max-Age": "86400",
-    };
-
-    const acrh = request.headers.get("Access-Control-Request-Headers");
-    if (acrh) {
-        corsHeaders["Access-Control-Allow-Headers"] = acrh;
-    }
-
-    return new Response("", { headers: corsHeaders });
 };
