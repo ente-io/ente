d76c6dd63c5ba553562d19d98b32710d51374618
Manav Rathi <manav@mrmr.io>
2024-06-16 13:24:56 +0530

    Sketch

---
+++ infra/workers/thumbnails/src/index.ts
@@ -0,0 +1,15 @@
+/** Proxy requests for thumbnails. */
+
+export default {
+    async fetch(request: Request) {
+        switch (request.method) {
+            // case "OPTIONS":
+            //     return handleOPTIONS(request);
+            // case "GET":
+            //     return handleGET(request);
+            default:
+                console.log(`Unsupported HTTP method ${request.method}`);
+                return new Response(null, { status: 405 });
+        }
+    },
+} satisfies ExportedHandler;
