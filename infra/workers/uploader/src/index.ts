d5a858615277f078ec4cc9cb2e7d23c66c58e095
Manav Rathi <manav@mrmr.io>
2024-06-17 03:40:20 +0530

    Import functionality
    Rewritten but referencing the code imported from dashboard

--- infra/workers/uploader/src/index.ts
+++ infra/workers/uploader/src/index.ts
@@ -9,8 +9,10 @@ export default {
         switch (request.method) {
             case "OPTIONS":
                 return handleOPTIONS(request);
-            // case "GET":
-            //     return handleGET(request);
+            case "POST":
+                return handlePOSTOrPUT(request);
+            case "PUT":
+                return handlePOSTOrPUT(request);
             default:
                 console.log(`Unsupported HTTP method ${request.method}`);
                 return new Response(null, { status: 405 });
@@ -31,7 +33,7 @@ const handleOPTIONS = (request: Request) => {
             "Access-Control-Max-Age": "86400",
             // "Access-Control-Allow-Headers": "UPLOAD-URL",
             "Access-Control-Allow-Headers": "*",
-            "Access-Control-Expose-Headers": "X-Request-ID, CF-Ray",
+            "Access-Control-Expose-Headers": "X-Request-Id, CF-Ray",
         },
     });
 };
@@ -63,4 +65,59 @@ const areAllowedHeaders = (headers: string | null) => {
         if (!allowed.includes(header.trim().toLowerCase())) return false;
     }
     return true;
+};
+
+const handlePOSTOrPUT = async (request: Request) => {
+    const url = new URL(request.url);
+
+    const uploadURL = request.headers.get("UPLOAD-URL");
+    if (!uploadURL) {
+        console.error("No uploadURL provided");
+        return new Response(null, { status: 400 });
+    }
+
+    let response: Response;
+    switch (url.pathname) {
+        case "/file-upload":
+            response = await fetch(uploadURL, {
+                method: request.method,
+                body: request.body,
+            });
+            break;
+        case "/multipart-upload":
+            response = await fetch(uploadURL, {
+                method: request.method,
+                body: request.body,
+            });
+            if (response.ok) {
+                const etag = response.headers.get("etag");
+                if (etag === null) {
+                    console.log("No etag in response", response);
+                    response = new Response(null, { status: 500 });
+                } else {
+                    response = new Response(JSON.stringify({ etag }));
+                }
+            }
+            break;
+        case "/multipart-complete":
+            response = await fetch(uploadURL, {
+                method: request.method,
+                body: request.body,
+                headers: {
+                    "Content-Type": "text/xml",
+                },
+            });
+            break;
+        default:
+            response = new Response(null, { status: 404 });
+            break;
+    }
+
+    response = new Response(response.body, response);
+    response.headers.set("Access-Control-Allow-Origin", "*");
+    response.headers.set(
+        "Access-Control-Expose-Headers",
+        "X-Request-Id, CF-Ray"
+    );
+    return response;
 };
