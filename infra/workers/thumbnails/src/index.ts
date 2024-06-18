64dedb7edd29483ba9a5d322b7064276d5246d36
Manav Rathi <manav@mrmr.io>
2024-06-18 09:50:20 +0530

    thumb

--- infra/workers/thumbnails/src/index.ts
+++ infra/workers/thumbnails/src/index.ts
@@ -32,20 +32,18 @@ const handleOPTIONS = (request: Request) => {
 };
 
 const isAllowedOrigin = (origin: string | null) => {
-    const desktopApp = "ente://app";
-    const allowedHostnames = [
-        "web.ente.io",
-        "photos.ente.io",
-        "photos.ente.sh",
-        "localhost",
-    ];
-
     if (!origin) return false;
     try {
         const url = new URL(origin);
-        return origin == desktopApp || allowedHostnames.includes(url.hostname);
+        const hostname = url.hostname;
+        return (
+            origin == "ente://app" /* desktop app */ ||
+            hostname.endsWith("ente.io") ||
+            hostname.endsWith("ente.sh") ||
+            hostname == "localhost"
+        );
     } catch {
-        // origin is likely an invalid URL
+        // `origin` is likely an invalid URL.
         return false;
     }
 };
