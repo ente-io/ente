f8e5bd3d66d56480240e6d068c41cde2ae2c9aab
Manav Rathi <manav@mrmr.io>
2024-07-02 12:07:45 +0530

    Workaround failures on GitHub action

--- web/packages/new/photos/services/ffmpeg/worker.ts
+++ web/packages/new/photos/services/ffmpeg/worker.ts
@@ -1,3 +1,10 @@
+// TODO: These can be removed when we start using ffmpeg upstream. For an reason
+// I haven't investigated much, when we run eslint on our CI, it seems to behave
+// differently than locally and give a lot of warnings that possibly arise from
+// it not being able to locate ffmpeg-wasm.
+
+/* eslint-disable */
+
 import log from "@/next/log";
 import QueueProcessor from "@ente/shared/utils/queueProcessor";
 import { expose } from "comlink";
