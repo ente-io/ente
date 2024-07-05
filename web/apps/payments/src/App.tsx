7cef60f8b9654418e6a16f6cda023f2e14c786b9
Manav Rathi <manav@mrmr.io>
2024-07-05 19:39:20 +0530

    Inline

--- web/apps/payments/src/App.tsx
+++ web/apps/payments/src/App.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useRef, useState } from "react";
-import { Container } from "./components/Container";
 import { parseAndHandleRequest } from "./services/billing";
 import S from "./utils/strings";
 
@@ -17,5 +16,9 @@ export const App: React.FC = () => {
 
     return <Container>{failed ? S.error_generic : <Spinner />}</Container>;
 };
+
+const Container: React.FC<React.PropsWithChildren> = ({ children }) => (
+    <div className="container">{children}</div>
+);
 
 const Spinner: React.FC = () => <div className="loading-spinner"></div>;
