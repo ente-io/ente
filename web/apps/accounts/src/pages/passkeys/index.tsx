cf7020ab782703cb0664b121bf8db38c2ec3f5e2
Manav Rathi <manav@mrmr.io>
2024-07-18 21:08:25 +0530

    Use prebuilt dialog box

--- web/apps/accounts/src/pages/passkeys/index.tsx
+++ web/apps/accounts/src/pages/passkeys/index.tsx
@@ -5,7 +5,6 @@ import log from "@/base/log";
 import { ensure } from "@/utils/ensure";
 import { CenteredFlex } from "@ente/shared/components/Container";
 import DialogBoxV2 from "@ente/shared/components/DialogBoxV2";
-import EnteButton from "@ente/shared/components/EnteButton";
 import FormPaper from "@ente/shared/components/Form/FormPaper";
 import InfoItem from "@ente/shared/components/Info/InfoItem";
 import { EnteMenuItem } from "@ente/shared/components/Menu/EnteMenuItem";
@@ -16,14 +15,7 @@ import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 import DeleteIcon from "@mui/icons-material/Delete";
 import EditIcon from "@mui/icons-material/Edit";
 import KeyIcon from "@mui/icons-material/Key";
-import {
-    Box,
-    Button,
-    Stack,
-    Typography,
-    styled,
-    useMediaQuery,
-} from "@mui/material";
+import { Box, Stack, Typography, styled, useMediaQuery } from "@mui/material";
 import { t } from "i18next";
 import React, { useCallback, useEffect, useState } from "react";
 import {
@@ -265,8 +257,34 @@ const ManagePasskeyDrawer: React.FC<ManagePasskeyDrawerProps> = ({
     passkey,
     onUpdateOrDeletePasskey,
 }) => {
+    const { setDialogBoxAttributesV2 } = useAppContext();
+
     const [showRenameDialog, setShowRenameDialog] = useState(false);
-    const [showDeleteDialog, setShowDeleteDialog] = useState(false);
+
+    const showDeleteConfirmationDialog = useCallback(() => {
+        const handleDelete = async (setLoading?: (value: boolean) => void) => {
+            setLoading && setLoading(true);
+            try {
+                await deletePasskey(ensure(token), ensure(passkey).id);
+                onUpdateOrDeletePasskey();
+            } catch (e) {
+                log.error("Failed to delete passkey", e);
+            } finally {
+                setLoading && setLoading(false);
+            }
+        };
+
+        setDialogBoxAttributesV2({
+            title: t("delete_passkey"),
+            content: t("delete_passkey_confirmation"),
+            proceed: {
+                text: t("DELETE"),
+                action: handleDelete,
+                variant: "critical",
+            },
+            close: { text: t("CANCEL") },
+        });
+    }, [token, onUpdateOrDeletePasskey, passkey, setDialogBoxAttributesV2]);
 
     return (
         <>
@@ -297,9 +315,7 @@ const ManagePasskeyDrawer: React.FC<ManagePasskeyDrawerProps> = ({
                             />
                             <MenuItemDivider />
                             <EnteMenuItem
-                                onClick={() => {
-                                    setShowDeleteDialog(true);
-                                }}
+                                onClick={showDeleteConfirmationDialog}
                                 startIcon={<DeleteIcon />}
                                 label={t("delete_passkey")}
                                 color="critical"
@@ -317,19 +333,6 @@ const ManagePasskeyDrawer: React.FC<ManagePasskeyDrawerProps> = ({
                     passkey={passkey}
                     onRenamePasskey={() => {
                         setShowRenameDialog(false);
-                        onUpdateOrDeletePasskey();
-                    }}
-                />
-            )}
-
-            {token && passkey && (
-                <DeletePasskeyDialog
-                    open={showDeleteDialog}
-                    onClose={() => setShowDeleteDialog(false)}
-                    token={token}
-                    passkey={passkey}
-                    onDeletePasskey={() => {
-                        setShowDeleteDialog(false);
                         onUpdateOrDeletePasskey();
                     }}
                 />
@@ -384,66 +387,6 @@ const RenamePasskeyDialog: React.FC<RenamePasskeyDialogProps> = ({
                 secondaryButtonAction={onClose}
                 submitButtonProps={{ sx: { mt: 1, mb: 2 } }}
             />
-        </DialogBoxV2>
-    );
-};
-
-interface DeletePasskeyDialogProps {
-    /** If `true`, then the dialog is shown. */
-    open: boolean;
-    /** Callback to invoke when the dialog wants to be closed. */
-    onClose: () => void;
-    /** Auth token for API requests. */
-    token: string;
-    /** The {@link Passkey} to delete. */
-    passkey: Passkey;
-    /** Callback to invoke when the passkey is deleted. */
-    onDeletePasskey: () => void;
-}
-
-const DeletePasskeyDialog: React.FC<DeletePasskeyDialogProps> = ({
-    open,
-    onClose,
-    token,
-    passkey,
-    onDeletePasskey,
-}) => {
-    const [isDeleting, setIsDeleting] = useState(false);
-    const fullScreen = useMediaQuery("(max-width: 428px)");
-
-    const handleConfirm = async () => {
-        setIsDeleting(true);
-        try {
-            await deletePasskey(token, passkey.id);
-            onDeletePasskey();
-        } catch (e) {
-            log.error("Failed to delete passkey", e);
-        } finally {
-            setIsDeleting(false);
-        }
-    };
-
-    return (
-        <DialogBoxV2
-            fullWidth
-            {...{ open, onClose, fullScreen }}
-            attributes={{ title: t("delete_passkey") }}
-        >
-            <Stack spacing={"8px"}>
-                <Typography>{t("delete_passkey_confirmation")}</Typography>
-                <EnteButton
-                    type="submit"
-                    size="large"
-                    color="critical"
-                    loading={isDeleting}
-                    onClick={handleConfirm}
-                >
-                    {t("DELETE")}
-                </EnteButton>
-                <Button size="large" color={"secondary"} onClick={onClose}>
-                    {t("CANCEL")}
-                </Button>
-            </Stack>
         </DialogBoxV2>
     );
 };
