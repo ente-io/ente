cdcc349157a0850dc021f032f83b912de219d730
laurenspriem <github_uni.fishers@aleeas.com>
2024-07-25 17:54:18 +0200

    [mob][photos] Abstract choice of onnx package

--- mobile/lib/services/machine_learning/ml_model.dart
+++ mobile/lib/services/machine_learning/ml_model.dart
@@ -16,10 +16,12 @@ abstract class MlModel {
 
   String get modelName;
 
+  static final bool usePlatformPlugin = Platform.isAndroid;
+
   bool get isInitialized =>
-      Platform.isAndroid ? isNativePluginInitialized : isFfiInitialized;
+      usePlatformPlugin ? isNativePluginInitialized : isFfiInitialized;
   int get sessionAddress =>
-      Platform.isAndroid ? _nativePluginSessionIndex : _ffiSessionAddress;
+      usePlatformPlugin ? _nativePluginSessionIndex : _ffiSessionAddress;
 
   // isInitialized is used to check if the model is loaded by the ffi based
   // plugin
@@ -36,7 +38,7 @@ abstract class MlModel {
   }
 
   void storeSessionAddress(int address) {
-    if (Platform.isAndroid) {
+    if (usePlatformPlugin) {
       _nativePluginSessionIndex = address;
       isNativePluginInitialized = true;
     } else {
@@ -53,7 +55,7 @@ abstract class MlModel {
     String modelName,
     String modelPath,
   ) async {
-    if (Platform.isAndroid) {
+    if (usePlatformPlugin) {
       return await _loadModelWithEntePlugin(modelName, modelPath);
     } else {
       return await _loadModelWithFFI(modelName, modelPath);
