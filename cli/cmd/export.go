395f0384a063dbb9693b70ba04c79d6f07994803
Neeraj Gupta <254676+ua741@users.noreply.github.com>
2024-08-01 19:13:03 +0530

    [cli] Add option to exlude shared or hidden folders

--- cli/cmd/export.go
+++ cli/cmd/export.go
@@ -1,7 +1,7 @@
 package cmd
 
 import (
-	"github.com/ente-io/cli/pkg/model/export"
+	"github.com/ente-io/cli/pkg/model"
 	"github.com/spf13/cobra"
 )
 
@@ -13,20 +13,16 @@ var exportCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		// Retrieve flag values
 		shared, _ := cmd.Flags().GetBool("shared")
-		sharedFiles, _ := cmd.Flags().GetBool("shared-files")
 		hidden, _ := cmd.Flags().GetBool("hidden")
 		albums, _ := cmd.Flags().GetStringSlice("albums")
 		emails, _ := cmd.Flags().GetStringSlice("emails")
-
 		// Create Filters struct with flag values
-		filters := export.Filters{
-			ExcludeShared:      !shared,
-			ExcludeSharedFiles: !sharedFiles,
-			ExcludeHidden:      !hidden,
-			Albums:             albums,
-			Emails:             emails,
+		filters := model.Filter{
+			ExcludeShared: !shared,
+			ExcludeHidden: !hidden,
+			Albums:        albums,
+			Emails:        emails,
 		}
-
 		// Call the Export function with the filters
 		ctrl.Export(filters)
 	},
@@ -36,9 +32,8 @@ func init() {
 	rootCmd.AddCommand(exportCmd)
 
 	// Add flags for Filters struct fields with default value true
-	exportCmd.Flags().Bool("shared", true, "Include shared albums in export")
-	exportCmd.Flags().Bool("shared-files", true, "Include shared files in export")
-	exportCmd.Flags().Bool("hidden", true, "Include hidden albums in export")
+	exportCmd.Flags().Bool("shared", true, "to exclude shared albums, pass --shared=false")
+	exportCmd.Flags().Bool("hidden", true, "to exclude hidden albums, pass --hidden=false")
 	exportCmd.Flags().StringSlice("albums", []string{}, "Comma-separated list of album names to export")
 	exportCmd.Flags().StringSlice("emails", []string{}, "Comma-separated list of emails to export files shared with")
 }
