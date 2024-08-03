17e57b7486236bbad0182281300d05937584d556
Manav Rathi <manav@mrmr.io>
2024-08-03 17:14:21 +0530

    Inline

--- web/apps/photos/src/utils/common/index.ts
+++ web/apps/photos/src/utils/common/index.ts
@@ -1,11 +1,3 @@
-export const preloadImage = (imgBasePath: string) => {
-    const srcSet = [];
-    for (let i = 1; i <= 3; i++) {
-        srcSet.push(`${imgBasePath}/${i}x.png ${i}x`);
-    }
-    new Image().srcset = srcSet.join(",");
-};
-
 export function batch<T>(arr: T[], batchSize: number): T[][] {
     const batches: T[][] = [];
     for (let i = 0; i < arr.length; i += batchSize) {
