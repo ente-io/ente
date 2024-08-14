5bd7170755a4de983d45eeff89e8d2db3c526a4c
Neeraj Gupta <254676+ua741@users.noreply.github.com>
2024-08-14 17:11:13 +0530

    [cli] Fix error log

--- cli/pkg/model/remote.go
+++ cli/pkg/model/remote.go
@@ -151,7 +151,7 @@ func (r *RemoteFile) GetCreationTime() time.Time {
 func (r *RemoteFile) GetModificationTime() time.Time {
 	value, ok := r.Metadata["modificationTime"]
 	if !ok {
-		panic("creationTime not found in metadata")
+		panic("modificationTime not found in metadata")
 	}
 	return time.UnixMicro(int64(value.(float64)))
 }
