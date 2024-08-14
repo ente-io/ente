9f96ef8d83df66dad64c53d8a04e65a5708291b0
Neeraj Gupta <254676+ua741@users.noreply.github.com>
2024-08-14 12:27:36 +0530

    [mob] Switch to person_v2 where data is gzipped

--- mobile/lib/models/api/entity/type.dart
+++ mobile/lib/models/api/entity/type.dart
@@ -3,6 +3,7 @@ import "package:flutter/foundation.dart";
 enum EntityType {
   location,
   person,
+  personV2,
   unknown,
 }
 
@@ -12,18 +13,29 @@ EntityType typeFromString(String type) {
       return EntityType.location;
     case "person":
       return EntityType.location;
+    case "person_v2":
+      return EntityType.personV2;
   }
   debugPrint("unexpected collection type $type");
   return EntityType.unknown;
 }
 
 extension EntityTypeExtn on EntityType {
+  bool isZipped() {
+    if (this == EntityType.location || this == EntityType.person) {
+      return false;
+    }
+    return true;
+  }
+
   String typeToString() {
     switch (this) {
       case EntityType.location:
         return "location";
       case EntityType.person:
         return "person";
+      case EntityType.personV2:
+        return "person_v2";
       case EntityType.unknown:
         return "unknown";
     }
