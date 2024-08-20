c7c22bee88428fe28e592fc4f54035109eaed14b
Neeraj Gupta <254676+ua741@users.noreply.github.com>
2024-08-20 15:35:17 +0530

    Update copy

--- mobile/lib/generated/intl/messages_en.dart
+++ mobile/lib/generated/intl/messages_en.dart
@@ -970,7 +970,7 @@ class MessageLookup extends MessageLookupByLibrary {
         "mlConsentConfirmation": MessageLookupByLibrary.simpleMessage(
             "I understand, and wish to enable machine learning"),
         "mlConsentDescription": MessageLookupByLibrary.simpleMessage(
-            "If you enable machine learning, Ente will extract information like face geometry from files. This will happen on your device, and any generated biometric information will be end-to-end encrypted."),
+            "If you enable machine learning, Ente will extract information like face geometry from files, including those shared with you.\n\nThis will happen on your device, and any generated biometric information will be end-to-end encrypted."),
         "mlConsentPrivacy": MessageLookupByLibrary.simpleMessage(
             "Please click here for more details about this feature in our privacy policy"),
         "mlConsentTitle":
