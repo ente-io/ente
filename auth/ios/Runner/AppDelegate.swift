77bd056f40850342979ecd8f557425e8ed260bf3
ashilkn <ashilkn99@gmail.com>
2024-08-21 14:53:35 +0530

    [mob][auth] generated code on running flutter clean and flutter pub get

--- auth/ios/Runner/AppDelegate.swift
+++ auth/ios/Runner/AppDelegate.swift
@@ -2,7 +2,7 @@ import Flutter
 import UIKit
 import app_links
 
-@UIApplicationMain
+@main
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
