eb3637deef5cc5788e38b361b6ceebb3b43132d4
Neeraj Gupta <254676+ua741@users.noreply.github.com>
2024-08-21 10:28:51 +0530

    [mob] Remove unused method

--- mobile/lib/services/user_remote_flag_service.dart
+++ mobile/lib/services/user_remote_flag_service.dart
@@ -55,14 +55,6 @@ class UserRemoteFlagService {
     return _prefs.getBool(key) ?? defaultValue;
   }
 
-  Future<bool> getBoolValue(String key) async {
-    if (_prefs.containsKey(key)) {
-      return _prefs.getBool(key)!;
-    }
-    return _getValue(key, "false")
-        .then((value) => value.toLowerCase() == "true");
-  }
-
   Future<bool> setBoolValue(String key, bool value) async {
     await _updateKeyValue(key, value.toString());
     return _prefs.setBool(key, value);
