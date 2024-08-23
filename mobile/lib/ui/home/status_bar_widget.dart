67ee52177526845b748c55ea1e7d486d646ebb04
ashilkn <ashilkn99@gmail.com>
2024-08-23 18:24:06 +0530

    Revert "[mob][photos] Create separate widget for home tab's app bar"
    This reverts commit 0e642cd254933e07c47a177ab71ed0547cfd5ca9.

--- mobile/lib/ui/home/status_bar_widget.dart
+++ mobile/lib/ui/home/status_bar_widget.dart
@@ -38,8 +38,6 @@ class _StatusBarWidgetState extends State<StatusBarWidget> {
 
   @override
   void initState() {
-    super.initState();
-
     _subscription = Bus.instance.on<SyncStatusUpdate>().listen((event) {
       _logger.info("Received event " + event.status.toString());
       if (event.status == SyncStatus.error) {
@@ -74,6 +72,7 @@ class _StatusBarWidgetState extends State<StatusBarWidget> {
         setState(() {});
       }
     });
+    super.initState();
   }
 
   @override
@@ -143,14 +142,13 @@ class _SyncStatusWidgetState extends State<SyncStatusWidget> {
 
   @override
   void initState() {
-    super.initState();
-
     _subscription = Bus.instance.on<SyncStatusUpdate>().listen((event) {
       setState(() {
         _event = event;
       });
     });
     _event = SyncService.instance.getLastSyncStatusEvent();
+    super.initState();
   }
 
   @override
