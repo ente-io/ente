36f9d1feb20011b736ff68ef2c43e0b29bac745a
Prateek Sunal <prtksunal@gmail.com>
2024-08-24 19:37:52 +0530

    fix: scaling of text in code, etc

--- auth/lib/ui/home/speed_dial_label_widget.dart
+++ auth/lib/ui/home/speed_dial_label_widget.dart
@@ -11,17 +11,22 @@ class SpeedDialLabelWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.all(4),
-      padding: const EdgeInsets.all(12),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(8),
-        color: Theme.of(context).colorScheme.fabBackgroundColor,
+    return MediaQuery(
+      data: const MediaQueryData(
+        textScaler: TextScaler.linear(1),
       ),
-      child: Text(
-        label,
-        style: TextStyle(
-          color: Theme.of(context).colorScheme.fabForegroundColor,
+      child: Container(
+        margin: const EdgeInsets.all(4),
+        padding: const EdgeInsets.all(12),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8),
+          color: Theme.of(context).colorScheme.fabBackgroundColor,
+        ),
+        child: Text(
+          label,
+          style: TextStyle(
+            color: Theme.of(context).colorScheme.fabForegroundColor,
+          ),
         ),
       ),
     );
