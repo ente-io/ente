e5939e3ca57d4456d1efdae9e54241fce810df63
Manav Rathi <manav@mrmr.io>
2024-09-09 16:42:47 +0530

    Move

--- web/apps/photos/src/constants/gallery.ts
+++ web/apps/photos/src/constants/gallery.ts
@@ -7,9 +7,5 @@ export const MIN_COLUMNS = 4;
 export const SPACE_BTW_DATES = 44;
 export const SPACE_BTW_DATES_TO_IMAGE_CONTAINER_WIDTH_RATIO = 0.244;
 
-export enum PLAN_PERIOD {
-    MONTH = "month",
-    YEAR = "year",
-}
 
 export const SYNC_INTERVAL_IN_MICROSECONDS = 1000 * 60 * 5; // 5 minutes
