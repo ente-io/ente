1d8f4b2ebc9098ffd1062e5388ba2b407277cd45
Manav Rathi <manav@mrmr.io>
2024-10-03 18:03:31 +0530

    Usability

--- web/apps/photos/src/components/Upload/CollectionMappingChoiceModal.tsx
+++ web/apps/photos/src/components/Upload/CollectionMappingChoiceModal.tsx
@@ -1,11 +1,13 @@
 import type { CollectionMapping } from "@/base/types/ipc";
+import { FocusVisibleButton } from "@/new/photos/components/FocusVisibleButton";
+import CloseIcon from "@mui/icons-material/Close";
 import FolderIcon from "@mui/icons-material/Folder";
 import FolderCopyIcon from "@mui/icons-material/FolderCopy";
 import {
-    Button,
     Dialog,
     DialogContent,
     DialogTitle,
+    IconButton,
     Stack,
     Typography,
 } from "@mui/material";
@@ -27,23 +29,39 @@ export const CollectionMappingChoiceModal: React.FC<
             fullWidth
             PaperProps={{
                 sx: {
-                    padding: "8px 12px",
+                    paddingBlockEnd: "8px",
+                    paddingInline: "12px",
                     maxWidth: "390px",
                 },
             }}
         >
-            <DialogTitle sx={{ "&&&": { paddingBlockEnd: 0 } }}>
+            <DialogTitle
+                sx={{
+                    display: "flex",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                    "&&&": { paddingBlockEnd: 0 },
+                }}
+            >
                 <Typography variant="large" fontWeight={"bold"}>
                     {t("MULTI_FOLDER_UPLOAD")}
                 </Typography>
+                <IconButton
+                    aria-label={t("close")}
+                    color="secondary"
+                    onClick={onClose}
+                    sx={{ marginInlineEnd: "-12px" }}
+                >
+                    <CloseIcon />
+                </IconButton>
             </DialogTitle>
-            <DialogContent sx={{ "&&&": { paddingBlockStart: "16px" } }}>
+            <DialogContent sx={{ "&&&": { paddingBlockStart: "8px" } }}>
                 <Stack sx={{ gap: "16px" }}>
-                    <Typography color="text.muted" mt={0}>
+                    <Typography color="text.muted">
                         {t("UPLOAD_STRATEGY_CHOICE")}
                     </Typography>
-                    <Stack sx={{ gap: 1 }}>
-                        <Button
+                    <Stack sx={{ mt: "4px", gap: "8px" }}>
+                        <FocusVisibleButton
                             size="medium"
                             color="accent"
                             startIcon={<FolderIcon />}
@@ -53,9 +71,9 @@ export const CollectionMappingChoiceModal: React.FC<
                             }}
                         >
                             {t("UPLOAD_STRATEGY_SINGLE_COLLECTION")}
-                        </Button>
+                        </FocusVisibleButton>
 
-                        <Button
+                        <FocusVisibleButton
                             size="medium"
                             color="accent"
                             startIcon={<FolderCopyIcon />}
@@ -65,7 +83,7 @@ export const CollectionMappingChoiceModal: React.FC<
                             }}
                         >
                             {t("UPLOAD_STRATEGY_COLLECTION_PER_FOLDER")}
-                        </Button>
+                        </FocusVisibleButton>
                     </Stack>
                 </Stack>
             </DialogContent>
