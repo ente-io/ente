38e9496b8cdf1c8d6caec28437b720efac7e2383
ashilkn <ashilkn99@gmail.com>
2024-10-03 07:09:37 +0530

    [mob][photos] Add comment

--- mobile/lib/models/search/hierarchical/hierarchical_search_filter.dart
+++ mobile/lib/models/search/hierarchical/hierarchical_search_filter.dart
@@ -5,6 +5,9 @@ int kMostRelevantFilter = 10000;
 int kLeastRelevantFilter = -1;
 
 abstract class HierarchicalSearchFilter {
+  //These matches should be from list of all files in db and not just all files in
+  //gallery if we plan to use this cache for faster filtering when adding/removing
+  //applied filters.
   final Set<int> matchedUploadedIDs = {};
 
   String name();
