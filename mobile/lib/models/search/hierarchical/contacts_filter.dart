d7eb0e9dc3c052b8c3cc0fd5bfefe757f9b55138
ashilkn <ashilkn99@gmail.com>
2024-10-05 16:14:55 +0530

    [mob][photos] Create ContactsFilter class

---
+++ mobile/lib/models/search/hierarchical/contacts_filter.dart
@@ -0,0 +1,47 @@
+import "package:flutter/material.dart";
+import "package:photos/models/api/collection/user.dart";
+import "package:photos/models/file/file.dart";
+import "package:photos/models/search/hierarchical/hierarchical_search_filter.dart";
+
+class ContactsFilter extends HierarchicalSearchFilter {
+  final User user;
+  final int occurrence;
+
+  ContactsFilter({
+    required this.user,
+    required this.occurrence,
+  });
+
+  @override
+  String name() {
+    return "Contacts";
+  }
+
+  @override
+  int relevance() {
+    return occurrence;
+  }
+
+  @override
+  bool isMatch(EnteFile file) {
+    return file.ownerID == user.id;
+  }
+
+  @override
+  Set<int> getMatchedUploadedIDs() {
+    return matchedUploadedIDs;
+  }
+
+  @override
+  bool isSameFilter(HierarchicalSearchFilter other) {
+    if (other is ContactsFilter) {
+      return other.user.id == user.id;
+    }
+    return false;
+  }
+
+  @override
+  IconData? icon() {
+    return Icons.person_outlined;
+  }
+}
