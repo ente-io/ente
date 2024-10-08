ca89e3160292b26a4e86a8b5b30943c4a39d58f7
Manav Rathi <manav@mrmr.io>
2024-10-08 20:06:05 +0530

    Doc

--- web/packages/shared/components/EnteLogo.tsx
+++ web/packages/shared/components/EnteLogo.tsx
@@ -3,13 +3,27 @@ import React from "react";
 
 interface EnteLogoProps {
     /**
-     * The height of the logo image.
+     * The height of the logo image, in pixels.
      *
-     * Default: 18
+     * Default: 18px
      */
     height?: number;
 }
 
+/**
+ * The Ente logo ("ente" in Montserrat).
+ *
+ * This is meant as a standard img element that can be used in places where we
+ * need to show the Ente branding. The img is backed by an an SVG.
+ *
+ * @see {@link EnteLogoSvg} for a variant but visually similar component that
+ * uses an inline svg instead.
+ *
+ * The img has a default height of 18px, but can be customized using the
+ * {@link height} prop.
+ *
+ * The img also has a 3px vertical margin on both sides.
+ */
 export const EnteLogo: React.FC<EnteLogoProps> = ({ height }) => {
     return (
         <LogoImage height={height ?? 18} alt="logo" src="/images/ente.svg" />
