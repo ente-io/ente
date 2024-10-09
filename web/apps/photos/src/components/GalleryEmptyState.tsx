97c28b4fb7c7f6bd4c01627f5ab3c8601e261743
Manav Rathi <manav@mrmr.io>
2024-10-09 09:40:40 +0530

    Rename

--- web/apps/photos/src/components/GalleryEmptyState.tsx
+++ web/apps/photos/src/components/GalleryEmptyState.tsx
@@ -29,12 +29,12 @@ export default function GalleryEmptyState({ openUploader }) {
                         }}
                     >
                         <Trans
-                            i18nKey="WELCOME_TO_ENTE_HEADING"
+                            i18nKey="welcome_to_ente_title"
                             components={{ a: <EnteLogoSVG /> }}
                         />
                     </Typography>
                     <Typography variant="h2">
-                        {t("WELCOME_TO_ENTE_SUBHEADING")}
+                        {t("welcome_to_ente_subtitle")}
                     </Typography>
                 </VerticallyCentered>
             </Stack>
