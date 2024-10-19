d05b11665ade690fc4f87c7baa9175393f1f7e34
Neeraj Gupta <254676+ua741@users.noreply.github.com>
2024-10-19 11:45:17 +0530

    [Fix] Enable interfaction handling for MLController in iOS

--- mobile/lib/app.dart
+++ mobile/lib/app.dart
@@ -124,21 +124,26 @@ class _EnteAppState extends State<EnteApp> with WidgetsBindingObserver {
         ),
       );
     } else {
-      return MaterialApp(
-        title: "ente",
-        themeMode: ThemeMode.system,
-        theme: lightThemeData,
-        darkTheme: darkThemeData,
-        home: const HomeWidget(),
-        debugShowCheckedModeBanner: false,
-        builder: EasyLoading.init(),
-        locale: locale,
-        supportedLocales: appSupportedLocales,
-        localeListResolutionCallback: localResolutionCallBack,
-        localizationsDelegates: const [
-          ...AppLocalizations.localizationsDelegates,
-          S.delegate,
-        ],
+      return Listener(
+        onPointerDown: (event) {
+          MachineLearningController.instance.onUserInteraction();
+        },
+        child: MaterialApp(
+          title: "ente",
+          themeMode: ThemeMode.system,
+          theme: lightThemeData,
+          darkTheme: darkThemeData,
+          home: const HomeWidget(),
+          debugShowCheckedModeBanner: false,
+          builder: EasyLoading.init(),
+          locale: locale,
+          supportedLocales: appSupportedLocales,
+          localeListResolutionCallback: localResolutionCallBack,
+          localizationsDelegates: const [
+            ...AppLocalizations.localizationsDelegates,
+            S.delegate,
+          ],
+        ),
       );
     }
   }
