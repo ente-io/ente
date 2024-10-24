d2f20140da4b46922254ee8b2ff607d47e8194ba
laurenspriem <github_uni.fishers@aleeas.com>
2024-10-24 19:02:13 +0530

    [mob][photos] format

--- mobile/lib/utils/ram_check_util.dart
+++ mobile/lib/utils/ram_check_util.dart
@@ -3,7 +3,8 @@ import "package:system_info_plus/system_info_plus.dart";
 /// The total amount of RAM in the device in MB
 int? deviceTotalRAM;
 
-bool get enoughRamForLocalIndexing => deviceTotalRAM == null || deviceTotalRAM! >= 5 * 1024;
+bool get enoughRamForLocalIndexing =>
+    deviceTotalRAM == null || deviceTotalRAM! >= 5 * 1024;
 
 /// Return the total amount of RAM in the device in MB
 Future<int?> checkDeviceTotalRAM() async {
