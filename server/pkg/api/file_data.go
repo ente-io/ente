70f79ebae492c5ec0fc16a6bf3cb96cc1c839f82
Neeraj Gupta <254676+ua741@users.noreply.github.com>
2024-10-28 15:09:32 +0530

    [server] Change method type

--- server/pkg/api/file_data.go
+++ server/pkg/api/file_data.go
@@ -48,9 +48,11 @@ func (h *FileHandler) GetFilesData(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, resp)
 }
 
+// FileDataStatusDiff API won't really return status/diff for deleted files. The clients will primarily use this data to identify for which all files we already have preview generated or it's ML inference is done.
+// This doesn't simulate perfect diff behaviour as we won't maintain a tombstone entries for the deleted API.
 func (h *FileHandler) FileDataStatusDiff(ctx *gin.Context) {
 	var req fileData.IndexDiffRequest
-	if err := ctx.ShouldBindQuery(&req); err != nil {
+	if err := ctx.ShouldBindJSON(&req); err != nil {
 		ctx.JSON(http.StatusBadRequest, ente.NewBadRequestWithMessage(err.Error()))
 		return
 	}
