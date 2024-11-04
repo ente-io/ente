31cdb63c7d99b20db3f4d6cc4a00370efadff2c6
Manav Rathi <manav@mrmr.io>
2024-11-04 14:19:48 +0530

    S2

--- web/packages/base/components/mui/SidebarDrawer.tsx
+++ web/packages/base/components/mui/SidebarDrawer.tsx
@@ -1,4 +1,5 @@
-import { Drawer, styled } from "@mui/material";
+import { Drawer, styled, type DrawerProps } from "@mui/material";
+import type { ModalVisibilityProps } from "../utils/modal";
 
 /**
  * A MUI {@link Drawer} with a standard set of styling that we use for our left
@@ -17,7 +18,37 @@ export const SidebarDrawer = styled(Drawer)(({ theme }) => ({
 }));
 
 /**
+ * Common props for a {@link NestedSidebarDrawer} component. In addition to the
+ * regular modal visibility controls for opening and closing itself, these also
+ * surface an option to close the entire drawer.
+ */
+export type NestedSidebarDrawerVisibilityProps = ModalVisibilityProps & {
+    /**
+     * Called when the user wants to close the entire stack of drawers.
+     *
+     * Note that this does not automatically imply onClose. Each step in the
+     * nesting will have to chain their own onCloses to construct a new
+     * `onRootClose` suitable for passing to its children.
+     */
+    onRootClose: () => void;
+};
+
+/**
  * A variant of {@link SidebarDrawer} for second level, nested drawers that are
  * shown atop an already visible {@link SidebarDrawer}.
  */
-// export const NestedDrawerVisibilityProps
+export const NestedSidebarDrawer: React.FC<
+    NestedSidebarDrawerVisibilityProps & DrawerProps
+> = (props) => (
+    // MUI doesn't (currently, AFAIK) have support for nested drawers, so we
+    // emulate that by showing a drawer atop another. To make it fit, we need to
+    // modify two knobs:
+    //
+    // 1. Disable the transition (otherwise our nested drawer visibly slides in
+    //    from the wrong direction).
+    //
+    // 2. Disable the backdrop (otherwise we'd end up with two of them - one
+    //    from the original drawer, and one from this nested one).
+    //
+    <SidebarDrawer transitionDuration={0} hideBackdrop {...props} />
+);
