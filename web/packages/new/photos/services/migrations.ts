d2b3de08c1475d19a331eb766c42ee2127ce20b4
Manav Rathi <manav@mrmr.io>
2024-11-05 13:20:53 +0530

    Placeholder

--- web/packages/new/photos/services/migrations.ts
+++ web/packages/new/photos/services/migrations.ts
@@ -100,3 +100,11 @@ const m3 = () =>
         removeKV("entityKey/location"),
         removeKV("latestUpdatedAt/location"),
     ]);
+
+// TODO: Not enabled yet.
+// // Added: Nov 2025 (v1.7.7-beta). Prunable.
+// const m4 = () => {
+//     // Delete legacy keys for storing individual settings
+//     localStorage.removeItem("mapEnabled");
+//     localStorage.removeItem("cfProxyDisabled");
+// };
