47f70250a240f55c00483729416988353e1e792e
Manav Rathi <manav@mrmr.io>
2024-11-06 14:25:20 +0530

    Unused

--- web/apps/photos/src/types/user/index.ts
+++ web/apps/photos/src/types/user/index.ts
@@ -27,12 +27,3 @@ export interface DeleteChallengeResponse {
     allowDelete: boolean;
     encryptedChallenge: string;
 }
-
-export interface GetRemoteStoreValueResponse {
-    value: string;
-}
-
-export interface UpdateRemoteStoreValueRequest {
-    key: string;
-    value: string;
-}
