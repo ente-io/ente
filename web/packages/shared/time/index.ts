d9256eaf95fe969e7a80da87e89debda0b6dcb28
Manav Rathi <manav@mrmr.io>
2024-11-09 18:06:32 +0530

    Inline

--- web/packages/shared/time/index.ts
+++ web/packages/shared/time/index.ts
@@ -6,42 +6,24 @@ export interface TimeDelta {
 }
 
 export function getUnixTimeInMicroSecondsWithDelta(delta: TimeDelta): number {
-    let currentDate = new Date();
+    let date = new Date();
     if (delta?.hours) {
-        currentDate = _addHours(currentDate, delta.hours);
+        date = _addHours(date, delta.hours);
     }
     if (delta?.days) {
-        currentDate = _addDays(currentDate, delta.days);
+        date.setDate(date.getDate() + delta.days);
     }
     if (delta?.months) {
-        currentDate = _addMonth(currentDate, delta.months);
+        date.setMonth(date.getMonth() + delta.months);
     }
     if (delta?.years) {
-        currentDate = _addYears(currentDate, delta.years);
+        date.setFullYear(date.getFullYear() + delta.years);
     }
-    return currentDate.getTime() * 1000;
-}
-
-function _addDays(date: Date, days: number): Date {
-    const result = new Date(date);
-    result.setDate(date.getDate() + days);
-    return result;
+    return date.getTime() * 1000;
 }
 
 function _addHours(date: Date, hours: number): Date {
     const result = new Date(date);
     result.setHours(date.getHours() + hours);
-    return result;
-}
-
-function _addMonth(date: Date, months: number) {
-    const result = new Date(date);
-    result.setMonth(date.getMonth() + months);
-    return result;
-}
-
-function _addYears(date: Date, years: number) {
-    const result = new Date(date);
-    result.setFullYear(date.getFullYear() + years);
     return result;
 }
