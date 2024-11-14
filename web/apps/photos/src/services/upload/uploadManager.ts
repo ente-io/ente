65a2b5aeebd06cb2b3dce095b5e5938fb431a84f
Manav Rathi <manav@mrmr.io>
2024-11-14 10:19:14 +0530

    LF

--- web/apps/photos/src/services/upload/uploadManager.ts
+++ web/apps/photos/src/services/upload/uploadManager.ts
@@ -258,8 +258,7 @@ class UIService {
         index = 0,
     ) {
         const cancel: { exec: Canceler } = { exec: () => {} };
-        const cancelTimedOutRequest = () =>
-            cancel.exec("Request timed out");
+        const cancelTimedOutRequest = () => cancel.exec("Request timed out");
 
         const cancelCancelledUploadRequest = () =>
             cancel.exec(CustomError.UPLOAD_CANCELLED);
