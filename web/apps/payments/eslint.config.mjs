56b72bd55b79d3a8e063d9a8d7eaa99441d4871b
Manav Rathi <manav@mrmr.io>
2024-11-19 18:05:45 +0530

    Chain

---
+++ web/apps/payments/eslint.config.mjs
@@ -0,0 +1,7 @@
+// module.exports = {
+//     extends: ["@/build-config/eslintrc-vite"],
+// };
+
+import config from "@/build-config/eslintrc-vite.mjs";
+
+export default config;
---
+++ web/packages/build-config/eslintrc-vite.mjs
@@ -0,0 +1,9 @@
+import config from "./eslintrc-react.mjs";
+
+export default config;
+
+// /* eslint-env node */
+// module.exports = {
+//     extends: ["./eslintrc-react.js", "plugin:react/jsx-runtime"],
+//     ignorePatterns: [".eslintrc.cjs", "vite.config.ts", "dist"],
+// };
