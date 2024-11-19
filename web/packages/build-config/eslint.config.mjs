143d346ed4b28151847001d32e121a5f3d8a2b14
Manav Rathi <manav@mrmr.io>
2024-11-19 17:49:37 +0530

    eslint migration wip checkpoint

---
+++ web/packages/build-config/eslint.config.mjs
@@ -0,0 +1,25 @@
+import js from "@eslint/js";
+
+
+export default [
+    js.configs.recommended,
+    {
+        ignores: ["eslintrc-*.js"]
+    }
+]
+// import path from "node:path";
+// import { fileURLToPath } from "node:url";
+// import js from "@eslint/js";
+// import { FlatCompat } from "@eslint/eslintrc";
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+// const compat = new FlatCompat({
+//     baseDirectory: __dirname,
+//     recommendedConfig: js.configs.recommended,
+//     allConfig: js.configs.all
+// });
+
+// export default [{
+//     ignores: ["**/.eslintrc.js"],
+// }, ...compat.extends("eslint:recommended")];
