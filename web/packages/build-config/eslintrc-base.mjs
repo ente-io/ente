da71a34f75a7fdf2c0fb5aaf6640631728f9414b
Manav Rathi <manav@mrmr.io>
2024-11-19 18:15:51 +0530

    TC

--- web/packages/build-config/eslintrc-base.mjs
+++ web/packages/build-config/eslintrc-base.mjs
@@ -5,7 +5,15 @@ import tseslint from "typescript-eslint";
 
 export default tseslint.config(
     eslint.configs.recommended,
-    tseslint.configs.recommended,
+    tseslint.configs.recommendedTypeChecked,
+    {
+        languageOptions: {
+            parserOptions: {
+                projectService: true,
+                tsconfigRootDir: import.meta.dirname,
+            },
+        },
+    },
 );
 // /* eslint-env node */
 // module.exports = {
