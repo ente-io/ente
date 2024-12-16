75ae27733421d18c1e54d0b603c8c44c1349b2c3
Aman Raj <amanrajmourya7@gmail.com>
2024-12-16 16:00:40 +0530

    [auth] minor fixes

--- auth/lib/ui/components/custom_icon_widget.dart
+++ auth/lib/ui/components/custom_icon_widget.dart
@@ -4,36 +4,27 @@ import 'package:ente_auth/utils/totp_util.dart';
 import 'package:flutter/material.dart';
 
 class CustomIconWidget extends StatelessWidget {
-  final bool isCustomIcon;
   final String iconData;
-  final void Function() onTap;
   CustomIconWidget({
     super.key,
-    required this.isCustomIcon,
     required this.iconData,
-    required this.onTap,
   });
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onTap,
-      child: Container(
-        decoration: BoxDecoration(
-          border: Border.all(
-            width: 1.5,
-            color: isCustomIcon
-                ? getEnteColorScheme(context).tagChipSelectedColor
-                : getEnteColorScheme(context).tagChipUnselectedColor,
-          ),
-          borderRadius: const BorderRadius.all(Radius.circular(12.0)),
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(
+          width: 1.5,
+          color: getEnteColorScheme(context).tagChipSelectedColor,
         ),
-        padding: const EdgeInsets.all(8),
-        child: IconUtils.instance.getIcon(
-          context,
-          safeDecode(iconData).trim(),
-          width: 50,
-        ),
+        borderRadius: const BorderRadius.all(Radius.circular(12.0)),
+      ),
+      padding: const EdgeInsets.all(8),
+      child: IconUtils.instance.getIcon(
+        context,
+        safeDecode(iconData).trim(),
+        width: 50,
       ),
     );
   }
