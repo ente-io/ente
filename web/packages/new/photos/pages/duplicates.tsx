11d32752d4cf755c2c294331ab1a2ab8e1517070
Manav Rathi <manav@mrmr.io>
2024-12-17 16:09:18 +0530

    back

--- web/packages/new/photos/pages/duplicates.tsx
+++ web/packages/new/photos/pages/duplicates.tsx
@@ -1,15 +1,16 @@
+import { pt } from "@/base/i18n";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 import SortIcon from "@mui/icons-material/Sort";
 import { Box, IconButton, Stack, Typography } from "@mui/material";
+import { useRouter } from "next/router";
 import React, { useEffect } from "react";
 import { useAppContext } from "../types/context";
 
 const Page: React.FC = () => {
     const { showNavBar } = useAppContext();
-
     useEffect(() => {
-        showNavBar(true);
+        showNavBar(false);
     }, []);
     return (
         <div>
@@ -22,17 +23,24 @@ const Page: React.FC = () => {
 export default Page;
 
 const Navbar: React.FC = () => {
+    const router = useRouter();
+
     return (
         <Stack
             direction="row"
-            sx={{ alignItems: "center", justifyContent: "space-between" }}
+            sx={(theme) => ({
+                alignItems: "center",
+                justifyContent: "space-between",
+                padding: "8px 4px",
+                borderBottom: `1px solid ${theme.palette.divider}`,
+            })}
         >
             <Box sx={{ minWidth: "100px" /* 2 icons + gap */ }}>
-                <IconButton>
+                <IconButton onClick={router.back}>
                     <ArrowBackIcon />
                 </IconButton>
             </Box>
-            <Typography variant="large">Remove duplicates</Typography>
+            <Typography variant="large">{pt("Remove duplicates")}</Typography>
             <Stack direction="row" sx={{ gap: "4px" }}>
                 <IconButton>
                     <SortIcon />
