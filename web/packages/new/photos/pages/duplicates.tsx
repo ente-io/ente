c1ac9d22ba7e52f43fa87b293b14c6d99018dc8c
Manav Rathi <manav@mrmr.io>
2024-12-17 13:30:37 +0530

    wip

--- web/packages/new/photos/pages/duplicates.tsx
+++ web/packages/new/photos/pages/duplicates.tsx
@@ -1,7 +1,31 @@
-import React from "react";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import { Box, IconButton, Typography } from "@mui/material";
+import React, { useEffect } from "react";
+import { useAppContext } from "../types/context";
 
 const Page: React.FC = () => {
-    return <div>Hello</div>;
+    const { showNavBar } = useAppContext();
+
+    useEffect(() => {
+        showNavBar(true);
+    }, []);
+    return (
+        <div>
+            <Navbar />
+            Hello
+        </div>
+    );
 };
 
 export default Page;
+
+const Navbar: React.FC = () => {
+    return (
+        <Box>
+            <IconButton>
+                <ArrowBackIcon />
+            </IconButton>
+            <Typography variant="h3">Duplicates</Typography>
+        </Box>
+    );
+};
