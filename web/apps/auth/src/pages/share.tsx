8cd2267f5d053c0020c8092b3c4e04cf7f7a2dfa
Manav Rathi <manav@mrmr.io>
2025-01-17 20:31:54 +0530

    Fix imports

--- web/apps/auth/src/pages/share.tsx
+++ web/apps/auth/src/pages/share.tsx
@@ -1,4 +1,4 @@
-import { EnteLogo } from "@/base/components/EnteLogo";
+import { EnteLogoSVG } from "@/base/components/EnteLogo";
 import { decryptMetadataJSON_New } from "@/base/crypto";
 import { Box, Button, Stack, Typography, useTheme } from "@mui/material";
 import React, { useEffect, useMemo, useState } from "react";
@@ -119,7 +119,7 @@ const Page: React.FC = () => {
                 padding: "20px",
             }}
         >
-            <EnteLogo />
+            <EnteLogoSVG />
 
             <Box sx={{ width: "min(100%, 300px)" }}>
                 {error && (
