48dc09ad384b22cfcaa172116ec61353c619359a
ashilkn <ashilkn99@gmail.com>
2025-01-22 16:14:48 +0530

    [mob][photos] Use User.displayName instead of User.name since User.name is now deprecated

--- mobile/lib/extensions/user_extension.dart
+++ mobile/lib/extensions/user_extension.dart
@@ -2,13 +2,7 @@ import "package:photos/models/api/collection/user.dart";
 import "package:photos/services/machine_learning/face_ml/person/person_service.dart";
 
 extension UserExtension on User {
-  String? displayName() {
-    final emailToName = PersonService.instance.emailToNameMapCache;
-    if (emailToName.containsKey(email)) {
-      return emailToName[email];
-    } else {
-      //Some initial users have name in name field.
-      return name;
-    }
-  }
+  //Some initial users have name in name field.
+  String? get displayName =>
+      PersonService.instance.emailToNameMapCache[email] ?? name;
 }
