27beac3288b3d85095272b98bb8ce7f872175e0d
mngshm <mangeshrex@tutanota.com>
2025-01-22 15:09:40 +0530

    Formatting

--- infra/staff/src/App.tsx
+++ infra/staff/src/App.tsx
@@ -1,4 +1,3 @@
-import { Link } from "@mui/material";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import CircularProgress from "@mui/material/CircularProgress";
