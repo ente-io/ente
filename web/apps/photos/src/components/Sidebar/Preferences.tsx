737977358afa103339a06df6686e07dfd513039d
Manav Rathi <manav@mrmr.io>
2025-02-04 18:35:48 +0530

    tr

--- web/apps/photos/src/components/Sidebar/Preferences.tsx
+++ web/apps/photos/src/components/Sidebar/Preferences.tsx
@@ -12,7 +12,6 @@ import {
 import { useModalVisibility } from "@/base/components/utils/modal";
 import {
     getLocaleInUse,
-    pt,
     setLocaleInUse,
     supportedLocales,
     type SupportedLocale,
@@ -186,17 +185,17 @@ const ThemeSelector = () => {
     // During SSR, mode is always undefined.
     if (!mode) return null;
 
-    // TODO(LM): Use translations, also remove unused t("CHOSE_THEME")
+    // TODO(LM): Enable for all
     return (
         <Stack sx={{ gap: 1 }}>
             <Typography variant="small" sx={{ px: 1, color: "text.muted" }}>
-                {pt("Theme")}
+                {t("theme")}
             </Typography>
             <DropdownInput
                 options={[
-                    { label: pt("System"), value: "system" },
-                    { label: pt("Light"), value: "light" },
-                    { label: pt("Dark"), value: "dark" },
+                    { label: t("system"), value: "system" },
+                    { label: t("light"), value: "light" },
+                    { label: t("dark"), value: "dark" },
                 ]}
                 selected={mode}
                 onSelect={setMode}
