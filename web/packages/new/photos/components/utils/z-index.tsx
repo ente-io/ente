719ac8539b170289b090804d282c3bd44afdd778
Manav Rathi <manav@mrmr.io>
2025-02-06 07:42:32 +0530

    Outline

--- web/packages/new/photos/components/utils/z-index.tsx
+++ web/packages/new/photos/components/utils/z-index.tsx
@@ -1,4 +1,26 @@
 /**
+ * @file [Note: Custom z-indices]
+ *
+ * The default MUI z-index values (as of 6.4) are
+ * https://mui.com/material-ui/customization/default-theme/
+ *
+ * zIndex: Object
+ * - mobileStepper: 1000
+ * - fab: 1050
+ * - speedDial: 1050
+ * - appBar: 1100
+ * - drawer: 1200
+ * - modal: 1300
+ * - snackbar: 1400
+ * - tooltip: 1500
+ *
+ * We don't customize any of those, but photoswipe, the library we use for the
+ * image gallery, sets its base zIndex to a high value, so we need to tweak the
+ * zIndices of components that need to appear atop it accordingly. This file
+ * tries to hold those customizations.
+ */
+
+/**
  * PhotoSwipe sets the zIndex of its "pswp" class to 1500. We need to go higher
  * than that for our drawers and dialogs to get them to show above it.
  */
