4881f0879036d93ac6f27582bf7726b00c0ab86e
Manav Rathi <manav@mrmr.io>
2025-02-07 09:49:48 +0530

    Try import

--- web/packages/new/photos/components/FileViewer5.tsx
+++ web/packages/new/photos/components/FileViewer5.tsx
@@ -1,6 +1,8 @@
 // TODO(PS): WIP gallery using upstream photoswipe
 //
-// Needs yarn workspace gallery add photoswipe@^5.4.4 (not committed yet).
+// Needs (not committed yet):
+// yarn workspace gallery add photoswipe@^5.4.4
+// mv node_modules/photoswipe packages/new/photos/components/ps5
 
 if (process.env.NEXT_PUBLIC_ENTE_WIP_PS5) {
     console.warn("Using WIP upstream photoswipe");
@@ -9,8 +11,12 @@ if (process.env.NEXT_PUBLIC_ENTE_WIP_PS5) {
 }
 
 import { Button } from "@mui/material";
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import PhotoSwipeLightBox from "./ps5/dist/photoswipe-lightbox.esm.js";
 
 const FileViewer: React.FC = () => {
+    console.log(PhotoSwipeLightBox);
     return (
         <div>
             Hello<Button>Test</Button>
