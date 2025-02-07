782688c1f74cd6891de905de8cd592e49925deea
Manav Rathi <manav@mrmr.io>
2025-02-07 08:01:13 +0530

    Scaffold

---
+++ web/packages/new/photos/pages/g2.tsx
@@ -0,0 +1,7 @@
+// TODO(PS): WIP gallery using upstream photoswipe
+//
+// Needs yarn workspace gallery add photoswipe@^5.4.4 (not committed yet).
+
+export const Page: React.FC = () => {
+    return <div>Hello</div>;
+};
