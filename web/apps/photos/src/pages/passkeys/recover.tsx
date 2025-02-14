342ac3258a70ff4894d751a3406cd695f1ebe94b
Manav Rathi <manav@mrmr.io>
2025-02-14 14:55:26 +0530

    Direct

--- web/apps/photos/src/pages/passkeys/recover.tsx
+++ web/apps/photos/src/pages/passkeys/recover.tsx
@@ -1,8 +1,5 @@
 import Page_ from "@/accounts/pages/two-factor/recover";
-import { useAppContext } from "@/new/photos/types/context";
 
-const Page = () => (
-    <Page_ appContext={useAppContext()} twoFactorType="passkey" />
-);
+const Page = () => <Page_ twoFactorType="passkey" />;
 
 export default Page;
