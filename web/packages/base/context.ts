cf25cc40e42964e0639148c84b8d91e3ddc3b47b
Manav Rathi <manav@mrmr.io>
2025-02-14 14:09:42 +0530

    Extra

--- web/packages/base/context.ts
+++ web/packages/base/context.ts
@@ -1,8 +1,11 @@
 import type { MiniDialogAttributes } from "@/base/components/MiniDialog";
+import { createContext, useContext } from "react";
 
 /**
  * The type of the context expected to be present in the React tree for all apps
  * that use the base package.
+ *
+ * It is usually provided by the _app.tsx for the corresponding app.
  */
 export interface BaseContextT {
     /**
@@ -18,3 +21,15 @@ export interface BaseContextT {
      */
     showMiniDialog: (attributes: MiniDialogAttributes) => void;
 }
+
+/**
+ * The React {@link Context} of type {@link BaseContextT} available to all React
+ * components that refer to the base package.
+ */
+export const BaseContext = createContext<BaseContextT | undefined>(undefined);
+
+/**
+ * Utility hook to get the required {@link BaseContextT} that is expected to be
+ * available to all React components that refer to the base package.
+ */
+export const useBaseContext = (): BaseContextT => useContext(BaseContext)!;
