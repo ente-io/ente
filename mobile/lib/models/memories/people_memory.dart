f0328f6563e1dee795f6731c680724b58c673892
laurenspriem <github_uni.fishers@aleeas.com>
2025-03-14 11:23:22 +0530

    [mob][photos] Add more people activities

--- mobile/lib/models/memories/people_memory.dart
+++ mobile/lib/models/memories/people_memory.dart
@@ -29,25 +29,50 @@ PeopleMemoryType peopleMemoryTypeFromString(String type) {
   }
 }
 
-enum PeopleActivity { party, hiking, feast, selfies, sports }
+enum PeopleActivity {
+  admiring,
+  embracing,
+  party,
+  hiking,
+  feast,
+  selfies,
+  posing,
+  background,
+  sports,
+  roadtrip
+}
 
 String activityQuery(PeopleActivity activity) {
   switch (activity) {
+    case PeopleActivity.admiring:
+      return "Photo of two people lovingly admiring or looking at each other";
+    case PeopleActivity.embracing:
+      return "Photo of people hugging or embracing each other lovingly";
     case PeopleActivity.party:
       return "Photo of people celebrating together";
     case PeopleActivity.hiking:
-      return "Photo of people hiking together in nature";
+      return "Photo of people hiking or walking together in nature";
     case PeopleActivity.feast:
       return "Photo of people having a big feast together";
     case PeopleActivity.selfies:
       return "Happy and nostalgic selfie with people";
+    case PeopleActivity.posing:
+      return "Photo of people posing together in a funny manner for the camera";
+    case PeopleActivity.background:
+      return "Photo of people with an absolutely stunning or interesting background";
     case PeopleActivity.sports:
       return "Photo of people joyfully playing sports together";
+    case PeopleActivity.roadtrip:
+      return "Photo of people on a road trip together";
   }
 }
 
 String activityTitle(PeopleActivity activity, String personName) {
   switch (activity) {
+    case PeopleActivity.admiring:
+      return "Admiring $personName";
+    case PeopleActivity.embracing:
+      return "Embracing $personName";
     case PeopleActivity.party:
       return "Party with $personName";
     case PeopleActivity.hiking:
@@ -56,8 +81,14 @@ String activityTitle(PeopleActivity activity, String personName) {
       return "Feasting with $personName";
     case PeopleActivity.selfies:
       return "Selfies with $personName";
+    case PeopleActivity.posing:
+      return "Posing with $personName";
+    case PeopleActivity.background:
+      return "You, $personName, and what a background!";
     case PeopleActivity.sports:
       return "Sports with $personName";
+    case PeopleActivity.roadtrip:
+      return "Road trip with $personName";
   }
 }
 
