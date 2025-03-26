715e305e09ff5ace8d3e73ee54575d394a61c196
Neeraj Gupta <254676+ua741@users.noreply.github.com>
2025-03-26 22:08:43 +0530

    Add local metadata models

---
+++ mobile/lib/models/local/local_metadata.dart
@@ -0,0 +1,33 @@
+import "package:photos/models/location/location.dart";
+
+class DroidMetadata {
+  int creationTime;
+  int modificationTime;
+  String hash;
+  int size;
+  Location? location;
+  bool? isPanorama;
+  int? mviIndex;
+  DroidMetadata({
+    required this.hash,
+    required this.size,
+    required this.creationTime,
+    required this.modificationTime,
+    this.mviIndex,
+    this.location,
+    this.isPanorama,
+  });
+}
+
+class IOSMetadata {
+  // https://developer.apple.com/documentation/photos/phcloudidentifier
+  // Bulk mapping from local to cloud identifiers & vice versa
+  String? cloudIdentifier;
+  // https://developer.apple.com/documentation/photos/phassetsourcetype
+  int? sourceType;
+  bool? hasAdjustments;
+  String? adjustmentFormatIdentifier;
+  bool? representsBurst;
+  String? burstIdentifier;
+  int? burstSelectionTypes;
+}
