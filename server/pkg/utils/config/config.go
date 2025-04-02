101e27514b6491851741dfa48c343890b01a423f
mngshm <mangeshrex@tutanota.com>
2025-04-02 16:02:52 +0530

    Comment

--- server/pkg/utils/config/config.go
+++ server/pkg/utils/config/config.go
@@ -40,6 +40,9 @@ func ConfigureViper(environment string) error {
 	viper.SetEnvPrefix("ENTE")
 	// Ask Viper to look for underscores (instead of dots) for nested configs.
 	viper.SetEnvKeyReplacer(strings.NewReplacer(`.`, `_`))
+
+	// Also replace "-" with underscores since "-" cannot be used in
+	// environment variable names.
 	viper.SetEnvKeyReplacer(strings.NewReplacer(`-`, `_`))
 
 	viper.SetConfigFile("configurations/" + environment + ".yaml")
