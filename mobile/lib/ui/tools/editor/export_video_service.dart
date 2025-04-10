79a73ffd0977e3332c85b04d9369cdc6697d5f35
Prateek Sunal <prtksunal@gmail.com>
2025-04-11 00:07:17 +0530

    fix(ios): switch to ffmpeg_kit_custom

--- mobile/lib/ui/tools/editor/export_video_service.dart
+++ mobile/lib/ui/tools/editor/export_video_service.dart
@@ -1,11 +1,11 @@
 import 'dart:developer';
 import 'dart:io';
 
-import 'package:ffmpeg_kit_flutter_full_gpl/ffmpeg_kit.dart';
-import 'package:ffmpeg_kit_flutter_full_gpl/ffmpeg_kit_config.dart';
-import 'package:ffmpeg_kit_flutter_full_gpl/ffmpeg_session.dart';
-import 'package:ffmpeg_kit_flutter_full_gpl/return_code.dart';
-import 'package:ffmpeg_kit_flutter_full_gpl/statistics.dart';
+import 'package:ffmpeg_kit_flutter/ffmpeg_kit.dart';
+import 'package:ffmpeg_kit_flutter/ffmpeg_kit_config.dart';
+import 'package:ffmpeg_kit_flutter/ffmpeg_session.dart';
+import 'package:ffmpeg_kit_flutter/return_code.dart';
+import 'package:ffmpeg_kit_flutter/statistics.dart';
 import 'package:video_editor/video_editor.dart';
 
 class ExportService {
