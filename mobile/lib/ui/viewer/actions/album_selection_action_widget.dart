13f78ecc19f0ab12050a7a7a935158686416834b
Aman Raj Singh Mourya <amanrajmourya7@gmail.com>
2025-04-19 21:43:01 +0530

    [mob][photos] feat: enhance AddParticipantPage to support multiple collections and update album selection actions

--- mobile/lib/ui/viewer/actions/album_selection_action_widget.dart
+++ mobile/lib/ui/viewer/actions/album_selection_action_widget.dart
@@ -1,7 +1,15 @@
 import "package:flutter/material.dart";
+import "package:logging/logging.dart";
+import "package:photos/db/files_db.dart";
 import "package:photos/generated/l10n.dart";
+import "package:photos/models/collection/collection.dart";
+import "package:photos/models/metadata/common_keys.dart";
 import "package:photos/models/selected_albums.dart";
+import "package:photos/services/collections_service.dart";
+import "package:photos/ui/actions/collection/collection_sharing_actions.dart";
 import "package:photos/ui/components/bottom_action_bar/selection_action_button_widget.dart";
+import "package:photos/utils/dialog_util.dart";
+import "package:photos/utils/magic_util.dart";
 
 class AlbumSelectionActionWidget extends StatefulWidget {
   final SelectedAlbums selectedAlbums;
@@ -17,6 +25,15 @@ class AlbumSelectionActionWidget extends StatefulWidget {
 
 class _AlbumSelectionActionWidgetState
     extends State<AlbumSelectionActionWidget> {
+  final _logger = Logger("AlbumSelectionActionWidgetState");
+  late CollectionActions collectionActions;
+
+  @override
+  initState() {
+    collectionActions = CollectionActions(CollectionsService.instance);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     if (widget.selectedAlbums.albums.isEmpty) {
@@ -27,28 +44,28 @@ class _AlbumSelectionActionWidgetState
       SelectionActionButton(
         labelText: S.of(context).share,
         icon: Icons.adaptive.share,
-        onTap: () {},
+        onTap: _shareCollection,
       ),
     );
     items.add(
       SelectionActionButton(
         labelText: "Pin",
         icon: Icons.push_pin_rounded,
-        onTap: () {},
+        onTap: _onPinClick,
       ),
     );
     items.add(
       SelectionActionButton(
         labelText: S.of(context).delete,
         icon: Icons.delete_outline,
-        onTap: () {},
+        onTap: _trashCollection,
       ),
     );
     items.add(
       SelectionActionButton(
         labelText: S.of(context).hide,
         icon: Icons.visibility_off_outlined,
-        onTap: () {},
+        onTap: _onHideClick,
       ),
     );
     final scrollController = ScrollController();
@@ -81,5 +98,74 @@ class _AlbumSelectionActionWidgetState
         ),
       ),
     );
+  }
+
+  Future<void> _shareCollection() async {
+    await collectionActions.shareMultipleCollectionSheet(
+      context,
+      widget.selectedAlbums.albums.toList(),
+    );
+    widget.selectedAlbums.clearAll();
+  }
+
+  Future<void> _trashCollection() async {
+    int count = 0;
+    final List<Collection> nonEmptyCollection = [];
+
+    for (final collection in widget.selectedAlbums.albums) {
+      count = await FilesDB.instance.collectionFileCount(collection.id);
+      final bool isEmptyCollection = count == 0;
+      if (isEmptyCollection) {
+        try {
+          await CollectionsService.instance.trashEmptyCollection(collection);
+        } catch (e, s) {
+          _logger.warning("failed to trash collection", e, s);
+          await showGenericErrorDialog(context: context, error: e);
+        }
+      } else {
+        nonEmptyCollection.add(collection);
+      }
+    }
+
+    if (nonEmptyCollection.isNotEmpty) {
+      final bool result = await collectionActions.deleteMultipleCollectionSheet(
+        context,
+        nonEmptyCollection,
+      );
+      if (result == true) {
+        Navigator.of(context).pop();
+      } else {
+        debugPrint("No pop");
+      }
+    }
+    widget.selectedAlbums.clearAll();
+  }
+
+  Future<void> _onPinClick() async {
+    for (final collection in widget.selectedAlbums.albums) {
+      if (collection.isPinned) continue;
+      await updateOrder(
+        context,
+        collection,
+        collection.isPinned ? 1 : 1,
+      );
+    }
+    widget.selectedAlbums.clearAll();
+  }
+
+  Future<void> _onHideClick() async {
+    for (final collection in widget.selectedAlbums.albums) {
+      final isHidden = collection.isHidden();
+      final int prevVisiblity = isHidden ? hiddenVisibility : visibleVisibility;
+      final int newVisiblity = isHidden ? visibleVisibility : hiddenVisibility;
+
+      await changeCollectionVisibility(
+        context,
+        collection: collection,
+        newVisibility: newVisiblity,
+        prevVisibility: prevVisiblity,
+      );
+    }
+    widget.selectedAlbums.clearAll();
   }
 }
