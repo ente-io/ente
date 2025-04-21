b6a1a77bf728081cd95bc05eb11402dcd23d0101
ashilkn <ashilkn99@gmail.com>
2025-04-21 14:09:43 +0530

    Revert "[mob][photos] Fix: audio not playing on iOS when in silent mode"
    This reverts commit dc6fde9f77c5afcbf24982d6d1b85fc013c8c836.

--- mobile/ios/Runner/AppDelegate.swift
+++ mobile/ios/Runner/AppDelegate.swift
@@ -3,7 +3,7 @@ import Flutter
 import UIKit
 import app_links
 
-@main
+@UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
@@ -13,20 +13,6 @@ import app_links
     if #available(iOS 10.0, *) {
       UNUserNotificationCenter.current().delegate = self as UNUserNotificationCenterDelegate
     }
-
-    let controller: FlutterViewController = window?.rootViewController as! FlutterViewController
-    let audioSessionChannel = FlutterMethodChannel(
-      name: "io.ente.frame/audio_session",
-      binaryMessenger: controller.binaryMessenger)
-
-    audioSessionChannel.setMethodCallHandler({
-      (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
-      if call.method == "setAudioSessionCategory" {
-        self.setAudioSessionCategory(result: result)
-      } else {
-        result(FlutterMethodNotImplemented)
-      }
-    })
 
     GeneratedPluginRegistrant.register(with: self)
 
@@ -38,21 +24,6 @@ import app_links
     }
 
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
-
-  private func setAudioSessionCategory(result: @escaping FlutterResult) {
-    do {
-      try AVAudioSession.sharedInstance().setCategory(
-        .playback, mode: .default, options: [.mixWithOthers, .defaultToSpeaker])
-      try AVAudioSession.sharedInstance().setActive(true)
-      result(nil)
-    } catch {
-      result(
-        FlutterError(
-          code: "AUDIO_SESSION_ERROR",
-          message: "Failed to set audio session category",
-          details: error.localizedDescription))
-    }
   }
 
   override func applicationDidBecomeActive(_ application: UIApplication) {
