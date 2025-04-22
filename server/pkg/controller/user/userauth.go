e6deea15338830d65b849c17353a28e22bdb0af4
mngshm <mangeshrex@tutanota.com>
2025-04-22 19:10:42 +0530

    separate ott generation purpose checks

--- server/pkg/controller/user/userauth.go
+++ server/pkg/controller/user/userauth.go
@@ -88,21 +88,27 @@ func (c *UserController) SendEmailOTT(context *gin.Context, email string, purpos
 			return err
 		}
 	}
-	if purpose == ente.SignUpOTTPurpose || purpose == ente.LoginOTTPurpose {
-		isComplete, err := c.isSignUpComplete(email)
-		if err != nil {
-			return stacktrace.Propagate(err, "")
-		}
+
+	isComplete, err := c.isSignUpComplete(email)
+	if err != nil {
+		return stacktrace.Propagate(err, "")
+	}
+	// Purpose of OTT is Sign up
+	if purpose == ente.SignUpOTTPurpose {
 		if isComplete && purpose == ente.SignUpOTTPurpose {
 			return stacktrace.Propagate(ente.ErrUserAlreadyRegistered, "user has already completed sign up process")
 		}
 		if !isComplete && purpose == ente.SignUpOTTPurpose && viper.GetBool("internal.disable-registration") {
 			return stacktrace.Propagate(ente.ErrPermissionDenied, "registration is disabled")
 		}
+	}
+	// Purpose of OTT is Login
+	if purpose == ente.LoginOTTPurpose {
 		if !isComplete && purpose == ente.LoginOTTPurpose {
 			return stacktrace.Propagate(ente.ErrUserNotRegistered, "user has not completed sign up process")
 		}
 	}
+
 	ott, err := random.GenerateSixDigitOtp()
 	if err != nil {
 		return stacktrace.Propagate(err, "")
