3efd36ab7b8172a95c1d88d376d4952c7dda676f
Prateek Sunal <prtksunal@gmail.com>
2025-04-29 00:55:40 +0530

    fix: statitify things and log error

--- mobile/lib/services/isolated_ffmpeg_service.dart
+++ mobile/lib/services/isolated_ffmpeg_service.dart
@@ -1,19 +1,28 @@
 import "package:computer/computer.dart";
 import "package:ffmpeg_kit_flutter/ffmpeg_kit.dart";
 import "package:ffmpeg_kit_flutter/ffmpeg_session.dart";
+import "package:logging/logging.dart";
 
 class IsolatedFfmpegService {
-  static final Computer _computer = Computer.create();
+  static Computer? _computer;
+  static final _logger = Logger("IsolatedFfmpegService");
 
-  static void init() {
-    _computer.turnOn(workersCount: 1);
+  static Future<void> init() async {
+    _computer = Computer.create();
+    await _computer?.turnOn(workersCount: 1);
   }
 
   static Future<FFmpegSession> runFfmpeg(String command) async {
-    return _computer.compute<Map, FFmpegSession>(
+    if (_computer == null) {
+      await init();
+    }
+    return await _computer!.compute<Map, FFmpegSession>(
       _ffmpegRun,
       param: {'command': command},
-    );
+    ).onError((error, st) {
+      _logger.warning("Error: $error");
+      throw error!;
+    });
   }
 
   static Future<FFmpegSession> _ffmpegRun(Map args) async {
