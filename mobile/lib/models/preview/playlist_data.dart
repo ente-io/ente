d6a8373e5df691525a561762107b362e1e9e30f8
Neeraj Gupta <254676+ua741@users.noreply.github.com>
2025-05-16 11:33:14 +0530

    Clean up

--- mobile/lib/models/preview/playlist_data.dart
+++ mobile/lib/models/preview/playlist_data.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
-import 'dart:convert';
 import "dart:io";
 
 class PlaylistData {
@@ -16,52 +14,4 @@ class PlaylistData {
     this.size,
     this.durationInSeconds,
   });
-
-  PlaylistData copyWith({
-    File? preview,
-    int? width,
-    int? height,
-    int? size,
-    int? durationInSeconds,
-  }) {
-    return PlaylistData(
-      preview: preview ?? this.preview,
-      width: width ?? this.width,
-      height: height ?? this.height,
-      size: size ?? this.size,
-      durationInSeconds: durationInSeconds ?? this.durationInSeconds,
-    );
-  }
-
-  Map<String, dynamic> toMap() {
-    return <String, dynamic>{
-      'preview': preview.readAsStringSync(),
-      'width': width,
-      'height': height,
-      'size': size,
-      'durationInSeconds': durationInSeconds,
-    };
-  }
-
-  String toJson() => json.encode(toMap());
-
-  @override
-  String toString() {
-    return 'PlaylistData(preview: $preview, width: $width, height: $height, size: $size, durationInSeconds: $durationInSeconds)';
-  }
-
-  @override
-  bool operator ==(covariant PlaylistData other) {
-    if (identical(this, other)) return true;
-
-    return other.preview == preview &&
-        other.width == width &&
-        other.height == height &&
-        other.size == size;
-  }
-
-  @override
-  int get hashCode {
-    return preview.hashCode ^ width.hashCode ^ height.hashCode ^ size.hashCode;
-  }
 }
