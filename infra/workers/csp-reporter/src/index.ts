8824df29d49a53fec5203fca006abe46c7f3e207
Manav Rathi <manav@mnvr.in>
2025-05-19 11:51:10 +0530

    They're POST

--- infra/workers/csp-reporter/src/index.ts
+++ infra/workers/csp-reporter/src/index.ts
@@ -7,8 +7,8 @@
 export default {
     async fetch(request: Request) {
         switch (request.method) {
-            case "GET":
-                return handleGET(request);
+            case "POST":
+                return handlePOST(request);
             default:
                 console.log(`Unsupported HTTP method ${request.method}`);
                 return new Response(null, { status: 405 });
@@ -16,7 +16,7 @@ export default {
     },
 } satisfies ExportedHandler;
 
-const handleGET = async (request: Request) => {
-    console.log("csp-report", await request.text());
+const handlePOST = async (request: Request) => {
+    console.log("[csp-report]", await request.text());
     return new Response(null, { status: 200 });
 };
