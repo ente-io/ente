53a22a8d589e9a35a48060bfe53c5288eb439721
ashilkn <ashilkn99@gmail.com>
2025-05-26 19:32:21 +0530

    chore

--- mobile/lib/ui/settings/widgets/people_widget_settings.dart
+++ mobile/lib/ui/settings/widgets/people_widget_settings.dart
@@ -3,7 +3,6 @@ import "package:photos/generated/l10n.dart";
 import "package:photos/l10n/l10n.dart";
 import "package:photos/models/selected_people.dart";
 import "package:photos/services/people_home_widget_service.dart";
-import 'package:photos/theme/ente_theme.dart';
 import "package:photos/ui/components/buttons/button_widget.dart";
 import 'package:photos/ui/components/buttons/icon_button_widget.dart';
 import "package:photos/ui/components/models/button_type.dart";
@@ -60,7 +59,6 @@ class _PeopleWidgetSettingsState extends State<PeopleWidgetSettings> {
 
   @override
   Widget build(BuildContext context) {
-    final textTheme = getEnteTextTheme(context);
     return Scaffold(
       bottomNavigationBar: hasInstalledAny
           ? Padding(
