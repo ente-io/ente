0d95f8c5a27a0cb1e98f5378021989e484601fb5
laurenspriem <github_uni.fishers@aleeas.com>
2025-05-30 17:09:10 +0530

    Remove redundant

--- mobile/lib/ui/viewer/people/person_row_item.dart
+++ mobile/lib/ui/viewer/people/person_row_item.dart
@@ -3,40 +3,6 @@ import "package:photos/models/file/file.dart";
 import "package:photos/models/ml/face/person.dart";
 import "package:photos/ui/viewer/people/person_face_widget.dart";
 
-class PersonRowItem extends StatelessWidget {
-  final PersonEntity person;
-  final EnteFile personFile;
-  final VoidCallback onTap;
-
-  const PersonRowItem({
-    super.key,
-    required this.person,
-    required this.personFile,
-    required this.onTap,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      dense: false,
-      minLeadingWidth: 0,
-      contentPadding: const EdgeInsets.symmetric(horizontal: 0),
-      leading: SizedBox(
-        width: 56,
-        height: 56,
-        child: ClipRRect(
-          borderRadius: const BorderRadius.all(
-            Radius.elliptical(16, 12),
-          ),
-          child: PersonFaceWidget(personId: person.remoteID),
-        ),
-      ),
-      title: Text(person.data.name),
-      onTap: onTap,
-    );
-  }
-}
-
 class PersonGridItem extends StatelessWidget {
   final PersonEntity person;
   final EnteFile personFile;
