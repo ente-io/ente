54b5100e89d66762b95d5efeb1bae35894b29215
Manav Rathi <manav@mnvr.in>
2025-06-03 12:55:36 +0530

    Use

--- web/packages/accounts/pages/recover.tsx
+++ web/packages/accounts/pages/recover.tsx
@@ -7,7 +7,6 @@ import { appHomeRoute, stashRedirect } from "ente-accounts/services/redirect";
 import { sendOTT } from "ente-accounts/services/user";
 import { LinkButton } from "ente-base/components/LinkButton";
 import { useBaseContext } from "ente-base/context";
-import { sharedCryptoWorker } from "ente-base/crypto";
 import log from "ente-base/log";
 import SingleInputForm, {
     type SingleInputFormProps,
@@ -22,11 +21,7 @@ import type { KeyAttributes, User } from "ente-shared/user/types";
 import { t } from "i18next";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
-
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-const bip39 = require("bip39");
-// mobile client library only supports english.
-bip39.setDefaultWordlist("english");
+import { decryptUsingRecoveryKeyMnemonic } from "../utils/recovery-key";
 
 const Page: React.FC = () => {
     const { showMiniDialog } = useBaseContext();
@@ -65,25 +60,11 @@ const Page: React.FC = () => {
         setFieldError,
     ) => {
         try {
-            recoveryKey = recoveryKey
-                .trim()
-                .split(" ")
-                .map((part) => part.trim())
-                .filter((part) => !!part)
-                .join(" ");
-            // check if user is entering mnemonic recovery key
-            if (recoveryKey.indexOf(" ") > 0) {
-                if (recoveryKey.split(" ").length !== 24) {
-                    throw new Error("recovery code should have 24 words");
-                }
-                recoveryKey = bip39.mnemonicToEntropy(recoveryKey);
-            }
-            const cryptoWorker = await sharedCryptoWorker();
             const keyAttr = keyAttributes!;
-            const masterKey = await cryptoWorker.decryptB64(
+            const masterKey = await decryptUsingRecoveryKeyMnemonic(
                 keyAttr.masterKeyEncryptedWithRecoveryKey!,
                 keyAttr.masterKeyDecryptionNonce!,
-                await cryptoWorker.fromHex(recoveryKey),
+                recoveryKey,
             );
             await saveKeyInSessionStore("encryptionKey", masterKey);
             await decryptAndStoreToken(keyAttr, masterKey);
--- web/packages/accounts/utils/recovery-key.ts
+++ web/packages/accounts/utils/recovery-key.ts
@@ -9,21 +9,21 @@ bip39.setDefaultWordlist("english");
  * Decrypt the provided data that was encrypted with the user's recovery key
  * using the recovery key derived from the provided mnemonic string.
  *
- * @param recoveryKey The BIP-39 mnemonic (24 word) string representing the
- * recovery key. For legacy compatibility, the function also works if provided
- * the hex representation of the recovery key.
- *
  * @param encryptedData The data to decrypt. The data should've been encrypted
  * using the same recovery key otherwise the decryption would fail.
  *
  * @param decryptionNonce The nonce that was using encryption.
  *
+ * @param recoveryKey The BIP-39 mnemonic (24 word) string representing the
+ * recovery key. For legacy compatibility, the function also works if provided
+ * the hex representation of the recovery key.
+ *
  * @returns A base64 string representing the decrypted data.
  */
 export const decryptUsingRecoveryKeyMnemonic = async (
-    recoveryKey: string,
     encryptedData: any,
     decryptionNonce: any,
+    recoveryKey: string,
 ) => {
     recoveryKey = recoveryKey
         .trim()
