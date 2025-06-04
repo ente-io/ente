3dbc336687825080bad67d5e497904bbd5171745
Manav Rathi <manav@mnvr.in>
2025-06-04 18:36:49 +0530

    Doc

--- web/packages/shared/user/types.ts
+++ web/packages/shared/user/types.ts
@@ -1,9 +1,20 @@
 /**
- * A structure containing the key related attributes for a user.
+ * The user's various encrypted keys and their related attributes.
+ *
+ * - Attributes to derive the KEK, the (master) key encryption key.
+ * - Encrypted master key (with KEK)
+ * - Encrypted master key (with recovery key)
+ * - Encrypted recovery key (with master key).
+ * - Public key and encrypted private key (with master key).
+ *
+ * The various "key" attributes are base64 encoded representations of the
+ * underlying binary data.
  */
 export interface KeyAttributes {
     /**
      * The user's master key encrypted with the key encryption key.
+     *
+     * Base 64 encoded.
      *
      * [Note: Key encryption key]
      *
@@ -25,11 +36,15 @@ export interface KeyAttributes {
     /**
      * The nonce used during the encryption of the master key.
      *
+     * Base 64 encoded.
+     *
      * @see {@link encryptedKey}.
      */
     keyDecryptionNonce: string;
     /**
      * The salt used during the derivation of the kek.
+     *
+     * Base 64 encoded.
      *
      * See: [Note: Key encryption key].
      */
@@ -51,11 +66,28 @@ export interface KeyAttributes {
      * See {@link opsLimit} for more details.
      */
     memLimit: number;
+    /**
+     * The user's public key (part of their public-key keypair, the other half
+     * being the {@link encryptedSecretKey}).
+     *
+     * Base 64 encoded.
+     */
     publicKey: string;
+    /**
+     * The user's private key (part of their public-key keypair, the other half
+     * being the {@link publicKey}) encrypted with their master key.
+     *
+     * Base 64 encoded.
+     */
     encryptedSecretKey: string;
+    /**
+     * The nonce used during the encryption of {@link encryptedSecretKey}.
+     */
     secretKeyDecryptionNonce: string;
     /**
      * The user's master key after being encrypted with their recovery key.
+     *
+     * Base 64 encoded.
      *
      * This allows the user to recover their master key if they forget their
      * passphrase but still have their recovery key.
@@ -66,10 +98,14 @@ export interface KeyAttributes {
     /**
      * The nonce used during the encryption of
      * {@link masterKeyEncryptedWithRecoveryKey}.
+     *
+     * Base 64 encoded.
      */
     masterKeyDecryptionNonce?: string;
     /**
      * The user's recovery key after being encrypted with their master key.
+     *
+     * Base 64 encoded.
      *
      * Note: This value doesn't change after being initially created.
      */
@@ -77,6 +113,8 @@ export interface KeyAttributes {
     /**
      * The nonce used during the encryption of
      * {@link recoveryKeyEncryptedWithMasterKey}.
+     *
+     * Base 64 encoded.
      */
     recoveryKeyDecryptionNonce?: string;
 }
@@ -89,4 +127,3 @@ export interface User {
     isTwoFactorEnabled: boolean;
     twoFactorSessionID: string;
 }
-
