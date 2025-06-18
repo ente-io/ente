773d98e20c8d887a1748011735531ebdec195363
Manav Rathi <manav@mnvr.in>
2025-06-18 20:27:41 +0530

    Doc

--- web/packages/media/file.ts
+++ web/packages/media/file.ts
@@ -563,7 +563,22 @@ export interface TrashItem extends Omit<EncryptedTrashItem, "file"> {
 
 export interface EncryptedTrashItem {
     file: EncryptedEnteFile;
+    /**
+     * `true` if the file no longer in trash because it was permanently deleted.
+     *
+     * This field is relevant when we obtain a trash item as part of the trash
+     * diff. It indicates that the file which was previously in trash is no
+     * longer in the trash because it was permanently deleted.
+     */
     isDeleted: boolean;
+    /**
+     * `true` if the file no longer in trash because it was restored to some
+     * collection.
+     *
+     * This field is relevant when we obtain a trash item as part of the trash
+     * diff. It indicates that the file which was previously in trash is no
+     * longer in the trash because it was restored to a collection.
+     */
     isRestored: boolean;
     deleteBy: number;
     createdAt: number;
