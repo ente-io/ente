92f2a613d529ef493f4aef925dc17d5af346c059
Prateek Sunal <prtksunal@gmail.com>
2025-07-03 01:52:31 +0530

    fix: only share image link when using Send Link

--- mobile/apps/photos/lib/utils/share_util.dart
+++ mobile/apps/photos/lib/utils/share_util.dart
@@ -294,15 +294,9 @@ Future<void> shareAlbumLinkWithPlaceholder(
     await shareText(url);
     return;
   } else {
-    final placeholderBytes = await _createAlbumPlaceholder(
-      filesInCollection,
-      screenshotController,
-      context,
-    );
     await dialog.hide();
 
-    await shareImageAndUrl(
-      placeholderBytes,
+    await shareText(
       url,
       context: context,
       key: key,
