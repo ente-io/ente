8a9f73ada5ea2450344a2cad1feaa39797aafac7
vishnukvmd <vishnu@ente.io>
2025-07-19 21:07:16 +0530

    Refactor utils

--- mobile/packages/utils/lib/share_utils.dart
+++ mobile/packages/utils/lib/share_utils.dart
@@ -1,6 +1,56 @@
+import 'dart:io';
+
+import 'package:ente_strings/extensions.dart';
+import 'package:ente_ui/components/buttons/button_widget.dart';
+import 'package:ente_ui/components/buttons/models/button_type.dart';
+import 'package:ente_ui/components/dialog_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:logging/logging.dart';
 import 'package:share_plus/share_plus.dart';
+
+Future<void> shareDialog(
+  BuildContext context,
+  String title, {
+  required Function saveAction,
+  required Function sendAction,
+}) async {
+  final l10n = context.strings;
+  await showDialogWidget(
+    context: context,
+    title: title,
+    body: Platform.isLinux || Platform.isWindows
+        ? l10n.saveOnlyDescription
+        : l10n.saveOrSendDescription,
+    buttons: [
+      ButtonWidget(
+        isInAlert: true,
+        buttonType: ButtonType.neutral,
+        labelText: l10n.save,
+        buttonAction: ButtonAction.first,
+        shouldSurfaceExecutionStates: false,
+        onTap: () async {
+          await saveAction();
+        },
+      ),
+      if (!Platform.isWindows && !Platform.isLinux)
+        ButtonWidget(
+          isInAlert: true,
+          buttonType: ButtonType.secondary,
+          labelText: l10n.send,
+          buttonAction: ButtonAction.second,
+          onTap: () async {
+            await sendAction();
+          },
+        ),
+      ButtonWidget(
+        isInAlert: true,
+        buttonType: ButtonType.secondary,
+        labelText: l10n.cancel,
+        buttonAction: ButtonAction.cancel,
+      ),
+    ],
+  );
+}
 
 Rect _sharePosOrigin(BuildContext? context, GlobalKey? key) {
   late final Rect rect;
