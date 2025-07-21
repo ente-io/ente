c3b90e68dc6838069b65fc21b79deb2dca80323a
vishnukvmd <vishnu@ente.io>
2025-07-21 16:29:34 +0530

    Lint strings

--- mobile/packages/strings/lib/ente_strings.dart
+++ mobile/packages/strings/lib/ente_strings.dart
@@ -1,5 +1,2 @@
-/// A Flutter package containing shared localization strings for Ente apps
-library ente_strings;
-
-export 'l10n/strings_localizations.dart';
 export 'extensions.dart';
+export 'l10n/strings_localizations.dart';
