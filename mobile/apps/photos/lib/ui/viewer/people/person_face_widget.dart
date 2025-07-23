c2d1c668887ec5baca472d00f520c2cce24387ba
laurenspriem <github_uni.fishers@aleeas.com>
2025-07-23 12:45:09 +0200

    keep alive face thumbnail when scrolling fast

--- mobile/apps/photos/lib/ui/viewer/people/person_face_widget.dart
+++ mobile/apps/photos/lib/ui/viewer/people/person_face_widget.dart
@@ -21,6 +21,7 @@ class PersonFaceWidget extends StatefulWidget {
   final String? clusterID;
   final bool useFullFile;
   final VoidCallback? onErrorCallback;
+  final bool keepAlive;
 
   // PersonFaceWidget constructor checks that both personId and clusterID are not null
   // and that the file is not null
@@ -29,6 +30,7 @@ class PersonFaceWidget extends StatefulWidget {
     this.clusterID,
     this.useFullFile = true,
     this.onErrorCallback,
+    this.keepAlive = false,
     super.key,
   }) : assert(
           personId != null || clusterID != null,
@@ -39,11 +41,15 @@ class PersonFaceWidget extends StatefulWidget {
   State<PersonFaceWidget> createState() => _PersonFaceWidgetState();
 }
 
-class _PersonFaceWidgetState extends State<PersonFaceWidget> {
+class _PersonFaceWidgetState extends State<PersonFaceWidget>
+    with AutomaticKeepAliveClientMixin {
   Future<Uint8List?>? faceCropFuture;
   EnteFile? fileForFaceCrop;
 
   bool get isPerson => widget.personId != null;
+
+  @override
+  bool get wantKeepAlive => widget.keepAlive;
 
   @override
   void initState() {
@@ -64,6 +70,10 @@ class _PersonFaceWidgetState extends State<PersonFaceWidget> {
 
   @override
   Widget build(BuildContext context) {
+    super.build(
+      context,
+    ); // Calling super.build for AutomaticKeepAliveClientMixin
+
     return FutureBuilder<Uint8List?>(
       future: faceCropFuture,
       builder: (context, snapshot) {
