5fe86858ef360d879b1cef50cf372f02d43bf2c1
AmanRajSinghMourya <amanrajmourya7@gmail.com>
2025-08-13 11:38:27 +0530

    Update dependencies and upgrade SDK version; replace qr_code_scanner with qr_code_scanner_plus

--- mobile/packages/utils/lib/platform_util.dart
+++ mobile/packages/utils/lib/platform_util.dart
@@ -43,14 +43,14 @@ class PlatformUtil {
       if (Platform.isAndroid || Platform.isIOS) {
         await FileSaver.instance.saveAs(
           name: fileName,
-          fileExtension: extension,
+          ext: extension,
           bytes: bytes,
           mimeType: type,
         );
       } else {
         await FileSaver.instance.saveFile(
           name: fileName,
-          fileExtension: extension,
+          ext: extension,
           bytes: bytes,
           mimeType: type,
         );
