d85121862d0ddbc10db0b23de351657f9f36a5d3
laurenspriem <github_uni.fishers@aleeas.com>
2025-08-22 11:34:40 +0530

    Use view of index instead of loading in memory

--- mobile/apps/photos/rust/src/api/usearch_api.rs
+++ mobile/apps/photos/rust/src/api/usearch_api.rs
@@ -32,7 +32,8 @@ impl VectorDB {
 
         if file_exists {
             println!("Loading index from disk.");
-            db.index.load(file_path).expect("Failed to load index");
+            // Creates a view of the index from a file without loading it into memory. https://docs.rs/usearch/latest/usearch/struct.Index.html#method.view
+            db.index.view(file_path).expect("Failed to load index");
         } else {
             println!("Creating new index.");
             db.save_index();
