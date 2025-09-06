c51dff5a29ba24351c823e558532d6fc068d0f5b
AmanRajSinghMourya <amanrajmourya7@gmail.com>
2025-09-06 06:45:02 +0530

    Move user_dialog to package

--- mobile/packages/ui/lib/components/user_dialogs.dart
+++ mobile/packages/ui/lib/components/user_dialogs.dart
@@ -1,28 +1,29 @@
 import "dart:async";
 
+import "package:ente_strings/ente_strings.dart";
 import "package:ente_ui/components/buttons/button_widget.dart";
 import "package:ente_ui/components/buttons/models/button_type.dart";
 import "package:ente_ui/components/dialog_widget.dart";
 import "package:ente_utils/share_utils.dart";
 import "package:flutter/material.dart";
-import "package:locker/l10n/l10n.dart";
+
 Future<void> showInviteDialog(BuildContext context, String email) async {
   await showDialogWidget(
     context: context,
-    title: context.l10n.inviteToEnte,
+    title: context.strings.inviteToEnte,
     icon: Icons.info_outline,
-    body: context.l10n.emailNoEnteAccount(email),
+    body: context.strings.emailNoEnteAccount(email),
     isDismissible: true,
     buttons: [
       ButtonWidget(
         buttonType: ButtonType.neutral,
         icon: Icons.adaptive.share,
-        labelText: context.l10n.sendInvite,
+        labelText: context.strings.sendInvite,
         isInAlert: true,
         onTap: () async {
           unawaited(
             shareText(
-              context.l10n.shareTextRecommendUsingEnte,
+              context.strings.shareTextRecommendUsingEnte,
             ),
           );
         },
