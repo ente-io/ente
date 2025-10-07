a249fa928ee4c0d3ab2c43d57fb07a7049ca1995
Neeraj Gupta <254676+ua741@users.noreply.github.com>
2025-10-07 17:25:17 +0530

    Santize errors from computer

--- mobile/apps/photos/plugins/ente_crypto/lib/src/models/errors.dart
+++ mobile/apps/photos/plugins/ente_crypto/lib/src/models/errors.dart
@@ -16,3 +16,27 @@ class CryptoErr implements Exception {
     return 'CryptoErr: $message';
   }
 }
+
+extension FutureErrorHandling<T> on Future<T> {
+  Future<T> unwrapExceptionInComputer() {
+    return catchError((e) {
+      final msg = e.toString();
+      if (msg.contains(kPartialReadErrorTag)) {
+        throw PartialReadException(msg);
+      }
+      throw e;
+    });
+  }
+}
+
+const kPartialReadErrorTag = 'PartialRead';
+const kLibSodiumErrorTag = 'crypto_secretstream_xchacha20poly1305_pull';
+
+// Exception counterpart for upstream handling via catchError chains
+class PartialReadException implements Exception {
+  final String message;
+  PartialReadException(this.message);
+
+  @override
+  String toString() => 'PartialReadException: $message';
+}
