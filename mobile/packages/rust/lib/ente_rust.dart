6721c2851c6051c6ebba24c9de5dcdd5133fb195
Manav Rathi <manav@mnvr.in>
2025-12-01 16:05:47 +0530

    int 2

---
+++ mobile/packages/rust/lib/ente_rust.dart
@@ -0,0 +1,4 @@
+library;
+
+export 'src/rust/api/simple.dart';
+export 'src/rust/frb_generated.dart' show EnteRust;
---
+++ mobile/packages/rust/rust/src/api/init.rs
@@ -0,0 +1,5 @@
+#[flutter_rust_bridge::frb(init)]
+pub fn init_app() {
+    // Default utilities - feel free to customize
+    flutter_rust_bridge::setup_default_user_utils();
+}
