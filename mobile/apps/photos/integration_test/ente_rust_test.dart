fbff58273543f43b722cb7033d37bcdfe5faf931
Manav Rathi <manav@mnvr.in>
2025-12-01 19:56:05 +0530

    fix mobile lints

--- mobile/apps/photos/integration_test/ente_rust_test.dart
+++ mobile/apps/photos/integration_test/ente_rust_test.dart
@@ -1,4 +1,5 @@
 import 'package:ente_rust/ente_rust.dart';
+import 'package:ente_rust/src/rust/api/urls.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
