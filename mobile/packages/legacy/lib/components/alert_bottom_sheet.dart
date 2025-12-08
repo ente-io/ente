31af66c26302d98e1c0661f87a49c9cf58eda74f
AmanRajSinghMourya <amanrajmourya7@gmail.com>
2025-12-08 11:50:01 +0530

    Add AlertBottomSheet component and remove ConfirmationBottomSheet

--- mobile/packages/legacy/lib/components/alert_bottom_sheet.dart
+++ mobile/packages/legacy/lib/components/alert_bottom_sheet.dart
@@ -1,38 +1,37 @@
-import "package:ente_legacy/components/gradient_button.dart";
 import "package:ente_ui/theme/ente_theme.dart";
 import "package:flutter/material.dart";
 
-Future<bool?> showConfirmationBottomSheet(
+Future<T?> showAlertBottomSheet<T>(
   BuildContext context, {
   required String title,
   required String message,
-  required String buttonText,
   required String assetPath,
+  List<Widget>? buttons,
 }) {
-  return showModalBottomSheet<bool>(
+  return showModalBottomSheet<T>(
     context: context,
     isScrollControlled: true,
     isDismissible: true,
-    builder: (context) => ConfirmationBottomSheet(
+    builder: (context) => AlertBottomSheet<T>(
       title: title,
       message: message,
-      buttonText: buttonText,
       assetPath: assetPath,
+      buttons: buttons,
     ),
   );
 }
 
-class ConfirmationBottomSheet extends StatelessWidget {
+class AlertBottomSheet<T> extends StatelessWidget {
   final String title;
   final String message;
-  final String buttonText;
   final String assetPath;
+  final List<Widget>? buttons;
 
-  const ConfirmationBottomSheet({
+  const AlertBottomSheet({
     required this.title,
     required this.message,
-    required this.buttonText,
     required this.assetPath,
+    this.buttons,
     super.key,
   });
 
@@ -57,32 +56,10 @@ class ConfirmationBottomSheet extends StatelessWidget {
             mainAxisSize: MainAxisSize.min,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              SizedBox(
-                width: double.infinity,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    GestureDetector(
-                      onTap: () => Navigator.of(context).pop(false),
-                      child: Container(
-                        decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          color: colorScheme.backgroundElevated,
-                        ),
-                        padding: const EdgeInsets.all(8),
-                        child: Icon(
-                          Icons.close,
-                          size: 24,
-                          color: colorScheme.textBase,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              const SizedBox(height: 8),
+              _buildCloseButton(context, colorScheme),
+              const SizedBox(height: 12),
               Center(child: Image.asset(assetPath)),
-              const SizedBox(height: 24),
+              const SizedBox(height: 20),
               Text(
                 title,
                 style: textTheme.h3Bold,
@@ -96,18 +73,42 @@ class ConfirmationBottomSheet extends StatelessWidget {
                 ),
                 textAlign: TextAlign.center,
               ),
-              const SizedBox(height: 32),
-              SizedBox(
-                width: double.infinity,
-                child: GradientButton(
-                  text: buttonText,
-                  backgroundColor: colorScheme.warning700,
-                  onTap: () => Navigator.of(context).pop(true),
-                ),
-              ),
+              if (buttons != null && buttons!.isNotEmpty) ...[
+                const SizedBox(height: 20),
+                for (int i = 0; i < buttons!.length; i++) ...[
+                  buttons![i],
+                  if (i < buttons!.length - 1) const SizedBox(height: 12),
+                ],
+              ],
             ],
           ),
         ),
+      ),
+    );
+  }
+
+  Widget _buildCloseButton(BuildContext context, colorScheme) {
+    return SizedBox(
+      width: double.infinity,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          GestureDetector(
+            onTap: () => Navigator.of(context).pop(),
+            child: Container(
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: colorScheme.backgroundElevated,
+              ),
+              padding: const EdgeInsets.all(8),
+              child: Icon(
+                Icons.close,
+                size: 24,
+                color: colorScheme.textBase,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
