9b7f530a6292ad979c8258f686e88768bba63563
Prateek Sunal <prtksunal@gmail.com>
2025-12-10 07:45:38 +0000

    [mob][photos] Fix iOS back navigation for Trash, Hidden, and Archive pages
    The SwipeableRouteBuilder was using CupertinoPageTransitionsBuilder for
    the slide animation but this doesn't include the iOS swipe-to-go-back
    gesture functionality. The back gesture is provided by
    CupertinoRouteTransitionMixin.
    
    This change updates SwipeableRouteBuilder to use CupertinoRouteTransitionMixin
    which provides both the Cupertino slide transition and the back gesture,
    fixing the back navigation on Trash, Hidden, Archive, and similar pages
    on iOS.

--- mobile/apps/photos/lib/utils/navigation_util.dart
+++ mobile/apps/photos/lib/utils/navigation_util.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 Future<T?> routeToPage<T extends Object>(
@@ -14,9 +15,7 @@ Future<T?> routeToPage<T extends Object>(
   } else {
     return Navigator.of(context).push(
       SwipeableRouteBuilder(
-        pageBuilder: (context, animation, secondaryAnimation) {
-          return page;
-        },
+        builder: (context) => page,
       ),
     );
   }
@@ -60,28 +59,19 @@ PageRouteBuilder<T> _buildPageRoute<T extends Object>(Widget page) {
   );
 }
 
-class SwipeableRouteBuilder<T> extends PageRoute<T> {
-  final RoutePageBuilder pageBuilder;
-  final PageTransitionsBuilder matchingBuilder =
-      const CupertinoPageTransitionsBuilder(); // Default iOS/macOS (to get the swipe right to go back gesture)
-  // final PageTransitionsBuilder matchingBuilder = const FadeUpwardsPageTransitionsBuilder(); // Default Android/Linux/Windows
+class SwipeableRouteBuilder<T> extends PageRoute<T>
+    with CupertinoRouteTransitionMixin<T> {
+  final WidgetBuilder builder;
 
-  SwipeableRouteBuilder({required this.pageBuilder});
+  SwipeableRouteBuilder({required this.builder});
 
   @override
-  Null get barrierColor => null;
-
-  @override
-  Null get barrierLabel => null;
-
-  @override
-  Widget buildPage(
-    BuildContext context,
-    Animation<double> animation,
-    Animation<double> secondaryAnimation,
-  ) {
-    return pageBuilder(context, animation, secondaryAnimation);
+  Widget buildContent(BuildContext context) {
+    return builder(context);
   }
+
+  @override
+  String? get title => null;
 
   @override
   bool get maintainState => true;
@@ -90,25 +80,6 @@ class SwipeableRouteBuilder<T> extends PageRoute<T> {
   Duration get transitionDuration => const Duration(
         milliseconds: 300,
       ); // Can give custom Duration, unlike in MaterialPageRoute
-
-  @override
-  Widget buildTransitions(
-    BuildContext context,
-    Animation<double> animation,
-    Animation<double> secondaryAnimation,
-    Widget child,
-  ) {
-    return matchingBuilder.buildTransitions<T>(
-      this,
-      context,
-      animation,
-      secondaryAnimation,
-      child,
-    );
-  }
-
-  @override
-  bool get opaque => false;
 }
 
 class TransparentRoute extends PageRoute<void> {
