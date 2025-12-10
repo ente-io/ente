c3c1f1e631b59fe35f97196e0a880509fbb93945
Prateek Sunal <prtksunal@gmail.com>
2025-12-10 20:43:26 +0530

    [mob][photos] Fix undefined useRootNavigator parameter in showActionSheet

--- mobile/apps/photos/lib/ui/viewer/people/save_or_edit_person.dart
+++ mobile/apps/photos/lib/ui/viewer/people/save_or_edit_person.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-import "dart:io";
 
 import "package:flutter/foundation.dart";
 import "package:flutter/material.dart";
@@ -361,7 +360,6 @@ class _SaveOrEditPersonState extends State<SaveOrEditPerson> {
   Future<dynamic> _saveChangesPrompt(BuildContext context) async {
     PersonEntity? updatedPersonEntity;
     return await showActionSheet(
-      useRootNavigator: Platform.isIOS ? true : false,
       body: context.l10n.saveChangesBeforeLeavingQuestion,
       context: context,
       buttons: [
