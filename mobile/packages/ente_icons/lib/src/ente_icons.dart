3c9c887a05006e2f97fe8b1cd5092978fa4fe0b8
laurenspriem <laurens-pilot-git@lrns.me>
2025-12-12 16:59:38 +0530

    Add lightning icon

--- mobile/packages/ente_icons/lib/src/ente_icons.dart
+++ mobile/packages/ente_icons/lib/src/ente_icons.dart
@@ -6,8 +6,19 @@ class EnteIcons {
   static const _kFontFam = 'EnteIcons';
   static const String _kFontPkg = 'ente_icons';
 
-  static const IconData favoriteStroke =
-      IconData(0xe800, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData favoriteFilled =
-      IconData(0xe801, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData favoriteStroke = IconData(
+    0xe800,
+    fontFamily: _kFontFam,
+    fontPackage: _kFontPkg,
+  );
+  static const IconData favoriteFilled = IconData(
+    0xe801,
+    fontFamily: _kFontFam,
+    fontPackage: _kFontPkg,
+  );
+  static const IconData lightningFilled = IconData(
+    0xe802,
+    fontFamily: _kFontFam,
+    fontPackage: _kFontPkg,
+  );
 }
