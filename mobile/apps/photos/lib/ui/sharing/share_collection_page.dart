48f41e2216f450f4b04c3ad3fe1a2528c547d193
AmanRajSinghMourya <amanrajmourya7@gmail.com>
2025-12-16 12:19:44 +0530

    Fix: border radius of menuitem widget

--- mobile/apps/photos/lib/ui/sharing/share_collection_page.dart
+++ mobile/apps/photos/lib/ui/sharing/share_collection_page.dart
@@ -114,6 +114,7 @@ class _ShareCollectionPageState extends State<ShareCollectionPage> {
             leadingIcon: Icons.add,
             menuItemColor: getEnteColorScheme(context).fillFaint,
             isTopBorderRadiusRemoved: _sharees.isNotEmpty,
+            isBottomBorderRadiusRemoved: true,
             onTap: () async {
               await routeToPage(
                 context,
@@ -145,7 +146,7 @@ class _ShareCollectionPageState extends State<ShareCollectionPage> {
           menuItemColor: getEnteColorScheme(context).fillFaint,
           isTopBorderRadiusRemoved:
               adminRoleEnabled ? true : _sharees.isNotEmpty,
-          isBottomBorderRadiusRemoved: false,
+          isBottomBorderRadiusRemoved: true,
           onTap: () async {
             // ignore: unawaited_futures
             routeToPage(
@@ -177,7 +178,7 @@ class _ShareCollectionPageState extends State<ShareCollectionPage> {
           leadingIcon: Icons.add,
           menuItemColor: getEnteColorScheme(context).fillFaint,
           isTopBorderRadiusRemoved: true,
-          isBottomBorderRadiusRemoved: true,
+          isBottomBorderRadiusRemoved: false,
           onTap: () async {
             await routeToPage(
               context,
