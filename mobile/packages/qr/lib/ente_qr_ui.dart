305a751a00ef92aef11059b41e6336e39e41396f
Prateek Sunal <prtksunal@gmail.com>
2025-12-16 16:35:09 +0530

    [mob][qr] Revert ente_qr_ui exports to original

--- mobile/packages/qr/lib/ente_qr_ui.dart
+++ mobile/packages/qr/lib/ente_qr_ui.dart
@@ -1,16 +1,4 @@
 library ente_qr_ui;
 
-export 'package:pretty_qr_code/pretty_qr_code.dart'
-    show
-        PrettyQrDecoration,
-        PrettyQrDecorationImage,
-        PrettyQrDecorationImagePosition,
-        PrettyQrDotsSymbol,
-        PrettyQrQuietZone,
-        PrettyQrShape,
-        PrettyQrSquaresSymbol,
-        PrettyQrView,
-        QrCode,
-        QrErrorCorrectLevel,
-        QrImage;
+export 'package:pretty_qr_code/pretty_qr_code.dart' show QrErrorCorrectLevel;
 export 'qr_code_dialog.dart';
