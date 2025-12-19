e6c236590ba735b9cb6033bb03fc8ae1b956af1e
Prateek Sunal <prtksunal@gmail.com>
2025-12-19 18:37:00 +0530

    fix: Separate _isStopped check from null check

--- mobile/apps/photos/lib/services/video_preview_service.dart
+++ mobile/apps/photos/lib/services/video_preview_service.dart
@@ -438,8 +438,9 @@ class VideoPreviewService {
 
       // get file
       file ??= await getFile(enteFile, isOrigin: true);
-      if (_isStopped || file == null) {
-        if (file == null) error = "Unable to fetch file";
+      if (_isStopped) return;
+      if (file == null) {
+        error = "Unable to fetch file";
         return;
       }
 
