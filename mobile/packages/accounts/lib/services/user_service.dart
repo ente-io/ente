6f9db250a873ebcb4fe7561c8d68e2231ddf81ef
Neeraj Gupta <neeraj.gupta983@gmail.com>
2026-01-14 09:32:30 +0530

    [mob] Fix linter warnings in mobile packages
    - Fix unawaited_futures in accounts package by adding unawaited() to Navigator calls
    - Remove unused _documentsDirectory field from configuration package
    - Sort dependencies alphabetically in legacy package pubspec.yaml

--- mobile/packages/accounts/lib/services/user_service.dart
+++ mobile/packages/accounts/lib/services/user_service.dart
@@ -276,7 +276,7 @@ class UserService {
       final response = await _enteDio.post("/users/logout");
       if (response.statusCode == 200) {
         await _config.logout();
-        Navigator.of(context).pushNamedAndRemoveUntil('/', (route) => false);
+        unawaited(Navigator.of(context).pushNamedAndRemoveUntil('/', (route) => false));
       } else {
         throw Exception("Log out action failed");
       }
@@ -285,7 +285,7 @@ class UserService {
       // check if token is already invalid
       if (e is DioException && e.response?.statusCode == 401) {
         await _config.logout();
-        Navigator.of(context).pushNamedAndRemoveUntil('/', (route) => false);
+        unawaited(Navigator.of(context).pushNamedAndRemoveUntil('/', (route) => false));
         return;
       }
       //This future is for waiting for the dialog from which logout() is called
