6120e2519cb130b6b5e430fa11c2447218705325
Prateek Sunal <prtksunal@gmail.com>
2026-02-05 17:06:00 +0530

    sync on app launch

--- mobile/native/darwin/Apps/ensu/ensu/HomeView.swift
+++ mobile/native/darwin/Apps/ensu/ensu/HomeView.swift
@@ -4,17 +4,25 @@ struct HomeView: View {
     @EnvironmentObject private var appState: EnsuAppState
     @StateObject private var chatModel = ChatViewModel()
     @State private var isShowingAuth = false
+    @State private var didTriggerInitialSync = false
 
     var body: some View {
         ChatView(viewModel: chatModel, isShowingAuth: $isShowingAuth)
             .platformFullScreenCover(isPresented: $isShowingAuth) {
                 AuthFlowView()
             }
+            .task {
+                guard appState.isLoggedIn, !didTriggerInitialSync else { return }
+                didTriggerInitialSync = true
+                chatModel.syncAfterLogin()
+            }
             .onChange(of: appState.isLoggedIn) { isLoggedIn in
                 if isLoggedIn {
                     isShowingAuth = false
+                    didTriggerInitialSync = true
                     chatModel.syncAfterLogin()
                 } else {
+                    didTriggerInitialSync = false
                     chatModel.handleLogout()
                 }
             }
