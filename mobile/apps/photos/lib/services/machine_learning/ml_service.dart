81759e36fe866d3f100410f4d80ba31e78796687
laurenspriem <laurens-pilot-git@lrns.me>
2026-02-12 17:12:12 +0530

    force clustering earlier for offline mode

--- mobile/apps/photos/lib/services/machine_learning/ml_service.dart
+++ mobile/apps/photos/lib/services/machine_learning/ml_service.dart
@@ -56,6 +56,9 @@ class MLService {
       _isRunningML || memoriesCacheService.isUpdatingMemories;
 
   static const _kForceClusteringFaceCount = 8000;
+  static const _kForceClusteringFaceCountOffline = 100;
+  int get _forceClusteringFaceCount => isOfflineMode ? _kForceClusteringFaceCountOffline : _kForceClusteringFaceCount;
+
   MLDataDB get _mlDataDB =>
       isOfflineMode ? MLDataDB.offlineInstance : MLDataDB.instance;
 
@@ -179,7 +182,7 @@ class MLService {
 
       final int unclusteredFacesCount =
           await mlDataDB.getUnclusteredFaceCount();
-      if (unclusteredFacesCount > _kForceClusteringFaceCount) {
+      if (unclusteredFacesCount > _forceClusteringFaceCount) {
         _logger.info(
           "There are $unclusteredFacesCount unclustered faces, doing clustering first",
         );
