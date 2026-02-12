828acaf6fd86e802c75a446d09bda8433dfca561
AmanRajSinghMourya <amanrajmourya7@gmail.com>
2026-02-12 15:39:39 +0530

    remove callbacks

--- mobile/apps/photos/lib/ui/home/header_widget.dart
+++ mobile/apps/photos/lib/ui/home/header_widget.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:logging/logging.dart';
 import 'package:photos/service_locator.dart';
 import "package:photos/services/wrapped/wrapped_service.dart";
-import "package:photos/ui/account/email_entry_page.dart";
 import "package:photos/ui/components/banners/get_started_banner.dart";
 import "package:photos/ui/home/memories/memories_widget.dart";
 import 'package:photos/ui/home/status_bar_widget.dart';
@@ -20,13 +19,11 @@ class HeaderWidget extends StatefulWidget {
 class _HeaderWidgetState extends State<HeaderWidget> {
   final Logger _logger = Logger("Header");
   late WrappedEntryState _wrappedState;
-  bool _bannerDismissed = false;
 
   @override
   void initState() {
     super.initState();
     _wrappedState = wrappedService.state;
-    _bannerDismissed = localSettings.isOfflineGetStartedBannerDismissed;
     wrappedService.stateListenable.addListener(_onWrappedStateChanged);
   }
 
@@ -49,33 +46,17 @@ class _HeaderWidgetState extends State<HeaderWidget> {
     });
   }
 
-  Future<void> _onDismissBanner() async {
-    await localSettings.setOfflineGetStartedBannerDismissed(true);
-    setState(() {
-      _bannerDismissed = true;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     _logger.info("Building header widget");
     final bool showWrappedBanner = wrappedService.shouldShowHomeBanner;
-    final bool showGetStartedBanner = isOfflineMode && !_bannerDismissed;
+    final bool showGetStartedBanner = isOfflineMode;
     final List<Widget> children = <Widget>[
       const StatusBarWidget(),
       if (showGetStartedBanner)
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 2, vertical: 8),
-          child: GetStartedBanner(
-            onGetStarted: () async {
-              await Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (_) => const EmailEntryPage(),
-                ),
-              );
-            },
-            onDismiss: _onDismissBanner,
-          ),
+        const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 2, vertical: 8),
+          child: GetStartedBanner(),
         ),
       const MemoriesWidget(),
     ];
