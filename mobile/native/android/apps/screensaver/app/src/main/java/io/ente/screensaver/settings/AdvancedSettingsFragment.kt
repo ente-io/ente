ca66f40c73a513d6daead2c546dc87351134a8df
Prateek Sunal <prtksunal@gmail.com>
2026-02-16 18:59:54 +0530

    Show ADB instructions in advanced settings

--- mobile/native/android/apps/screensaver/app/src/main/java/io/ente/screensaver/settings/AdvancedSettingsFragment.kt
+++ mobile/native/android/apps/screensaver/app/src/main/java/io/ente/screensaver/settings/AdvancedSettingsFragment.kt
@@ -9,6 +9,7 @@ import androidx.preference.ListPreference
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import io.ente.photos.screensaver.R
+import io.ente.photos.screensaver.diagnostics.AdbInstructionsActivity
 import io.ente.photos.screensaver.diagnostics.DiagnosticsActivity
 import io.ente.photos.screensaver.ente.EntePublicAlbumRepository
 import io.ente.photos.screensaver.prefs.SsaverPreferenceDataStore
@@ -30,6 +31,11 @@ class AdvancedSettingsFragment : PreferenceFragmentCompat() {
 
         findPreference<Preference>("pref_diagnostics")?.setOnPreferenceClickListener {
             startActivity(Intent(requireContext(), DiagnosticsActivity::class.java))
+            true
+        }
+
+        findPreference<Preference>("pref_adb_instructions")?.setOnPreferenceClickListener {
+            startActivity(Intent(requireContext(), AdbInstructionsActivity::class.java))
             true
         }
 
