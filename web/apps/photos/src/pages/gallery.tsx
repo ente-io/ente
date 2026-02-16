996f94b9eb5766ed0e8d2b4150d43c452a5a89b5
Anand Baburajan <anandbaburajan@gmail.com>
2026-02-16 13:11:21 +0530

    Disable default album join links

--- web/apps/photos/src/pages/gallery.tsx
+++ web/apps/photos/src/pages/gallery.tsx
@@ -976,6 +976,7 @@ const Page: React.FC = () => {
                     );
                     const publicURL = await createPublicURL(
                         quickLinkCollection.id,
+                        { enableJoin: false },
                     );
                     const resolvedURL = await resolveQuickLinkURL(
                         publicURL.url,
@@ -1302,7 +1303,10 @@ const Page: React.FC = () => {
                     quickLinkNameForFiles([file]),
                 );
                 await addToCollection(quickLinkCollection, [file]);
-                const publicURL = await createPublicURL(quickLinkCollection.id);
+                const publicURL = await createPublicURL(
+                    quickLinkCollection.id,
+                    { enableJoin: false },
+                );
                 const resolvedURL = await resolveQuickLinkURL(
                     publicURL.url,
                     quickLinkCollection.key,
