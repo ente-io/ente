22972bf53005b34cb312a357de3aa2daa9215d5a
Prateek Sunal <prtksunal@gmail.com>
2026-03-05 12:43:33 +0530

    [tv] use photos-tv tag prefix and bump app version

--- mobile/native/android/apps/tv/app/build.gradle.kts
+++ mobile/native/android/apps/tv/app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "io.ente.photos.tv"
         minSdk = 21
         targetSdk = 34
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.1.1"
     }
 
     signingConfigs {
