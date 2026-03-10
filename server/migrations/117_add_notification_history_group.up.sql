75de9e6e463681215e7e4da9125e9d0efd9fcafa
Neeraj Gupta <neeraj.gupta983@gmail.com>
2026-03-10 16:40:36 +0530

    Remove unused notification history backfill

--- server/migrations/117_add_notification_history_group.up.sql
+++ server/migrations/117_add_notification_history_group.up.sql
@@ -1,24 +1,6 @@
 ALTER TABLE notification_history
     ADD COLUMN IF NOT EXISTS notification_group TEXT;
 
-UPDATE notification_history
-SET notification_group = 'storage_warning_active_overage'
-WHERE template_id IN (
-    'family_storage_warning_active_overage',
-    'family_storage_warning_active_overage_30d',
-    'family_storage_warning_active_overage_60d',
-    'family_storage_warning_active_overage_89d'
-);
-
-UPDATE notification_history
-SET notification_group = 'storage_warning_expired'
-WHERE template_id IN (
-    'family_storage_warning_expired_30d',
-    'family_storage_warning_expired_60d',
-    'family_storage_warning_expired_90d',
-    'family_storage_warning_expired_119d'
-);
-
 CREATE INDEX IF NOT EXISTS notification_history_user_template_sent_time_idx
     ON notification_history(user_id, template_id, sent_time DESC);
 
