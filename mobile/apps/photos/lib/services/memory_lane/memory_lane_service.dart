188c396a225be0c54490e320c29870f56703291e
laurenspriem <laurens-pilot-git@lrns.me>
2026-04-01 15:31:23 +0530

    Lower criteria to 2 years of 4 faces each

--- mobile/apps/photos/lib/services/memory_lane/memory_lane_service.dart
+++ mobile/apps/photos/lib/services/memory_lane/memory_lane_service.dart
@@ -31,11 +31,11 @@ class MemoryLaneService {
 
   static final MemoryLaneService instance = MemoryLaneService._internal();
 
-  static const _minimumYears = 5;
+  static const _minimumYears = 2;
   static const _minimumFacesPerYear = 4;
   static const _minimumEligibleAgeYears = 5;
   static const _recomputeCooldown = Duration(hours: 2);
-  static const _timelineLogicVersion = 2;
+  static const _timelineLogicVersion = 3;
   static const _startupBackfillDelay = Duration(seconds: 15);
   static const _startupBackfillBatchSize = 200;
 
