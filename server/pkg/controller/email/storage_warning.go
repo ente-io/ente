8ecea2d5848b471f415c067e01eb5075c26866c8
Neeraj Gupta <neeraj.gupta983@gmail.com>
2026-05-22 15:39:25 +0530

    Increase storage warning rollout

--- server/pkg/controller/email/storage_warning.go
+++ server/pkg/controller/email/storage_warning.go
@@ -29,7 +29,7 @@ const (
 	storageWarningPreviousStageFreshnessWindow = 35 * 24 * time.MicroSecondsInOneHour
 	storageWarningOneDayInMicroseconds         = 24 * time.MicroSecondsInOneHour
 	storageWarningBufferedCadenceExtraGrace    = 2 * storageWarningOneDayInMicroseconds
-	storageWarningRolloutPercentage            = 80
+	storageWarningRolloutPercentage            = 90
 	storageWarningRolloutNonce                 = "storage-warning-v1"
 
 	storageWarningActiveOverageNotificationGroup = "storage_warning_active_overage"
