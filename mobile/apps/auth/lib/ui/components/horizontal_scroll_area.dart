5bf489a332716dfdc3bd99ffbf360ed36ac80f1b
Neeraj Gupta <neeraj.gupta983@gmail.com>
2026-05-22 13:45:51 +0530

    [auth] Make tag strips horizontally scrollable

---
+++ mobile/apps/auth/lib/ui/components/horizontal_scroll_area.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+
+class HorizontalScrollArea extends StatefulWidget {
+  final Widget Function(BuildContext context, ScrollController controller)
+  builder;
+
+  const HorizontalScrollArea({super.key, required this.builder});
+
+  @override
+  State<HorizontalScrollArea> createState() => _HorizontalScrollAreaState();
+}
+
+class _HorizontalScrollAreaState extends State<HorizontalScrollArea> {
+  final ScrollController _controller = ScrollController();
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _handlePointerSignal(PointerSignalEvent event) {
+    if (event is! PointerScrollEvent || !_controller.hasClients) {
+      return;
+    }
+
+    if (event.scrollDelta.dx != 0 || event.scrollDelta.dy == 0) {
+      return;
+    }
+
+    final scrollDelta = event.scrollDelta.dy;
+    GestureBinding.instance.pointerSignalResolver.register(event, (_) {
+      final position = _controller.position;
+      final nextOffset = (_controller.offset + scrollDelta)
+          .clamp(position.minScrollExtent, position.maxScrollExtent)
+          .toDouble();
+      if (nextOffset != _controller.offset) {
+        _controller.jumpTo(nextOffset);
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Listener(
+      onPointerSignal: _handlePointerSignal,
+      child: ScrollConfiguration(
+        behavior: const _HorizontalScrollBehavior(),
+        child: widget.builder(context, _controller),
+      ),
+    );
+  }
+}
+
+class _HorizontalScrollBehavior extends MaterialScrollBehavior {
+  const _HorizontalScrollBehavior();
+
+  @override
+  Set<PointerDeviceKind> get dragDevices => {
+    ...super.dragDevices,
+    PointerDeviceKind.mouse,
+    PointerDeviceKind.trackpad,
+  };
+}
