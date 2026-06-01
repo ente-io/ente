1a5164bd74788af5e69a347808d5a31bc46645ec
AmanRajSinghMourya <amanrajmourya7@gmail.com>
2026-06-01 16:30:04 +0530

    [mob][photos] Remove stale albums and feed widgets

--- mobile/apps/photos/lib/ui/tabs/albums/empty_states/empty_state_feature_row.dart
+++ mobile/apps/photos/lib/ui/tabs/albums/empty_states/empty_state_feature_row.dart
@@ -1,39 +1,5 @@
 import "package:ente_components/ente_components.dart";
 import "package:flutter/widgets.dart";
-import "package:hugeicons/hugeicons.dart";
-import "package:photos/theme/ente_theme.dart";
-
-class EmptyStateFeatureRow extends StatelessWidget {
-  const EmptyStateFeatureRow({
-    required this.icon,
-    required this.label,
-    super.key,
-  });
-
-  final List<List<dynamic>> icon;
-  final String label;
-
-  @override
-  Widget build(BuildContext context) {
-    final textTheme = getEnteTextTheme(context);
-
-    return Row(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(6),
-          child: HugeIcon(
-            icon: icon,
-            size: 20,
-            color: const Color.fromRGBO(222, 222, 222, 1),
-          ),
-        ),
-        const SizedBox(width: 8),
-        Expanded(child: Text(label, style: textTheme.small)),
-      ],
-    );
-  }
-}
 
 class EmptyStateBulletFeatureRow extends StatelessWidget {
   const EmptyStateBulletFeatureRow({required this.label, super.key});
