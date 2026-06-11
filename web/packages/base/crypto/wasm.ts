97088d04ed7722bf5e94505c36ec553ea67aedfd
Priyanshu Dangare <priyanshu@ente.com>
2026-06-11 17:06:36 +0530

    [web] fix loading of wasm package in crypto lib

--- web/packages/base/crypto/wasm.ts
+++ web/packages/base/crypto/wasm.ts
@@ -1,9 +1,6 @@
-import * as wasm from "ente-wasm";
-
 function fromB64(b64: string): Uint8Array {
     const binaryString = atob(b64);
     const bytes = new Uint8Array(binaryString.length);
-
     for (let i = 0; i < binaryString.length; i++) {
         bytes[i] = binaryString.charCodeAt(i);
     }
@@ -13,11 +10,9 @@ function fromB64(b64: string): Uint8Array {
 
 function toB64(bytes: Uint8Array): string {
     let binaryString = "";
-
     for (let i = 0; i < bytes.length; i++) {
         binaryString += String.fromCharCode(bytes[i]!);
     }
-
     return btoa(binaryString);
 }
 
@@ -25,28 +20,26 @@ export async function generateKeypairPQ(): Promise<{
     publicKey: string;
     privateKey: string;
 }> {
+    await init();
     const keypair = wasm.crypto_generate_keypair_pq();
-
-    return {
-        publicKey: keypair.public_key,
-        privateKey: keypair.secret_key,
-    };
+    return { publicKey: keypair.public_key, privateKey: keypair.secret_key };
 }
 
-export function boxSealPQBytes(
+export async function boxSealPQBytes(
     data: Uint8Array,
     publicKey: string,
-): Uint8Array {
+): Promise<Uint8Array> {
+    await init();
     const sealedDataB64 = wasm.crypto_box_seal_pq(toB64(data), publicKey);
-
     return fromB64(sealedDataB64);
 }
 
-export function boxSealOpenPQBytes(
+export async function boxSealOpenPQBytes(
     sealedData: string,
     publicKey: string,
     privateKey: string,
-): Uint8Array {
+): Promise<Uint8Array> {
+    await init();
     const dataB64 = wasm.crypto_box_seal_open_pq(
         sealedData,
         publicKey,
@@ -54,4 +47,11 @@ export function boxSealOpenPQBytes(
     );
 
     return fromB64(dataB64);
+}
+
+// @ts-ignore
+let wasm: typeof import("ente-wasm") = undefined;
+async function init() {
+    if (wasm !== undefined) return;
+    wasm = await import("ente-wasm");
 }
