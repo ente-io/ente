2ad0923caa035184e00a970e3b9d1b940b5935fd
Neeraj Gupta <neeraj.gupta983@gmail.com>
2026-06-17 03:55:09 +0530

    Remove Auth scanner device preflight

--- mobile/apps/auth/lib/ui/components/scanner_camera_view.dart
+++ mobile/apps/auth/lib/ui/components/scanner_camera_view.dart
@@ -1,6 +1,3 @@
-import 'dart:io';
-
-import 'package:device_info_plus/device_info_plus.dart';
 import 'package:ente_qr_scanner/ente_qr_scanner.dart';
 import 'package:flutter/material.dart';
 
@@ -21,44 +18,12 @@ class ScannerCameraView extends StatefulWidget {
 }
 
 class _ScannerCameraViewState extends State<ScannerCameraView> {
-  late final Future<bool> _isCameraScannerAvailable =
-      _isCameraScannerAvailableOnDevice();
-
   @override
   Widget build(BuildContext context) {
-    if (!Platform.isIOS) {
-      return _buildScannerView();
-    }
-
-    return FutureBuilder<bool>(
-      future: _isCameraScannerAvailable,
-      builder: (context, snapshot) {
-        if (snapshot.data == true) {
-          return _buildScannerView();
-        }
-        return const ColoredBox(color: Colors.black);
-      },
-    );
-  }
-
-  Widget _buildScannerView() {
     return EnteQrScannerView(
       overlay: widget.overlay,
       onScannerCreated: widget.onScannerCreated,
       onError: widget.onError,
     );
-  }
-}
-
-Future<bool> _isCameraScannerAvailableOnDevice() async {
-  if (!Platform.isIOS) {
-    return true;
-  }
-
-  try {
-    final iosInfo = await DeviceInfoPlugin().iosInfo;
-    return iosInfo.isPhysicalDevice;
-  } catch (_) {
-    return true;
   }
 }
